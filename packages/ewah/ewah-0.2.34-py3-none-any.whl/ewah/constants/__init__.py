86b06440a0c9b26bf4bca5995e57b52b6b244ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:51 +0000

    Add package ewah/0.2.34/ewah-0.2.34-py3-none-any.whl

---
+++ packages/ewah/ewah-0.2.34-py3-none-any.whl/ewah/constants/__init__.py
@@ -0,0 +1,108 @@
+from datetime import datetime, date
+
+class EWAHConstants:
+    "This class contains a number of constants for use throughout Ewah."
+
+    # Available DWH Engines
+    DWH_ENGINE_POSTGRES = 'PostgreSQL'
+    DWH_ENGINE_SNOWFLAKE = 'Snowflake'
+    DWH_ENGINE_BIGQUERY = 'BigQuery'
+    DWH_ENGINE_REDSHIFT = 'Redshift'
+    DWH_ENGINE_S3 = 'S3'
+    DWH_ENGINE_GS = 'GoogleSheets'
+    DWH_ENGINES = [
+        DWH_ENGINE_POSTGRES,
+        DWH_ENGINE_SNOWFLAKE,
+        # DWH_ENGINE_BIGQUERY, To Be Implemented
+        # DWH_ENGINE_REDSHIFT, To Be Implemented
+        # DWH_ENGINE_S3, To Be Implemented
+        DWH_ENGINE_GS,
+    ]
+
+    # Query Building Constants
+    QBC_FIELD_TYPE = 'data_type'
+
+    QBC_FIELD_PK = 'is_primary_key'
+    QBC_FIELD_NN = 'is_not_null'
+    QBC_FIELD_UQ = 'is_unique'
+    QBC_FIELD_HASH = 'is_hash_column'
+    QBC_FIELD_GSHEET_COLNO = 'column' # Gsheet operator: position of the column
+    QBC_FIELD_CONSTRAINTS_MAPPING = {
+        DWH_ENGINE_POSTGRES: {
+            QBC_FIELD_PK: 'PRIMARY KEY',
+            QBC_FIELD_NN: 'NOT NULL',
+            QBC_FIELD_UQ: 'UNIQUE',
+        },
+        DWH_ENGINE_SNOWFLAKE: {
+            QBC_FIELD_PK: 'PRIMARY KEY',
+            QBC_FIELD_NN: 'NOT NULL',
+            QBC_FIELD_UQ: 'UNIQUE',
+        },
+        # DWH_ENGINE_BIGQUERY: {},
+        # DWH_ENGINE_REDSHIFT: {},
+        # DWH_ENGINE_S3: {},
+        DWH_ENGINE_GS: {
+            # Not applicable
+        },
+    }
+
+    """When a columns_definition is given, but a column does not have a field
+    type defined, use default as specified here. If no columns_definition is
+    given, and a field is of inconsistent data type or type does not exist
+    in the mapping below, use inconsistent value specified here."""
+    QBC_TYPE_MAPPING_DEFAULT = 'tm_default'
+    QBC_TYPE_MAPPING_INCONSISTENT = 'tm_incon'
+    QBC_TYPE_MAPPING = {
+        DWH_ENGINE_POSTGRES: {
+            QBC_TYPE_MAPPING_DEFAULT: 'text',
+            QBC_TYPE_MAPPING_INCONSISTENT: 'text',
+            str: 'text',
+            int: 'bigint',
+            float: 'numeric',
+            dict: 'jsonb',
+            list: 'jsonb',
+            tuple: 'jsonb',
+            set: 'jsonb',
+            frozenset: 'jsonb',
+            bool: 'boolean',
+            datetime: 'timestamp with time zone',
+            date: 'date',
+        },
+        DWH_ENGINE_SNOWFLAKE: {
+            QBC_TYPE_MAPPING_DEFAULT: 'VARIANT',
+            QBC_TYPE_MAPPING_INCONSISTENT: 'VARIANT',
+            str: 'VARCHAR',
+            int: 'NUMBER',
+            float: 'NUMBER',
+            dict: 'OBJECT',
+            list: 'ARRAY',
+            tuple: 'ARRAY',
+            set: 'ARRAY',
+            frozenset: 'ARRAY',
+            bool: 'BOOLEAN',
+            datetime: 'TIMESTAMP_TZ',
+            date: 'DATE',
+        },
+        DWH_ENGINE_BIGQUERY: {
+            QBC_TYPE_MAPPING_DEFAULT: 'STRING',
+            QBC_TYPE_MAPPING_INCONSISTENT: 'STRING',
+            str: 'STRING',
+            int: 'INT64',
+            float: 'NUMERIC',
+            dict: 'STRUCT',
+            list: 'STRUCT',
+            tuple: 'STRUCT',
+            set: 'STRUCT',
+            frozenset: 'STRUCT',
+            bool: 'BOOL',
+            datetime: 'TIMESTAMP',
+            date: 'DATE',
+            bytes: 'BYTES',
+        },
+        # DWH_ENGINE_REDSHIFT: {},
+        # DWH_ENGINE_S3: {},
+        DWH_ENGINE_GS: { # must have these two values evaluate to True as bool
+            QBC_TYPE_MAPPING_DEFAULT: 'x',
+            QBC_TYPE_MAPPING_INCONSISTENT: 'x',
+        },
+    }
---
+++ packages/ewah/ewah-0.2.34-py3-none-any.whl/ewah/operators/__init__.py
@@ -0,0 +1,76 @@
+try: # load operators with dependencies installed, ignore others
+    from ewah.operators.fx_operator import EWAHFXOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHFXOperator
+try:
+    from ewah.operators.google_analytics_operator import EWAHGAOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHGAOperator
+try:
+    from ewah.operators.mysql_operator import EWAHMySQLOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHMySQLOperator
+try:
+    from ewah.operators.oracle_operator import EWAHOracleSQLOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHOracleSQLOperator
+try:
+    from ewah.operators.postgres_operator import EWAHPostgresOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHPostgresOperator
+try:
+    from ewah.operators.s3_operator import EWAHS3Operator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHS3Operator
+try:
+    from ewah.operators.facebook_operator import EWAHFBOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHFBOperator
+try:
+    from ewah.operators.google_sheets_operator import EWAHGSpreadOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHGSpreadOperator
+try:
+    from ewah.operators.mongodb_operator import EWAHMongoDBOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHGSpreadOperator
+try:
+    from ewah.operators.shopify_operator import EWAHShopifyOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHShopifyOperator
+try:
+    from ewah.operators.zendesk_operator import EWAHZendeskOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHZendeskOperator
+try:
+    from ewah.operators.google_ads_operator import EWAHGoogleAdsOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHGoogleAdsOperator
+try:
+    from ewah.operators.google_maps_operator import EWAHGMapsOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHGMapsOperator
+try:
+    from ewah.operators.mailchimp_operator import EWAHMailchimpOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHMailchimpOperator
+try:
+    from ewah.operators.bigquery_operator import EWAHBigQueryOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHBigQueryOperator
+try:
+    from ewah.operators.hubspot_operator import EWAHHubspotOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHHubspotOperator
+try:
+    from ewah.operators.stripe_operator import EWAHStripeOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHStripeOperator
+try:
+    from ewah.operators.mailingwork_operator import EWAHMailingworkOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHMailingworkOperator
+try:
+    from ewah.operators.dynamodb_operator import EWAHDynamoDBOperator
+except ImportError:
+    from ewah.operators.base_operator import EWAHEmptyOperator as EWAHDynamoDBOperator

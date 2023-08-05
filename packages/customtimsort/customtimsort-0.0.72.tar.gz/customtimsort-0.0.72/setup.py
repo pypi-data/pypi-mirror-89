a7c0780917865a8aae93d2d27a08418615f856a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:01 +0000

    Add package customtimsort/0.0.72/customtimsort-0.0.72.tar.gz

---
+++ packages/customtimsort/customtimsort-0.0.72.tar.gz/customtimsort-0.0.72/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from setuptools import find_packages
+from distutils.core import setup, Extension
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+path_now = "/home/leha/Desktop/projects/library/CustomTimSort_2/sorting_code/"
+
+main_module = Extension('timsort',
+                        sources=[path_now + "timsort.c"],
+                        library_dirs=[path_now, path_now + "clinic/"],
+                        include_dirs=[path_now + "include/"])
+
+
+setuptools.setup(
+    name="customtimsort",
+    version="0.0.72",
+    author="lehatr",
+    author_email="lehatrutenb@gmail.com",
+    description="Timsort sorting algorithm with custom minrun",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lehatrutenb/FastTimSort",
+    #packages=find_packages(),
+    ext_modules=[main_module],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2',
+)
+

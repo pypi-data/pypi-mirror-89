292f8b63653199b8ea23867f196b50bdf24c3324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:07 +0000

    Add package encrypedloader/0.0.16/encrypedloader-0.0.16.tar.gz

---
+++ packages/encrypedloader/encrypedloader-0.0.16.tar.gz/encrypedloader-0.0.16/encrypedloader/encrypedloader.py
@@ -0,0 +1,26 @@
+from .loader import encrypt_file, gen_key
+import click
+
+@click.group()
+def cmd():
+    pass
+
+
+@cmd.command()
+@click.argument('keyfile')
+@click.argument('file_path')
+def encrypt(keyfile, file_path):
+    encrypt_file(file_path, keyfile)
+    click.echo('encrypted!')
+
+
+@cmd.command()
+def genkey():
+    gen_key()
+
+
+cli = click.CommandCollection(sources=[cmd,])
+
+
+def main():
+    cli()

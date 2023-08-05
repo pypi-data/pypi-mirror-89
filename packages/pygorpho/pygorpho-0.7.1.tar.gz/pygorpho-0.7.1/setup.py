938b007d24c1532ef1995044734e66840297e02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:15 +0000

    Add package pygorpho/0.7.1/pygorpho-0.7.1.tar.gz

---
+++ packages/pygorpho/pygorpho-0.7.1.tar.gz/pygorpho-0.7.1/setup.py
@@ -0,0 +1,34 @@
+from skbuild import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='pygorpho',
+    version='0.7.1',
+    description='Python bindings for gorpho',
+    url='https://pygorpho.readthedocs.io/',
+    project_urls={
+        'Documentation':
+            'https://pygorpho.readthedocs.io/en/latest/api-doc.html',
+        'PyPI': 'https://pypi.org/project/pygorpho/',
+        'Source': 'https://github.com/patmjen/pygorpho',
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='Patrick M. Jensen',
+    author_email='patmjen@gmail.com',
+    package_dir={'': 'src'},
+    packages=["pygorpho"],
+    setup_requires=['numpy', 'scikit-build>=0.7.0'],
+    install_requires=['numpy', 'scikit-build>=0.7.0'],
+    cmake_languages=('CUDA',),
+    cmake_minimum_required_version='3.10',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
+)

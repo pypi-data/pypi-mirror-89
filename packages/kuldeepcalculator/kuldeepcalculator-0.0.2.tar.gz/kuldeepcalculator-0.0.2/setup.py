7f79f1f4a55dffbac7e652bac41ffe67b3902079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:55 +0000

    Add package kuldeepcalculator/0.0.2/kuldeepcalculator-0.0.2.tar.gz

---
+++ packages/kuldeepcalculator/kuldeepcalculator-0.0.2.tar.gz/kuldeepcalculator-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+classification = [
+	'Development status :: 5 - Production/stable',
+	'Intended Audience :: Education',
+	'Operating System :: Linux :: ubuntu',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: PYthon :: 3'
+]
+
+setup(
+	name='kuldeepcalculator',
+	version='0.0.2',
+	description='A baisc calulcator',
+	Long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+	url='',
+	author='kuldeep khatana',
+	author_email='kuldeep.si.softprodigy@gmail.com',
+	License='MIT',
+	classifiers=[],
+	keywords='calulcator',
+	package=find_packages(),
+	install_requires=['']
+
+
+)

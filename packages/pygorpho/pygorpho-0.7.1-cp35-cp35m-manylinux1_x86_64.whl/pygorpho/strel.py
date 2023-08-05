eb5adf3bc7e3234e2ba71d639de3c49ec6de8b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:16 +0000

    Add package pygorpho/0.7.1/pygorpho-0.7.1-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/pygorpho/pygorpho-0.7.1-cp35-cp35m-manylinux1_x86_64.whl/pygorpho/strel.py
@@ -0,0 +1,48 @@
+"""Structuring elements for mathematical morhology"""
+import ctypes
+import numpy as np
+from . import _thin
+from . import constants
+
+
+def flat_ball_approx(radius, type=constants.BEST):
+    """
+    Returns approximation to flat ball using line segments.
+
+    The approximation is constructed according to [J19]_.
+
+    Parameters
+    ----------
+    r
+        Integer radius of flat ball.
+    type
+        Whether to constrain the zonohedral approximation inside or outside
+        the sphere. Must either ``INSIDE``, ``BEST``, or ``OUTSIDE`` from
+        constants.
+
+    Returns
+    -------
+    (numpy.array, numpy.array)
+        Tuple with step vectors and line lengths which parameterizes the line
+        segments.
+
+    References
+    ----------
+    .. [J19] P. M. Jensen et al., "Zonohedral Approximation of Spherical
+       Structuring Element for Volumetric Morphology," Scandinavian
+       Conference on Image Analysis (pp. 128-139). Springer. 2019.
+    """
+    LINE_COUNT = 13
+    assert (type == constants.INSIDE or type == constants.BEST or
+            type == constants.OUTSIDE)
+
+    radius = ctypes.c_int(radius)
+    type = ctypes.c_int(type)
+
+    line_steps = np.empty((LINE_COUNT, 3), dtype=np.int32, order='C')
+    line_lens = np.empty(LINE_COUNT, dtype=np.int32)
+
+    ret = _thin.flat_ball_approx_impl(line_steps, line_lens, radius, type)
+    _thin.raise_on_error(ret)
+
+    return (line_steps, line_lens)

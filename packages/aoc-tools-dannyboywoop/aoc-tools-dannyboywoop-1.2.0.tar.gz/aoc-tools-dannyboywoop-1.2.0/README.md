55c7a52664d1bf828f2dc90730b1ad58544e8a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:08 +0000

    Add package aoc-tools-dannyboywoop/1.2.0/aoc-tools-dannyboywoop-1.2.0.tar.gz

---
+++ packages/aoc-tools-dannyboywoop/aoc-tools-dannyboywoop-1.2.0.tar.gz/aoc-tools-dannyboywoop-1.2.0/README.md
@@ -0,0 +1,2 @@
+# AdventOfCode Tools
+This project is intended to store useful tools to speed-up/clean-up solutions to the AdventOfCode.
---
+++ packages/aoc-tools-dannyboywoop/aoc-tools-dannyboywoop-1.2.0.tar.gz/aoc-tools-dannyboywoop-1.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aoc-tools-dannyboywoop",
+    version="1.2.0",
+    author="Daniel Holmes",
+    author_email="DanielJHolmes@hotmail.co.uk",
+    description="AdventOfCode tools.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dannyboywoop/AOC_Tools",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

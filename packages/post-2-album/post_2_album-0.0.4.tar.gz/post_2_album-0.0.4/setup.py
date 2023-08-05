2c6b24d5f9baeeb2b2e3f571fa410948d9b979d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:42 +0000

    Add package post-2-album/0.0.4/post_2_album-0.0.4.tar.gz

---
+++ packages/post-2-album/post_2_album-0.0.4.tar.gz/post_2_album-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="post_2_album",
+    version="0.0.4",
+    author="Yunzhi Gao",
+    author_email="gaoyunzhi@gmail.com",
+    description="Return photo list and caption from telegram post link.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaoyunzhi/post_2_album",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'webgram',
+        'telegram_util',
+    ],
+    python_requires='>=3.0',
+)

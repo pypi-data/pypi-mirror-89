cfcf7545ebe896f281312294d923ac2747a2397d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:24 +0000

    Add package tapfile/0.3/tapfile-0.3.tar.gz

---
+++ packages/tapfile/tapfile-0.3.tar.gz/tapfile-0.3/tap_file/dipole.py
@@ -0,0 +1,38 @@
+from .profile import Profile
+
+
+class Dipole:
+    def __init__(self, profile=None):
+        if profile is None:
+            profile = Profile()
+        self.profile = profile
+        self.scaling = [1]*profile.buckets
+
+    def classify(self, dur):
+        """Translate a pulse duration in to S, M, L or X."""
+        if dur is None:
+            return 'X'
+
+        scaled_dur = dur * (sum(self.scaling) / len(self.scaling))
+
+        if scaled_dur < self.profile.low or scaled_dur > self.profile.hi:
+            return 'X'
+
+        if scaled_dur < self.profile.hi_s:
+            # short
+            ret = 'S'
+            ideal = self.profile.ideal_s
+        elif scaled_dur < self.profile.hi_m:
+            # medium
+            ret = 'M'
+            ideal = self.profile.ideal_m
+        else:
+            # long
+            ret = 'L'
+            ideal = self.profile.ideal_l
+
+        self.scaling.pop(0)
+        val = ideal/dur
+        self.scaling.append(val)
+
+        return ret

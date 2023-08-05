925a5e2d059b7c4c81a1e73c8b5931fbb38a1222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:40 +0000

    Add package python-monerorpc/0.6.2/python-monerorpc-0.6.2.tar.gz

---
+++ packages/python-monerorpc/python-monerorpc-0.6.2.tar.gz/python-monerorpc-0.6.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+__version__ = "v0.6.2"
+
+setup(
+    name="python-monerorpc",
+    version=__version__,
+    description="Enhanced version of python-jsonrpc for Monero (monerod, monero-wallet-rpc).",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Norman Moeschter-Schenck",
+    author_email="<norman.moeschter@gmail.com>",
+    maintainer="Norman Moeschter-Schenck",
+    maintainer_email="<norman.moeschter@gmail.com>",
+    url="https://www.github.com/monero-ecosystem/python-monerorpc",
+    download_url=f"https://github.com/monero-ecosystem/python-monerorpc/archive/{__version__}.tar.gz",
+    packages=["monerorpc"],
+    install_requires=["requests>=2.23.0"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)

ca52a7bd8c00305f15a16c3857e3132ac8fda87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:42 +0000

    Add package pytorch-to-returnn/1.20201218.173405/pytorch_to_returnn-1.20201218.173405.tar.gz

---
+++ packages/pytorch-to-returnn/pytorch_to_returnn-1.20201218.173405.tar.gz/pytorch_to_returnn-1.20201218.173405/pytorch_to_returnn/torch/__init__.py
@@ -0,0 +1,9 @@
+
+from .tensor import *
+from ._C import *
+from .nn.functional import *
+from .serialization import load
+from .autograd import *
+from .jit import *
+from .onnx import *
+from . import nn

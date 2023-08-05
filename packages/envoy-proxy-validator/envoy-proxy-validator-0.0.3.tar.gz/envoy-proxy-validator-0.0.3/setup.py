f0593a8449d6d5957eaac15ac1291edddfc2376f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:27 +0000

    Add package envoy-proxy-validator/0.0.3/envoy-proxy-validator-0.0.3.tar.gz

---
+++ packages/envoy-proxy-validator/envoy-proxy-validator-0.0.3.tar.gz/envoy-proxy-validator-0.0.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='envoy-proxy-validator',
+    version='0.0.3',
+    author='yunqiang-sun',
+    author_email='admin@sunyunqiang.com',
+    url='https://sunyunqiang.com',
+    description=u'python compile files for envoy proxy validator',
+    packages=find_packages(),
+)

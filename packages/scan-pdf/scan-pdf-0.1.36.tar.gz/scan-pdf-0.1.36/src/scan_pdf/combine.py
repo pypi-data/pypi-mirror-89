3abcc44bf8a4144080d32adc1ee6a806d0b5a8f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:30 +0000

    Add package scan-pdf/0.1.36/scan-pdf-0.1.36.tar.gz

---
+++ packages/scan-pdf/scan-pdf-0.1.36.tar.gz/scan-pdf-0.1.36/src/scan_pdf/combine.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+import subprocess
+
+logger = logging.getLogger(__name__)
+
+
+class Combiner(object):
+    def __init__(self, options):
+        self.options = options
+
+    def combine(self, page_file_names):
+        output_file_name = self.options.output_file_name[0]
+        logger.info("combine %d pages into %s", len(page_file_names), output_file_name)
+        combine_args = ['pdftk']
+        combine_args += page_file_names
+        combine_args += ['output', os.path.basename(output_file_name), 'compress']
+        logger.debug("call: %s", " ".join(combine_args))
+        returncode = subprocess.call(combine_args)
+
+        if returncode != 0:
+            logger.error("combine failed: %s", " ".join(combine_args))
+
+        if not os.path.exists(output_file_name):
+            logger.error("output file '%s' does not exist", output_file_name)
+            subprocess.call(["ls", "-la"])
+            subprocess.call(["pwd"])
+
+        return returncode

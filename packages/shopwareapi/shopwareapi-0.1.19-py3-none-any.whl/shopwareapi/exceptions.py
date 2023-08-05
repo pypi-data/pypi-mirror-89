b39a2c72d9024f57ecc135cfcf6fafa07111a02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:36 +0000

    Add package shopwareapi/0.1.19/shopwareapi-0.1.19-py3-none-any.whl

---
+++ packages/shopwareapi/shopwareapi-0.1.19-py3-none-any.whl/shopwareapi/exceptions.py
@@ -0,0 +1,52 @@
+class ConfigurationError(ValueError):
+    """
+        Raises when the given Configuration doesnt available
+    """
+    pass
+
+
+class ConfigurationValueError(ConfigurationError):
+    """
+        Raises when a value is invalid for configuration
+    """
+    pass
+
+
+class FronzenConfigurationError(ConfigurationError):
+    """
+        Raises if changes will me made after the object is frozen
+    """
+    pass
+
+
+class ShopwareClientHttpError(RuntimeError):
+    """
+        Raises when statuscode is not 299 <= response.status_code >= 200
+    """
+
+    def __init__(self, message, errors=None):
+        self.message = message
+        self.errors = errors
+
+    def __str__(self):
+        return str(self.message)
+
+
+class ParameterError(KeyError):
+    """
+        Raises every time when invalid get, create, find etc. parameters are provided
+    """
+
+
+class ObjectDoesNotExist(ValueError):
+    """
+        Subexception Raises when requested data not found at remote shop
+        This exception will be attached to model class
+    """
+
+
+class MultipleObjectsReturned(ValueError):
+    """
+        Raises also as Subexception when too many objects returned for given request
+        (get() returns multiple results)
+    """

25b6ed7a14418c9f22300ada4007dcc609286a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:34 +0000

    Add package gorilla-core/0.2.5.2/gorilla_core-0.2.5.2-py3-none-any.whl

---
+++ packages/gorilla-core/gorilla_core-0.2.5.2-py3-none-any.whl/gorilla/core/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) Gorilla-Lab. All rights reserved.
+from .dist import init_dist, get_dist_info, master_only
+
+from .misc import (concat_list, convert_list, convert_list_str,
+                   convert_list_int, convert_list_float, iter_cast, slice_list,
+                   concat_list, check_prerequisites, deprecated_api_warning,
+                   is_seq_of, is_list_of, is_tuple_of, multi_apply,
+                   is_multiple, is_power2)
+
+from .comm import (get_world_size, get_rank, get_local_rank, get_local_size,
+                   is_main_process, synchronize, gather, all_gather,
+                   shared_random_seed, reduce_dict)
+
+from .env import set_random_seed, collect_env_info
+
+from .launch import launch
+
+from .registry import Registry, build_from_cfg, auto_registry
+
+MODELS = Registry("models")
+MODULES = Registry("modules")
+DATASETS = Registry("datasets")
+
+__all__ = [k for k in globals().keys() if not k.startswith("_")]
---
+++ packages/gorilla-core/gorilla_core-0.2.5.2-py3-none-any.whl/gorilla/nn/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) Gorilla-Lab. All rights reserved.
+from .weight_init import (bias_init_with_prob, constant_init, kaiming_init,
+                          normal_init, uniform_init, xavier_init, c2_msra_init,
+                          c2_xavier_init, geometric_init)
+
+from .conv import GorillaConv
+from .fc import GorillaFC, MultiFC, DenseFC
+from .vgg import VGG
+from .alexnet import AlexNet
+from .resnet import (BasicBlock, Bottleneck, ResNet, conv3x3, resnet18,
+                     resnet34, resnet50, resnet101, resnet152, resnet)
+from .gcn import GraphConvolution, GCN
+from .transformer import (Transformer, TransformerEncoder, TransformerDecoder,
+                          TransformerEncoderLayer, TransformerDecoderLayer,
+                          build_transformer)
+
+from ..core import MODULES, auto_registry
+auto_registry(MODULES, globals())
+
+from .layer_builder import NAME_MAP, build_from_package, get_torch_layer_caller
+
+__all__ = [k for k in globals().keys() if not k.startswith("_")]

8e592d17fd47886a4ef178852425ebe172bc8d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:07 +0000

    Add package keras-dense-sparse-matmul/0.3.0/keras-dense-sparse-matmul-0.3.0.tar.gz

---
+++ packages/keras-dense-sparse-matmul/keras-dense-sparse-matmul-0.3.0.tar.gz/keras-dense-sparse-matmul-0.3.0/keras_dense_sparse_matmul/__init__.py
@@ -0,0 +1,6 @@
+import warnings
+warnings.warn((
+    "The 'keras_dense_sparse_matmul' module is deprecated. "
+    "Please use 'keras_tweaks.dense_sparse_matmul' instead."),
+    DeprecationWarning,
+    stacklevel=2)

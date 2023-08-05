b6f66443966f0f65de9f91523a08645bedf658c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:58 +0000

    Add package RateMyProfessorAPI/1.0.1/RateMyProfessorAPI-1.0.1-py2.py3-none-any.whl

---
+++ packages/RateMyProfessorAPI/RateMyProfessorAPI-1.0.1-py2.py3-none-any.whl/ratemyprofessor/school.py
@@ -0,0 +1,28 @@
+import re
+import requests
+
+
+class School:
+    """Represents a school."""
+
+    def __init__(self, school_id: int):
+        """
+        Initializes a school to the school id.
+
+        :param school_id: The school's id.
+        """
+
+        self.id = school_id
+        self.name = self._get_name()
+
+    def _get_name(self):
+        url = f'https://www.ratemyprofessors.com/campusRatings.jsp?sid={self.id}'
+        page = requests.get(url)
+        school_names = re.findall(r"schoolName: '(.*?)'", page.text)
+
+        if school_names:
+            school_name = str(school_names[0])
+        else:
+            raise ValueError('Invalid school id!')
+
+        return school_name

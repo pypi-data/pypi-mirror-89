cbb10bc7facb6c96bba6ec994d5e0544619359f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:37 +0000

    Add package youtube-search-requests/0.0.23/youtube-search-requests-0.0.23.tar.gz

---
+++ packages/youtube-search-requests/youtube-search-requests-0.0.23.tar.gz/youtube-search-requests-0.0.23/youtube_search_requests/utils/errors.py
@@ -0,0 +1,10 @@
+# youtube-search-requests 
+# errors.py
+
+# raised when one of each argument is invalid
+class InvalidArgument(Exception):
+    pass
+
+# raised when url is invalid
+class InvalidURL(Exception):
+    pass

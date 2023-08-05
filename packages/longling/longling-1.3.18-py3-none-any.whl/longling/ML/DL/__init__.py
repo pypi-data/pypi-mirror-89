cc61eab06b1d0e9b6d444a77f194c07dd59bf1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:34 +0000

    Add package longling/1.3.18/longling-1.3.18-py3-none-any.whl

---
+++ packages/longling/longling-1.3.18-py3-none-any.whl/longling/ML/DL/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# create by tongshiwei on 2019-9-4
+
+from .ecli import cli as ecli
+from .module import Module
+from .optimizer_cfg import get_optimizer_cfg
+from .service import CliServiceModule, service_wrapper

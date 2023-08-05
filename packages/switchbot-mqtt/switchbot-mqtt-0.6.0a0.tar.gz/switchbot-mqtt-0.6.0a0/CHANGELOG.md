33c6ed8da0645ab743cbd2a01161411c472f00a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:41 +0000

    Add package switchbot-mqtt/0.6.0a0/switchbot-mqtt-0.6.0a0.tar.gz

---
+++ packages/switchbot-mqtt/switchbot-mqtt-0.6.0a0.tar.gz/switchbot-mqtt-0.6.0a0/CHANGELOG.md
@@ -0,0 +1,62 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+### Added
+- Control [SwitchBot Curtain](https://www.switch-bot.com/products/switchbot-curtain) motors
+  via `OPEN`, `CLOSE`, and `STOP` on topic `homeassistant/cover/switchbot-curtain/aa:bb:cc:dd:ee:ff/set`
+
+### Changed
+- Docker image:
+  - Upgrade `paho-mqtt` to no longer suppress exceptions occuring in mqtt callbacks
+    ( https://github.com/eclipse/paho.mqtt.python/blob/v1.5.1/ChangeLog.txt#L4 )
+  - Build stage: revert user after applying `chown` workaround for inter-stage copy
+- Log format: added name of logger between level and message
+
+## [0.5.0] - 2020-11-22
+### Added
+- Docker image: support parametrization via environment variables
+  (`MQTT_HOST`, `MQTT_PORT`, `MQTT_USERNAME` & `MQTT_PASSWORD`)
+
+## [0.4.1] - 2020-06-18
+### Fixed
+- Compatibility with python3.5:
+  - Replaced [PEP526](https://www.python.org/dev/peps/pep-0526/#abstract)-style variable type hint
+    with [PEP484](https://www.python.org/dev/peps/pep-0484/)-compatible
+  - Tests: Fixed `AttributeError` due to unavailable `MagicMock.assert_called_once`
+
+## [0.4.0] - 2020-06-14
+### Added
+- Added command line parameter `--mqtt-password-file`
+
+### Fixed
+- Docker build: fix `pipenv` failing to create cache
+
+## [0.3.0] - 2020-05-08
+### Added
+- Publish new state to `homeassistant/switch/switchbot/MAC_ADDRESS/state` on success
+
+## [0.2.0] - 2020-05-08
+### Added
+- Added command line parameters `--mqtt-username` and `--mqtt-password`
+
+### Fixed
+- Fixed executable name in command line help
+- Docker: no longer require build arg `SWITCHBOT_MQTT_VERSION`
+  (fixes auto build on hub.docker.com)
+
+## [0.1.0] - 2020-05-08
+### Added
+- Subscribe to `homeassistant/switch/switchbot/+/set`.
+  Handle `ON` and `OFF` messages.
+
+[Unreleased]: https://github.com/fphammerle/switchbot-mqtt/compare/v0.5.0...HEAD
+[0.5.0]: https://github.com/fphammerle/switchbot-mqtt/compare/v0.4.1...v0.5.0
+[0.4.1]: https://github.com/fphammerle/switchbot-mqtt/compare/v0.4.0...v0.4.1
+[0.4.0]: https://github.com/fphammerle/switchbot-mqtt/compare/v0.3.0...v0.4.0
+[0.3.0]: https://github.com/fphammerle/switchbot-mqtt/compare/v0.2.0...v0.3.0
+[0.2.0]: https://github.com/fphammerle/switchbot-mqtt/compare/v0.1.0...v0.2.0
+[0.1.0]: https://github.com/fphammerle/switchbot-mqtt/releases/tag/v0.1.0
---
+++ packages/switchbot-mqtt/switchbot-mqtt-0.6.0a0.tar.gz/switchbot-mqtt-0.6.0a0/tests/test_mqtt.py
@@ -0,0 +1,367 @@
+# switchbot-mqtt - MQTT client controlling SwitchBot button & curtain automators,
+# compatible with home-assistant.io's MQTT Switch & Cover platform
+#
+# Copyright (C) 2020 Fabian Peter Hammerle <fabian@hammerle.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+import typing
+import unittest.mock
+
+import pytest
+from paho.mqtt.client import MQTT_ERR_QUEUE_SIZE, MQTT_ERR_SUCCESS, MQTTMessage, Client
+
+import switchbot_mqtt
+
+# pylint: disable=protected-access
+
+
+@pytest.mark.parametrize("mqtt_host", ["mqtt-broker.local"])
+@pytest.mark.parametrize("mqtt_port", [1833])
+def test__run(mqtt_host, mqtt_port):
+    with unittest.mock.patch("paho.mqtt.client.Client") as mqtt_client_mock:
+        switchbot_mqtt._run(
+            mqtt_host=mqtt_host,
+            mqtt_port=mqtt_port,
+            mqtt_username=None,
+            mqtt_password=None,
+        )
+    mqtt_client_mock.assert_called_once_with()
+    assert not mqtt_client_mock().username_pw_set.called
+    mqtt_client_mock().connect.assert_called_once_with(host=mqtt_host, port=mqtt_port)
+    mqtt_client_mock().socket().getpeername.return_value = (mqtt_host, mqtt_port)
+    mqtt_client_mock().on_connect(mqtt_client_mock(), None, {}, 0)
+    assert mqtt_client_mock().subscribe.call_args_list == [
+        unittest.mock.call("homeassistant/switch/switchbot/+/set"),
+        unittest.mock.call("homeassistant/cover/switchbot-curtain/+/set"),
+    ]
+    assert mqtt_client_mock().message_callback_add.call_args_list == [
+        unittest.mock.call(
+            sub="homeassistant/switch/switchbot/+/set",
+            callback=switchbot_mqtt._ButtonAutomator._mqtt_command_callback,
+        ),
+        unittest.mock.call(
+            sub="homeassistant/cover/switchbot-curtain/+/set",
+            callback=switchbot_mqtt._CurtainMotor._mqtt_command_callback,
+        ),
+    ]
+    mqtt_client_mock().loop_forever.assert_called_once_with()
+
+
+@pytest.mark.parametrize("mqtt_host", ["mqtt-broker.local"])
+@pytest.mark.parametrize("mqtt_port", [1833])
+@pytest.mark.parametrize("mqtt_username", ["me"])
+@pytest.mark.parametrize("mqtt_password", [None, "secret"])
+def test__run_authentication(mqtt_host, mqtt_port, mqtt_username, mqtt_password):
+    with unittest.mock.patch("paho.mqtt.client.Client") as mqtt_client_mock:
+        switchbot_mqtt._run(
+            mqtt_host=mqtt_host,
+            mqtt_port=mqtt_port,
+            mqtt_username=mqtt_username,
+            mqtt_password=mqtt_password,
+        )
+    mqtt_client_mock.assert_called_once_with()
+    mqtt_client_mock().username_pw_set.assert_called_once_with(
+        username=mqtt_username, password=mqtt_password
+    )
+
+
+@pytest.mark.parametrize("mqtt_host", ["mqtt-broker.local"])
+@pytest.mark.parametrize("mqtt_port", [1833])
+@pytest.mark.parametrize("mqtt_password", ["secret"])
+def test__run_authentication_missing_username(mqtt_host, mqtt_port, mqtt_password):
+    with unittest.mock.patch("paho.mqtt.client.Client"):
+        with pytest.raises(ValueError):
+            switchbot_mqtt._run(
+                mqtt_host=mqtt_host,
+                mqtt_port=mqtt_port,
+                mqtt_username=None,
+                mqtt_password=mqtt_password,
+            )
+
+
+@pytest.mark.parametrize(
+    ("command_topic_levels", "topic", "payload", "expected_mac_address"),
+    [
+        (
+            switchbot_mqtt._ButtonAutomator.MQTT_COMMAND_TOPIC_LEVELS,
+            b"homeassistant/switch/switchbot/aa:bb:cc:dd:ee:ff/set",
+            b"ON",
+            "aa:bb:cc:dd:ee:ff",
+        ),
+        (
+            switchbot_mqtt._ButtonAutomator.MQTT_COMMAND_TOPIC_LEVELS,
+            b"homeassistant/switch/switchbot/aa:bb:cc:dd:ee:ff/set",
+            b"OFF",
+            "aa:bb:cc:dd:ee:ff",
+        ),
+        (
+            switchbot_mqtt._ButtonAutomator.MQTT_COMMAND_TOPIC_LEVELS,
+            b"homeassistant/switch/switchbot/aa:bb:cc:dd:ee:ff/set",
+            b"on",
+            "aa:bb:cc:dd:ee:ff",
+        ),
+        (
+            switchbot_mqtt._ButtonAutomator.MQTT_COMMAND_TOPIC_LEVELS,
+            b"homeassistant/switch/switchbot/aa:bb:cc:dd:ee:ff/set",
+            b"off",
+            "aa:bb:cc:dd:ee:ff",
+        ),
+        (
+            switchbot_mqtt._ButtonAutomator.MQTT_COMMAND_TOPIC_LEVELS,
+            b"homeassistant/switch/switchbot/aa:01:23:45:67:89/set",
+            b"ON",
+            "aa:01:23:45:67:89",
+        ),
+        (
+            ["switchbot", switchbot_mqtt._MQTTTopicPlaceholder.MAC_ADDRESS],
+            b"switchbot/aa:01:23:45:67:89",
+            b"ON",
+            "aa:01:23:45:67:89",
+        ),
+        (
+            switchbot_mqtt._CurtainMotor.MQTT_COMMAND_TOPIC_LEVELS,
+            b"homeassistant/cover/switchbot-curtain/aa:01:23:45:67:89/set",
+            b"OPEN",
+            "aa:01:23:45:67:89",
+        ),
+    ],
+)
+def test__mqtt_command_callback(
+    caplog,
+    command_topic_levels: typing.List[switchbot_mqtt._MQTTTopicLevel],
+    topic: bytes,
+    payload: bytes,
+    expected_mac_address: str,
+):
+    class _ActorMock(switchbot_mqtt._MQTTControlledActor):
+        MQTT_COMMAND_TOPIC_LEVELS = command_topic_levels
+
+        def __init__(self, mac_address):
+            super().__init__(mac_address=mac_address)
+
+        def execute_command(self, mqtt_message_payload: bytes, mqtt_client: Client):
+            pass
+
+    message = MQTTMessage(topic=topic)
+    message.payload = payload
+    with unittest.mock.patch.object(
+        _ActorMock, "__init__", return_value=None
+    ) as init_mock, unittest.mock.patch.object(
+        _ActorMock, "execute_command"
+    ) as execute_command_mock, caplog.at_level(
+        logging.DEBUG
+    ):
+        _ActorMock._mqtt_command_callback("client_dummy", None, message)
+    init_mock.assert_called_once_with(mac_address=expected_mac_address)
+    execute_command_mock.assert_called_once_with(
+        mqtt_client="client_dummy", mqtt_message_payload=payload
+    )
+    assert caplog.record_tuples == [
+        (
+            "switchbot_mqtt",
+            logging.DEBUG,
+            "received topic={} payload={!r}".format(topic.decode(), payload),
+        )
+    ]
+
+
+@pytest.mark.parametrize(
+    ("topic", "payload"),
+    [
+        (b"homeassistant/switch/switchbot/aa:bb:cc:dd:ee:ff", b"on"),
+        (b"homeassistant/switch/switchbot/aa:bb:cc:dd:ee:ff/change", b"ON"),
+        (b"homeassistant/switch/switchbot/aa:bb:cc:dd:ee:ff/set/suffix", b"ON"),
+    ],
+)
+def test__mqtt_command_callback_unexpected_topic(caplog, topic: bytes, payload: bytes):
+    class _ActorMock(switchbot_mqtt._MQTTControlledActor):
+        MQTT_COMMAND_TOPIC_LEVELS = (
+            switchbot_mqtt._ButtonAutomator.MQTT_COMMAND_TOPIC_LEVELS
+        )
+
+        def __init__(self, mac_address):
+            super().__init__(mac_address=mac_address)
+
+        def execute_command(self, mqtt_message_payload: bytes, mqtt_client: Client):
+            pass
+
+    message = MQTTMessage(topic=topic)
+    message.payload = payload
+    with unittest.mock.patch.object(
+        _ActorMock, "__init__", return_value=None
+    ) as init_mock, unittest.mock.patch.object(
+        _ActorMock, "execute_command"
+    ) as execute_command_mock, caplog.at_level(
+        logging.DEBUG
+    ):
+        _ActorMock._mqtt_command_callback("client_dummy", None, message)
+    init_mock.assert_not_called()
+    execute_command_mock.assert_not_called()
+    assert caplog.record_tuples == [
+        (
+            "switchbot_mqtt",
+            logging.DEBUG,
+            "received topic={} payload={!r}".format(topic.decode(), payload),
+        ),
+        (
+            "switchbot_mqtt",
+            logging.WARNING,
+            "unexpected topic {}".format(topic.decode()),
+        ),
+    ]
+
+
+@pytest.mark.parametrize(("mac_address", "payload"), [("aa:01:23:4E:RR:OR", b"ON")])
+def test__mqtt_command_callback_invalid_mac_address(
+    caplog, mac_address: str, payload: bytes
+):
+    class _ActorMock(switchbot_mqtt._MQTTControlledActor):
+        MQTT_COMMAND_TOPIC_LEVELS = (
+            switchbot_mqtt._ButtonAutomator.MQTT_COMMAND_TOPIC_LEVELS
+        )
+
+        def __init__(self, mac_address):
+            super().__init__(mac_address=mac_address)
+
+        def execute_command(self, mqtt_message_payload: bytes, mqtt_client: Client):
+            pass
+
+    topic = "homeassistant/switch/switchbot/{}/set".format(mac_address).encode()
+    message = MQTTMessage(topic=topic)
+    message.payload = payload
+    with unittest.mock.patch.object(
+        _ActorMock, "__init__", return_value=None
+    ) as init_mock, unittest.mock.patch.object(
+        _ActorMock, "execute_command"
+    ) as execute_command_mock, caplog.at_level(
+        logging.DEBUG
+    ):
+        _ActorMock._mqtt_command_callback("client_dummy", None, message)
+    init_mock.assert_not_called()
+    execute_command_mock.assert_not_called()
+    assert caplog.record_tuples == [
+        (
+            "switchbot_mqtt",
+            logging.DEBUG,
+            "received topic={} payload={!r}".format(topic.decode(), payload),
+        ),
+        (
+            "switchbot_mqtt",
+            logging.WARNING,
+            "invalid mac address {}".format(mac_address),
+        ),
+    ]
+
+
+@pytest.mark.parametrize(
+    ("topic", "payload"),
+    [(b"homeassistant/switch/switchbot/aa:bb:cc:dd:ee:ff/set", b"ON")],
+)
+def test__mqtt_command_callback_ignore_retained(caplog, topic: bytes, payload: bytes):
+    class _ActorMock(switchbot_mqtt._MQTTControlledActor):
+        MQTT_COMMAND_TOPIC_LEVELS = (
+            switchbot_mqtt._ButtonAutomator.MQTT_COMMAND_TOPIC_LEVELS
+        )
+
+        def __init__(self, mac_address):
+            super().__init__(mac_address=mac_address)
+
+        def execute_command(self, mqtt_message_payload: bytes, mqtt_client: Client):
+            pass
+
+    message = MQTTMessage(topic=topic)
+    message.payload = payload
+    message.retain = True
+    with unittest.mock.patch.object(
+        _ActorMock, "__init__", return_value=None
+    ) as init_mock, unittest.mock.patch.object(
+        _ActorMock, "execute_command"
+    ) as execute_command_mock, caplog.at_level(
+        logging.DEBUG
+    ):
+        _ActorMock._mqtt_command_callback("client_dummy", None, message)
+    init_mock.assert_not_called()
+    execute_command_mock.assert_not_called()
+    assert caplog.record_tuples == [
+        (
+            "switchbot_mqtt",
+            logging.DEBUG,
+            "received topic={} payload={!r}".format(topic.decode(), payload),
+        ),
+        ("switchbot_mqtt", logging.INFO, "ignoring retained message"),
+    ]
+
+
+@pytest.mark.parametrize(
+    ("state_topic_levels", "mac_address", "expected_topic"),
+    # https://www.home-assistant.io/docs/mqtt/discovery/#switches
+    [
+        (
+            switchbot_mqtt._ButtonAutomator.MQTT_STATE_TOPIC_LEVELS,
+            "aa:bb:cc:dd:ee:ff",
+            "homeassistant/switch/switchbot/aa:bb:cc:dd:ee:ff/state",
+        ),
+        (
+            ["switchbot", switchbot_mqtt._MQTTTopicPlaceholder.MAC_ADDRESS, "state"],
+            "aa:bb:cc:dd:ee:gg",
+            "switchbot/aa:bb:cc:dd:ee:gg/state",
+        ),
+    ],
+)
+@pytest.mark.parametrize("state", [b"ON", b"CLOSE"])
+@pytest.mark.parametrize("return_code", [MQTT_ERR_SUCCESS, MQTT_ERR_QUEUE_SIZE])
+def test__report_state(
+    caplog,
+    state_topic_levels: typing.List[switchbot_mqtt._MQTTTopicLevel],
+    mac_address: str,
+    expected_topic: str,
+    state: bytes,
+    return_code: int,
+):
+    # pylint: disable=too-many-arguments
+    class _ActorMock(switchbot_mqtt._MQTTControlledActor):
+        MQTT_STATE_TOPIC_LEVELS = state_topic_levels
+
+        def __init__(self, mac_address):
+            super().__init__(mac_address=mac_address)
+
+        def execute_command(self, mqtt_message_payload: bytes, mqtt_client: Client):
+            pass
+
+    mqtt_client_mock = unittest.mock.MagicMock()
+    mqtt_client_mock.publish.return_value.rc = return_code
+    with caplog.at_level(logging.DEBUG):
+        _ActorMock(mac_address=mac_address).report_state(
+            state=state, mqtt_client=mqtt_client_mock
+        )
+    mqtt_client_mock.publish.assert_called_once_with(
+        topic=expected_topic, payload=state, retain=True
+    )
+    assert caplog.record_tuples[0] == (
+        "switchbot_mqtt",
+        logging.DEBUG,
+        "publishing topic={} payload={!r}".format(expected_topic, state),
+    )
+    if return_code == MQTT_ERR_SUCCESS:
+        assert not caplog.records[1:]
+    else:
+        assert caplog.record_tuples[1:] == [
+            (
+                "switchbot_mqtt",
+                logging.ERROR,
+                "failed to publish state (rc={})".format(return_code),
+            )
+        ]

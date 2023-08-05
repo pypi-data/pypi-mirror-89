9941b299665e624695468ba47665cd4162ee955b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:44 +0000

    Add package createf-example-pkg/0.0.1/createf_example-pkg-0.0.1.tar.gz

---
+++ packages/createf-example-pkg/createf_example-pkg-0.0.1.tar.gz/createf_example-pkg-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="createf_example-pkg",
+    # Replace with your own username above
+    version="0.0.1",
+    author="Nessa Kenny",
+    author_email="author@example.com",
+    description="A file create package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    # if you have libraries that your module/package/library
+    #you would include them in the install_requires argument
+    install_requires=[''],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

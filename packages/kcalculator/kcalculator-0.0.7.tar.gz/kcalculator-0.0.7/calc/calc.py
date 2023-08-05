180543d51f3afa7fce61db3d5226b122d9ba624d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:48 +0000

    Add package kcalculator/0.0.7/kcalculator-0.0.7.tar.gz

---
+++ packages/kcalculator/kcalculator-0.0.7.tar.gz/kcalculator-0.0.7/calc/calc.py
@@ -0,0 +1,12 @@
+def add_numbers(num1,num2):
+	return num1 + num2
+
+def subtact_numbers(num1,num2):
+	return num1 - num2
+
+def multy_numbers(num1,num2):
+	return num1 * num2
+
+def divide_numbers(num1,num2):
+	return num1 / num2
+

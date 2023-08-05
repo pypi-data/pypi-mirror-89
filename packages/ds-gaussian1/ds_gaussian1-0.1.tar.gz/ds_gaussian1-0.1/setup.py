02c6ba57b4e39c44c38359713fba4fb4385208b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:55 +0000

    Add package ds-gaussian1/0.1/ds_gaussian1-0.1.tar.gz

---
+++ packages/ds-gaussian1/ds_gaussian1-0.1.tar.gz/ds_gaussian1-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ds_gaussian1',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ds_gaussian1'],
+      zip_safe=False)

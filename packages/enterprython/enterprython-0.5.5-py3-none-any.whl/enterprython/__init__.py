bf5b59aa0993458bdb180e60d2c14eb2b20b76e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:10 +0000

    Add package enterprython/0.5.5/enterprython-0.5.5-py3-none-any.whl

---
+++ packages/enterprython/enterprython-0.5.5-py3-none-any.whl/enterprython/__init__.py
@@ -0,0 +1,9 @@
+from ._inject import add_value, add_values, set_values, set_values_from_config
+from ._inject import assemble, component, factory, value
+
+name = "enterprython"
+
+__author__ = "Tobias Hermann"
+__copyright__ = "Copyright 2018, Tobias Hermann"
+__email__ = "editgym@gmail.com"
+__license__ = "MIT"

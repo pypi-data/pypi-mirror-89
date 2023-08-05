8afe045f447b824fe558949691b12ee1c6b4778e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:25 +0000

    Add package khmer-nltk/1.2/khmer-nltk-1.2.tar.gz

---
+++ packages/khmer-nltk/khmer-nltk-1.2.tar.gz/khmer-nltk-1.2/khmernltk/sentence_tokenize/__init__.py
@@ -0,0 +1,20 @@
+import re
+from khmernltk.sentence_tokenize.features import SENTENCE_SEPARATOR
+
+
+def sentence_tokenize(text: str):
+    """
+    Khmer language sentence tokenization
+    ====================================
+    :param text: (str) Raw text
+    ====================================
+    :return: (list) List of sentences
+    """
+    sentences = re.split(r"(?<=[" + "".join(SENTENCE_SEPARATOR) + "])\s*", text)
+    return sentences
+
+
+if __name__ == "__main__":
+    text = "ខេត្តកំពង់ធំ ៖ ឪពុកនិង​កូន​ប្រុស​នាំគ្នា​យកម៉ូទ័រ​​ទៅ​ ។"
+    print(len(sentence_tokenize(text)))
+    print(sentence_tokenize(text))

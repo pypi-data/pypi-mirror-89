80e3de793a0516d9669a44c4f030f91a61ad4a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:30 +0000

    Add package security-utils/0.0.1/security_utils-0.0.1.tar.gz

---
+++ packages/security-utils/security_utils-0.0.1.tar.gz/security_utils-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+ 
+classifiers = []
+ 
+setup(
+  name='security_utils',
+  version='0.0.1',
+  description='Security utils',
+  long_description="Security utils",
+  url='',  
+  author='Alex Demure',
+  author_email='alexanderdemure@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='itsdangerous',
+  packages=find_packages(),
+  install_requires=[
+    'itsdangerous>=1.1.0'
+  ],
+  dependency_links=['https://github.com/AlexDemure/security_utils']
+)

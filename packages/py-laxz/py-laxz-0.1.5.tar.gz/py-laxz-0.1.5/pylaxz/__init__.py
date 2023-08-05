e0cb0c5b58297ee3f5ac92f29af6446cd269f36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:11 +0000

    Add package py-laxz/0.1.5/py-laxz-0.1.5.tar.gz

---
+++ packages/py-laxz/py-laxz-0.1.5.tar.gz/py-laxz-0.1.5/pylaxz/__init__.py
@@ -0,0 +1,10 @@
+"""
+pylaxz
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Copyright (c) 2020 Min Latt.
+License: MIT, see LICENSE for more details.
+"""
+
+# populate fields for >>> help(pylaxz)
+from .utils import *
+from .iots import *

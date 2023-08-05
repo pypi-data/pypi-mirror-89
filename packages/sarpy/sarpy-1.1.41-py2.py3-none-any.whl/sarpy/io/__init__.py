366e682b2ec0d01634027880831b0d7c6e5bfd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:40 +0000

    Add package sarpy/1.1.41/sarpy-1.1.41-py2.py3-none-any.whl

---
+++ packages/sarpy/sarpy-1.1.41-py2.py3-none-any.whl/sarpy/io/__init__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+from .complex.converter import open_complex
+from .phase_history.converter import open_phase_history
+from .product.converter import open_product
+from .other_image.converter import open_other
+from .general.converter import open_general
+
+
+__classification__ = "UNCLASSIFIED"
+
+
+def open(file_name):
+    """
+    Given a file, try to find and return the appropriate reader object.
+
+    Parameters
+    ----------
+    file_name : str
+
+    Returns
+    -------
+    BaseReader
+
+    Raises
+    ------
+    IOError
+    """
+
+    try:
+        return open_complex(file_name)
+    except IOError:
+        pass
+
+    try:
+        return open_product(file_name)
+    except IOError:
+        pass
+
+    try:
+        return open_phase_history(file_name)
+    except IOError:
+        pass
+
+    try:
+        return open_other(file_name)
+    except IOError:
+        pass
+
+    try:
+        return open_general(file_name)
+    except IOError:
+        pass
+
+    raise IOError(
+        'The format of file {} does not match any reader in the complex, '
+        'product, phase_history, other_image, or general  modules.')

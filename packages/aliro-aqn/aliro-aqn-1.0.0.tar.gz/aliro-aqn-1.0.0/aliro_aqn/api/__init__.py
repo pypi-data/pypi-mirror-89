c2d0107f4d65dfa8145454a1a77e05f9f3683b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:37 +0000

    Add package aliro-aqn/1.0.0/aliro-aqn-1.0.0.tar.gz

---
+++ packages/aliro-aqn/aliro-aqn-1.0.0.tar.gz/aliro-aqn-1.0.0/aliro_aqn/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from aliro_aqn.api.authentication_api import AuthenticationApi
+from aliro_aqn.api.submissions_api import SubmissionsApi
+from aliro_aqn.api.user_api import UserApi

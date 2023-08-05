e347c319b9edb78c531dfc89d029ba10d5c071a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:23 +0000

    Add package xiaobaiauto2/0.1.6.5/xiaobaiauto2-0.1.6.5-py3-none-any.whl

---
+++ packages/xiaobaiauto2/xiaobaiauto2-0.1.6.5-py3-none-any.whl/xiaobaiauto2/utils/xiaobaiauto2CheckEnv.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+__author__ = 'Tser'
+__email__ = '807447312@qq.com'
+__project__ = 'xiaobaiauto2'
+__script__ = 'xiaobaiauto2CheckEnv.py'
+__create_time__ = '2020/12/10 14:00'
+
+'''
+    工具用于环境的检测
+    
+'''

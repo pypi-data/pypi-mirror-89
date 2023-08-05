9055bf201c74085718f87f9d8d4a49bb377cd973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:28 +0000

    Add package shipflowmotionshelpers/0.1.7/shipflowmotionshelpers-0.1.7-py2.py3-none-any.whl

---
+++ packages/shipflowmotionshelpers/shipflowmotionshelpers-0.1.7-py2.py3-none-any.whl/shipflowmotionshelpers/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for shipflowmotionshelpers."""
+
+__author__ = """Martin Alexandersson"""
+__email__ = 'maralex@chalmers.se'
+__version__ = '0.1.0'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions

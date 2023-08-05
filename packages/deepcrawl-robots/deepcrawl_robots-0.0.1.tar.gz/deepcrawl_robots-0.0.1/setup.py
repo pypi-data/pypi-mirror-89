6be78b593735339f2b8e16d7ef21b36045ebd573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:43 +0000

    Add package deepcrawl-robots/0.0.1/deepcrawl_robots-0.0.1.tar.gz

---
+++ packages/deepcrawl-robots/deepcrawl_robots-0.0.1.tar.gz/deepcrawl_robots-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="deepcrawl_robots",
+    version="0.0.1",
+    author="Andrei Mutu & Richard Barrett",
+    author_email="support@deepcrawl.com",
+    description="A package to bulk match urls to robots.txt files",
+    long_description_content_type="text/x-rst",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

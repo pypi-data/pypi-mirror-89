78973894b07ed19f21614a8d0253570e5a804b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:25 +0000

    Add package k3proc/0.2.10/k3proc-0.2.10.tar.gz

---
+++ packages/k3proc/k3proc-0.2.10.tar.gz/k3proc-0.2.10/k3proc/__init__.py
@@ -0,0 +1,34 @@
+"""
+k3proc is utility to create sub process.
+
+Execute a shell script::
+
+    >>> returncode, out, err = k3proc.shell_script('ls / | grep bin')
+
+Run a command::
+
+    # Unlike the above snippet, following statement does not start an sh process.
+    returncode, out, err = k3proc.command('ls', 'a*', cwd='/usr/local')
+
+"""
+
+from .proc import CalledProcessError
+from .proc import ProcError
+from .proc import TimeoutExpired
+from .proc import command
+from .proc import command_ex
+from .proc import shell_script
+from .proc import start_process
+
+__version__ = "0.2.10"
+__name__ = 'k3proc'
+
+__all__ = [
+    'CalledProcessError',
+    'TimeoutExpired',
+    'ProcError',
+    'command',
+    'command_ex',
+    'shell_script',
+    'start_process',
+]

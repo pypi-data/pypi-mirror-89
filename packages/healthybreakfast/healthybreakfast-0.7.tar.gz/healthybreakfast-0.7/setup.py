80b23c759394f7ac018b3367531944741f549f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:05 +0000

    Add package healthybreakfast/0.7/healthybreakfast-0.7.tar.gz

---
+++ packages/healthybreakfast/healthybreakfast-0.7.tar.gz/healthybreakfast-0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='healthybreakfast',
+    version='0.007',
+    packages=find_packages(exclude=['test*']),
+    license='MIT',
+    description='Eat healthy stay healthy',
+    url='https://github.com/Sowmya-Chenthi-Kumar/DATA533_LAB4',
+    author='Sowmya and Yatin',
+    author_email='yatin.behl@gmail.com'
+    
+)

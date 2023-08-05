4fe9f6526d9c3be9e6716bbacf9128c2fedb5cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:23 +0000

    Add package post-2-album/0.0.3/post_2_album-0.0.3-py3-none-any.whl

---
+++ packages/post-2-album/post_2_album-0.0.3-py3-none-any.whl/post_2_album/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+name = 'post_2_album'
+
+from telegram_util import AlbumResult as Result
+import webgram
+
+def getCap(post):
+	return ''.join([str(item) for item in post.text])
+
+def getImgs(post):
+	return list(post.yieldPhotos())
+
+def get(path):
+	parts = path.split('/')
+	channel = parts[3]
+	post_id = int(parts[4])
+	post = webgram.getPost(channel, post_id)
+	result = Result()
+	result.url = path
+	result.cap_html = getCap(post)
+	result.imgs = getImgs(post)
+	result.video = post.getVideo()
+	return result

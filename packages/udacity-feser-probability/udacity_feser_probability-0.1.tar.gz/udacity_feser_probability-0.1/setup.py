ceb9694e2c19e20e0e2cccd09c6759a1b004bac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:45 +0000

    Add package udacity-feser-probability/0.1/udacity_feser_probability-0.1.tar.gz

---
+++ packages/udacity-feser-probability/udacity_feser_probability-0.1.tar.gz/udacity_feser_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='udacity_feser_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacity_feser_probability'],
+      author = 'Fatih Eser',
+      zip_safe=False)

f2b7fccb9106ba29993581b75d4b843cba8d7c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package anilistpy/0.0.2.2/anilistpy-0.0.2.2.tar.gz

---
+++ packages/anilistpy/anilistpy-0.0.2.2.tar.gz/anilistpy-0.0.2.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anilistpy", 
+    version="0.0.2.2",
+    author="kalekale",
+    author_email="kalekale.anon@gmail.com",
+    description="python API wrapper for anilist.co",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anilistpy/anilistpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+#python3 -m twine upload --repository pypi dist/* 
+#python3 setup.py sdist bdist_wheel

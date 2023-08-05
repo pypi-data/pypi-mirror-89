302de26b4f20205cb615d2ee00882b7954cb7d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:10 +0000

    Add package shopwareapi/0.1.16/shopwareapi-0.1.16-py3-none-any.whl

---
+++ packages/shopwareapi/shopwareapi-0.1.16-py3-none-any.whl/shopwareapi/fields/relation_field.py
@@ -0,0 +1,23 @@
+from shopwareapi.core.field import BaseRelationField
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class ForeignKey(BaseRelationField):
+    # Single Reference to another Object
+
+    def to_simple(self, val):
+        pk_val = getattr(val, self.related_model._meta.pk.name)
+        return val._meta.pk.to_simple(pk_val)
+
+
+class ManyToOneField(BaseRelationField):
+    # List of references to other Objects
+
+    def get_attname(self):
+        return self.name
+
+    def from_api(self, *args, **kwargs):
+        print(args)
+        print(kwargs)

688ccfc1af2848cf958d92649be06a43719f0744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:12 +0000

    Add package python-owasp-zap-v2.4/0.0.18/python-owasp-zap-v2.4-0.0.18.tar.gz

---
+++ packages/python-owasp-zap-v2.4/python-owasp-zap-v2.4-0.0.18.tar.gz/python-owasp-zap-v2.4-0.0.18/README.md
@@ -0,0 +1,25 @@
+# OWASP ZAP Python API
+
+[![Version](https://img.shields.io/pypi/v/python-owasp-zap-v2.4.svg)](https://pypi.python.org/pypi/python-owasp-zap-v2.4)
+[![License](https://img.shields.io/badge/license-Apache%202-4EB1BA.svg)](https://www.apache.org/licenses/LICENSE-2.0.html)
+[![Build Status](https://api.travis-ci.com/zaproxy/zap-api-python.svg?branch=master)](https://travis-ci.com/zaproxy/zap-api-python)
+
+The Python implementation to access the [OWASP ZAP API](https://www.zaproxy.org/docs/api/). For more information
+about OWASP ZAP consult the (main) [OWASP ZAP project](https://github.com/zaproxy/zaproxy/).
+
+## How to Obtain
+
+The latest released version can be downloaded from the [https://pypi.python.org/pypi/python-owasp-zap-v2.4](https://pypi.python.org/pypi/python-owasp-zap-v2.4) using:
+
+    pip install python-owasp-zap-v2.4
+
+## Getting Help
+
+For help using OWASP ZAP API refer to:
+  * [Examples](https://github.com/zaproxy/zap-api-python/tree/master/src/examples) - collection of examples using the library;
+  * [API Documentation](https://www.zaproxy.org/docs/api/)
+  * [OWASP ZAP User Group](https://groups.google.com/group/zaproxy-users) - for asking questions;
+  
+## Issues
+
+To report issues related to OWASP ZAP API, bugs and enhancements requests, use the [issue tracker of the main OWASP ZAP project](https://github.com/zaproxy/zaproxy/issues).
---
+++ packages/python-owasp-zap-v2.4/python-owasp-zap-v2.4-0.0.18.tar.gz/python-owasp-zap-v2.4-0.0.18/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+Standard build script.
+"""
+
+__docformat__ = 'restructuredtext'
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print('You must have setuptools installed to use setup.py. Exiting...')
+    raise SystemExit(1)
+
+
+install_dependencies = (
+    'requests',
+    'six'
+)
+test_requirements = (
+    'mock',
+    'pyhamcrest',
+    'pylama',
+    'pytest',
+    'requests_mock'
+)
+setup(
+    name="python-owasp-zap-v2.4",
+    version="0.0.18",
+    description="OWASP ZAP 2.10 API client",
+    long_description="OWASP Zed Attack Proxy 2.10 API Python client (the 2.4 package name has been kept to make it easier to upgrade)",
+    author="ZAP development team",
+    author_email='',
+    url="https://www.zaproxy.org/",
+    download_url="https://github.com/zaproxy/zap-api-python/releases/tag/0.0.18",
+    platforms=['any'],
+    license="ASL2.0",
+    package_dir={
+        '': 'src',
+    },
+    packages=find_packages('src'),
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=install_dependencies,
+    tests_require=test_requirements,
+    extras_require={'tests': test_requirements}
+)

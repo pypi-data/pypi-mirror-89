c47555a83435b7e8cdbfa11c8b636765295b5cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package HiveMind-voice-sat/1.0.7/HiveMind-voice-sat-1.0.7.tar.gz

---
+++ packages/HiveMind-voice-sat/HiveMind-voice-sat-1.0.7.tar.gz/HiveMind-voice-sat-1.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='HiveMind-voice-sat',
+    version='1.0.7',
+    packages=['mycroft_voice_satellite', 'mycroft_voice_satellite.speech',
+              'mycroft_voice_satellite.res',
+              'mycroft_voice_satellite.configuration'],
+    install_requires=["jarbas_hive_mind>=0.10.3",
+                      "speech2text",
+                      "pyee",
+                      "requests",
+                      "requests_futures",
+                      "psutil",
+                      "PyAudio==0.2.11",
+                      "pocketsphinx==0.1.15",
+                      "jarbas_utils",
+                      "text2speech"],
+    include_package_data=True,
+    url='https://github.com/OpenJarbas/HiveMind-voice-sat',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='Mycroft Voice Satellite',
+    entry_points={
+        'console_scripts': [
+            'HiveMind-voice-sat=mycroft_voice_satellite.__main__:main'
+        ]
+    }
+)

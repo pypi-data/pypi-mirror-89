b1af25d2ca59e6ea8f74d50786014958c58aa1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:44 +0000

    Add package ccimport/0.1.8/ccimport-0.1.8.tar.gz

---
+++ packages/ccimport/ccimport-0.1.8.tar.gz/ccimport-0.1.8/test/test_build.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+import ccimport
+
+
+def test_cpp_build():
+    source = ccimport.autoimport([Path(__file__).parent / "source.cc"],
+                                 Path(__file__).parent / "source")
+    assert source.sub(2, 1) == 1
+    obj = source.TestClass(5)
+    assert obj.add(3) == 8

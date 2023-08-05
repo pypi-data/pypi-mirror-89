56de26268ddde666ea01ea371ba0639e14c21474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:31 +0000

    Add package py-laxz/0.1.8/py-laxz-0.1.8.tar.gz

---
+++ packages/py-laxz/py-laxz-0.1.8.tar.gz/py-laxz-0.1.8/pylaxz/__init__.py
@@ -0,0 +1,10 @@
+"""
+module - pylaxz
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Copyright (c) 2020 Min Latt.
+License: MIT, see LICENSE for more details.
+
+"""
+
+from .utils import *
+from .iots import *

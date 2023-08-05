011a13356a9c3199c1fb317acbaef81f54294c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:36 +0000

    Add package masci-tools/0.3.12.dev6/masci_tools-0.3.12.dev6-py2-none-any.whl

---
+++ packages/masci-tools/masci_tools-0.3.12.dev6-py2-none-any.whl/masci_tools/vis/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Copyright (c), Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany.         #
+#                All rights reserved.                                         #
+# This file is part of the Masci-tools package.                               #
+# (Material science tools)                                                    #
+#                                                                             #
+# The code is hosted on GitHub at https://github.com/judftteam/masci-tools    #
+# For further information on the license, see the LICENSE.txt file            #
+# For further information please visit http://www.flapw.de or                 #
+#                                                                             #
+###############################################################################
+"""
+Here are all plot varaiables/constants,
+
+"""
+#TODO: ggf write a plotter class
+
+from __future__ import absolute_import
+from builtins import object
+
+
+class Ploter(object):
+    pass
+
+    def __init__(self):
+        pass
+
+    def set_defaults(self):
+        pass

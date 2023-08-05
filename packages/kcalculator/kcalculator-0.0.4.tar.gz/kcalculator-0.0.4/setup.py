fc890e48ed3d6a73d330c250206aaf57c287bd43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:31 +0000

    Add package kcalculator/0.0.4/kcalculator-0.0.4.tar.gz

---
+++ packages/kcalculator/kcalculator-0.0.4.tar.gz/kcalculator-0.0.4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+print('11111111111')
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+print(long_description)
+classification = [
+	'Development status :: 5 - Production/stable',
+	'Intended Audience :: Education',
+	'Operating System :: Linux :: ubuntu',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: PYthon :: 3'
+]
+
+setup(
+	name='kcalculator',
+	version='0.0.4',
+	description='A baisc calulcator',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url='https://github.com/softprodigyofficial/sample-python-package.git',
+	author='kuldeep khatana',
+	author_email='kuldeep.si.softprodigy@gmail.com',
+	License='MIT',
+	classifiers=[
+		"Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+	],
+	keywords='calulcator',
+	package=find_packages(),
+	install_requires=['']
+
+
+)

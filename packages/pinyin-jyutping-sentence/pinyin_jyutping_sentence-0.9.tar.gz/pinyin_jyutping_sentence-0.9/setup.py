82e7edc26c058ac400746863fe4ee49f547e14e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:24 +0000

    Add package pinyin-jyutping-sentence/0.9/pinyin_jyutping_sentence-0.9.tar.gz

---
+++ packages/pinyin-jyutping-sentence/pinyin_jyutping_sentence-0.9.tar.gz/pinyin_jyutping_sentence-0.9/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# build instructions
+#  python3 setup.py sdist
+# python3 setup.py sdist upload
+
+setup(name='pinyin_jyutping_sentence',
+      version='0.9',
+      description='Convert a Chinese sentence to Pinyin or Jyutping',
+      long_description=open('README.rst', encoding='utf-8').read(),
+      url='https://github.com/lucwastiaux/python-pinyin-jyutping-sentence',
+      author='Luc Wastiaux',
+      author_email='lucw@airpost.net',
+      classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],      
+      license='GPL',
+      packages=['pinyin_jyutping_sentence'],
+      install_requires=[
+          'jieba',
+      ],      
+      zip_safe=False,
+      include_package_data=True)

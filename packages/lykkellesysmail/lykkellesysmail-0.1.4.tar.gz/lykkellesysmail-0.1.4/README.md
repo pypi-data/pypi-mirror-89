660a97fad8d1695a28fa9bb89695a683ac7b6609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:52 +0000

    Add package lykkellesysmail/0.1.4/lykkellesysmail-0.1.4.tar.gz

---
+++ packages/lykkellesysmail/lykkellesysmail-0.1.4.tar.gz/lykkellesysmail-0.1.4/README.md
@@ -0,0 +1,13 @@
+This is package that contains all modules needed to run
+the database and other configurations
+Contents:
+    1. Lykkelle system email sender
+    2. parameters are as follows:
+    sendername="name of sender"
+    sender_email="sender email"
+    receivername="Deb Mishra"
+    receivers=[{"email":"<email id>","name":"sender name"}]
+    subject="Email subject. Not needed if template id 2 is there"
+    message="Message body for plain test message"
+    template = possible values- jobname,jobdetails,firstname,password
+    templateid= possible values - 2,3
---
+++ packages/lykkellesysmail/lykkellesysmail-0.1.4.tar.gz/lykkellesysmail-0.1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lykkellesysmail", # Replace with your own username
+    version="0.1.4",
+    author="Lykkelle Inc",
+    author_email="admin@lykkelle.com",
+    description="Modules pertaining to lykkellesysmail",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://dev.azure.com/lykkelle/Lykkelle/_git/Lykkellescripts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

6430f13e24b86f9dc64a6a8bb219c86bc16aca67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:21 +0000

    Add package nk-createf-example-pkg/0.0.1/nk_createf_example_pkg-0.0.1-py3-none-any.whl

---
+++ packages/nk-createf-example-pkg/nk_createf_example_pkg-0.0.1-py3-none-any.whl/createfile_properties_pkg/createfile_properties.py
@@ -0,0 +1,22 @@
+class CreateFile
+
+	def createf(title, ing, meth, notes, kw)
+		
+    	tit = title + '.html'
+	    fh = open (tit,  "w")
+    
+	    fh.write('Ingredients: \n\n')
+    	fh.write(ing + '\n\n\n')
+	    
+    	fh.write('Method: \n\n')
+    	fh.write(meth + '\n\n\n')
+    
+    	fh.write('Notes: \n\n')
+    	fh.write(notes + '\n\n\n')
+    
+    	fh.write('Keywords: \n\n')
+    	fh.write(kw)
+    
+    	fh.close()
+    
+    	return upload_file(tit, BUCKET)

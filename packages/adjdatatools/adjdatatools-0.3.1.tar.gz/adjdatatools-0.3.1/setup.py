9affd449a7eec4cbce531cd5167ce8ed403d02f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:18 +0000

    Add package adjdatatools/0.3.1/adjdatatools-0.3.1.tar.gz

---
+++ packages/adjdatatools/adjdatatools-0.3.1.tar.gz/adjdatatools-0.3.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adjdatatools',
+    version='0.3.1',
+    packages=find_packages(),
+    url='https://github.com/newchronik/adjdatatools',
+    license='MIT',
+    keywords='data preprocessing scaler',
+    author='Anton Kvasnitsa',
+    author_email='anton.kvasnitsa@gmail.com',
+    description='This library contains adjusted tools for data preprocessing and working with mixed data types.',
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+    include_package_data=True,
+)

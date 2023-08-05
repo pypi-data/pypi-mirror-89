66a2a3792112d500cd058d1ee3dd776eb7fcd319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:29 +0000

    Add package taskcat/0.9.21.dev6/taskcat-0.9.21.dev6.tar.gz

---
+++ packages/taskcat/taskcat-0.9.21.dev6.tar.gz/taskcat-0.9.21.dev6/taskcat/_s3_stage.py
@@ -0,0 +1,40 @@
+import logging
+from functools import partial
+from multiprocessing.dummy import Pool as ThreadPool
+
+from taskcat._s3_sync import S3Sync
+from taskcat.exceptions import TaskCatException
+
+LOG = logging.getLogger(__name__)
+
+
+class S3APIResponse:
+    def __init__(self, x):
+        self._http_code = x["ResponseMetadata"]["HTTPStatusCode"]
+
+    @property
+    def ok(self):
+        if self._http_code == 200:
+            return True
+        return False
+
+
+class S3BucketCreatorException(TaskCatException):
+    pass
+
+
+def stage_in_s3(buckets, project_name, project_root):
+    distinct_buckets = {}
+
+    for test in buckets.values():
+        for bucket in test.values():
+            distinct_buckets[f"{bucket.name}-{bucket.partition}"] = bucket
+    pool = ThreadPool(32)
+    func = partial(_sync_wrap, project_name=project_name, project_root=project_root)
+    pool.map(func, distinct_buckets.values())
+    pool.close()
+    pool.join()
+
+
+def _sync_wrap(bucket, project_name, project_root):
+    S3Sync(bucket.s3_client, bucket.name, project_name, project_root, bucket.object_acl)

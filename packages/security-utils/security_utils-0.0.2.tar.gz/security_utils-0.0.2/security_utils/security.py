c7be37acf0b58270b362f2c6feb7580f9ca9bf18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:06 +0000

    Add package security-utils/0.0.2/security_utils-0.0.2.tar.gz

---
+++ packages/security-utils/security_utils-0.0.2.tar.gz/security_utils-0.0.2/security_utils/security.py
@@ -0,0 +1,31 @@
+import random
+import string
+from typing import Optional, Any
+
+import itsdangerous.exc
+from itsdangerous import URLSafeTimedSerializer
+
+from backend.core.config import settings
+
+signer = URLSafeTimedSerializer(settings.SECRET_KEY)
+
+
+def generate_security_token(context: dict):
+    """Генерирование токена с помощью библиотеки ItsDangerous."""
+    return signer.dumps(context)
+
+
+def verify_security_token(token: str) -> Optional[Any]:
+    """Проверка токена с помощью библиотеки ItsDangerous."""
+    try:
+        return signer.loads(token, max_age=settings.SECURITY_TOKEN_EXPIRE_MINUTES)
+    except (itsdangerous.exc.SignatureExpired, itsdangerous.exc.BadSignature):
+        return None
+
+
+def generate_random_code(size=6, only_digits: bool = True):
+    if only_digits:
+        chars = string.digits
+    else:
+        chars = string.ascii_uppercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(size))

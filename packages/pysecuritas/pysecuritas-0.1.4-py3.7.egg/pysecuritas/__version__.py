cb44c50a4d858ba17b78ebf7101ad2000b8c198c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:56 +0000

    Add package pysecuritas/0.1.4/pysecuritas-0.1.4-py3.7.egg

---
+++ packages/pysecuritas/pysecuritas-0.1.4-py3.7.egg/pysecuritas/__version__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: © pysecuritas, All Rights Reserved
+"""
+
+__title__ = "pysecuritas"
+__description__ = "Python library to retrieve and interact with securitas devices."
+__url__ = "https://github.com/Cebeerre/pysecuritas"
+__version__ = "0.1.4"
+__license__ = "MIT License"
+__author__ = ""
+__author_email__ = ""

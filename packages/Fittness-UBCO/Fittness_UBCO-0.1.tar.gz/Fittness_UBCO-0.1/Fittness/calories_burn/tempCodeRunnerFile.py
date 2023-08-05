d68991df5ef9a0135a73571d828c37c11361900c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:00 +0000

    Add package Fittness-UBCO/0.1/Fittness_UBCO-0.1.tar.gz

---
+++ packages/Fittness-UBCO/Fittness_UBCO-0.1.tar.gz/Fittness_UBCO-0.1/Fittness/calories_burn/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+        # try:
+        #     if self.gender!="female" or self.gender!="male":
+        #         raise ValueError("plase enter your gender, male or female")
+        # except ValueError as ex:
+        #     print("Message:", ex)
---
+++ packages/Fittness-UBCO/Fittness_UBCO-0.1.tar.gz/Fittness_UBCO-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+name='Fittness_UBCO',
+version='0.1', 
+packages=find_packages(exclude=['tests*']), 
+license='MIT',
+description="A test python package",
+url='https://github.com/RaineShen/data533Lab4.git', 
+author='YuxuanIsme,raineShen',
+author_email='cuiyuuxuan@gmail.com,rain.ya1213@gmail.com'
+)
+ 

f968a7c8c408c84ec6166cee895b78b19d5a93c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:21 +0000

    Add package first-gauss-binomial-package/0.1/first_gauss_binomial_package-0.1.tar.gz

---
+++ packages/first-gauss-binomial-package/first_gauss_binomial_package-0.1.tar.gz/first_gauss_binomial_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='first_gauss_binomial_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['first_gauss_binomial_package'],
+      zip_safe=False)

10643cfb4f8d4f618b67559628f3b82559134f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:40 +0000

    Add package seldom/1.9.0/seldom-1.9.0-py3-none-any.whl

---
+++ packages/seldom/seldom-1.9.0-py3-none-any.whl/seldom/running/config.py
@@ -0,0 +1,20 @@
+"""
+Seldom configuration file
+"""
+
+
+class Seldom:
+    """
+    Seldom browser driver
+    """
+    driver = None
+    timeout = None
+    debug = None
+
+
+class BrowserConfig:
+    """
+    Define run browser config
+    """
+    name = None
+    report_path = None

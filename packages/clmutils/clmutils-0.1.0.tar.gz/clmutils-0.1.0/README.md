93d5edcd4a42c1080e58bde8e269c27428a96f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:10 +0000

    Add package clmutils/0.1.0/clmutils-0.1.0.tar.gz

---
+++ packages/clmutils/clmutils-0.1.0.tar.gz/clmutils-0.1.0/README.md
@@ -0,0 +1,17 @@
+# colab miscellaneous utils [![Codacy Badge](https://app.codacy.com/project/badge/Grade/83b7b2cb3ade4589812917f187a8abab)](https://www.codacy.com/gh/ffreemt/colab-misc-utils/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ffreemt/colab-misc-utils&amp;utm_campaign=Badge_Grade)[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![PyPI version](https://badge.fury.io/py/colab-misc-utils.svg)](https://badge.fury.io/py/colab-misc-utils)
+Miscellaneous utils mainly intended for use in colab
+
+Utils planned
+*  `create_file`
+  creates a file with given mode, e.g. for `.ssh/id_rsa` or `IdentityFile` in `.ssh/config`
+
+*  `apppend_content
+ appends some content to a file, e.g., for appended a public key to `.ssh/authorized_keys`
+
+*  `chmod600` 
+   `chmod` of a file
+
+*  reverse_ssh_tunnel
+ sets up a reverse ssh tunnel to a remote host with via autossh
+
+*  More
---
+++ packages/clmutils/clmutils-0.1.0.tar.gz/clmutils-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['clmutils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['logzero>=1.6.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'clmutils',
+    'version': '0.1.0',
+    'description': 'colab miscellaneous utils',
+    'long_description': '# colab miscellaneous utils [![Codacy Badge](https://app.codacy.com/project/badge/Grade/83b7b2cb3ade4589812917f187a8abab)](https://www.codacy.com/gh/ffreemt/colab-misc-utils/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ffreemt/colab-misc-utils&amp;utm_campaign=Badge_Grade)[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![PyPI version](https://badge.fury.io/py/colab-misc-utils.svg)](https://badge.fury.io/py/colab-misc-utils)\nMiscellaneous utils mainly intended for use in colab\n\nUtils planned\n*  `create_file`\n  creates a file with given mode, e.g. for `.ssh/id_rsa` or `IdentityFile` in `.ssh/config`\n\n*  `apppend_content\n appends some content to a file, e.g., for appended a public key to `.ssh/authorized_keys`\n\n*  `chmod600` \n   `chmod` of a file\n\n*  reverse_ssh_tunnel\n sets up a reverse ssh tunnel to a remote host with via autossh\n\n*  More\n',
+    'author': 'ffreemt',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ffreemt/colab-misc-utils',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)

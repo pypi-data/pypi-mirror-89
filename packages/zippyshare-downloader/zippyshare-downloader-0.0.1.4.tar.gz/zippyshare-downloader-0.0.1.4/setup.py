97374e01b1b343de28b0097df9fddfcdd09383d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:57 +0000

    Add package zippyshare-downloader/0.0.1.4/zippyshare-downloader-0.0.1.4.tar.gz

---
+++ packages/zippyshare-downloader/zippyshare-downloader-0.0.1.4.tar.gz/zippyshare-downloader-0.0.1.4/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+from zippyshare_downloader.__version__ import VERSION
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+  name = 'zippyshare-downloader',         
+  packages = ['zippyshare_downloader'],   
+  version = VERSION,
+  license='MIT',
+  description = 'Download file from zippyshare directly from python',
+  long_description= README,
+  long_description_content_type= 'text/markdown',
+  author = 'Rahman Yusuf',              
+  author_email = 'danipart4@gmail.com',
+  url = 'https://github.com/trollfist20/zippyshare-downloader',  
+  download_url = 'https://github.com/trollfist20/zippyshare-downloader/archive/%s.tar.gz' % (VERSION),
+  keywords = ['zippyshare', 'zippyshare-download'], 
+  install_requires=[           
+          'requests',
+          'bs4',
+          'download'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',
+  ],
+  python_requires='>=3'
+)

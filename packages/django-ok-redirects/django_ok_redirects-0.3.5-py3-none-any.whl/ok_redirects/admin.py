5b444024314b9a5ff7a399f4e84f55892d2b43a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:11 +0000

    Add package django-ok-redirects/0.3.5/django_ok_redirects-0.3.5-py3-none-any.whl

---
+++ packages/django-ok-redirects/django_ok_redirects-0.3.5-py3-none-any.whl/ok_redirects/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+
+from .models import Redirect
+
+
+@admin.register(Redirect)
+class RedirectAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'is_active',
+                'site',
+                'status_code',
+                ('old_path', 'is_ignore_get_params'),
+                'new_path',
+                'languages',
+                'counter',
+            ),
+        }),
+    )
+    list_display = (
+        '__str__',
+        'old_path',
+        'is_ignore_get_params',
+        'new_path',
+        'is_active'
+    )
+    list_editable = (
+        'old_path',
+        'new_path',
+        'is_ignore_get_params',
+        'is_active'
+    )
+    list_filter = (
+        'site',
+    )
+    search_fields = (
+        'old_path',
+        'new_path'
+    )
+    radio_fields = {'site': admin.VERTICAL}
+    readonly_fields = (
+        'counter',
+    )

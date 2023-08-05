1b7cad47b85a18f7da26faf0aaab4b7a810e5d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:23 +0000

    Add package fpyutils/1.2.2/fpyutils-1.2.2-py3-none-any.whl

---
+++ packages/fpyutils/fpyutils-1.2.2-py3-none-any.whl/fpyutils/__init__.py
@@ -0,0 +1,29 @@
+#
+# __init__.py
+#
+# Copyright (C) 2017-2020 frnmst (Franco Masotti) <franco.masotti@live.com>
+#
+# This file is part of fpyutils.
+#
+# fpyutils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# fpyutils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with fpyutils.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""Exposed API."""
+
+from .filelines import (insert_string_at_line, remove_line_interval,
+                        get_line_matches)
+from .shell import (execute_command_live_output)
+from .yaml import (load_configuration)
+from .path import (add_trailing_slash, gen_pseudorandom_path)
+from .notify import (send_email, send_gotify_message)
+from .exceptions import (LineOutOfFileBoundsError, NegativeLineRangeError)

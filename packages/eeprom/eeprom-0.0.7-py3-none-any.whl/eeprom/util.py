7db3f61e9db01c7f8ad0583dd32619f1b119ee38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:35 +0000

    Add package eeprom/0.0.7/eeprom-0.0.7-py3-none-any.whl

---
+++ packages/eeprom/eeprom-0.0.7-py3-none-any.whl/eeprom/util.py
@@ -0,0 +1,18 @@
+import subprocess
+
+
+def check_device_exists(bus, addr):
+    if not hasattr(check_device_exists, 'i2cdetect_bin'):
+        check_device_exists.i2cdetect_bin = subprocess.getoutput('which i2cdetect')
+    if check_device_exists.i2cdetect_bin:
+        cp = subprocess.run(
+            [check_device_exists.i2cdetect_bin, '-y', str(bus), str(addr), str(addr)],
+            capture_output=True
+        )
+        if cp.returncode != 0:
+            if "Permission denied" in cp.stderr.decode():
+                raise PermissionError(cp.stderr.decode())
+            return False
+        if '--' in cp.stdout.decode():
+            return False
+    return True

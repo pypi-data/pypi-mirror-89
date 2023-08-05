deff96d5088939c682ab34c013d85534b607cb9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:18 +0000

    Add package patlib/0.2.1/patlib-0.2.1.tar.gz

---
+++ packages/patlib/patlib-0.2.1.tar.gz/patlib-0.2.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['patlib']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'patlib',
+    'version': '0.2.1',
+    'description': 'A collection of tools developed in conjunction with DAPPER.',
+    'long_description': '# patlib\n\nA collection of tools developed in conjunction with DAPPER.\n',
+    'author': 'patricknraanes',
+    'author_email': 'patrick.n.raanes@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

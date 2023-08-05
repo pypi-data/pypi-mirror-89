3f3c27867e72a6ff937384bc29eadb682d3d3974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:19 +0000

    Add package flowtorch/0.0.dev0/flowtorch-0.0.dev0.tar.gz

---
+++ packages/flowtorch/flowtorch-0.0.dev0.tar.gz/flowtorch-0.0.dev0/setup.py
@@ -0,0 +1,89 @@
+# Copyright (c) FlowTorch Development Team. All Rights Reserved
+# SPDX-License-Identifier: MIT
+
+import os
+import re
+import sys
+
+from setuptools import find_packages, setup
+
+REQUIRED_MAJOR = 3
+REQUIRED_MINOR = 7
+
+
+TEST_REQUIRES = ["pytest", "pytest-cov"]
+DEV_REQUIRES = TEST_REQUIRES + [
+    "black",
+    "flake8",
+    "isort",
+    "recommonmark",
+    "sphinx",
+    "sphinx-autodoc-typehints",
+    "sphinx_rtd_theme",
+]
+
+
+# Check for python version
+if sys.version_info < (REQUIRED_MAJOR, REQUIRED_MINOR):
+    error = (
+        "Your version of python ({major}.{minor}) is too old. You need "
+        "python >= {required_major}.{required_minor}."
+    ).format(
+        major=sys.version_info.major,
+        minor=sys.version_info.minor,
+        required_minor=REQUIRED_MINOR,
+        required_major=REQUIRED_MAJOR,
+    )
+    sys.exit(error)
+
+
+# get version string from module
+current_dir = os.path.dirname(__file__)
+init_file = os.path.join(current_dir, "flowtorch", "__init__.py")
+version_regexp = r"__version__ = ['\"]([^'\"]*)['\"]"
+with open(init_file, "r") as f:
+    version = re.search(version_regexp, f.read(), re.M).group(1)
+
+# read in README.md as the long description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="flowtorch",
+    version=version,
+    description="Normalizing Flows for PyTorch",
+    author="FlowTorch Development Team",
+    author_email="info@stefanwebb.me",
+    license="MIT",
+    url="https://www.github.com/stefanwebb/flowtorch",
+    project_urls={
+        "Documentation": "https://www.github.com/stefanwebb/flowtorch",
+        "Source": "https://www.github.com/stefanwebb/flowtorch",
+    },
+    keywords=[
+        "Deep Learning",
+        "Bayesian Inference",
+        "Statistical Modeling",
+        "Variational Inference",
+        "PyTorch",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    install_requires=[
+        "torch>=1.6.0",
+    ],
+    packages=find_packages("flowtorch", "flowtorch.*"),
+    extras_require={
+        "dev": DEV_REQUIRES,
+        "test": TEST_REQUIRES,
+    },
+)

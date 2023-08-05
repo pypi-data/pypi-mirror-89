a0b1b9c76d515541e98c39440c57bd4eb25ae1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:43 +0000

    Add package seven-cloudapp/1.0.100/seven_cloudapp-1.0.100.tar.gz

---
+++ packages/seven-cloudapp/seven_cloudapp-1.0.100.tar.gz/seven_cloudapp-1.0.100/seven_cloudapp/handlers/client/lottery.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: CaiYouBin
+@Date: 2020-05-26 17:51:04
+@LastEditTime: 2020-11-10 16:02:18
+@LastEditors: HuangJingCan
+@Description: 抽奖
+"""
+from seven_cloudapp.handlers.seven_base import *
+
+
+class LotteryHandler(SevenBaseHandler):
+    """
+    @description: 抽奖逻辑实现（业务各自实现）
+    """
+    def get_async(self):
+        """
+        @description: 抽奖逻辑实现（业务各自实现）
+        @param {*}
+        @return {*}
+        @last_editors: HuangJingCan
+        """
+        pass

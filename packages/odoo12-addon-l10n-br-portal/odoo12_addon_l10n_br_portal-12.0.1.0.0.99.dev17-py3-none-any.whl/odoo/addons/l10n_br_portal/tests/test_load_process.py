423098fd787b5dd666a5c2c0df69c9010b0fd8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:18 +0000

    Add package odoo12-addon-l10n-br-portal/12.0.1.0.0.99.dev17/odoo12_addon_l10n_br_portal-12.0.1.0.0.99.dev17-py3-none-any.whl

---
+++ packages/odoo12-addon-l10n-br-portal/odoo12_addon_l10n_br_portal-12.0.1.0.0.99.dev17-py3-none-any.whl/odoo/addons/l10n_br_portal/tests/test_load_process.py
@@ -0,0 +1,25 @@
+# Copyright 2016 KMEE - Luis Felipe Miléo <mileo@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
+
+import odoo.tests
+
+
+@odoo.tests.tagged('post_install', '-at_install')
+class TestUi(odoo.tests.HttpCase):
+    def test_01_l10n_br_portal_load_tour(self):
+        tour = (
+            "odoo.__DEBUG__.services['web_tour.tour']",
+            "l10n_br_portal_tour",
+        )
+        self.phantom_js(
+            url_path="/my/account",
+            code="%s.run('%s')" % tour,
+            ready="%s.tours['%s'].ready" % tour,
+            login="admin",
+            timeout=180
+        )
+        # check result
+        record = self.env.ref('base.partner_admin')
+        self.assertEqual(record.country_id.code, 'BR')
+        self.assertEqual(record.state_id.code, 'MG')
+        self.assertEqual(record.city_id.ibge_code, '32404')

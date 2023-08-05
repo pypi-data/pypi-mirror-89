5216db1a9621985f40353cc4b5010a20dafca305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:54 +0000

    Add package nqueens/1.0.2/nqueens-1.0.2.tar.gz

---
+++ packages/nqueens/nqueens-1.0.2.tar.gz/nqueens-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nqueens", # Replace with your own username
+    version="1.0.2",
+    author="Athul Mathew Konoor",
+    author_email="athulmathewkonoor@gmail.com",
+    description="A package to get nqueens.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toshihiroryuu/nqueens---PyPI-Package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+       "gtts >= 2.2.0"
+   ],
+    python_requires='>=3.6',
+)

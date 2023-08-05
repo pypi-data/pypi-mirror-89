7e3ebe2d8b1794cd4681a613ba27331c9553a4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:38 +0000

    Add package imshowmasks/0.0.6/imshowmasks-0.0.6.tar.gz

---
+++ packages/imshowmasks/imshowmasks-0.0.6.tar.gz/imshowmasks-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imshowmasks", # Replace with your own username
+    version="0.0.6",
+    author="Malte Brammerloh",
+    author_email="m.brammerloh@gmail.com",
+    description="A slim package to handle and display binary masks in color over images shown with matplotlib.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mbrammerloh/imshowmasks",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

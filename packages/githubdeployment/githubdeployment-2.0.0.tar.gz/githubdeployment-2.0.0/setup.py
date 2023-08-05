b87a12111160574afce00a147d2ac7831f1d02ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:22 +0000

    Add package githubdeployment/2.0.0/githubdeployment-2.0.0.tar.gz

---
+++ packages/githubdeployment/githubdeployment-2.0.0.tar.gz/githubdeployment-2.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="githubdeployment",
+    version="2.0.0",
+    author="Komron Miralizoda",
+    author_email="miralizoda.komron@gmail.com",
+    description="Simple tool for auto-deployment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/komron-m/deployment",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

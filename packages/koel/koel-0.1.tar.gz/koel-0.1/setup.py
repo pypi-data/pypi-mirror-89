5378542372e14f810e91e39c1472b9daa6548c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:37 +0000

    Add package koel/0.1/koel-0.1.tar.gz

---
+++ packages/koel/koel-0.1.tar.gz/koel-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='koel',
+      version='0.1',
+      description='General scraping framework',
+      url='http://github.com/whoiscc/koel',
+      author='whoiscc',
+      author_email='correctizer@gmail.com',
+      license='MIT',
+      packages=['koel'],
+      zip_safe=False)

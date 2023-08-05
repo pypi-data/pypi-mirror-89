19349fec733d349c4a07402cd40055cb59e893b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:06 +0000

    Add package keras-sparsity-pattern/0.3.0/keras-sparsity-pattern-0.3.0.tar.gz

---
+++ packages/keras-sparsity-pattern/keras-sparsity-pattern-0.3.0.tar.gz/keras-sparsity-pattern-0.3.0/keras_sparsity_pattern/__init__.py
@@ -0,0 +1,6 @@
+import warnings
+warnings.warn((
+    "The 'keras_sparsity_pattern' module is deprecated. "
+    "The 'get' method is available as 'keras_tweaks.get_sparsity_pattern'."),
+    DeprecationWarning,
+    stacklevel=2)
---
+++ packages/keras-sparsity-pattern/keras-sparsity-pattern-0.3.0.tar.gz/keras-sparsity-pattern-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='keras-sparsity-pattern',
+      version='0.3.0',
+      description='deprecated. Please use "keras_tweaks.get_sparsity_pattern" instead.',
+      url='http://github.com/kmedian',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='Apache License 2.0',
+      packages=['keras_sparsity_pattern'],
+      install_requires=['setuptools>=40.0.0'],
+      python_requires='>=3.6',
+      zip_safe=False)

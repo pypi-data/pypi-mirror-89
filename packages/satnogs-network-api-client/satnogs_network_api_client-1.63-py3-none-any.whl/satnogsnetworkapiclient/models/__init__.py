ec6270296b2a43b7a5f66f64d446ae3261a7b8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:27 +0000

    Add package satnogs-network-api-client/1.63/satnogs_network_api_client-1.63-py3-none-any.whl

---
+++ packages/satnogs-network-api-client/satnogs_network_api_client-1.63-py3-none-any.whl/satnogsnetworkapiclient/models/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from satnogsnetworkapiclient.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from satnogsnetworkapiclient.model.inline_response200 import InlineResponse200
+from satnogsnetworkapiclient.model.inline_response2001 import InlineResponse2001
+from satnogsnetworkapiclient.model.inline_response2002 import InlineResponse2002

e2abebf7624a660ef18836e55467b5b63fdf9d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:08 +0000

    Add package groupme-bot/0.1.12/groupme-bot-0.1.12.tar.gz

---
+++ packages/groupme-bot/groupme-bot-0.1.12.tar.gz/groupme-bot-0.1.12/groupme_bot/__init__.py
@@ -0,0 +1,18 @@
+"""
+groupme-bot
+
+A simple bot builder for GroupMe
+"""
+from .application import Application
+from .attachment import ImageAttachment, LocationAttachment, SplitAttachment, EmojiAttachment, MentionsAttachment
+from .bot import Bot, Context
+from .callback import Callback
+
+__version__ = "0.1.12"
+__author__ = "Branden Colen"
+__all__ = [
+    "Bot", "Application", "Callback", "Context",
+    "ImageAttachment", "LocationAttachment",
+    "SplitAttachment", "EmojiAttachment",
+    "MentionsAttachment"
+]

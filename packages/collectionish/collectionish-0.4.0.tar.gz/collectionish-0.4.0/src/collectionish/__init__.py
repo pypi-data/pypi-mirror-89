d6650373afc7b71ce05c3c81ccb62fc5d27d7d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:21 +0000

    Add package collectionish/0.4.0/collectionish-0.4.0.tar.gz

---
+++ packages/collectionish/collectionish-0.4.0.tar.gz/collectionish-0.4.0/src/collectionish/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""collectionish is a repository of possibly useful, fairly generic container types.
+"""
+
+__author__ = """Lea Provenzano"""
+__email__ = 'leaprovenzano@gmail.com'
+__version__ = '0.4.0'
+
+from ._uniquetuple import UniqueTuple
+from ._attydict import AttyDict
+from ._sentry import Sentry
+from ._ancestor_chain_map import AncestorChainMap

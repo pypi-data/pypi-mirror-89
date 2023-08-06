31767528ebff442abd2825f7d8f79f5fb152e31a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:43 +0000

    Add package amocrm-api/2.2.0/amocrm_api-2.2.0.tar.gz

---
+++ packages/amocrm-api/amocrm_api-2.2.0.tar.gz/amocrm_api-2.2.0/amocrm/v2/__init__.py
@@ -0,0 +1,7 @@
+from .entity import custom_field
+from .entity.company import Company
+from .entity.contact import Contact
+from .entity.lead import Lead
+from .entity.pipeline import Pipeline, Status
+from .entity.tag import Tag
+from .entity.user import User

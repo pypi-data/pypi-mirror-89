1ff91ffca8406d5ffc236c3ee9dcf1dbc9feff05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:19 +0000

    Add package nekobot/1.2/nekobot-1.2.tar.gz

---
+++ packages/nekobot/nekobot-1.2.tar.gz/nekobot-1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="nekobot",
+    version="1.2",
+    author="hibikidesu",
+    description="NekoBot API Python Wrapper",
+    keywords="nekobot api discord",
+    url="https://github.com/hibikidesu/NekoBotAPI-py",
+    packages=find_packages()
+)

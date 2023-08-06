29b3bd7c0277178928d28ee2ac8ae7f9fe1798c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:15 +0000

    Add package pre-notifier/0.1.1/pre_notifier-0.1.1-py3-none-any.whl

---
+++ packages/pre-notifier/pre_notifier-0.1.1-py3-none-any.whl/pre_notifier/__main__.py
@@ -0,0 +1,33 @@
+import sys
+import subprocess
+import logging
+from datetime import datetime
+
+
+def send_result(arguments, start_time, end_time, return_code, text):
+    title = 'Success' if return_code == 0 else "Error"
+    command = ' '.join(arguments)
+
+
+def notify(arguments):
+    return_code = 0
+    text = ""
+
+    try:
+        start_time = datetime.today()
+
+        result = subprocess.check_output(arguments, stderr=subprocess.STDOUT)
+        text = result.decode('utf-8')
+
+        end_time = datetime.today()
+        print(end_time - start_time)
+        logging.info(f"Execution time is: {end_time - start_time}")
+
+    except subprocess.CalledProcessError as error:
+        return_code = error.returncode
+        text = error.stdout.decode('utf-8')
+
+
+
+if __name__ == '__main__':
+    notify(sys.argv[1:])

ecfe888c3f3617d7ab546aded84076e8e11b4901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:11 +0000

    Add package pythontron/0.1b0/pythontron-0.1b0.tar.gz

---
+++ packages/pythontron/pythontron-0.1b0.tar.gz/pythontron-0.1b0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='pythontron',
+    version='0.1beta',
+    description='Python library for creating perceptrons.',
+    url='http://github.com/HonestHacker123/pythontron',
+    install_requires=['numpy'],
+    packages=['pythontron'],
+    author="Makar Kuznetsov",
+    author_email='makar10kuznetsov@mail.ru',
+    zip_safe=False
+)

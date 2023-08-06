92786c38551cd9dd0fe4ef1cab91d9929223e955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:12 +0000

    Add package robotframework-PuppeteerLibrary/2.1.7/robotframework-PuppeteerLibrary-2.1.7.tar.gz

---
+++ packages/robotframework-PuppeteerLibrary/robotframework-PuppeteerLibrary-2.1.7.tar.gz/robotframework-PuppeteerLibrary-2.1.7/PuppeteerLibrary/ikeywords/iformelement_async.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from PuppeteerLibrary.ikeywords.base_async_keywords import BaseAsyncKeywords
+
+
+class iFormElementAsync(BaseAsyncKeywords, ABC):
+    
+    @abstractmethod
+    async def input_text(self, locator: str, text: str, clear=True):
+        pass
+    
+    @abstractmethod
+    async def clear_element_text(self, locator: str):
+        pass
+    
+    @abstractmethod
+    async def download_file(self, locator: str):
+        pass

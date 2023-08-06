9a2e3c9ea74ddaa745975daa02526ff59f04b4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:17 +0000

    Add package gstore/0.0.5/gstore-0.0.5.tar.gz

---
+++ packages/gstore/gstore-0.0.5.tar.gz/gstore-0.0.5/CHANGELOG.rst
@@ -0,0 +1,54 @@
+Changelog
+=========
+
+This file contains a brief summary of new features and dependency changes or
+releases, in reverse chronological order.
+
+v0.0.5
+------
+
+New features
+~~~~~~~~~~~~
+
+* Add better logging subsystem
+
+Packaging changes
+~~~~~~~~~~~~~~~~~
+
+* Bump GitPython version to fix a crash for users with ``gitpython>=3.0.0, <3.0.6``.
+  For more see: https://github.com/gitpython-developers/GitPython/issues/983
+
+v0.0.4
+------
+
+* Rename ``GHS_DIR`` environment variable to ``GSTORE_DIR``.
+
+Packaging changes
+~~~~~~~~~~~~~~~~~
+
+* Rename package name from ``ghs`` to ``gstore`` to avoid collision with the
+  existing package with the same name.
+
+v0.0.3
+------
+
+New features
+~~~~~~~~~~~~
+
+* Add ability to use ``GHS_DIR`` as a sync base directory.
+* Add ability to omit target directory and use current working directory.
+* Add ability to fetch objects and refs from an existent repository.
+
+v0.0.2
+------
+
+Packaging changes
+~~~~~~~~~~~~~~~~~
+
+* Rename package name from ``ghsync`` to ``ghs`` to avoid collision with the
+  existing package with the same name.
+
+v0.0.1
+------
+
+* Initial release
---
+++ packages/gstore/gstore-0.0.5.tar.gz/gstore-0.0.5/gstore/args.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2020 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+
+from argparse import ArgumentParser
+
+
+def argparse():
+    p = ArgumentParser(
+        description="Synchronize organizations' repositories from GitHub.")
+
+    p.add_argument('--user', dest='user',
+                   help='username to use to get organizations list')
+    p.add_argument('--token', dest='token', required=True,
+                   help='personal auth token')
+    p.add_argument('--org', dest='org', nargs='*',
+                   help='organizations you have access to (by default all)')
+    p.add_argument('target', nargs='?',
+                   default=os.environ.get('GSTORE_DIR', os.getcwd()),
+                   help='base target to sync repos (e.g. folder on disk)')
+
+    return p.parse_args()

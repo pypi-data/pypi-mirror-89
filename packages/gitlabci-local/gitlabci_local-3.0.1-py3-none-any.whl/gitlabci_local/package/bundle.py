97af7cf24813f0967b62ed719e4b04cd0fc36fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:37 +0000

    Add package gitlabci-local/3.0.1/gitlabci_local-3.0.1-py3-none-any.whl

---
+++ packages/gitlabci-local/gitlabci_local-3.0.1-py3-none-any.whl/gitlabci_local/package/bundle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Bundle class
+class Bundle:
+
+    # Aliases
+    ALIAS = 'gcil'
+
+    # Names
+    NAME = 'gitlabci-local'
+
+    # Configurations
+    CONFIGURATION = '.gitlab-ci.yml'
+
+    # Sources
+    REPOSITORY = 'https://gitlab.com/AdrianDC/gitlabci-local'
+
+    # Releases
+    RELEASE_FIRST_TIMESTAMP = 1579337311
+
+    # Environment
+    ENV_ENGINE = 'CI_LOCAL_ENGINE'
+    ENV_UPDATES_DISABLE = 'CI_LOCAL_UPDATES_DISABLE'
+    ENV_UPDATES_FAKE = 'CI_LOCAL_UPDATES_FAKE'
+    ENV_UPDATES_OFFLINE = 'CI_LOCAL_UPDATES_OFFLINE'
+    ENV_VERSION_FAKE = 'CI_LOCAL_VERSION_FAKE'
+    ENV_WINPTY = 'CI_LOCAL_WINPTY'

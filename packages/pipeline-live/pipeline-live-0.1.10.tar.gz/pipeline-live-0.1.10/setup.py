5ce531cb348234b8311ef53330db714edf79922a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:30 +0000

    Add package pipeline-live/0.1.10/pipeline-live-0.1.10.tar.gz

---
+++ packages/pipeline-live/pipeline-live-0.1.10.tar.gz/pipeline-live-0.1.10/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import ast
+import re
+from setuptools import setup, find_packages
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('pipeline_live/_version.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup(
+    name='pipeline-live',
+    version=version,
+    description='Zipline Pipeline extension for live trade',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Alpaca',
+    author_email='oss@alpaca.markets',
+    url='https://github.com/alpacahq/pipeline_live',
+    keywords='financial,zipline,pipeline,stock,screening,api,trade',
+    packages=find_packages(),
+    install_requires=[
+        'alpaca-trade-api>=0.29',
+        'iexfinance>=0.4.1,<0.5.0',
+        'zipline==1.3.0',
+        'numpy',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'flake8',
+    ],
+    setup_requires=['pytest-runner', 'flake8'],
+)
---
+++ packages/pipeline-live/pipeline-live-0.1.10.tar.gz/pipeline-live-0.1.10/tests/test_engine.py
@@ -0,0 +1,36 @@
+from pipeline_live.engine import LivePipelineEngine
+from pipeline_live.data.iex.pricing import USEquityPricing
+from pipeline_live.data.iex.classifiers import Sector
+from pipeline_live.data.iex.factors import AverageDollarVolume
+from zipline.pipeline import Pipeline
+
+
+from .datamock import mock_iex
+
+
+def test_engine(refdata, stocks, data_path):
+    def list_symbols():
+        return ['A', 'AA']
+
+    mock_iex.get_available_symbols(refdata)
+    mock_iex.get_key_stats(stocks)
+    mock_iex.get_chart(stocks)
+
+    eng = LivePipelineEngine(list_symbols)
+    ADV = AverageDollarVolume(window_length=20,)
+    top5 = ADV.top(5, groupby=Sector())
+    pipe = Pipeline({
+        'top5': top5,
+        'close': USEquityPricing.close.latest,
+    }, screen=top5)
+
+    df = eng.run_pipeline(pipe)
+    assert sorted(df.columns) == ['close', 'top5']
+
+    pipe = Pipeline({
+        'close': USEquityPricing.close.latest,
+    })
+
+    df = eng.run_pipeline(pipe)
+    assert df.shape == (2, 1)
+    assert df.close['AA'] > 40

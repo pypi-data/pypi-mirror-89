5fb695db5eee3b974a8ccf223b3f4b3a01115c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:32 +0000

    Add package pydrift/0.2.9/pydrift-0.2.9-py3-none-any.whl

---
+++ packages/pydrift/pydrift-0.2.9-py3-none-any.whl/pydrift/__init__.py
@@ -0,0 +1,8 @@
+from .core.drift_checker import (DriftChecker,
+                                 DataDriftChecker,
+                                 ModelDriftChecker)
+from .core.interpretable_drift import InterpretableDrift
+from .core.drift_checker_estimator import DriftCheckerEstimator
+
+
+__version__ = '0.2.9'

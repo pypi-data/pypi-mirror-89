b7f0a5ffb8090652819c78d14b652df9fd1fd6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:53 +0000

    Add package FaceBagNet/0.0.1/FaceBagNet-0.0.1.tar.gz

---
+++ packages/FaceBagNet/FaceBagNet-0.0.1.tar.gz/FaceBagNet-0.0.1/readme.md
@@ -0,0 +1,4 @@
+# 使用办法:
+    FA = FaceAnti()
+    img = cv2.imread('1.jpg',1)
+    FA.detect(img)
---
+++ packages/FaceBagNet/FaceBagNet-0.0.1.tar.gz/FaceBagNet-0.0.1/setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+setup(
+
+    name = 'FaceBagNet',
+
+    version = '0.0.1',
+
+    keywords = ('活体检测', '计算机视觉'),
+
+    description = '输入一张图片，输出这个图片上的人是活体还是非活体',
+    long_description=long_description,
+
+    license = 'MIT',
+
+    author = 'zhaomingming',
+    url='https://github.com/pypa/sampleproject',  # Optional
+
+    author_email = '13271929138@163.com',
+    #package_dir={'': 'src'},  
+    #packages = find_packages(),
+    #py_modules=['nacla.nacla']
+    packages=find_packages(),  # Required
+    python_requires='>=3.5, <4',
+     # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    #package_data={  # Optional
+    #    'src/model': ['rnn.pth'],
+    #},
+    include_package_data=True,
+    #data_files=[('model', ['nacla/model/rnn.pth'])],
+
+    #py_modules=['nacla.nacla']
+    #packages=find_packages(where='nacla'),  # Required
+
+)
+

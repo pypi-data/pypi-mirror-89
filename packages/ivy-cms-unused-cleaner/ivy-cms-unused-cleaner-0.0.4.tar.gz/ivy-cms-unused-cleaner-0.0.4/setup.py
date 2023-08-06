7ea2906e024b07469f37269d8982d374e35ae467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:45 +0000

    Add package ivy-cms-unused-cleaner/0.0.4/ivy-cms-unused-cleaner-0.0.4.tar.gz

---
+++ packages/ivy-cms-unused-cleaner/ivy-cms-unused-cleaner-0.0.4.tar.gz/ivy-cms-unused-cleaner-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ivy-cms-unused-cleaner",
+    version="0.0.4",
+    author="Loc Truong",
+    author_email="loctvuit@gmail.com",
+    description="Use this to remove unused CMS in your ivy project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MrNocTV/ivy_unused_cms_scanner",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[            # I get to this in a second
+        'rich'
+    ],
+    python_requires='>=3.6',
+)

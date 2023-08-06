d1a30d0e6b37943e542fe2ad50ebb7aa248af43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:44 +0000

    Add package tamarin-utils/0.8.10/tamarin-utils-0.8.10.tar.gz

---
+++ packages/tamarin-utils/tamarin-utils-0.8.10.tar.gz/tamarin-utils-0.8.10/tamarin/logs/decorators/log.py
@@ -0,0 +1,27 @@
+from functools import wraps
+from ..functions import log as log_provider
+
+
+def log(index: str, doc, params=None, headers=None):
+    def decorator(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            response, status_code = func(*args, **kwargs)
+            # args = {
+            #     'args': args,
+            #     'kwargs': kwargs
+            # }
+            # response, status_code = log_provider(response, status_code, index, doc, args, params, headers)
+            response, status_code = log_provider(
+                response=response,
+                status_code=status_code,
+                index=index,
+                doc=doc,
+                params=params,
+                headers=headers
+            )
+            return response, status_code
+
+        return inner
+
+    return decorator
---
+++ packages/tamarin-utils/tamarin-utils-0.8.10.tar.gz/tamarin-utils-0.8.10/tamarin/logs/todo.py
@@ -0,0 +1,19 @@
+from ..multiprocessing import Todo
+from ..sentry.commands import SentryCommand
+from ..elasticsearch.commands import ElasticSearchCommand
+from ..lamagram.commands import LamagramCommand
+
+
+class LogToDo(Todo):
+    def __init__(self, response, status_code, index, doc, args, headers, params):
+        sentry_cmd = SentryCommand(response, status_code)
+        elastic_cmd = ElasticSearchCommand(response, status_code, index, doc, args, headers, params)
+        commands = [sentry_cmd, elastic_cmd]
+        super(LogToDo, self).__init__(commands)
+
+
+class LamagramToDo(Todo):
+    def __init__(self, chat_id, text, status_code):
+        lamagram_cmd = LamagramCommand(chat_id, text, status_code)
+        commands = [lamagram_cmd]
+        super(LamagramToDo, self).__init__(commands)

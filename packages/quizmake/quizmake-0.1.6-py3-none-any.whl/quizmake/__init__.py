e79dade340b3d9e3a8dcb86c0360817411894158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:21 +0000

    Add package quizmake/0.1.6/quizmake-0.1.6-py3-none-any.whl

---
+++ packages/quizmake/quizmake-0.1.6-py3-none-any.whl/quizmake/__init__.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Initiate the file."""
+__author__ = "jnguyen1098"
+__copyright__ = "Copyright 2020, jnguyen1098"
+__credits__ = ["jnguyen1098"]
+__license__ = "MIT"
+__maintainer__ = "jnguyen1098"
+__status__ = "Planning"
+__version__ = "0.1.6"
+
+# from .core import main
+# from .corpus import Corpus, corn
+# __all__ = ["Corpus", "corn", "main"]
---
+++ packages/quizmake/quizmake-0.1.6-py3-none-any.whl/quizmake/quizmake.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Common helper functions.
+
+This will represent the bread and butter of the application.
+"""
+
+
+def add_two_numbers(x_val: int, y_val: int) -> int:
+    """Just adds two numbers together."""
+    return x_val + y_val
+
+
+def return_string() -> str:
+    """Return the string doggo."""
+    return "string"

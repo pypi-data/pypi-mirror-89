5b4897c62386a4c54baeb5dcd2242d258e23fcee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:41 +0000

    Add package sampleTowelStuff/0.1dev/sampleTowelStuff-0.1dev.tar.gz

---
+++ packages/sampleTowelStuff/sampleTowelStuff-0.1dev.tar.gz/sampleTowelStuff-0.1dev/README.txt
@@ -0,0 +1,4 @@
+# python3
+
+[Welcome to The Hitchhiker’s Guide to Packaging](https://the-hitchhikers-guide-to-packaging.readthedocs.io/en/latest/quickstart.html)
+
---
+++ packages/sampleTowelStuff/sampleTowelStuff-0.1dev.tar.gz/sampleTowelStuff-0.1dev/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='sampleTowelStuff',
+    version='0.1dev',
+    packages=['towelstuff',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
+

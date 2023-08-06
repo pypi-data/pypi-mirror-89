4e832ae04dd8d37b1c6252760226ae04f103f96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:44 +0000

    Add package python-wise-utils/0.3.1/python_wise_utils-0.3.1.tar.gz

---
+++ packages/python-wise-utils/python_wise_utils-0.3.1.tar.gz/python_wise_utils-0.3.1/python_wise_utils/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.3.1'
+from .persistent_dict import DictPersistYAML
+from .misc_functions import *
---
+++ packages/python-wise-utils/python_wise_utils-0.3.1.tar.gz/python_wise_utils-0.3.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['python_wise_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyyaml>=5.3.1,<6.0.0']
+
+setup_kwargs = {
+    'name': 'python-wise-utils',
+    'version': '0.3.1',
+    'description': 'Python interpreter convenience utilities',
+    'long_description': None,
+    'author': 'Wisdom Wolf',
+    'author_email': 'wisdomwolf@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/WisdomWolf/python_wise_utils',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)

e4fb00bcc788ed875f7af68220b27be90c4c754d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:53 +0000

    Add package mincepy-sci/0.1.1/mincepy_sci-0.1.1-py2.py3-none-any.whl

---
+++ packages/mincepy-sci/mincepy_sci-0.1.1-py2.py3-none-any.whl/mincepy_sci/provides.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from . import ase_types
+from . import numpy_types
+from . import pandas_types
+from . import plams_types
+
+
+def get_types():
+    """The central entry point """
+    types = list()
+    types.extend(ase_types.TYPES)
+    types.extend(numpy_types.TYPES)
+    types.extend(pandas_types.TYPES)
+    types.extend(plams_types.TYPES)
+
+    return types

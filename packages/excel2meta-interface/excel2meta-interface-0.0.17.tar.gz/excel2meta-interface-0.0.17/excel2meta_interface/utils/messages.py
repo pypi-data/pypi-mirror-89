905839011da85c7ab3e2ba7f5b0be4f3e4ebef8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:13 +0000

    Add package excel2meta-interface/0.0.17/excel2meta-interface-0.0.17.tar.gz

---
+++ packages/excel2meta-interface/excel2meta-interface-0.0.17.tar.gz/excel2meta-interface-0.0.17/excel2meta_interface/utils/messages.py
@@ -0,0 +1,48 @@
+message = {
+    "ok": {
+        "code": "OK"
+        , "message": "No errors encountered"
+        , "level": "INFO"
+    },
+    "not_implemented": {
+        "code": "EMI-GEN-000"
+        , "message": "Function not yet implemented. Please contact the development team and mention code 'EMI-GEN-000'."
+        , "level": "ERROR"
+    },
+    "not_found": {
+        "code": "EMI-GEN-001"
+        , "message": "Could not find what we were looking for"
+        , "level": "ERROR"
+    },
+    "os_error": {
+        "code": "EMI-GEN-002"
+        , "message": "An OS error occurred"
+        , "level": "ERROR"
+    },
+    "ignore": {
+        "code": "EMI-GEN-003"
+        , "message": "Entry has been ignored"
+        , "level": "DEBUG"
+    },
+    "undetermined": {
+        "code": "EMI-GEN-004"
+        ,
+        "message": "Undetermined result. This should not happen. Please contact the development team and mention code 'EMI-GEN-004'."
+        , "level": "ERROR"
+    },
+    "main_config_not_found": {
+        "code": "EMI-GEN-005"
+        , "message": "Main configuration file not found."
+        , "level": "FATAL"
+    },
+    "jinja_template_name_not_provided": {
+        "code": "EMI-JIN-001"
+        , "message": "Internal error: Jinja template name not provided"
+        , "level": "ERROR"
+    },
+    "jinja_template_not_found": {
+        "code": "EMI-JIN-002"
+        , "message": "Jinja template not found."
+        , "level": "ERROR"
+    }
+}

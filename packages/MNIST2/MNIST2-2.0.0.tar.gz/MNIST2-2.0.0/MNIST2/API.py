92e29ce57b380ce47edbf9fe2fd3a492f0e3da2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:09 +0000

    Add package MNIST2/2.0.0/MNIST2-2.0.0.tar.gz

---
+++ packages/MNIST2/MNIST2-2.0.0.tar.gz/MNIST2-2.0.0/MNIST2/API.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+from PIL import Image
+from Functions import *
+
+def NumOCR(path, label=None, debug=False, show=False):
+
+    # Calling for target photo
+    ImageFile = Image.open(path).convert("L")
+    if show:
+        ImageFile.show()
+    ContentArray = np.array(ImageFile.resize((28, 28)))
+    ImageFile.close()
+
+    # Calling for trained model
+    try:
+        ThetaArray = np.load("theta.npy")
+    except:
+        os.chdir("./MNIST2")
+        ThetaArray = np.load("theta.npy")
+
+    # Comparison
+    ThetaArray = np.fabs(ThetaArray - ContentArray)
+    Theta = func(ThetaArray).sum(axis=1).sum(axis=1)
+    
+    #print(Theta)
+    Prediction = np.argmin(Theta)
+
+    if debug:
+        print(Theta)
+
+    if not label:
+        print("Prediction :", Prediction)
+
+    if label:
+        return True if Prediction == label else False
+
+if __name__ == "__main__":
+    NumOCR("tmpl6deljrh.PNG", label=None, debug=False, show=False)
---
+++ packages/MNIST2/MNIST2-2.0.0.tar.gz/MNIST2-2.0.0/README.rst
@@ -0,0 +1,7 @@
+Hello!
+
+this package was finished by my team
+you can easily call this API by using function"NumOCR"
+function "help" will teach you how to use it
+
+thank you again for downloading our package

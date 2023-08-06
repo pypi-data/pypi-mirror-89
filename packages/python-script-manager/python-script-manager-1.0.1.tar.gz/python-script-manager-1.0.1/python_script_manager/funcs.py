5a7bd13fdf04a5bb4a7e5cae0e7a4d43de04db72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:36 +0000

    Add package python-script-manager/1.0.1/python-script-manager-1.0.1.tar.gz

---
+++ packages/python-script-manager/python-script-manager-1.0.1.tar.gz/python-script-manager-1.0.1/python_script_manager/funcs.py
@@ -0,0 +1,50 @@
+import json
+import os
+try:
+    from . import const
+except ImportError:
+    import const
+
+def loadScripts():
+    with open(const.filename, 'rt') as s:
+        return json.load(s)['scripts']
+
+def runScriptDirectly(script):
+    print(f'\n\t> {script}\n')
+    os.system(script)
+
+def addScript(name, command):
+    with open(const.filename, 'rt') as s:
+        data = json.load(s)
+    with open(const.filename, 'wt') as s:
+        data['scripts'][name] = command
+        json.dump(data, s)
+    print('Script added successfully.')
+
+
+def initialize():
+    with open(const.filename, 'wt') as s:
+        data = {
+            "scripts": {
+                "hello": "echo Hello World!!!"
+            }
+        }
+        json.dump(data, s)
+    print('Successfully initialized '+const.filename)
+
+def removeScript(name):
+    with open(const.filename, 'rt') as s:
+        data = json.load(s)
+    with open(const.filename, 'wt') as s:
+        data['scripts'].pop(name)
+        json.dump(data, s)
+    print('Script removed successfully.')
+
+def runScript(name):
+    scripts = loadScripts()
+    called_script = scripts.get(name, None)
+    if called_script:
+        print(f'\n\t> {called_script}\n')
+        os.system(called_script)
+    else:
+        raise Exception(f'Can not find script named "{name}"')

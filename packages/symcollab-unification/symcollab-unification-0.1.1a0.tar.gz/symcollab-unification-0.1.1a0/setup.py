3ad08ef420e09cc181eede2c18b08f4a9467bec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:30 +0000

    Add package symcollab-unification/0.1.1a0/symcollab-unification-0.1.1a0.tar.gz

---
+++ packages/symcollab-unification/symcollab-unification-0.1.1a0.tar.gz/symcollab-unification-0.1.1a0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name="symcollab-unification",
+    version="0.1.1a",
+    packages=find_namespace_packages(include=["symcollab.*"]),
+    url="https://github.com/symcollab/cryptosolve",
+    install_requires = [
+        # Our own projects
+        "symcollab-algebra",
+        "symcollab-xor",
+        # Outside dependencies
+        "mypy~=0.740",
+        "mypy-extensions~=0.4.0",
+        "scipy~=1.5.4",
+        "sympy==1.4"
+    ],
+)

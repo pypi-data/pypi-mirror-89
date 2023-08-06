1433af35903666ef72329699ed7b6d1950b9ac13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:57 +0000

    Add package myimpute/0.0.2/myimpute-0.0.2.tar.gz

---
+++ packages/myimpute/myimpute-0.0.2.tar.gz/myimpute-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="myimpute",
+    version="0.0.2",
+    author="Shaobo Zhuang",
+    license="MIT",
+    author_email="dcbajk@vip.qq.com",
+    description="Imputes missing data like a linear equation",
+    keywords=['Imputation', 'Missing Values', 'Missing', 'Linear', 'Mean'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/imjxydhh/MyImputation",
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)

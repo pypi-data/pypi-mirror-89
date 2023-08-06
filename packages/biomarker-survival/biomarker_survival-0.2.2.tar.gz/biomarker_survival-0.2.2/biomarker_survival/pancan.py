63fb9f48a854386fa65bfb852ee29c5fafe00641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:52 +0000

    Add package biomarker-survival/0.2.2/biomarker_survival-0.2.2.tar.gz

---
+++ packages/biomarker-survival/biomarker_survival-0.2.2.tar.gz/biomarker_survival-0.2.2/biomarker_survival/pancan.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import sys
+import os
+import re
+
+from . import analysis
+
+
+def pancan(cancer_types_dir, multivariate=False):
+  files = os.listdir(cancer_types_dir)
+  ctype = re.compile('([A-Z]+).*.zscores.out.csv')
+
+  z_name = 'z'
+  if multivariate:
+    z_name = 'var-z'
+
+  pancan_dict = {}
+  for f in files:
+    cancer_type_match = re.match(ctype, f)
+    if not cancer_type_match:
+      continue
+    cancer_type = cancer_type_match.group(1)
+
+    # sometimes rpy2 gives back this monstrosity of a NaN value.
+    df = pd.read_csv(os.path.join(cancer_types_dir, f), index_col=0, na_values=[' NA'])
+    if df.shape[0] > 0:
+      if '\'' not in df.index[0]:
+        # ADD ' to index
+        df.reset_index(inplace=True)
+        df['gene'] = '\'' + df['gene']
+        df.set_index('gene', inplace=True)
+      pancan_dict[cancer_type] = df[z_name].astype(float)
+
+  pancan_df = pd.DataFrame(pancan_dict)
+  pancan_df['stouffer unweighted'] = analysis.stouffer_unweighted(pancan_df)
+  return pancan_df
+
---
+++ packages/biomarker-survival/biomarker_survival-0.2.2.tar.gz/biomarker_survival-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+# note: tzlocal is an rpy2 dependency that's left out of the rpy2 spec.
+setup(name='biomarker_survival',
+      version='0.2.2',
+      description='Utilities for performing biomarker survival analyses',
+      url='http://github.com/joansmith/biomarker-survival',
+      author='Joan Smith',
+      author_email='joans@alum.mit.edu',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      setup_requires=['pytest-runner'],
+      install_requires=[
+        'pandas>=0.25.1',
+        'rpy2==2.9.4',
+        'matplotlib',
+        'scipy>=1.3.1',
+        'tzlocal',
+        'openpyxl'],
+      tests_require=['pytest', 'pytest-datafiles'])
+

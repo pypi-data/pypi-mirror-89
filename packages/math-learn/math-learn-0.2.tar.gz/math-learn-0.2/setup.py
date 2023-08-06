c83d0c02bf0e17c731538ab372fa9f81bc0a5927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:05 +0000

    Add package math-learn/0.2/math-learn-0.2.tar.gz

---
+++ packages/math-learn/math-learn-0.2.tar.gz/math-learn-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='math-learn',
+      version='0.2',
+      description='Math manipulation library',
+      packages=['distributions'],
+      author = 'Usman IlamDin',
+      author_email = 'usmanilamdin65@gmail.com',
+      zip_safe=False)

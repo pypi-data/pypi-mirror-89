078102e25abbf22bf62b9fb44ccd727995b0678f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:06 +0000

    Add package nrv/0.0.7/nrv-0.0.7.tar.gz

---
+++ packages/nrv/nrv-0.0.7.tar.gz/nrv-0.0.7/nrv/v1.py
@@ -0,0 +1,47 @@
+data = {
+    'category': [
+        'goal',
+        'concept',
+        'question'
+    ],
+    'system': [
+        'project',
+        'structure',
+        'mechanism',
+        'organization',
+        'person',
+        'process',
+        'api'
+    ],
+    'method': [
+        'idea',
+        'principle',
+        'function',
+        'invention',
+        'equipment',
+        'product',
+        'endpoint'
+    ],
+    'location': [
+        'address',
+        'account',
+        'url'
+    ],
+    'request': [
+        'CATEGORY',
+        'query',
+        'order',
+        'task',
+        'call',
+        'operation',
+        'operation:shipment',
+        'operation:payment',
+        'operation:contract',
+    ],
+    'resource': [
+        'record',
+        'document',
+        'METHOD',
+        'SYSTEM'
+    ],
+}

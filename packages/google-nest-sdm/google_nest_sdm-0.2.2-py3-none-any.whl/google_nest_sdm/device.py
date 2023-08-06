781be0891cb78fd80f8d285b965e3846e9f6f989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:44 +0000

    Add package google-nest-sdm/0.2.2/google_nest_sdm-0.2.2-py3-none-any.whl

---
+++ packages/google-nest-sdm/google_nest_sdm-0.2.2-py3-none-any.whl/google_nest_sdm/device.py
@@ -0,0 +1,121 @@
+"""A device from the Smart Device Management API."""
+
+import logging
+
+# Import traits for registration
+from typing import Callable
+
+from . import camera_traits  # noqa: F401
+from . import device_traits  # noqa: F401
+from . import doorbell_traits  # noqa: F401
+from . import thermostat_traits  # noqa: F401
+from .auth import AbstractAuth
+from .event import AsyncEventCallback, EventMessage, EventProcessingError
+from .traits import BuildTraits, Command
+
+_LOGGER = logging.getLogger(__name__)
+
+DEVICE_NAME = "name"
+DEVICE_TYPE = "type"
+DEVICE_TRAITS = "traits"
+DEVICE_PARENT_RELATIONS = "parentRelations"
+PARENT = "parent"
+DISPLAYNAME = "displayName"
+
+
+class Device:
+    """Class that represents a device object in the Google Nest SDM API."""
+
+    def __init__(self, raw_data: dict, traits: dict):
+        """Initialize a device."""
+        self._raw_data = raw_data
+        self._traits = traits
+        self._relations = {}
+        for relation in self._raw_data.get(DEVICE_PARENT_RELATIONS, []):
+            if PARENT not in relation or DISPLAYNAME not in relation:
+                continue
+            self._relations[relation[PARENT]] = relation[DISPLAYNAME]
+        self._callbacks = []
+
+    @staticmethod
+    def MakeDevice(raw_data: dict, auth: AbstractAuth):
+        """Create a device with the appropriate traits."""
+        device_id = raw_data.get(DEVICE_NAME)
+        cmd = Command(device_id, auth)
+        traits = raw_data.get(DEVICE_TRAITS, {})
+        traits_dict = BuildTraits(traits, cmd, raw_data.get(DEVICE_TYPE))
+        return Device(raw_data, traits_dict)
+
+    @property
+    def name(self) -> str:
+        """Resource name of the device such as 'enterprises/XYZ/devices/123'."""
+        return self._raw_data[DEVICE_NAME]
+
+    @property
+    def type(self) -> str:
+        """Type of device for display purposes.
+
+        The device type should not be used to deduce or infer functionality of
+        the actual device it is assigned to. Instead, use the returned traits for
+        the device.
+        """
+        return self._raw_data[DEVICE_TYPE]
+
+    @property
+    def traits(self) -> dict:
+        """Return a trait mixin or None."""
+        return self._traits
+
+    def _traits_data(self, trait) -> dict:
+        """Return the raw dictionary for the specified trait."""
+        traits_dict = self._raw_data.get(DEVICE_TRAITS, {})
+        return traits_dict.get(trait, {})
+
+    @property
+    def parent_relations(self) -> dict:
+        """Room or structure for the device."""
+        return self._relations
+
+    @property
+    def raw_data(self) -> str:
+        """Return the raw data string."""
+        return self._raw_data
+
+    def add_event_callback(
+        self, event_callback: AsyncEventCallback
+    ) -> Callable[[], None]:
+        """Register an AsyncEventCallback for udpates to this device.
+
+        The return value is a callable that will unregister the callback.
+        """
+        self._callbacks.append(event_callback)
+
+        def remove_callback():
+            """Remove the event_callback."""
+            self._callbacks.remove(event_callback)
+
+        return remove_callback
+
+    async def async_handle_event(self, event_message: EventMessage) -> None:
+        """Process an event from the pubsub subscriber."""
+        _LOGGER.debug(
+            "Processing update %s @ %s", event_message.event_id, event_message.timestamp
+        )
+        if not event_message.resource_update_name:
+            raise EventProcessingError("Event was not resource update event")
+        if self.name != event_message.resource_update_name:
+            raise EventProcessingError(
+                f"Mismatch {self.name} != {event_message.resource_update_name}"
+            )
+        traits = event_message.resource_update_traits
+        if traits:
+            _LOGGER.debug("Trait update %s", traits.keys())
+        events = event_message.resource_update_events
+        if events:
+            _LOGGER.debug("Event Update %s", events.keys())
+
+        for (trait_name, trait) in traits.items():
+            self._traits[trait_name] = trait
+
+        for callback in self._callbacks:
+            await callback.async_handle_event(event_message)
---
+++ packages/google-nest-sdm/google_nest_sdm-0.2.2-py3-none-any.whl/google_nest_sdm/google_nest_subscriber.py
@@ -0,0 +1,211 @@
+"""Subscriber for the Smart Device Management event based API."""
+import asyncio
+import concurrent.futures
+import json
+import logging
+import re
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from aiohttp.client_exceptions import ClientError
+from google.api_core.exceptions import GoogleAPIError, NotFound, Unauthenticated
+from google.cloud import pubsub_v1
+
+from .auth import AbstractAuth
+from .device_manager import DeviceManager
+from .event import AsyncEventCallback, EventMessage
+from .exceptions import AuthException, ConfigurationException, SubscriberException
+from .google_nest_api import GoogleNestAPI
+
+_LOGGER = logging.getLogger(__name__)
+
+# Used to catch invalid subscriber id
+EXPECTED_SUBSCRIBER_REGEXP = re.compile("projects/.*/subscriptions/.*")
+
+# Used to catch a topic misconfiguration
+EXPECTED_TOPIC_REGEXP = re.compile("projects/sdm-[a-z]+/topics/.*")
+
+
+class AbstractSusbcriberFactory(ABC):
+    """Abstract class for creating a subscriber, to facilitate testing."""
+
+    @abstractmethod
+    async def async_new_subscriber(
+        self, creds, subscription_name, async_callback
+    ) -> pubsub_v1.subscriber.futures.StreamingPullFuture:
+        """Create a new event subscriber."""
+
+
+class DefaultSubscriberFactory(AbstractSusbcriberFactory):
+    """Default implementation that creates Google Pubsub subscriber."""
+
+    async def async_new_subscriber(
+        self, creds, subscription_name, loop, async_callback
+    ) -> pubsub_v1.subscriber.futures.StreamingPullFuture:
+        """Create a new subscriber with a blocking to async bridge."""
+
+        def callback_wrapper(message: pubsub_v1.subscriber.message.Message):
+            future = asyncio.run_coroutine_threadsafe(async_callback(message), loop)
+            future.result()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+            return await loop.run_in_executor(
+                executor,
+                self._new_subscriber,
+                creds,
+                subscription_name,
+                callback_wrapper,
+            )
+
+    def _new_subscriber(self, creds, subscription_name, callback_wrapper):
+        """Issue a command to verify subscriber creds are correct."""
+        subscriber = pubsub_v1.SubscriberClient(credentials=creds)
+        subscription = subscriber.get_subscription(subscription=subscription_name)
+        if subscription.topic:
+            if not EXPECTED_TOPIC_REGEXP.match(subscription.topic):
+                raise ConfigurationException(
+                    "Subscription misconfigured. Expected topic name to "
+                    f"match '{EXPECTED_TOPIC_REGEXP.pattern}' but was "
+                    f"'{subscription.topic}'."
+                )
+            else:
+                _LOGGER.debug(
+                    "Susbcriber '%s' configured on topic '%s'",
+                    subscription_name,
+                    subscription.topic,
+                )
+        return subscriber.subscribe(subscription_name, callback_wrapper)
+
+
+class GoogleNestSubscriber:
+    """Subscribe to events from the Google Nest feed."""
+
+    def __init__(
+        self,
+        auth: AbstractAuth,
+        project_id: str,
+        subscriber_id: str,
+        subscriber_factory: AbstractSusbcriberFactory = DefaultSubscriberFactory(),
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+        watchdog_delay: float = 10,
+    ):
+        """Initialize the subscriber for the specified topic."""
+        self._auth = auth
+        self._subscriber_id = subscriber_id
+        self._api = GoogleNestAPI(auth, project_id)
+        self._loop = loop or asyncio.get_event_loop()
+        self._device_manager_task = None
+        self._subscriber_factory = subscriber_factory
+        self._subscriber_future = None
+        self._callback = None
+        self._healthy = True
+        self._watchdog_delay = watchdog_delay
+        if self._watchdog_delay > 0:
+            self._watchdog_task = asyncio.create_task(self._watchdog())
+        else:
+            self._watchdog_task = None
+
+    def set_update_callback(self, callback: AsyncEventCallback):
+        """Register a callback invoked when new messages are received."""
+        self._callback = callback
+
+    async def start_async(self):
+        """Start the subscriber."""
+        if not EXPECTED_SUBSCRIBER_REGEXP.match(self._subscriber_id):
+            raise ConfigurationException(
+                "Subscription misconfigured. Expected subscriber_id to "
+                f"match '{EXPECTED_SUBSCRIBER_REGEXP.pattern}' but was "
+                f"'{self._subscriber_id}'"
+            )
+        try:
+            creds = await self._auth.async_get_creds()
+        except ClientError as err:
+            raise AuthException(f"Access token failure: {err}") from err
+
+        try:
+            self._subscriber_future = (
+                await self._subscriber_factory.async_new_subscriber(
+                    creds, self._subscriber_id, self._loop, self._async_message_callback
+                )
+            )
+        except NotFound as err:
+            raise SubscriberException(
+                f"Failed to create subscriber '{self._subscriber_id}' id was not found"
+            ) from err
+        except Unauthenticated as err:
+            raise AuthException("Failed to authenticate subscriber: {err}") from err
+        except GoogleAPIError as err:
+            raise SubscriberException(
+                f"Failed to create subscriber '{self._subscriber_id}': {err}"
+            ) from err
+
+        if not self._healthy:
+            _LOGGER.debug("Subscriber reconnected")
+            self._healthy = True
+        if self._subscriber_future:
+            self._subscriber_future.add_done_callback(self._done_callback)
+
+    async def _watchdog(self):
+        """Background task that watches the subscriber and restarts it."""
+        _LOGGER.debug("Starting background watchdog thread")
+        while True:
+            if self._subscriber_future and self._subscriber_future.done():
+                _LOGGER.debug("Watchdog: subscriber shut down; restarting")
+                await self.start_async()
+            await asyncio.sleep(self._watchdog_delay)
+
+    def wait(self):
+        """Block on the subscriber."""
+        self._subscriber_future.result()
+
+    def stop_async(self):
+        """Tell the subscriber to start shutting down."""
+        if self._device_manager_task:
+            self._device_manager_task.cancel()
+        if self._watchdog_task:
+            self._watchdog_task.cancel()
+        if self._subscriber_future:
+            self._subscriber_future.cancel()
+
+    async def async_get_device_manager(self) -> DeviceManager:
+        """Return the DeviceManger with the current state of devices."""
+        if not self._device_manager_task:
+            self._device_manager_task = asyncio.create_task(
+                self._async_create_device_manager()
+            )
+        return await self._device_manager_task
+
+    async def _async_create_device_manager(self):
+        """Create a DeviceManager, populated with initial state."""
+        device_manager = DeviceManager()
+        structures = await self._api.async_get_structures()
+        for structure in structures:
+            device_manager.add_structure(structure)
+        # Subscriber starts after a device fetch
+        devices = await self._api.async_get_devices()
+        for device in devices:
+            device_manager.add_device(device)
+        return device_manager
+
+    def _done_callback(self, future):
+        """Teardown subscriber."""
+        if future.exception():
+            ex = future.exception()
+            if self._healthy:
+                self._healthy = False
+            _LOGGER.debug("Subscriber disconnected, will restart: %s: %s", type(ex), ex)
+
+    async def _async_message_callback(
+        self, message: pubsub_v1.subscriber.message.Message
+    ):
+        """Handle a received message."""
+        payload = json.loads(bytes.decode(message.data))
+        event = EventMessage(payload, self._auth)
+        # Only accept device events once the Device Manager has been loaded.
+        # We are ok with missing messages on startup since the device manager
+        # will do a live read .
+        if self._device_manager_task.done():
+            await self._device_manager_task.result().async_handle_event(event)
+        if self._callback:
+            await self._callback.async_handle_event(event)
+        message.ack()

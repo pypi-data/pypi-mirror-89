48d6c71fdcbf695e3e8d268e8dd3f068cbc4cb16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:52 +0000

    Add package withtray/0.1.0/withtray-0.1.0.tar.gz

---
+++ packages/withtray/withtray-0.1.0.tar.gz/withtray-0.1.0/setup.py
@@ -0,0 +1,31 @@
+import site
+import sys
+from setuptools import setup, find_packages
+
+
+# Workaround to install in user dir but editable,
+# see https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='withtray',
+    version='0.1.0',
+    description='Run any blocking command with systray icon',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/krisfris/withtray',
+    author='Kris',
+    author_email='31852063+krisfris@users.noreply.github.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click', 'pygobject', 'Pillow', 'pystray'
+    ],
+    entry_points='''
+        [console_scripts]
+        withtray=withtray.main:main
+    ''',
+)

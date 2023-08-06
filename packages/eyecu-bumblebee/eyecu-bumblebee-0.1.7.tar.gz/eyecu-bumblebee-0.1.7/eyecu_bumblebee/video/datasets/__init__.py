083307c0a2809adb14c28967d2bb8cbe579048d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:15 +0000

    Add package eyecu-bumblebee/0.1.7/eyecu-bumblebee-0.1.7.tar.gz

---
+++ packages/eyecu-bumblebee/eyecu-bumblebee-0.1.7.tar.gz/eyecu-bumblebee-0.1.7/eyecu_bumblebee/video/datasets/__init__.py
@@ -0,0 +1,3 @@
+from .FullStreamDataset import FullStreamDataset
+from .SingleFrameDataset import SingleFrameDataset
+from .VideoDataset import VideoDataset
---
+++ packages/eyecu-bumblebee/eyecu-bumblebee-0.1.7.tar.gz/eyecu-bumblebee-0.1.7/eyecu_bumblebee/video/transfomers/__init__.py
@@ -0,0 +1,9 @@
+# Transformers work over numpy arrays.
+
+from .ChannelFirst import ChannelFirst
+from .FrameDownsampler import FrameDownsampler
+from .GrayScale import GrayScale
+from .Loop import Loop
+from .Normalization import Normalization
+from .Sobel import Sobel
+from .GoTo import GoTo

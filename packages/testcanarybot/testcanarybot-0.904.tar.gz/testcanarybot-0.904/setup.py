0a87ebdd7ffb0ec7061509d6c2888ff5ca6cde0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:56 +0000

    Add package testcanarybot/0.904/testcanarybot-0.904.tar.gz

---
+++ packages/testcanarybot/testcanarybot-0.904.tar.gz/testcanarybot-0.904/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testcanarybot",
+    version="0.904",
+    author="andprokofieff",
+    author_email="prokofieff.help@gmail.com",
+    description="VKontakte asynchronous bot framework like Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kensoi/testcanarybot",
+    packages = setuptools.find_packages(where='./testcanarybot'),
+    package_dir={
+        '': 'testcanarybot',
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ["aiohttp"],
+    python_requires='>=3.7',
+)

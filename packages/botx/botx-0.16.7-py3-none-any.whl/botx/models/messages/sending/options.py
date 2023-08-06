571322839b871727b93796876c57bc15ac4659a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:25 +0000

    Add package botx/0.16.7/botx-0.16.7-py3-none-any.whl

---
+++ packages/botx/botx-0.16.7-py3-none-any.whl/botx/models/messages/sending/options.py
@@ -0,0 +1,34 @@
+"""Special options for message."""
+
+from typing import List
+
+from pydantic import BaseModel
+
+from botx.models.entities import Mention
+from botx.models.typing import AvailableRecipients
+
+
+class NotificationOptions(BaseModel):
+    """Configurations for message notifications."""
+
+    #: show notification about message.
+    send: bool = True
+
+    #: break mute on bot messages.
+    force_dnd: bool = False
+
+
+class MessageOptions(BaseModel):
+    """Message options configuration."""
+
+    #: users that should receive message.
+    recipients: AvailableRecipients = "all"
+
+    #: attached to message mentions.
+    mentions: List[Mention] = []
+
+    #: if true don't show next user's input in chat
+    silent_response: bool = False
+
+    #: notification configuration.
+    notifications: NotificationOptions = NotificationOptions()

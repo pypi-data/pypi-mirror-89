eed9928a3a321a596aa581f5e13cbfebc7612759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:10 +0000

    Add package gblmf-distributions/0.1/gblmf_distributions-0.1.tar.gz

---
+++ packages/gblmf-distributions/gblmf_distributions-0.1.tar.gz/gblmf_distributions-0.1/README.md
@@ -0,0 +1,11 @@
+gblmf-distributions package
+
+Summary of the package
+
+# Files
+
+Binomialdistribution.py
+Gaussiandistribution.py
+Generaldistribution.py
+
+# Installation
---
+++ packages/gblmf-distributions/gblmf_distributions-0.1.tar.gz/gblmf_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gblmf_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gblmf_distributions'],
+      author='Luis Ferreira',
+      author_email='luismatosferreira@gmail.com',
+      zip_safe=False)

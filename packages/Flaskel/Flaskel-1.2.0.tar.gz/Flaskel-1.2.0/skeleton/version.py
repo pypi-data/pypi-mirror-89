5c87971ab327273e42e58528096b10efec068b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:57 +0000

    Add package Flaskel/1.2.0/Flaskel-1.2.0.tar.gz

---
+++ packages/Flaskel/Flaskel-1.2.0.tar.gz/Flaskel-1.2.0/skeleton/version.py
@@ -0,0 +1,16 @@
+__version__ = '0.0.0'
+
+__author_name__ = 'author name'
+__author_email__ = 'author@email.com'
+
+__author__ = "{} <{}>".format(
+    __author_name__,
+    __author_email__
+)
+
+__all__ = [
+    '__version__',
+    '__author__',
+    '__author_name__',
+    '__author_email__',
+]
---
+++ packages/Flaskel/Flaskel-1.2.0.tar.gz/Flaskel-1.2.0/tests/__init__.py
@@ -0,0 +1,86 @@
+import os
+
+import pytest
+from flask.testing import FlaskClient
+
+from flaskel import AppFactory
+from flaskel.ext import BASE_EXTENSIONS
+from flaskel.ext.crypto import Argon2
+from flaskel.ext.healthcheck import health_checks, health_mongo, health_redis, health_sqlalchemy
+from flaskel.ext.sqlalchemy import db as dbsqla
+from flaskel.ext.useragent import UserAgent
+from flaskel.patch import ForceHttps, HTTPMethodOverride, ReverseProxied
+from tests.blueprints import BLUEPRINTS
+
+BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+SKEL_DIR = os.path.join(BASE_DIR, 'skeleton')
+CONF_DIR = os.path.join(SKEL_DIR, 'config')
+
+
+class TestClient(FlaskClient):
+    """
+        Implement this to customize Flask client
+        Form example:
+                def fetch(self, url, *args, **kwargs):
+                    return self.open(url, method='FETCH', *args, **kwargs)
+    """
+
+
+@pytest.fixture
+def app_prod():
+    def test_health_true(**kwargs):
+        return True, None
+
+    def test_health_false(**kwargs):
+        return False, "error"
+
+    extra_ext = {
+        "empty":         (None, (None,)),  # NB: needed to complete coverage
+        "useragent":     (UserAgent(),),
+        "argon2":        (Argon2(),),
+        "health_checks": (health_checks, {'extensions': (
+            {
+                'name': 'health_true',
+                'func': test_health_true,
+            },
+            {
+                'func': test_health_false,
+            },
+        )},),
+    }
+
+    health_checks.register('mongo', db=dbsqla)(health_mongo)
+    health_checks.register('redis', db=dbsqla)(health_redis)
+    health_checks.register('sqlalchemy', db=dbsqla)(health_sqlalchemy)
+
+    _app = AppFactory(
+        blueprints=(*BLUEPRINTS, *(None,), *((None,),)),  # NB: needed to complete coverage
+        extensions={**BASE_EXTENSIONS, **extra_ext},
+        middlewares=(ForceHttps, HTTPMethodOverride, ReverseProxied),
+        folders=["skeleton/blueprints/web/templates"],
+        static_folder="skeleton/blueprints/web/static"
+    ).get_or_create(dict(TESTING=True))
+
+    _app.test_client_class = TestClient
+    _app.config.USER_AGENT_AUTO_PARSE = True
+    return _app
+
+
+@pytest.fixture
+def app_dev():
+    _app = AppFactory(
+        blueprints=BLUEPRINTS,
+        extensions=BASE_EXTENSIONS,
+        template_folder="skeleton/blueprints/web/templates",
+        static_folder="skeleton/blueprints/web/static"
+    ).get_or_create(
+        dict(DEBUG=True, FLASK_ENV='development')
+    )
+
+    _app.test_client_class = TestClient
+    return _app
+
+
+@pytest.fixture
+def testapp(app_prod):
+    return app_prod.test_client()

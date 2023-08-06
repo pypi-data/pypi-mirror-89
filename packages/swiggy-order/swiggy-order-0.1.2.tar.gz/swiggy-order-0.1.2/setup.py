c5ad64209f1620e59e1c6d704853b3bac02c1a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:00 +0000

    Add package swiggy-order/0.1.2/swiggy-order-0.1.2.tar.gz

---
+++ packages/swiggy-order/swiggy-order-0.1.2.tar.gz/swiggy-order-0.1.2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['swiggy_order', 'swiggy_order.apis']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['coloredlogs>=15.0,<16.0',
+ 'docopt>=0.6.2,<0.7.0',
+ 'prompt_toolkit>=3.0.8,<4.0.0',
+ 'requests>=2.25.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['order-food = swiggy_order:main']}
+
+setup_kwargs = {
+    'name': 'swiggy-order',
+    'version': '0.1.2',
+    'description': 'Order food via terminal.',
+    'long_description': None,
+    'author': 'Amresh Venugopal',
+    'author_email': 'amresh.venugopal@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

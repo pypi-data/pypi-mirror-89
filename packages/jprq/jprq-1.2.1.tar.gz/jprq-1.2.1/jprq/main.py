5e1330768c2faa6c3ad38a4483bddb45b8e8d678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:01 +0000

    Add package jprq/1.2.1/jprq-1.2.1.tar.gz

---
+++ packages/jprq/jprq-1.2.1.tar.gz/jprq-1.2.1/jprq/main.py
@@ -0,0 +1,35 @@
+import argparse
+import asyncio
+from getpass import getuser
+from .tunnel import open_tunnel
+from . import __version__
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Live And HTTPS Localhost')
+    parser.add_argument('port', type=int, help='Port number of the local server')
+    parser.add_argument('-s', '--subdomain', type=str, help='Sub-domain')
+    parser.add_argument('-v', '--version', help='Version number of jprq')
+
+    args = parser.parse_args()
+
+    if args.version:
+        print(__version__)
+        return
+    
+    if not args.port:
+        print("Please specify -p/--port argument and port.")
+        return
+
+    username = args.subdomain or getuser()
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            open_tunnel(
+                ws_uri=f'wss://open.jprq.live/_ws/?username={username}&port={args.port}',
+                http_uri=f'http://127.0.0.1:{args.port}',
+            )
+        )
+    except KeyboardInterrupt:
+        print("\njprq tunnel closed")

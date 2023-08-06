40358b52a58690d7519edc5228f8cc9c3cb8389c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:18 +0000

    Add package simplemailproxy/1.0.0.dev2/simplemailproxy-1.0.0.dev2.tar.gz

---
+++ packages/simplemailproxy/simplemailproxy-1.0.0.dev2.tar.gz/simplemailproxy-1.0.0.dev2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="simplemailproxy",
+    version="1.0.0.dev2",
+    author="Ondrej Sika",
+    author_email="ondrej@ondrejsika.com",
+    description="Simple Mail Proxy",
+    url="https://github.com/ondrejsika/python-simplemailproxy",
+    packages=setuptools.find_packages(),
+)

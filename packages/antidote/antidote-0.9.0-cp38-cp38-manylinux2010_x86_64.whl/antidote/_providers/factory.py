81008601e96ebe4aa93c688e5462f9fd74a25a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:24 +0000

    Add package antidote/0.9.0/antidote-0.9.0-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/antidote/antidote-0.9.0-cp38-cp38-manylinux2010_x86_64.whl/antidote/_providers/factory.py
@@ -0,0 +1,155 @@
+import weakref
+from typing import Any, Callable, Dict, Hashable, Optional, Union
+
+from .service import Build
+from .._internal import API
+from .._internal.utils import debug_repr, FinalImmutable, SlotRecord
+from ..core import Container, Dependency, DependencyInstance, Provider
+from ..core.utils import DependencyDebug
+
+
+@API.private
+class FactoryProvider(Provider[Hashable]):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__factories: Dict[Hashable, Factory] = dict()
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(factories={self.__factories})"
+
+    def clone(self, keep_singletons_cache: bool) -> 'FactoryProvider':
+        p = FactoryProvider()
+        if keep_singletons_cache:
+            factories = {
+                k: (f.copy() if f.dependency is not None else f)
+                for k, f in self.__factories.items()
+            }
+        else:
+            factories = {
+                k: (f.copy(keep_function=False) if f.dependency is not None else f)
+                for k, f in self.__factories.items()
+            }
+        p.__factories = factories
+        return p
+
+    def exists(self, dependency: Hashable) -> bool:
+        # For now we don't support multiple factories for a single dependency. Neither
+        # is sharing the dependency with another provider. Simply because I don't see a
+        # use case where it would make sense.
+        # Open for discussions though, create an issue if you a use case.
+        if isinstance(dependency, Build):
+            dependency = dependency.dependency
+        return (isinstance(dependency, FactoryDependency)
+                and dependency.dependency in self.__factories)
+
+    def maybe_debug(self, build: Hashable) -> Optional[DependencyDebug]:
+        dependency_factory = build.dependency if isinstance(build, Build) else build
+        if not isinstance(dependency_factory, FactoryDependency):
+            return None
+
+        try:
+            factory = self.__factories[dependency_factory.dependency]
+        except KeyError:
+            return None
+
+        return DependencyDebug(
+            debug_repr(build),
+            singleton=factory.singleton,
+            wired=[factory.function] if factory.dependency is None else [],
+            dependencies=([factory.dependency]
+                          if factory.dependency is not None else []))
+
+    def maybe_provide(self, build: Hashable, container: Container
+                      ) -> Optional[DependencyInstance]:
+        dependency_factory = build.dependency if isinstance(build, Build) else build
+        if not isinstance(dependency_factory, FactoryDependency):
+            return None
+
+        try:
+            factory = self.__factories[dependency_factory.dependency]
+        except KeyError:
+            return None
+
+        if factory.function is None:
+            f = container.provide(factory.dependency)
+            assert f.singleton, "factory dependency is expected to be a singleton"
+            factory.function = f.value
+
+        instance = (factory.function(**build.kwargs)
+                    if isinstance(build, Build) and build.kwargs
+                    else factory.function())
+
+        return DependencyInstance(instance, singleton=factory.singleton)
+
+    def register(self,
+                 dependency: Hashable,
+                 factory: Union[Callable[..., object], Dependency[Hashable]],
+                 singleton: bool = True) -> 'FactoryDependency':
+        # For now we don't support multiple factories for a single dependency.
+        # Simply because I don't see a use case where it would make sense. In
+        # Antidote the standard way would be to use `with_kwargs()` to customization
+        # Open for discussions though, create an issue if you a use case.
+        factory_dependency = FactoryDependency(dependency, weakref.ref(self))
+        self._assert_not_duplicate(factory_dependency)
+
+        if isinstance(factory, Dependency):
+            self.__factories[dependency] = Factory(dependency=factory.value,
+                                                   singleton=singleton)
+        elif callable(factory):
+            self.__factories[dependency] = Factory(singleton=singleton,
+                                                   function=factory)
+        else:
+            raise TypeError(f"factory must be callable, not {type(factory)!r}.")
+
+        return factory_dependency
+
+    def debug_get_registered_factory(self, dependency: Hashable
+                                     ) -> Union[Callable[..., object],
+                                                Dependency[Hashable]]:
+        factory = self.__factories[dependency]
+        if factory.dependency is not None:
+            return Dependency(factory.dependency)
+        else:
+            return factory.function
+
+
+@API.private
+class FactoryDependency(FinalImmutable):
+    __slots__ = ('dependency', '_provider_ref')
+    dependency: Hashable
+    _provider_ref: 'weakref.ReferenceType[FactoryProvider]'
+
+    def __repr__(self) -> str:
+        return f"FactoryDependency({self})"
+
+    def __antidote_debug_repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        provider = self._provider_ref()
+        dependency = debug_repr(self.dependency)
+        if provider is not None:
+            factory = provider.debug_get_registered_factory(self.dependency)
+            return f"{dependency} @ {debug_repr(factory)}"
+        # Should not happen, but we'll try to provide some debug information
+        return f"{dependency} @ ???"  # pragma: no cover
+
+
+@API.private
+class Factory(SlotRecord):
+    __slots__ = ('singleton', 'function', 'dependency')
+    singleton: bool
+    function: Callable[..., object]
+    dependency: Hashable
+
+    def __init__(self,
+                 singleton: bool = True,
+                 function: Callable[..., object] = None,
+                 dependency: Hashable = None):
+        assert function is not None or dependency is not None
+        super().__init__(singleton, function, dependency)
+
+    def copy(self, keep_function: bool = True) -> 'Factory':
+        return Factory(self.singleton,
+                       self.function if keep_function else None,
+                       self.dependency)
---
+++ packages/antidote/antidote-0.9.0-cp38-cp38-manylinux2010_x86_64.whl/antidote/core/utils.py
@@ -0,0 +1,89 @@
+from typing import cast, Generic, Hashable, Sequence, TypeVar
+
+from .._compatibility.typing import final
+from .._internal import API
+from .._internal.utils import FinalImmutable, Immutable
+from .._internal.utils.debug import debug_repr
+from .._internal.utils.immutable import ImmutableGenericMeta
+
+T = TypeVar('T')
+
+
+@API.public
+@final
+class Dependency(Immutable, Generic[T], metaclass=ImmutableGenericMeta):
+    """
+    Used to clearly state that a value should be treated as a dependency and must
+    be retrieved from Antidote. It is recommended to use it through
+    :py:func:`~antidote.world.lazy` as presented:
+
+    .. doctest:: core_Dependency
+
+        >>> from antidote import world
+        >>> world.singletons.add('dependency', 1)
+        >>> world.lazy('dependency')
+        Dependency(value='dependency')
+        >>> # to retrieve the dependency later, you may use get()
+        ... world.lazy[int]('dependency').get()
+        1
+
+    """
+    __slots__ = ('value',)
+    value: Hashable
+    """Dependency to be retrieved"""
+
+    def __init__(self, value: Hashable) -> None:
+        """
+        Args:
+            value: actual dependency to be retrieved later.
+        """
+        super().__init__(value=value)
+
+    def get(self) -> T:
+        """
+        Returns:
+            dependency instance retrieved from :py:mod:`~antidote.world`.
+        """
+        from antidote import world
+        return cast(T, world.get(self.value))
+
+    @API.private
+    def __antidote_debug_repr__(self) -> str:
+        return f"Dependency(value={debug_repr(self.value)})"
+
+
+@API.public
+@final
+class DependencyDebug(FinalImmutable):
+    """
+    Debug information on a dependency. Used by :py:mod:`.world.debug` to provide runtime
+    information for debugging.
+    """
+    __slots__ = ('info', 'singleton', 'wired', 'dependencies')
+    info: str
+    singleton: bool
+    wired: Sequence[object]
+    dependencies: Sequence[Hashable]
+
+    def __init__(self,
+                 info: str,
+                 *,
+                 singleton: bool,
+                 wired: Sequence[object] = tuple(),
+                 dependencies: Sequence[Hashable] = tuple()):
+        """
+        Args:
+            info: Short and concise information on the dependency, just enough to identify
+                clearly which one it is.
+            singleton: Whether the dependency is a singleton or not.
+            wired: Every class or function that may have been wired for this dependency.
+            dependencies: Any transient dependency, so dependencies of this dependency.
+        """
+        super().__init__(info, singleton, wired, dependencies)
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, DependencyDebug) \
+               and self.info == other.info \
+               and self.singleton == other.singleton \
+               and self.wired == other.wired \
+               and self.dependencies == other.dependencies  # noqa: E126

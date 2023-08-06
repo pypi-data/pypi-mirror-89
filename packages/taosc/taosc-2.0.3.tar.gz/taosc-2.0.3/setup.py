63ece409776420cc326f29bf80b5cffbd7b1c76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:05 +0000

    Add package taosc/2.0.3/taosc-2.0.3.tar.gz

---
+++ packages/taosc/taosc-2.0.3.tar.gz/taosc-2.0.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="taosc",
+    version="2.0.3",
+    author="Taosdata Inc.",
+    author_email="support@taosdata.com",
+    description="TDengine python client package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)

77982390601d5a6351ac055acefac58431b8e676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:20 +0000

    Add package az-iranian-bank-gateways/1.4.2a2/az_iranian_bank_gateways-1.4.2a2-py3-none-any.whl

---
+++ packages/az-iranian-bank-gateways/az_iranian_bank_gateways-1.4.2a2-py3-none-any.whl/azbankgateways/apps.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+
+class AZIranianBankGatewaysConfig(AppConfig):
+    name = 'azbankgateways'
+    verbose_name = _('Iranian bank gateway')
+    verbose_name_plural = _('Iranian bank gateways')

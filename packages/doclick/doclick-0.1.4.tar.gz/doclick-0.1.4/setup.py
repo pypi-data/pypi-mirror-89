8a3df4613e3333d2f365e5678797e65ec0a1204d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:01 +0000

    Add package doclick/0.1.4/doclick-0.1.4.tar.gz

---
+++ packages/doclick/doclick-0.1.4.tar.gz/doclick-0.1.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="doclick",
+    version="0.1.4",
+    author="DovaX",
+    author_email="dovax.ai@gmail.com",
+    description="Programmable pseudo-language simulating desktop actions such as clicking, sending keys etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DovaX/doclick",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'pillow',
+          'pynput',
+      ],
+    python_requires='>=3.6',
+    setup_requires=['setuptools_scm'],
+    include_package_data=True,
+)
+
+

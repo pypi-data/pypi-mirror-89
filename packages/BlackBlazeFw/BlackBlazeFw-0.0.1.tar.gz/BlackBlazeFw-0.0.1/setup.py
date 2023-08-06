d0603feacc523a854e0e7e585d6224934ac2e97e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:19 +0000

    Add package BlackBlazeFw/0.0.1/BlackBlazeFw-0.0.1.tar.gz

---
+++ packages/BlackBlazeFw/BlackBlazeFw-0.0.1.tar.gz/BlackBlazeFw-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BlackBlazeFw", # Replace with your own username
+    version="0.0.1",
+    author="Black Blaze",
+    author_email="blckblze@gmail.com",
+    description="A Minimalistic Web Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Black-Blaze/web-fw/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

0cbf700a2a4dc95677e2fd71bc060c22179a62c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:36 +0000

    Add package python-reapy/0.10.0/python-reapy-0.10.0.tar.gz

---
+++ packages/python-reapy/python-reapy-0.10.0.tar.gz/python-reapy-0.10.0/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from os import path, walk, sep
+from typing import Dict, List
+
+import reapy
+
+
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, "README.md")) as f:
+    long_description = f.read()
+
+
+def find_stubs(package: str) -> Dict[str, List[str]]:
+    stubs = []
+    for root, dirs, files in walk(package):
+        for file in files:
+            r_path = path.join(root, file).replace(package + sep, '', 1)
+            stubs.append(r_path)
+    stubs.append('py.typed')
+    return {package: stubs}
+
+
+setup(
+    name="python-reapy",
+    version=reapy.__version__,
+    description="A pythonic wrapper for REAPER's ReaScript Python API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Roméo Després",
+    author_email="mail.reapy@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    keywords="REAPER DAW ReaScript API wrapper music audio",
+    packages=find_packages(exclude=["docs"]),
+    package_data=find_stubs('reapy'),
+    install_requires=[
+        'psutil',
+        'typing_extensions'
+    ],
+    python_requires=">=3.0"
+)

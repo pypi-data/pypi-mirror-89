eccfed88fe79db642141518ee64d0f690c503b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:55 +0000

    Add package quantum-decomp/1.1.0/quantum_decomp-1.1.0-py3-none-any.whl

---
+++ packages/quantum-decomp/quantum_decomp-1.1.0-py3-none-any.whl/quantum_decomp/src/utils.py
@@ -0,0 +1,42 @@
+import math
+import numpy as np
+
+IDENTITY_2x2 = np.eye(2, dtype=np.complex128)
+PAULI_X = np.array([[0, 1], [1, 0]], dtype=np.complex128)
+
+
+def is_orthogonal(Q):
+    n = Q.shape[0]
+    assert Q.shape == (n, n)
+    return is_real(Q) and np.allclose(Q.T @ Q, np.eye(n))
+
+
+def is_unitary(A):
+    n = A.shape[0]
+    if (A.shape != (n, n)):
+        raise ValueError("Matrix is not square.")
+    A = np.array(A)
+    return np.allclose(np.eye(n), A @ A.conj().T)
+
+
+def is_real(x):
+    return np.allclose(np.real(x), x)
+
+
+def is_special_unitary(A):
+    return is_unitary(A) and np.allclose(np.linalg.det(A), 1.0)
+
+
+def is_power_of_two(x):
+    return (x & (x - 1)) == 0 and x != 0
+
+
+def cast_to_real(x):
+    """Converts complex np.array (known to be real) to real dtype."""
+    ans = np.real(x)
+    assert np.allclose(x, ans)
+    return ans
+
+
+def skip_identities(gates):
+    return [gate for gate in gates if not gate.gate2.is_identity()]

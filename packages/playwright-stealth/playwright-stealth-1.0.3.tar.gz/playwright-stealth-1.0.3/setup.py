80f77cc58912f8d5d2e19f4436a85b9c8fe1924e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:18 +0000

    Add package playwright-stealth/1.0.3/playwright-stealth-1.0.3.tar.gz

---
+++ packages/playwright-stealth/playwright-stealth-1.0.3.tar.gz/playwright-stealth-1.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="playwright-stealth",
+    version="1.0.3",
+    author="ASAS1314",
+    author_email="lcjasas@sina.com",
+    description="playwright stealth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ASAS1314/playwright_stealth",
+    license='MIT License',
+    packages=setuptools.find_packages(),
+    package_data={"playwright_stealth": ["js/*.js"]},
+)

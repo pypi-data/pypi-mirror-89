b0ce163f7802ae14edce07f859ddd5ee8825cced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:27 +0000

    Add package ztools/2.1.13/ztools-2.1.13.tar.gz

---
+++ packages/ztools/ztools-2.1.13.tar.gz/ztools-2.1.13/ztools/__init__.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2018-2020 ZouMingzhe <zoumingzhe@qq.com>
+# This module is part of the ztools package, which is released under a MIT licence.
+
+"""
+for database
+"""
+from ztools.db.CRUD             import (CRUD)
+from ztools.db.MySQL            import (MySQL)
+from ztools.db.SQLite           import (SQLite)
+
+"""
+for data structure
+"""
+from ztools.ds.base             import (queue, stack)
+
+
+"""
+for data
+"""
+from ztools.data.filebase       import (filebase)
+from ztools.data.xls            import (xls)
+from ztools.data.hash           import (hash)
+
+
+"""
+for GUI
+"""
+from ztools.GUI.plot            import (plot)
+
+
+"""
+for tool
+"""
+from ztools.tool.progressbar    import (progressbar)
+from ztools.tool.timeout        import (timeout)

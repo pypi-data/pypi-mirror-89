783bbcbe97a375dcffb8d3a81620a447b235a0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:01 +0000

    Add package sampleTowelStuff/0.1.1/sampleTowelStuff-0.1.1.tar.gz

---
+++ packages/sampleTowelStuff/sampleTowelStuff-0.1.1.tar.gz/sampleTowelStuff-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='sampleTowelStuff',
+    version='0.1.1',
+    author='John M. Gabriele',
+    author_email='jmg3000@gmail.com',
+    packages=['towel_stuff', 'towel_stuff.test'],
+    scripts=['bin/stowe-towels.py', 'bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='Useful towel-related stuff.',
+    long_description=open('README.txt').read(),
+)
+
---
+++ packages/sampleTowelStuff/sampleTowelStuff-0.1.1.tar.gz/sampleTowelStuff-0.1.1/towel_stuff/towel_location.py
@@ -0,0 +1,4 @@
+'''Helps you find where your towel is, however remote it may be.'''
+
+def where_is_my_towel():
+    return "on the brilliant marble-sanded beaches of Santraginus V"

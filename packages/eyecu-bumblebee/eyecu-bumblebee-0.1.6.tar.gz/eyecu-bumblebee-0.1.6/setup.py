9dd59cb65ca2bbc806723bf68f67d11e5d6cd8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:13 +0000

    Add package eyecu-bumblebee/0.1.6/eyecu-bumblebee-0.1.6.tar.gz

---
+++ packages/eyecu-bumblebee/eyecu-bumblebee-0.1.6.tar.gz/eyecu-bumblebee-0.1.6/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['eyecu_bumblebee',
+ 'eyecu_bumblebee.video',
+ 'eyecu_bumblebee.video.datasets',
+ 'eyecu_bumblebee.video.enhancers',
+ 'eyecu_bumblebee.video.interfaces',
+ 'eyecu_bumblebee.video.streams',
+ 'eyecu_bumblebee.video.transfomers']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'eyecu-bumblebee',
+    'version': '0.1.6',
+    'description': 'Advanced training methods on videos.',
+    'long_description': None,
+    'author': 'Oguz Vuruskaner',
+    'author_email': 'ovuruska@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

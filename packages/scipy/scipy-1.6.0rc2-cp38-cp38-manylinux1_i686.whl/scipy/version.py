c4cf0053a32048b6d5cef42536f5a021a425cd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:33 +0000

    Add package scipy/1.6.0rc2/scipy-1.6.0rc2-cp38-cp38-manylinux1_i686.whl

---
+++ packages/scipy/scipy-1.6.0rc2-cp38-cp38-manylinux1_i686.whl/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.6.0rc2'
+version = '1.6.0rc2'
+full_version = '1.6.0rc2'
+git_revision = '86ba0d0585dfbc3d9af79655fcc2cc5983a6444e'
+release = True
+
+if not release:
+    version = full_version

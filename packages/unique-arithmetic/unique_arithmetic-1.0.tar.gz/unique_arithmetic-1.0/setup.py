2265d277e64eb6317087d4066a22a28d4bc0d551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:27 +0000

    Add package unique-arithmetic/1.0/unique_arithmetic-1.0.tar.gz

---
+++ packages/unique-arithmetic/unique_arithmetic-1.0.tar.gz/unique_arithmetic-1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md",'r',encoding='utf8') as f:
+    long_description = f.read()
+setuptools.setup(
+    name ="unique_arithmetic",
+    version="1.0",
+    author="中国",
+    author_email="pgacr123@163.com",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages=setuptools.find_packages(),
+    url="https://www.vipcode.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+
+)
---
+++ packages/unique-arithmetic/unique_arithmetic-1.0.tar.gz/unique_arithmetic-1.0/unique_arithmetic/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from .unique_arithmetic import *
+
+name = "unique_arithmetic"
+
+
+
+import unique_arithmetic
+a = unique_arithmetic.unique_arithmetic()
+a.jiafa(55,58)
+a.jianfa(185,774)
+a.chengfa(554,558)
+a.chufa(18.84,3.14)
+a.pi()
+a.micifang(314,8)

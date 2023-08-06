eeb0c873eee531faf9b09fe2d1fcbf2ce5febf37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:53 +0000

    Add package jupytext-ebp/1.7.2a0/jupytext_ebp-1.7.2a0-py3-none-any.whl

---
+++ packages/jupytext-ebp/jupytext_ebp-1.7.2a0-py3-none-any.whl/jupytext/nbextension/Untitled.py
@@ -0,0 +1,32 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:hydrogen
+#     text_representation:
+#       extension: .py
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.3.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+print("A")
+
+# %% [markdown]
+# Hallo
+
+# %%
+a = 1
+a
+
+# %%
+a=1
+
+# %% [markdown]
+# a b
+
+# %%

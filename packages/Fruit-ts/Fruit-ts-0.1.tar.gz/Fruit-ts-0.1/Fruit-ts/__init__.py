5b74cdf84eb6a19d0ca78e4f872954d8bdf98a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:33 +0000

    Add package Fruit-ts/0.1/Fruit-ts-0.1.tar.gz

---
+++ packages/Fruit-ts/Fruit-ts-0.1.tar.gz/Fruit-ts-0.1/Fruit-ts/__init__.py
@@ -0,0 +1,8 @@
+class Fruit_ts:
+    def __init__(self, colour, seeds):
+        self.colour = colour
+        self.seeds = seeds
+        
+    def display(self):
+        print("Colour : ", self.colour, "\nNumber of seeds : ", self.seeds)
+        
---
+++ packages/Fruit-ts/Fruit-ts-0.1.tar.gz/Fruit-ts-0.1/Fruit-ts/python_code.py
@@ -0,0 +1,4 @@
+from Fruit-ts import Fruit_ts
+
+f = Fruit("green", 4)
+f.display()

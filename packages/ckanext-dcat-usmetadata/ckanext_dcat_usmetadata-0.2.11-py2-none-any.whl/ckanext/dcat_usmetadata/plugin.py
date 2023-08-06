c28f9e0590e98055ba3a90bb7c4df63b12967bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:53 +0000

    Add package ckanext-dcat-usmetadata/0.2.11/ckanext_dcat_usmetadata-0.2.11-py2-none-any.whl

---
+++ packages/ckanext-dcat-usmetadata/ckanext_dcat_usmetadata-0.2.11-py2-none-any.whl/ckanext/dcat_usmetadata/plugin.py
@@ -0,0 +1,30 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class Dcat_UsmetadataPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes)
+
+    # IConfigurer
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'dcat_usmetadata')
+
+    # IRoutes
+    def before_map(self, map):
+        controller = 'ckanext.dcat_usmetadata.controller:MetadataController'
+
+        map.connect('/dataset/new-metadata',
+                    controller=controller,
+                    action='new_metadata')
+
+        map.connect('/dataset/edit-new/{id}',
+                    controller=controller,
+                    action='edit_metadata')
+
+        return map
+
+    def after_map(self, map):
+        return map

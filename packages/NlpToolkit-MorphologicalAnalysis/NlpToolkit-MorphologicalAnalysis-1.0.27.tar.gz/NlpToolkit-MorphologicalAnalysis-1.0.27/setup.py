cbea77f99f9ea2170a41257753f768f42c211a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:21 +0000

    Add package NlpToolkit-MorphologicalAnalysis/1.0.27/NlpToolkit-MorphologicalAnalysis-1.0.27.tar.gz

---
+++ packages/NlpToolkit-MorphologicalAnalysis/NlpToolkit-MorphologicalAnalysis-1.0.27.tar.gz/NlpToolkit-MorphologicalAnalysis-1.0.27/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-MorphologicalAnalysis',
+    version='1.0.27',
+    packages=['MorphologicalAnalysis'],
+    url='https://github.com/olcaytaner/TurkishMorphologicalAnalysis-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Turkish Morphological Analysis',
+    install_requires=['NlpToolkit-Dictionary', 'NlpToolkit-Corpus', 'NlpToolkit-DataStructure']
+)

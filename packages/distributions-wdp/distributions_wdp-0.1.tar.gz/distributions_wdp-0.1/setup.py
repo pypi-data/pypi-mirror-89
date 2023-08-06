268ae349a6620d8391e17db667685e21937af675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:06 +0000

    Add package distributions-wdp/0.1/distributions_wdp-0.1.tar.gz

---
+++ packages/distributions-wdp/distributions_wdp-0.1.tar.gz/distributions_wdp-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_wdp',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Welly',
+      zip_safe=False)

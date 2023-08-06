aba3e6fa6f8c37a44c62d26369efa589869c9db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:05 +0000

    Add package runez/2.2.5/runez-2.2.5.tar.gz

---
+++ packages/runez/runez-2.2.5.tar.gz/runez-2.2.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    setup(
+        name="runez",
+        setup_requires="setupmeta",
+        versioning="dev",
+        author="Zoran Simic zoran@simicweb.com",
+        url="https://github.com/codrsquad/runez",
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Operating System :: MacOS",
+            "Operating System :: POSIX",
+            "Operating System :: POSIX :: Linux",
+            "Operating System :: Unix",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Utilities",
+        ],
+    )

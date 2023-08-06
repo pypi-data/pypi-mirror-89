e88191476ec63d6ef7f862a2b259967d3aba6e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:11 +0000

    Add package pysirix/0.3.3/pysirix-0.3.3-py3-none-any.whl

---
+++ packages/pysirix/pysirix-0.3.3-py3-none-any.whl/pysirix/info.py
@@ -0,0 +1,52 @@
+from enum import Enum
+
+try:
+    from dataclasses import dataclass
+
+    @dataclass
+    class TokenData:
+        access_token: str
+        expires_in: int
+        expires_at: int
+        refresh_expires_in: int
+        refresh_token: str
+        token_type: str
+        not_before_policy: int
+        session_state: str
+        scope: str
+
+
+except ImportError:
+
+    class TokenData(dict):
+        def __getattr__(self, item):
+            return self.get(item)
+
+
+class NodeType(Enum):
+    OBJECT = "OBJECT"
+    ARRAY = "ARRAY"
+    OBJECT_KEY = "OBJECT_KEY"
+    OBJECT_STRING_VALUE = "OBJECT_STRING_VALUE"
+    STRING_VALUE = "STRING_VALUE"
+    OBJECT_NUMBER_VALUE = "OBJECT_NUMBER_VALUE"
+    NUMBER_VALUE = "NUMBER_VALUE"
+    OBJECT_BOOLEAN_VALUE = "OBJECT_BOOLEAN_VALUE"
+    BOOLEAN_VALUE = "BOOLEAN_VALUE"
+    OBJECT_NULL_VALUE = "OBJECT_NULL_VALUE"
+    NULL_VALUE = "NULL_VALUE"
+
+
+class InsertPosition(Enum):
+    asFirstChild = "asFirstChild"
+    asLeftSibling = "asLeftSibling"
+    asRightSibling = "asRightSibling"
+    replace = "replace"
+
+
+class DataType(Enum):
+    string = "string"
+    number = "number"
+    boolean = "boolean"
+    null = "null"
+    jsonFragment = "jsonFragment"

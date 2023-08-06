8f91306ee777b92da6d813ea02be463005d14568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:39 +0000

    Add package pyobjc-framework-CryptoTokenKit/7.1/pyobjc-framework-CryptoTokenKit-7.1.tar.gz

---
+++ packages/pyobjc-framework-CryptoTokenKit/pyobjc-framework-CryptoTokenKit-7.1.tar.gz/pyobjc-framework-CryptoTokenKit-7.1/setup.py
@@ -0,0 +1,38 @@
+"""
+Wrappers for the "CryptoTokenKit" framework on macOS.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+
+import os
+
+from pyobjc_setup import Extension, setup
+
+VERSION = '7.1'
+
+setup(
+    name="pyobjc-framework-CryptoTokenKit",
+    description="Wrappers for the framework CryptoTokenKit on macOS",
+    min_os_level="10.10",
+    packages=["CryptoTokenKit"],
+    ext_modules=[
+        Extension(
+            "CryptoTokenKit._CryptoTokenKit",
+            ["Modules/_CryptoTokenKit.m"],
+            extra_link_args=["-framework", "CryptoTokenKit"],
+            py_limited_api=True,
+            depends=[
+                os.path.join("Modules", fn)
+                for fn in os.listdir("Modules")
+                if fn.startswith("_CryptoTokenKit")
+            ],
+        )
+    ],
+    version=VERSION,
+    install_requires=["pyobjc-core>=" + VERSION, "pyobjc-framework-Cocoa>=" + VERSION],
+    long_description=__doc__,
+    options={"bdist_wheel": {"py_limited_api": "cp36"}},
+)

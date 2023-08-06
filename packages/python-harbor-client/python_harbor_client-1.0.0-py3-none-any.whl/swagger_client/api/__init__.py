fc6d12607230e473d8fd93ba383d05b7115189d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:04 +0000

    Add package python-harbor-client/1.0.0/python_harbor_client-1.0.0-py3-none-any.whl

---
+++ packages/python-harbor-client/python_harbor_client-1.0.0-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.artifact_api import ArtifactApi
+from swagger_client.api.auditlog_api import AuditlogApi
+from swagger_client.api.gc_api import GcApi
+from swagger_client.api.icon_api import IconApi
+from swagger_client.api.ping_api import PingApi
+from swagger_client.api.preheat_api import PreheatApi
+from swagger_client.api.project_api import ProjectApi
+from swagger_client.api.replication_api import ReplicationApi
+from swagger_client.api.repository_api import RepositoryApi
+from swagger_client.api.robot_api import RobotApi
+from swagger_client.api.robotv1_api import Robotv1Api
+from swagger_client.api.scan_api import ScanApi
+from swagger_client.api.systeminfo_api import SysteminfoApi

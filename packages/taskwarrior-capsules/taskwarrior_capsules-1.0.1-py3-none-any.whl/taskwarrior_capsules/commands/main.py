398f1be2807ecc7be7ba43e4928f9f2eb4312649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:51 +0000

    Add package taskwarrior-capsules/1.0.1/taskwarrior_capsules-1.0.1-py3-none-any.whl

---
+++ packages/taskwarrior-capsules/taskwarrior_capsules-1.0.1-py3-none-any.whl/taskwarrior_capsules/commands/main.py
@@ -0,0 +1,34 @@
+from taskwarrior_capsules import __version__
+from taskwarrior_capsules.capsule import CommandCapsule
+from taskwarrior_capsules.cmdline import get_installed_capsules
+from taskwarrior_capsules.exceptions import CapsuleError
+
+
+class Capsules(CommandCapsule):
+    """ Capsule management utility """
+    TASKWARRIOR_VERSION_CHECK_NECESSARY = False
+    MIN_VERSION = __version__
+    MAX_VERSION = __version__
+
+    def handle(self, filter_args, extra_args, terminal=None, **kwargs):
+        try:
+            first_arg = extra_args[0].lower()
+        except IndexError:
+            raise CapsuleError("No context command specified")
+
+        if first_arg == 'list':
+            search_list = {
+                'Installed Preprocessors': 'preprocessor',
+                'Installed Commands': 'command',
+                'Installed Postprocessors': 'postprocessor',
+            }
+
+            for headline, variant in search_list.items():
+                print(f'{terminal.bold}{terminal.blue}{headline}{terminal.normal}:')
+
+                for name, module in get_installed_capsules(variant).items():
+                    summary = module.get_summary()
+                    print(f'- {terminal.bold}{name}{terminal.normal}: {summary if summary else "(No docstring)"}')
+                print("")
+        else:
+            raise CapsuleError("Command '%s' is not defined." % first_arg)

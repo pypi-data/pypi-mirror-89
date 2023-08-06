9c495eac0986a0ae1523073bc568273be8056b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:19 +0000

    Add package mapz/1.1.25/mapz-1.1.25.tar.gz

---
+++ packages/mapz/mapz-1.1.25.tar.gz/mapz-1.1.25/mapz/modifiers/__init__.py
@@ -0,0 +1,18 @@
+from .case import to_lowercase as to_lowercase, to_uppercase as to_uppercase
+from .flatten import to_flat as to_flat
+from .table import (
+    to_table as to_table,
+    RowType as RowType,
+    HeaderType as HeaderType,
+    TableType as TableType,
+)
+from .mapping import to_dict as to_dict
+
+
+# __all__ = [
+#     to_lowercase.__name__,
+#     to_uppercase.__name__,
+#     to_flat.__name__,
+#     to_table.__name__,
+#     to_dict.__name__,
+# ]

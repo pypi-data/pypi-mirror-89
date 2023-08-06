840ca34706a9947b928e974a59cb8e89fdeafdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:23 +0000

    Add package python-networkdays/1.0.1/python-networkdays-1.0.1.tar.gz

---
+++ packages/python-networkdays/python-networkdays-1.0.1.tar.gz/python-networkdays-1.0.1/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-networkdays",
+    version="1.0.1",
+
+    author="Carlos Leite",
+    author_email="caduado@gmail.com",
+
+    description="",
+    description_content_type="text/x-rst",
+
+    long_description='',
+    long_description_content_type="text/x-rst",
+
+    keywords=["workdays", "business day", "networkdays", "diasuteis", "calendar", "days", "schedule", "excel", "feriados", "holidays"],
+
+    url="https://github.com/cadu-leite/networkdays",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+
+        "Environment :: Console",
+        "Environment :: Web Environment",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Legal Industry",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Customer Service",
+
+        "License :: OSI Approved :: BSD License",
+
+        "Operating System :: OS Independent",
+
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+
+        "Topic :: Office/Business ",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.0",
+)

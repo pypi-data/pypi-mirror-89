a68592e0959640fcc3124625d0b195f23de13f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:08 +0000

    Add package spaceone-statistics/1.5.3rc1/spaceone_statistics-1.5.3rc1-py3-none-any.whl

---
+++ packages/spaceone-statistics/spaceone_statistics-1.5.3rc1-py3-none-any.whl/spaceone/statistics/conf/global_conf.py
@@ -0,0 +1,35 @@
+DATABASES = {
+    'default': {
+        'db': 'statistics',
+        'host': 'localhost',
+        'port': 27017,
+        'username': '',
+        'password': ''
+    }
+}
+
+CACHES = {
+    'default': {},
+    'local': {
+        'backend': 'spaceone.core.cache.local_cache.LocalCache',
+        'max_size': 128,
+        'ttl': 86400
+    }
+}
+
+HANDLERS = {
+}
+
+CONNECTORS = {
+    'ServiceConnector': {
+    }
+}
+
+ENDPOINTS = {}
+LOG = {}
+QUEUES = {}
+SCHEDULERS = {}
+WORKERS = {}
+TOKEN = ""
+TOKEN_INFO = {}
+CELERY = {}

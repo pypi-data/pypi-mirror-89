ed07c0b5e26b46df6ff8f97df011f9136a555adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:37 +0000

    Add package PyCRC-Hex/1.1.0/PyCRC-Hex-1.1.0.tar.gz

---
+++ packages/PyCRC-Hex/PyCRC-Hex-1.1.0.tar.gz/PyCRC-Hex-1.1.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding:utf8;
+"""
+    * Author:ChenJun
+    * Time:2020/12/24
+    * description:
+"""
+
+import setuptools
+
+setuptools.setup(name='PyCRC-Hex',
+                 packages=setuptools.find_packages(),
+                 version='1.1.0',
+                 description='Python Cyclic Redundancy Check,LRC,BCC',
+                 author='ChenJun0177112278@163.com',
+                 author_email='ChenJun0177112278@163.com',
+                 url='https://github.com/1484076353/PyCRC',
+                 license='MIT',
+                 zip_safe=False)
---
+++ packages/PyCRC-Hex/PyCRC-Hex-1.1.0.tar.gz/PyCRC-Hex-1.1.0/test/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# encoding:utf8;
+"""
+    * 作者：ChenJun
+    * 时间：2020/12/28
+    * 描述：
+"""

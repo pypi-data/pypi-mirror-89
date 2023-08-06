c0b56852ffcbd1448ef3c483a2ec2d2679706756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:14 +0000

    Add package depix/1.0.2/depix-1.0.2.tar.gz

---
+++ packages/depix/depix-1.0.2.tar.gz/depix-1.0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+
+def requirements():
+    requirements_list = []
+
+    with open('requirements.txt') as requirements:
+        for install in requirements:
+            requirements_list.append(install.strip())
+
+    return requirements_list
+
+
+packages = find_packages()
+print(packages)
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+
+setup(
+    name='depix',
+    version='1.0.2',
+    author='beurtschipper',
+    license='CC BY 4.0',
+    url='https://github.com/beurtschipper/Depix',
+    description='Depix is a tool for recovering passwords from pixelized screenshots.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    packages=packages,
+    install_requires=requirements(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    entry_points='''
+        [console_scripts]
+        depix=depix.depix
+    '''
+)

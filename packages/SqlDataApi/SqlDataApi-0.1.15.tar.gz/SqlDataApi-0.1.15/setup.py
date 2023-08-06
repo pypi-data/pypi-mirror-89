177bba1d0ef37769cedda7955794f57776d2a86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:15 +0000

    Add package SqlDataApi/0.1.15/SqlDataApi-0.1.15.tar.gz

---
+++ packages/SqlDataApi/SqlDataApi-0.1.15.tar.gz/SqlDataApi-0.1.15/setup.py
@@ -0,0 +1,34 @@
+"""
+Setup script for sql-data-api-client
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SqlDataApi",
+    version="0.1.15",
+    author="FalconSoft Ltd",
+    author_email="ppaska@falconsoft-ltd.com",
+    description="Python library for Sql Data Api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FalconSoft/sql-data-api-client-Python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+"""
+## to publish new version
+1:/> python setup.py sdist bdist_wheel
+2:/> python -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+
+https://pypi.org/project/SqlDataApi
+
+"""

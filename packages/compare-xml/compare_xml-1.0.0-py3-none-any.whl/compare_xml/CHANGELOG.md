ad5da0b52f95f769e42f0d5f982b038995410069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:05 +0000

    Add package compare-xml/1.0.0/compare_xml-1.0.0-py3-none-any.whl

---
+++ packages/compare-xml/compare_xml-1.0.0-py3-none-any.whl/compare_xml/CHANGELOG.md
@@ -0,0 +1,5 @@
+Changelog
+=========
+
+# 1.0.0
+* Compare XML Elements and lists of XML Elements
---
+++ packages/compare-xml/compare_xml-1.0.0-py3-none-any.whl/compare_xml/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8
+#  compare-xml
+#
+#  Comparator for XML Elements
+#
+#  Copyright (c) 2020 Fabian Fröhlich <compare-xml@f-froehlich.de> <https://projects.f-froehlich.de/compare-xml>
+#
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+#  Checkout this project on github <https://github.com/f-froehlich/compare-xml>
+#  and also my other projects <https://github.com/f-froehlich>

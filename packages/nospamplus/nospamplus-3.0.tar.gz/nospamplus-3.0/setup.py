68fb67679bc38911ca33181cd0a7645e7711de34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:21 +0000

    Add package nospamplus/3.0/nospamplus-3.0.tar.gz

---
+++ packages/nospamplus/nospamplus-3.0.tar.gz/nospamplus-3.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nospamplus",
+    version="3.0",
+    author="Leo",
+    url="http://nospamplus.tk",
+    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    author_email="dev@nospamplus.tk",
+    description="A Python Wrapper For Nospamplus Api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=["requests >= 2.22.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

331bea6e22ae1e97acf813ff09ccf099c17b2db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:23 +0000

    Add package oopschool/0.0.2/oopschool-0.0.2.tar.gz

---
+++ packages/oopschool/oopschool-0.0.2.tar.gz/oopschool-0.0.2/setup.py
@@ -0,0 +1,37 @@
+﻿import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'oopschool',      
+  packages = ['oopschool'], 
+  version = '0.0.2',  
+  license='MIT', 
+  description = 'OOP School Learning by Uncle Engineer',
+  long_description=DESCRIPTION,
+  author = 'Uncle Engineer',                 
+  author_email = 'loong.wissawakorn@gmail.com',     
+  url = 'https://github.com/UncleEngineer/OOPSchool',  
+  download_url = 'https://github.com/UncleEngineer/OOPSchool/archive/v0.0.2.zip',  
+  keywords = ['OOP', 'School', 'Uncle Engineer'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Education',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)

a0dc1b7232a8b1f0d8035105ee50951ddff5ef0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:07 +0000

    Add package ordinal/1.0.3/ordinal-1.0.3.tar.gz

---
+++ packages/ordinal/ordinal-1.0.3.tar.gz/ordinal-1.0.3/ordinal/__init__.py
@@ -0,0 +1,21 @@
+"""Ordinal: get ordinal strings from numbers"""
+
+
+def ordinal(num: int) -> str:
+    """Converts a number into an ordinal string"""
+    if num < 0:
+        raise ValueError('Cannot convert negative numbers to ordinals')
+
+    # special cases
+    if num % 100 in (11, 12, 13):
+        return f'{num}th'
+
+    last_digit = num % 10
+    if last_digit == 1:
+        return f'{num}st'
+    if last_digit == 2:
+        return f'{num}nd'
+    if last_digit == 3:
+        return f'{num}rd'
+
+    return f'{num}th'

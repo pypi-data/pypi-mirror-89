7725af470d87b3d954591918727a2dac46099fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:17 +0000

    Add package nrv/0.0.1/nrv-0.0.1.tar.gz

---
+++ packages/nrv/nrv-0.0.1.tar.gz/nrv-0.0.1/setup.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2020 WeFindX Foundation, CLG.
+# All Rights Reserved.
+
+from setuptools import find_packages, setup
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='nrv',
+    version='0.0.1',
+    description='Network Resource Vocabulary.',
+    long_description=long_description,
+    url='https://gitlab.com/wefindx/nrv',
+    author='Mindey',
+    author_email='mindey@qq.com',
+    license='MIT',
+    zip_safe=False
+)

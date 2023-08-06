96866e04a20ff75338fb28ffc80d4e19eba37ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:08 +0000

    Add package spaceone-identity/1.5.3rc1/spaceone_identity-1.5.3rc1-py3-none-any.whl

---
+++ packages/spaceone-identity/spaceone_identity-1.5.3rc1-py3-none-any.whl/spaceone/identity/model/policy_model.py
@@ -0,0 +1,44 @@
+from mongoengine import *
+from spaceone.core.error import *
+from spaceone.core.model.mongo_model import MongoModel
+
+
+class PolicyTag(EmbeddedDocument):
+    key = StringField(max_length=255)
+    value = StringField(max_length=255)
+
+
+class Policy(MongoModel):
+    policy_id = StringField(max_length=40, generate_id='policy', unique=True)
+    name = StringField(max_length=255, unique_with='domain_id')
+    policy_type = StringField(max_length=20, default='CUSTOM', choices=('CUSTOM', 'MANAGED'))
+    permissions = ListField(StringField())
+    tags = ListField(EmbeddedDocumentField(PolicyTag))
+    domain_id = StringField(max_length=40)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(default=None, null=True)
+
+    meta = {
+        'updatable_fields': [
+            'name',
+            'permissions',
+            'tags',
+            'updated_at'
+        ],
+        'exact_fields': [
+            'policy_id',
+            'policy_type',
+            'domain_id'
+        ],
+        'minimal_fields': [
+            'policy_id',
+            'name'
+        ],
+        'ordering': ['name'],
+        'indexes': [
+            'policy_id',
+            'policy_type',
+            'domain_id',
+            ('tags.key', 'tags.value')
+        ]
+    }

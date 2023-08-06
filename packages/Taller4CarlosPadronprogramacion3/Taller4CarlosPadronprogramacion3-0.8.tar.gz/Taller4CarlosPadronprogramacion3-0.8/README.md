4dc01c62fcdc9c02365163976a84bc3c81a3d2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:22 +0000

    Add package Taller4CarlosPadronprogramacion3/0.8/Taller4CarlosPadronprogramacion3-0.8.tar.gz

---
+++ packages/Taller4CarlosPadronprogramacion3/Taller4CarlosPadronprogramacion3-0.8.tar.gz/Taller4CarlosPadronprogramacion3-0.8/README.md
@@ -0,0 +1,4 @@
+# Codigo de Carlos Padron
+Para taller de Programacion 3
+
+https://gitlab.com/capadron/Taller4CarlosPadronPlacas
---
+++ packages/Taller4CarlosPadronprogramacion3/Taller4CarlosPadronprogramacion3-0.8.tar.gz/Taller4CarlosPadronprogramacion3-0.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Taller4CarlosPadronprogramacion3',
+    version='0.8',
+    packages=[''],
+    url='',
+    license='Publica',
+    author='capadron',
+    author_email='capadron@gmail.com',
+    description='Verificador de Placas Panama'
+)

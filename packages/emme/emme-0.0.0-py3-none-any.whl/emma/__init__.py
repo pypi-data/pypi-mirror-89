963864edc3fe270ad56fdfd342956f2059e75519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:27 +0000

    Add package emme/0.0.0/emme-0.0.0-py3-none-any.whl

---
+++ packages/emme/emme-0.0.0-py3-none-any.whl/emma/__init__.py
@@ -0,0 +1,23 @@
+
+__version__ = "0.0.0"
+
+# __all__ = ['a', 'b', 'c']
+
+
+import emma.analysis
+
+from emma.objects import *
+from emma.matrices import *
+
+from emma.graphics import *
+from emma.solvers import *
+
+import emma.utilities
+import emma.matlib
+
+
+settings = {
+    "DATAFRAME_LATEX": True,
+}
+
+

1d62c53709b995f403f95683aa45041e4a69373f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:14 +0000

    Add package hmeter/1.0.2/hmeter-1.0.2-py3-none-any.whl

---
+++ packages/hmeter/hmeter-1.0.2-py3-none-any.whl/nqueens/test.py
@@ -0,0 +1,27 @@
+from code import *
+
+
+# Test code
+
+qq = Queen(5, "backtrack")
+qq.pprint()
+
+# qq = Queen(algo = "backtrack")
+# qq.pprint()
+
+# qq = Queen(algo = "backtrack")
+# qq.scan_queen("q4.png")
+# qq.pprint()
+
+# qq = Queen(algo = "backtrack")
+# qq.scan_queen("q4.png")
+# qq.pprint()
+# qq.alexa()
+
+# qq = Queen(algo = "backtrack")
+# qq.scan_queen("q6.png")
+# qq.display()
+
+# qq = Queen(algo = "backtrack")
+# qq.scan_queen("q6.png")
+# qq.save()

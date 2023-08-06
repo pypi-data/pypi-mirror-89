86a9a4fdf1f4b968a29d28174cd2a47af5e6e1f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:55 +0000

    Add package digicloud/1.1.2/digicloud-1.1.2.tar.gz

---
+++ packages/digicloud/digicloud-1.1.2.tar.gz/digicloud-1.1.2/digicloud/commands/account.py
@@ -0,0 +1,113 @@
+"""
+    Authentication to digicloud API.
+"""
+import getpass
+
+from cliff.show import ShowOne
+from cliff.command import Command
+
+
+class Login(ShowOne):
+    """Digicloud Login"""
+
+    def get_parser(self, prog_name):
+        parser = super(Login, self).get_parser(prog_name)
+        parser.add_argument(
+            '--email',
+            required=True,
+            metavar='<EMAIL>',
+            dest='email',
+            help=("Your Digicloud email")
+        )
+
+        parser.add_argument(
+            '--password',
+            metavar='<PASSWORD>',
+            required=False,
+            default=None,
+            dest='password',
+            help=("Your Digicloud account password")
+        )
+
+        parser.add_argument(
+            '--namespace',
+            metavar='<NAMESPACE>',
+            dest='namespace',
+            help=("ID of the Digicloud namespace")
+        )
+
+        return parser
+
+    def take_action(self, parsed_args):
+        columns = ('Email', 'Namespace name', 'Namespace ID',)
+        payload = {
+            'email': parsed_args.email,
+            'password': parsed_args.password or getpass.getpass(),
+        }
+        data = self.app.session.post('/tokens', payload)
+
+        selected_ns = None
+        if parsed_args.namespace:
+            selected_ns = self._find_namespace(parsed_args.namespace, data['namespaces'])
+
+        selected_ns = selected_ns or data['namespaces'][0]
+        rows = (parsed_args.email, selected_ns['name'], selected_ns['id'])
+        self.app.config['AUTH_HEADERS'].update({
+            'Authorization': f'Bearer {data["token"]}',
+            'Digicloud-Namespace': selected_ns['id'],
+        })
+        self.app.config['USER'] = data['user']
+        return columns, rows
+
+    def _find_namespace(self, name_or_id, namespaces):
+        result = [ns for ns in namespaces
+                  if name_or_id == ns['name'] or name_or_id == ns['id']]
+        if len(result) == 0:
+            raise Exception(f'No namespace matches {name_or_id}')
+        elif len(result) > 1:
+            raise Exception(f'Multitple namespaces match {name_or_id}')
+        return result[0]
+
+
+class Logout(Command):
+    """DigiCloud Logout"""
+
+    def take_action(self, parsed_args):
+        del self.app.config['AUTH_HEADERS']
+        del self.app.session.headers['Authorization']
+
+        return 0
+
+
+class Register(Command):
+    """Register to Digicloud"""
+    # TODO: Will be removed from cli-client.
+
+    def get_parser(self, prog_name):
+        parser = super(Register, self).get_parser(prog_name)
+        parser.add_argument(
+            '--password',
+            metavar='<PASSWORD>',
+            dest='password',
+            required=True,
+            help=("Your Digicloud account password")
+        )
+
+        parser.add_argument(
+            '--email',
+            metavar='<email>',
+            dest='email',
+            required=True,
+            help=("Your email")
+        )
+
+        return parser
+
+    def take_action(self, parsed_args):
+        payload = {
+            'password': parsed_args.password,
+            'email': parsed_args.email,
+        }
+        self.app.session.post('/users', payload)
+
+        return 0

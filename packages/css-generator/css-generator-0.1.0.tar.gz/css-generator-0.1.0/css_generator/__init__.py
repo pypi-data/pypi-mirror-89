7990791711f130437381a64899091ff5a8a6ce0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:42 +0000

    Add package css-generator/0.1.0/css-generator-0.1.0.tar.gz

---
+++ packages/css-generator/css-generator-0.1.0.tar.gz/css-generator-0.1.0/css_generator/__init__.py
@@ -0,0 +1,7 @@
+from css_generator.ruleset.stylesheet import StyleSheet
+from css_generator.ruleset.rule import Rule
+from css_generator.ruleset.property import Property
+
+from css_generator.styleparser import StyleParser
+
+parser = StyleParser()
---
+++ packages/css-generator/css-generator-0.1.0.tar.gz/css-generator-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='css-generator',
+    version='0.1.0',
+    author='Hugo Paquet',
+    packages=['css_generator'],
+    include_package_data=True,
+    license='MIT',
+    description='',
+    install_requires=[],
+    classifiers=[
+    ],    
+)

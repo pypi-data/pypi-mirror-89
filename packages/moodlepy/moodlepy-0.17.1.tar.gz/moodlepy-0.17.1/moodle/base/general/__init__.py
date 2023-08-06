fcd671df8f4d76d16b0af7aa898e3c35efec491f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:01 +0000

    Add package moodlepy/0.17.1/moodlepy-0.17.1.tar.gz

---
+++ packages/moodlepy/moodlepy-0.17.1.tar.gz/moodlepy-0.17.1/moodle/base/general/__init__.py
@@ -0,0 +1,17 @@
+from .key_message import GeneralKeyMessage
+from .key_value import GeneralKeyValue
+from .name_value import GeneralNameValue
+from .result_error import GeneralResultError
+from .status import GeneralStatus
+from .success import GeneralSuccess
+from .type_value import GeneralTypeValue
+
+__all__ = [
+    'GeneralKeyMessage',
+    'GeneralKeyValue',
+    'GeneralNameValue',
+    'GeneralResultError',
+    'GeneralStatus',
+    'GeneralSuccess',
+    'GeneralTypeValue',
+]

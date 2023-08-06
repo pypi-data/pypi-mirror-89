c868c994e1261dfc46c4594d8175b67ca86e41fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:32 +0000

    Add package Geode-Common/12.11.2/Geode_Common-12.11.2-cp37-cp37m-win_amd64.whl

---
+++ packages/Geode-Common/Geode_Common-12.11.2-cp37-cp37m-win_amd64.whl/geode_common/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019 - 2020 Geode-solutions. All rights reserved.
+#
+
+from .geometry import *
+from .helpers import *
+from .modifier_edged_curve import *
+from .modifier_surface import *
+from .modifier_solid import *
+from .cutter_surface import *
+from .cutter_solid import *

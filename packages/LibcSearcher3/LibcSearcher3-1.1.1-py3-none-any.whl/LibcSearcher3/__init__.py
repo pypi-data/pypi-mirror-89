63e0ed79bf0304fbcddda27e23f4a0dac8aa4baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:03 +0000

    Add package LibcSearcher3/1.1.1/LibcSearcher3-1.1.1-py3-none-any.whl

---
+++ packages/LibcSearcher3/LibcSearcher3-1.1.1-py3-none-any.whl/LibcSearcher3/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+VERSION = 'v1.1.1'
---
+++ packages/LibcSearcher3/LibcSearcher3-1.1.1-py3-none-any.whl/LibcSearcher3/utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import subprocess
+
+from os import path
+
+
+def extract_libc():
+    fn = 'libc.tar.bz2'
+    fb = path.join(path.dirname(path.abspath(__file__)), 'db/')
+    fp = path.join(fb, fn)
+    cmd = ['tar', '-jxf', fp, '-C', fb]
+    proc = subprocess.Popen(cmd)
+    print('[*] init libc database')
+    try:
+        proc.communicate()
+    except Exception as e:
+        print(e)
+
+
+def parse_libc(path_):
+    if not path.exists(path_):
+        print('[-] libc file not exists: ', path_)
+        return
+    cmd = ['scripts/add', path_]
+    proc = subprocess.Popen(cmd)
+    try:
+        proc.communicate()
+    except Exception as e:
+        print(e)

6e41eabfed64823fdab687f45a51f1b5227083e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:37 +0000

    Add package NightWindGameLib/0.0.5/NightWindGameLib-0.0.5.tar.gz

---
+++ packages/NightWindGameLib/NightWindGameLib-0.0.5.tar.gz/NightWindGameLib-0.0.5/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="NightWindGameLib",
+    version="0.0.5",
+    author="Nova_NightWind0311",
+    author_email="",
+    descripiton="Some little games",
+    long_escription_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

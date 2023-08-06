fbabfae5697f7bca51b88817d790ad6c37e00ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:14 +0000

    Add package pywemo/0.5.6/pywemo-0.5.6-py3-none-any.whl

---
+++ packages/pywemo/pywemo-0.5.6-py3-none-any.whl/pywemo/ssdp.py
@@ -0,0 +1,353 @@
+"""Module that implements SSDP protocol."""
+import logging
+import re
+import select
+import socket
+import threading
+import xml.etree.ElementTree as XMLElementTree
+from datetime import datetime, timedelta
+
+import requests
+
+from .util import etree_to_dict, interface_addresses
+
+DISCOVER_TIMEOUT = 5
+
+RESPONSE_REGEX = re.compile(r'\n(.*)\: (.*)\r')
+
+MIN_TIME_BETWEEN_SCANS = timedelta(seconds=59)
+
+# Wemo specific urn:
+ST = "urn:Belkin:service:basicevent:1"
+
+
+class SSDP:
+    """Controls the scanning of uPnP devices and services and caches output."""
+
+    def __init__(self):
+        """Create SSDP object."""
+        self.entries = []
+        self.last_scan = None
+        self._lock = threading.RLock()
+
+    def scan(self):
+        """Scan the network."""
+        with self._lock:
+            self.update()
+
+    def all(self):
+        """
+        Return all found entries.
+
+        Will scan for entries if not scanned recently.
+        """
+        with self._lock:
+            self.update()
+
+            return list(self.entries)
+
+    def find_by_st(self, st):
+        """Return a list of entries that match the ST."""
+        with self._lock:
+            self.update()
+
+            return [entry for entry in self.entries if entry.st == st]
+
+    def find_by_device_description(self, values):
+        """
+        Return a list of entries that match the description.
+
+        Pass in a dict with values to match against the device tag in the
+        description.
+        """
+        with self._lock:
+            self.update()
+
+            return [
+                entry
+                for entry in self.entries
+                if entry.match_device_description(values)
+            ]
+
+    def update(self, force_update=False):
+        """Scan for new uPnP devices and services."""
+        with self._lock:
+            if (
+                self.last_scan is None
+                or force_update
+                or datetime.now() - self.last_scan > MIN_TIME_BETWEEN_SCANS
+            ):
+
+                self.remove_expired()
+
+                self.entries.extend(
+                    entry
+                    for entry in scan() + scan(ST)
+                    if entry not in self.entries
+                )
+
+                self.last_scan = datetime.now()
+
+    def remove_expired(self):
+        """Filter out expired entries."""
+        with self._lock:
+            self.entries = [
+                entry for entry in self.entries if not entry.is_expired
+            ]
+
+
+class UPNPEntry:
+    """Found uPnP entry."""
+
+    DESCRIPTION_CACHE = {'_NO_LOCATION': {}}
+
+    def __init__(self, values):
+        """Create a UPNPEntry object."""
+        self.values = values
+        self.created = datetime.now()
+
+        if 'cache-control' in self.values:
+            cache_seconds = int(self.values['cache-control'].split('=')[1])
+
+            self.expires = self.created + timedelta(seconds=cache_seconds)
+        else:
+            self.expires = None
+
+    @property
+    def is_expired(self):
+        """Return whether the entry is expired or not."""
+        return self.expires is not None and datetime.now() > self.expires
+
+    @property
+    def st(self):
+        """Return ST value."""
+        return self.values.get('st')
+
+    @property
+    def location(self):
+        """Return location value."""
+        return self.values.get('location')
+
+    @property
+    def description(self):
+        """Return the description from the uPnP entry."""
+        url = self.values.get('location', '_NO_LOCATION')
+
+        if url not in UPNPEntry.DESCRIPTION_CACHE:
+            try:
+                for _ in range(3):
+                    try:
+                        xml = requests.get(url, timeout=10).text
+
+                        tree = None
+                        if xml is not None:
+                            tree = XMLElementTree.fromstring(xml)
+
+                        if tree is not None:
+                            UPNPEntry.DESCRIPTION_CACHE[url] = etree_to_dict(
+                                tree
+                            ).get('root', {})
+                        else:
+                            UPNPEntry.DESCRIPTION_CACHE[url] = {}
+                        break
+
+                    except requests.RequestException:
+                        logging.getLogger(__name__).warning(
+                            "Error fetching description at %s", url
+                        )
+                        UPNPEntry.DESCRIPTION_CACHE[url] = {}
+
+            except XMLElementTree.ParseError:
+                # There used to be a log message here to record an error about
+                # malformed XML, but this only happens on non-WeMo devices
+                # and can be safely ignored.
+                UPNPEntry.DESCRIPTION_CACHE[url] = {}
+
+        return UPNPEntry.DESCRIPTION_CACHE[url]
+
+    def match_device_description(self, values):
+        """
+        Fetch description and match against it.
+
+        Values should only contain lowercase keys.
+        """
+        if self.description is None:
+            return False
+
+        device = self.description.get('device')
+
+        if device is None:
+            return False
+
+        return all(val == device.get(key) for key, val in values.items())
+
+    @classmethod
+    def from_response(cls, response):
+        """Create a uPnP entry from a response."""
+        return UPNPEntry(
+            {
+                key.lower(): item
+                for key, item in RESPONSE_REGEX.findall(response)
+            }
+        )
+
+    def __eq__(self, other):
+        """Equality operator."""
+        return (
+            self.__class__ == other.__class__ and self.values == other.values
+        )
+
+    def __repr__(self):
+        """Return the string representation of the object."""
+        return "<UPNPEntry {} - {}>".format(
+            self.values.get('st', ''), self.values.get('location', '')
+        )
+
+
+def build_ssdp_request(ssdp_st, ssdp_mx):
+    """Build the standard request to send during SSDP discovery."""
+    ssdp_st = ssdp_st or ST
+    return "\r\n".join(
+        [
+            'M-SEARCH * HTTP/1.1',
+            'ST: {}'.format(ssdp_st),
+            'MX: {:d}'.format(ssdp_mx),
+            'MAN: "ssdp:discover"',
+            'HOST: 239.255.255.250:1900',
+            '',
+            '',
+        ]
+    ).encode('ascii')
+
+
+def entry_in_entries(entry, entries, mac, serial):
+    """Check if a device entry is in a list of device entries."""
+    # If we don't have a mac or serial, let's just compare objects instead:
+    if mac is None and serial is None:
+        return entry in entries
+
+    for item in entries:
+        if item.description is not None:
+            e_device = item.description.get('device', {})
+            e_mac = e_device.get('macAddress')
+            e_serial = e_device.get('serialNumber')
+        else:
+            e_mac = None
+            e_serial = None
+
+        if e_mac == mac and e_serial == serial and item.st == entry.st:
+            return True
+
+    return False
+
+
+# pylint: disable=too-many-nested-blocks
+def scan(
+    st=None,
+    timeout=DISCOVER_TIMEOUT,
+    max_entries=None,
+    match_mac=None,
+    match_serial=None,
+):
+    """
+    Send a message over the network to discover upnp devices.
+
+    Inspired by Crimsdings
+    https://github.com/crimsdings/ChromeCast/blob/master/cc_discovery.py
+    """
+    ssdp_target = ("239.255.255.250", 1900)
+
+    entries = []
+
+    calc_now = datetime.now
+
+    ssdp_request = build_ssdp_request(st, ssdp_mx=1)
+    sockets = []
+    try:
+        for addr in interface_addresses():
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.bind((addr, 0))
+                s.sendto(ssdp_request, ssdp_target)
+                sockets.append(s)
+            except socket.error:
+                pass
+
+        start = calc_now()
+        while sockets:
+            time_diff = calc_now() - start
+
+            # pylint: disable=maybe-no-member
+            seconds_left = max(timeout - time_diff.seconds, 0)
+
+            ready = select.select(sockets, [], [], min(1, seconds_left))[0]
+            if not ready:
+                # Only check for timeout when there are no more results. Exit
+                # if the time has expired, or probe again if there is more
+                # time remaining.
+                if seconds_left <= 0:
+                    return entries
+                for s in sockets:
+                    s.sendto(ssdp_request, ssdp_target)
+
+            for sock in ready:
+                response = sock.recv(1024).decode("UTF-8", "replace")
+
+                # The device could be slow to respond when fetching the
+                # description. It is possible that fetching the results for a
+                # single device will take longer than the requested timeout.
+                entry = UPNPEntry.from_response(response)
+                if entry.description is not None:
+                    device = entry.description.get('device', {})
+                    mac = device.get('macAddress')
+                    serial = device.get('serialNumber')
+                    services = device.get("serviceList", {}).get("service", [])
+                    service_types = [
+                        service.get("serviceType")
+                        for service in services
+                        if isinstance(service, dict)
+                    ]
+                else:
+                    mac = None
+                    serial = None
+                    service_types = []
+
+                # Search for devices
+                if (
+                    st is not None
+                    or match_mac is not None
+                    or match_serial is not None
+                ):
+                    if not entry_in_entries(entry, entries, mac, serial):
+                        if match_mac is not None:
+                            if match_mac == mac:
+                                entries.append(entry)
+                        elif match_serial is not None:
+                            if match_serial == serial:
+                                entries.append(entry)
+                        elif st is not None:
+                            if st == entry.st or st in service_types:
+                                entries.append(entry)
+                elif not entry_in_entries(entry, entries, mac, serial):
+                    entries.append(entry)
+
+                # Return if we've found the max number of devices
+                if max_entries:
+                    if len(entries) == max_entries:
+                        return entries
+    except socket.error:
+        logging.getLogger(__name__).exception(
+            "Socket error while discovering SSDP devices"
+        )
+    finally:
+        for s in sockets:
+            s.close()
+
+    return entries
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    pprint("Scanning UPNP..")
+    pprint(scan())
---
+++ packages/pywemo/pywemo-0.5.6-py3-none-any.whl/pywemo/subscribe.py
@@ -0,0 +1,288 @@
+"""Module to listen for wemo events."""
+import collections
+import logging
+import sched
+import socket
+import threading
+import time
+from xml.etree import cElementTree
+
+try:
+    import BaseHTTPServer
+except ImportError:
+    import http.server as BaseHTTPServer
+
+import requests
+
+LOG = logging.getLogger(__name__)
+NS = "{urn:schemas-upnp-org:event-1-0}"
+SUCCESS = '<html><body><h1>200 OK</h1></body></html>'
+SUBSCRIPTION_RETRY = 60
+
+
+class SubscriptionRegistryFailed(Exception):
+    """General exceptions related to the subscription registry."""
+
+    pass
+
+
+def get_ip_address(host='1.2.3.4'):
+    """Return IP from hostname or IP."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        sock.connect((host, 9))
+        return sock.getsockname()[0]
+    except socket.error:
+        return None
+    finally:
+        del sock
+
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """Handles subscription responses received from devices."""
+
+    def do_NOTIFY(self):
+        """Handle subscription responses received from devices."""
+        sender_ip, _ = self.client_address
+        outer = self.server.outer
+        device = outer.devices.get(sender_ip)
+        content_len = int(self.headers.get('content-length', 0))
+        data = self.rfile.read(content_len)
+        if device is None:
+            LOG.warning('Received event for unregistered device %s', sender_ip)
+        else:
+            # trim garbage from end, if any
+            data = data.decode("UTF-8").split("\n\n")[0]
+            doc = cElementTree.fromstring(data)
+            for propnode in doc.findall('./{0}property'.format(NS)):
+                for property_ in list(propnode):
+                    text = property_.text
+                    outer.event(device, property_.tag, text)
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(SUCCESS))
+        self.send_header('Connection', 'close')
+        self.end_headers()
+        self.wfile.write(SUCCESS.encode("UTF-8"))
+
+    # pylint: disable=redefined-builtin
+    def log_message(self, format, *args):
+        """Disable error logging."""
+        return
+
+
+class SubscriptionRegistry:
+    """Class for subscribing to wemo events."""
+
+    def __init__(self):
+        """Create the subscription registry object."""
+        self.devices = {}
+        self._callbacks = collections.defaultdict(list)
+        self._exiting = False
+
+        self._event_thread = None
+        self._event_thread_cond = threading.Condition()
+        self._events = {}
+
+        def sleep(secs):
+            with self._event_thread_cond:
+                self._event_thread_cond.wait(secs)
+
+        self._sched = sched.scheduler(time.time, sleep)
+
+        self._http_thread = None
+        self._httpd = None
+        self._port = None
+
+    def register(self, device):
+        """Register a device for subscription updates."""
+        if not device:
+            LOG.error("Called with an invalid device: %r", device)
+            return
+
+        LOG.info("Subscribing to events from %r", device)
+        self.devices[device.host] = device
+
+        with self._event_thread_cond:
+            self._events[device.serialnumber] = self._sched.enter(
+                0, 0, self._resubscribe, [device]
+            )
+            self._event_thread_cond.notify()
+
+    def unregister(self, device):
+        """Unregister a device from subscription updates."""
+        if not device:
+            LOG.error("Called with an invalid device: %r", device)
+            return
+
+        LOG.info("Unsubscribing to events from %r", device)
+
+        with self._event_thread_cond:
+            # Remove any events, callbacks, and the device itself
+            if self._callbacks[device.serialnumber] is not None:
+                del self._callbacks[device.serialnumber]
+            if self._events[device.serialnumber] is not None:
+                del self._events[device.serialnumber]
+            if self.devices[device.host] is not None:
+                del self.devices[device.host]
+
+            self._event_thread_cond.notify()
+
+    def _resubscribe(self, device, sid=None, retry=0):
+        LOG.info("Resubscribe for %s", device)
+        headers = {'TIMEOUT': '300'}
+        if sid is not None:
+            headers['SID'] = sid
+        else:
+            host = get_ip_address(host=device.host)
+            headers.update(
+                {
+                    "CALLBACK": '<http://%s:%d>' % (host, self._port),
+                    "NT": "upnp:event",
+                }
+            )
+        try:
+            # Basic events
+            self._url_resubscribe(
+                device, headers, sid, device.basicevent.eventSubURL
+            )
+            # Insight events
+            # if hasattr(device, 'insight'):
+            #     self._url_resubscribe(
+            #         device, headers, sid, device.insight.eventSubURL)
+
+        except requests.exceptions.RequestException as exc:
+            LOG.warning(
+                "Resubscribe error for %s (%s), will retry in %ss",
+                device,
+                exc,
+                SUBSCRIPTION_RETRY,
+            )
+            retry += 1
+            if retry > 1:
+                # If this wasn't a one-off, try rediscovery
+                # in case the device has changed.
+                if device.rediscovery_enabled:
+                    device.reconnect_with_device()
+            with self._event_thread_cond:
+                self._events[device.serialnumber] = self._sched.enter(
+                    SUBSCRIPTION_RETRY,
+                    0,
+                    self._resubscribe,
+                    [device, sid, retry],
+                )
+
+    def _url_resubscribe(self, device, headers, sid, url):
+        request_headers = headers.copy()
+        response = requests.request(
+            method="SUBSCRIBE", url=url, headers=request_headers
+        )
+        if response.status_code == 412 and sid:
+            # Invalid subscription ID. Send an UNSUBSCRIBE for safety and
+            # start over.
+            requests.request(
+                method='UNSUBSCRIBE', url=url, headers={'SID': sid}
+            )
+            return self._resubscribe(device)
+        timeout = int(
+            response.headers.get('timeout', '1801').replace('Second-', '')
+        )
+        sid = response.headers.get('sid', sid)
+        with self._event_thread_cond:
+            self._events[device.serialnumber] = self._sched.enter(
+                int(timeout * 0.75), 0, self._resubscribe, [device, sid]
+            )
+
+    def event(self, device, type_, value):
+        """Execute the callback for a received event."""
+        LOG.info(
+            "Received event from %s(%s) - %s %s",
+            device,
+            device.host,
+            type_,
+            value,
+        )
+        for type_filter, callback in self._callbacks.get(
+            device.serialnumber, ()
+        ):
+            if type_filter is None or type_ == type_filter:
+                callback(device, type_, value)
+
+    def on(self, device, type_filter, callback):
+        """Add an event callback for a device."""
+        self._callbacks[device.serialnumber].append((type_filter, callback))
+
+    def _find_port(self):
+        """Find a valid open port to run the HTTP server on."""
+        for i in range(0, 128):
+            port = 8989 + i
+            try:
+                self._httpd = BaseHTTPServer.HTTPServer(
+                    ('', port), RequestHandler
+                )
+                self._port = port
+                break
+            except (OSError, socket.error):
+                continue
+
+    def start(self):
+        """Start the subscription registry."""
+        self._port = None
+        self._find_port()
+        if self._port is None:
+            raise SubscriptionRegistryFailed(
+                'Unable to bind a port for listening'
+            )
+        self._http_thread = threading.Thread(
+            target=self._run_http_server, name='Wemo HTTP Thread'
+        )
+        self._http_thread.deamon = True
+        self._http_thread.start()
+
+        self._event_thread = threading.Thread(
+            target=self._run_event_loop, name='Wemo Events Thread'
+        )
+        self._event_thread.deamon = True
+        self._event_thread.start()
+
+    def stop(self):
+        """Shutdown the HTTP server."""
+        self._httpd.shutdown()
+
+        with self._event_thread_cond:
+            self._exiting = True
+
+            # Remove any pending events
+            for event in self._events.values():
+                try:
+                    self._sched.cancel(event)
+                except ValueError:
+                    # event might execute and be removed from queue
+                    # concurrently.  Safe to ignore
+                    pass
+
+            # Wake up event thread if its sleeping
+            self._event_thread_cond.notify()
+        self.join()
+        LOG.info("Terminated threads")
+
+    def join(self):
+        """Block until the HTTP server and event threads have terminated."""
+        self._http_thread.join()
+        self._event_thread.join()
+
+    def _run_http_server(self):
+        """Start the HTTP server."""
+        self._httpd.allow_reuse_address = True
+        self._httpd.outer = self
+        LOG.info("Listening on port %d", self._port)
+        self._httpd.serve_forever()
+
+    def _run_event_loop(self):
+        """Run the event thread loop."""
+        while not self._exiting:
+            with self._event_thread_cond:
+                while not self._exiting and self._sched.empty():
+                    self._event_thread_cond.wait(10)
+            self._sched.run()

088765fa512178f01039696d74fc2608e07d1fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:14 +0000

    Add package toxopy/0.6.3/toxopy-0.6.3-py3-none-any.whl

---
+++ packages/toxopy/toxopy-0.6.3-py3-none-any.whl/toxopy/fwarnings.py
@@ -0,0 +1,14 @@
+"""
+Toxopy (https://github.com/bchaselab/Toxopy)
+© M. Alyetama, University of Nebraska at Omaha
+Licensed under the terms of the MIT license
+"""
+
+import warnings
+
+
+def fwarnings():
+    warnings.filterwarnings("ignore")
+
+
+fwarnings()

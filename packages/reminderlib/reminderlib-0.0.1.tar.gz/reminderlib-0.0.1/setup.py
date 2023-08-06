f830e6038582ab8ff1ef586b272edca694ef900d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:48 +0000

    Add package reminderlib/0.0.1/reminderlib-0.0.1.tar.gz

---
+++ packages/reminderlib/reminderlib-0.0.1.tar.gz/reminderlib-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Education',
+  'Operating System :: OS Independent',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='reminderlib',
+  version='0.0.1',
+  description='sample reminder library for AWS dynamodb',
+  long_description=open('README.MD').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Jayashathiskumar Jayakumar',
+  author_email='x20154810@student.ncirl.ie',
+  classifiers=classifiers,
+  keywords='reminder', 
+  packages=find_packages(),
+  install_requires=[''] 
+)

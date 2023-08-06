5f3bd749a36f7c5510c63e36c79aaafaf7fc4e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:09 +0000

    Add package flytekit/0.15.4/flytekit-0.15.4.tar.gz

---
+++ packages/flytekit/flytekit-0.15.4.tar.gz/flytekit-0.15.4/flytekit/__init__.py
@@ -0,0 +1,19 @@
+import logging as _logging
+
+import flytekit.plugins  # noqa: F401
+
+__version__ = "0.15.4"
+logger = _logging.getLogger("flytekit")
+
+# create console handler and set level to debug
+ch = _logging.StreamHandler()
+ch.setLevel(_logging.DEBUG)
+
+# create formatter
+formatter = _logging.Formatter("%(asctime)s-%(name)s-%(levelname)s$ %(message)s")
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)

c206da6779c0b420f9f635617ccd53d81b8b0766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:30 +0000

    Add package parser201/0.1.6/parser201-0.1.6-py2.py3-none-any.whl

---
+++ packages/parser201/parser201-0.1.6-py2.py3-none-any.whl/parser201/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for parser201."""
+
+__author__  = """Peter Nardi"""
+__email__   = 'pete@nardi.com'
+__version__ = '0.1.6'
+
+from .parser201 import LogParser
+parser201.__module__ = 'LogParser'

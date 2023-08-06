ebb1b8f151b4ab2ea1ef1f1ece0b8e973cabac84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:08 +0000

    Add package dynamo-dao/0.0.4/dynamo-dao-0.0.4.tar.gz

---
+++ packages/dynamo-dao/dynamo-dao-0.0.4.tar.gz/dynamo-dao-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import pathlib
+from setuptools import setup  # type: ignore
+
+
+setup(
+    name="dynamo-dao",
+    version="0.0.4",
+    description="Dynamo Dao",
+    author="Quinn Weber",
+    maintainer="Quinn Weber",
+    maintainer_email="quinn@quinnweber.com",
+    license="MIT",
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={"dynamo_dao": ["py.typed"]},
+    packages=["dynamo_dao"],
+    package_dir={"": "src"},
+    install_requires=["Boto3"],
+)

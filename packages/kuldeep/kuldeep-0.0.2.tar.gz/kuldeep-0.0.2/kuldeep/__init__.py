cb2e7aff90fa08131055c14dc44f1beea5c1f7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:05 +0000

    Add package kuldeep/0.0.2/kuldeep-0.0.2.tar.gz

---
+++ packages/kuldeep/kuldeep-0.0.2.tar.gz/kuldeep-0.0.2/kuldeep/__init__.py
@@ -0,0 +1,32 @@
+r"""
+
+"""
+
+import django
+
+__title__ = 'Django kuldeep'
+__version__ = '0.0.2'
+__author__ = 'kuldeep khatana'
+__license__ = 'BSD 3-Clause'
+__copyright__ = 'Copyright 2011-2019 Encode OSS Ltd'
+
+# Version synonym
+VERSION = __version__
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = 'iso-8859-1'
+
+# Default datetime input and output formats
+ISO_8601 = 'iso-8601'
+
+
+if django.VERSION < (3, 2):
+    default_app_config = 'kuldeep.apps.kuldeepConfig'
+
+
+class RemovedInDRF313Warning(DeprecationWarning):
+    pass
+
+
+class RemovedInDRF314Warning(PendingDeprecationWarning):
+    pass

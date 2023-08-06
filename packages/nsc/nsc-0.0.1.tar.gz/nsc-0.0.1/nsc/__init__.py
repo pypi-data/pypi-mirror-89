92886dd41af0cf284be2323e042d35df567795bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:40 +0000

    Add package nsc/0.0.1/nsc-0.0.1.tar.gz

---
+++ packages/nsc/nsc-0.0.1.tar.gz/nsc-0.0.1/nsc/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# ============================================================================
+#     Deep Time-Series Very Early Alpha v0.0.1
+#     Python v3.6+
+#     Created by [TBD]
+#     Licensed under Apache License v2
+# ============================================================================
+
+# Version
+from .__version__ import __version__
+# Endpoint functions for users
+from .run import run_package as run
+from .utils import load_data as load, clean_data as clean
+
+
+if __name__ == "__main__":
+    version_number = __version__
+    print(f"Running NSC v{version_number}. Execute package by "
+          "calling dnntime.run(config) with your provided config YAML file.")
+else:
+    version_number = __version__
+    print(f"Importing NSC v{version_number}. Execute package by "
+          "calling dnntime.run(config) with your provided config YAML file.")
---
+++ packages/nsc/nsc-0.0.1.tar.gz/nsc-0.0.1/nsc/__version__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Specifies the version of the Nonlinear-Statistical-Coupling package."""
+
+__title__ = 'nsc'
+__author__ = '[TBD]'
+__description__ = '[TBD]'
+__url__ = 'https://github.com/kenricnelson/Nonlinear-Statistical-Coupling.git'
+__version__ = '0.0.1'
+__license__ = 'Apache License 2.0'
+__copyright__ = '2020 [TBD]'

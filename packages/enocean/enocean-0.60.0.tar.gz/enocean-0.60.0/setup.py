ab9cb1bc474e6309983ba69c302d5539a3836c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:27 +0000

    Add package enocean/0.60.0/enocean-0.60.0.tar.gz

---
+++ packages/enocean/enocean-0.60.0.tar.gz/enocean-0.60.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='enocean',
+    version='0.60.0',
+    description='EnOcean serial protocol implementation',
+    author='Kimmo Huoman',
+    author_email='kipenroskaposti@gmail.com',
+    url='https://github.com/kipe/enocean',
+    packages=[
+        'enocean',
+        'enocean.protocol',
+        'enocean.communicators',
+    ],
+    scripts=[
+        'examples/enocean_example.py',
+    ],
+    package_data={
+        '': ['EEP.xml']
+    },
+    install_requires=[
+        'enum-compat>=0.0.2',
+        'pyserial>=3.0',
+        'beautifulsoup4>=4.3.2',
+    ])

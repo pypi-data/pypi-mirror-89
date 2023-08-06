3381f61ba5f707862bd856de695c8b1f3c4c1de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:52 +0000

    Add package iterators/0.0.2/iterators-0.0.2.tar.gz

---
+++ packages/iterators/iterators-0.0.2.tar.gz/iterators-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="iterators",
+    packages = ['iterators'],
+    version="0.0.2",
+    author="leangaurav",
+    author_email="leangaurav.me@gmail.com",
+    description="Iterator utility classes and functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leangaurav/pypi_iterator",
+    download_url="https://github.com/leangaurav/pypi_iterator/archive/v_0.0.2.tar.gz",
+    keywords = ['ITERATOR', 'TIMEOUT', 'SYNC'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6',
+)

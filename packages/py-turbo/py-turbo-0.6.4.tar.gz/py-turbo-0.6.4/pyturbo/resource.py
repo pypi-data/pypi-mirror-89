263f56064e466f18f3e4a4d2d0c92f989e4b6adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:44 +0000

    Add package py-turbo/0.6.4/py-turbo-0.6.4.tar.gz

---
+++ packages/py-turbo/py-turbo-0.6.4.tar.gz/py-turbo-0.6.4/pyturbo/resource.py
@@ -0,0 +1,85 @@
+from copy import deepcopy
+from typing import Any, Dict, List, Optional
+
+import GPUtil
+import psutil
+
+
+class Resources(object):
+
+    '''
+    Resource management and allocation.
+    '''
+
+    def __init__(self, resources: Dict[str, List[Any]] = None, **scan_args):
+        if resources is not None:
+            self.resources = resources
+        else:
+            self.resources = self.scan(**scan_args)
+
+    def __add__(self, other):
+        new_resources = deepcopy(self.resources)
+        for key, value in other.resources.items():
+            new_resources.setdefault(key, []).extend(value)
+        return self.__class__(new_resources)
+
+    def get(self, name: str, limit: Optional[int] = None):
+        return self.resources.get(name, [])[:limit]
+
+    def scan(self, gpu_load: float = 0.1, gpu_memory: float = 0.1):
+        '''
+        Initial global scan for a system.
+        '''
+        resources = {'cpu': psutil.Process().cpu_affinity()}
+        gpus = GPUtil.getAvailable(
+            limit=100, maxLoad=gpu_load, maxMemory=gpu_memory)
+        if len(gpus) > 0:
+            resources['gpu'] = gpus
+        return resources
+
+    def split(self, num_split: int):
+        '''
+        Even split between pipelines in a system, drop remainders.
+        '''
+        results = [{} for _ in range(num_split)]
+        for key, value in self.resources.items():
+            split_size = len(value) // num_split
+            for split_i in range(num_split):
+                results[split_i][key] = value[
+                    split_i * split_size: (split_i + 1) * split_size]
+        results = [Resources(r) for r in results]
+        return results
+
+    def select(self, **select_dict):
+        '''
+        Unbalanced select for stages in a pipeline.
+        select_dict value: True for all, False for None, 
+            (start, end) in int/float/None for slice
+        '''
+        selected_resources = {}
+        for key, value in select_dict.items():
+            if not key in self.resources:
+                continue
+            resource = self.resources[key]
+            if value is True:
+                selected_resources[key] = resource
+            elif value is False:
+                continue
+            else:
+                start, end = self._index_or_frac(value, len(resource))
+                selected_resources[key] = resource[start:end]
+        return Resources(selected_resources)
+
+    @staticmethod
+    def _index_or_frac(values, length):
+        is_float = any([isinstance(value, float) for value in values])
+        if not is_float:
+            return values
+        return [int(round(value * length)) if value is not None else None
+                for value in values]
+
+    def __str__(self):
+        return ', '.join(['%s=%s' % (k, v) for k, v in self.resources.items()])
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, str(self))
---
+++ packages/py-turbo/py-turbo-0.6.4.tar.gz/py-turbo-0.6.4/pyturbo/utils.py
@@ -0,0 +1,49 @@
+import logging
+import multiprocessing
+from typing import Iterable, Optional
+
+import psutil
+from tqdm.autonotebook import tqdm
+
+from .runtime import Options
+
+
+def progressbar(iterable: Optional[Iterable] = None, desc: Optional[str] = None,
+                total: Optional[int] = None, *, silent: bool = False,
+                **kwargs):
+    if not silent:
+        identity = multiprocessing.current_process()._identity
+        if 'position' not in kwargs and len(identity):
+            kwargs['position'] = identity[0]
+        return tqdm(iterable, desc, total, dynamic_ncols=True, **kwargs)
+    return iterable
+
+
+def get_logger(name: str, level: Optional[int] = None,
+               log_file: Optional[str] = None):
+    if level is None:
+        level = logging.INFO if not Options.print_debug_log else logging.DEBUG
+    if log_file is None:
+        log_file = Options.log_file
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    if len(logger.handlers) > 0:
+        return logger
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)s %(levelname)s %(message)s')
+    handlers = [logging.StreamHandler()]
+    if log_file is not None:
+        handlers.append(logging.FileHandler(log_file))
+    for handler in handlers:
+        handler.setLevel(level)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
+
+
+def process_map(worker, jobs, num_worker=None):
+    if num_worker is None:
+        num_worker = len(psutil.Process().cpu_affinity())
+    with multiprocessing.Pool(num_worker) as pool:
+        yield from progressbar(
+            pool.imap_unordered(worker, jobs), 'Jobs', len(jobs))

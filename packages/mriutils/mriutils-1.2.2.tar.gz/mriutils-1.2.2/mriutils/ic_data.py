7d6c08f78bb2e015968489d3c13c5450240cd27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:00 +0000

    Add package mriutils/1.2.2/mriutils-1.2.2.tar.gz

---
+++ packages/mriutils/mriutils-1.2.2.tar.gz/mriutils-1.2.2/mriutils/ic_data.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 14 22:42:15 2020
+
+@author: kuangmeng
+"""
+import os
+import h5py
+import numpy as np
+
+class LoadH5():
+    def __init__(self, data_dir, save_path):
+        self.data_dir = data_dir
+        self.data = {'data':[], 'label': []}
+        self.save_path = save_path
+        
+    
+    def readSingleH5(self, item_path):
+        f = h5py.File(item_path, 'r')
+        # keyset = f.keys() #get set of cine slice names enclosed in the file
+        # keylist = list(f) #get list of cine slice names enclosed in the file
+        key = 'MID' #define the name of the cine slice requested
+        if key not in f.keys():
+            print(item_path)
+            return [], []
+        img = f[key]['image'][:] #get the 50*512*512*4 image array data as img
+        # the label data will be a float array. You may want to normalise the data before use
+        lbl = f[key]['label'][:] #get the 50*512*512 label array data as lbl
+        # the label data will be a binary (0/1) float array
+        f.close()
+        return img, lbl
+    
+    def read(self):
+        dir_list = os.listdir(self.data_dir)
+        for item_path in dir_list:
+            if os.path.exists(os.path.join(self.data_dir, item_path)) and '.h5' in item_path:
+                img, lbl = self.readSingleH5(os.path.join(self.data_dir, item_path))
+                if len(img) > 0 and len(lbl) > 0:
+                    self.data['data'].append(img)
+                    self.data['label'].append(lbl)
+        return self.data
+    
+    def save(self):
+        np.save(os.path.join(self.save_path, 'ic_data'), self.data['data'])
+        np.save(os.path.join(self.save_path, 'ic_label'), self.data['label'])
+
+    
+if __name__ == '__main__':
+    lh5 = LoadH5('/Users/kuangmeng/Documents/SpyderProjects/mengutils/mengutils', './')
+    data = lh5.read()
+    print(data['data'][0].shape)
+    lh5.save()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

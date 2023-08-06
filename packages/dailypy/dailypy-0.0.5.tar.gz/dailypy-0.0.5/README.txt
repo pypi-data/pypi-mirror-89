fce29b6dd09cbb63378fdce3d89d2d59fc10af4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:34 +0000

    Add package dailypy/0.0.5/dailypy-0.0.5.tar.gz

---
+++ packages/dailypy/dailypy-0.0.5.tar.gz/dailypy-0.0.5/README.txt
@@ -0,0 +1,14 @@
+Useful Python Functions used almost everyday.
+
+# Install 
+
+```bash
+pip install dailypy
+```
+
+# Useage
+
+```python
+import dailypy as dp
+time = dp.sm.timeString()
+```
---
+++ packages/dailypy/dailypy-0.0.5.tar.gz/dailypy-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='dailypy',
+  version='0.0.5',
+  description='A useful library used everyday',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Jack Huang',
+  author_email='jackhuang.wz@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Tools', 
+  packages=find_packages(),
+  install_requires=['numpy'] 
+)
+

d26b4012db78a5467e8210143fb30933062aa3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:28 +0000

    Add package csverve/0.1.1/csverve-0.1.1-py2.py3-none-any.whl

---
+++ packages/csverve/csverve-0.1.1-py2.py3-none-any.whl/csverve/__init__.py
@@ -0,0 +1,26 @@
+"""Top-level package for csverve."""
+
+
+__author__ = """Shah Lab"""
+__email__ = 'todo@todo.com'
+__version__ = '0.1.0'
+
+
+# csverve methods
+from csverve.csverve import add_col_from_dict
+from csverve.csverve import annotate_csv
+from csverve.csverve import concatenate_csv
+from csverve.csverve import concatenate_csv_files_pandas
+from csverve.csverve import concatenate_csv_files_quick_lowmem
+from csverve.csverve import get_metadata
+from csverve.csverve import merge_csv
+from csverve.csverve import merge_dtypes
+from csverve.csverve import merge_frames
+from csverve.csverve import read_csv_and_yaml
+from csverve.csverve import rewrite_csv_file
+from csverve.csverve import write_dataframe_to_csv_and_yaml
+from csverve.csverve import write_metadata
+
+# extras methods
+from csverve.extras import union_categories
+from csverve.extras import concatenate_with_categories

ef85a70accbe09aee2f4103ebf114b1b49604280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:44 +0000

    Add package trajectory-analysis-tools/0.3.0.dev0/trajectory_analysis_tools-0.3.0.dev0.tar.gz

---
+++ packages/trajectory-analysis-tools/trajectory_analysis_tools-0.3.0.dev0.tar.gz/trajectory_analysis_tools-0.3.0.dev0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = ['numpy', 'scipy', 'networkx']
+TESTS_REQUIRE = ['pytest >= 2.7.1']
+
+setup(
+    name='trajectory_analysis_tools',
+    version='0.3.0.dev0',
+    license='MIT',
+    description=(''),
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+)

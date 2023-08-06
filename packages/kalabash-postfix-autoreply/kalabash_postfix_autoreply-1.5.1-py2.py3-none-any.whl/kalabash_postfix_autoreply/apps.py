1b8ef47963dd4587bfe429a1785eb0255bb31901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:14 +0000

    Add package kalabash-postfix-autoreply/1.5.1/kalabash_postfix_autoreply-1.5.1-py2.py3-none-any.whl

---
+++ packages/kalabash-postfix-autoreply/kalabash_postfix_autoreply-1.5.1-py2.py3-none-any.whl/kalabash_postfix_autoreply/apps.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""AppConfig for kalabash_postfix_autoreply."""
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class PostfixAutoreplyConfig(AppConfig):
+
+    """App configuration."""
+
+    name = "kalabash_postfix_autoreply"
+    verbose_name = "Auto-reply functionality using Postfix"
+
+    def ready(self):
+        from . import handlers  # NOQA:F401
---
+++ packages/kalabash-postfix-autoreply/kalabash_postfix_autoreply-1.5.1-py2.py3-none-any.whl/kalabash_postfix_autoreply/viewsets.py
@@ -0,0 +1,56 @@
+"""Autoreply viewsets."""
+
+from django_filters import rest_framework as filters
+from rest_framework import mixins, permissions, viewsets
+
+from kalabash.admin import models as admin_models
+from kalabash.lib.email_utils import split_mailbox
+
+from . import models
+from . import serializers
+
+
+class ARMessageFilterSet(filters.FilterSet):
+    """Filter set for ARmessage."""
+
+    mbox = filters.CharFilter(method="filter_mbox")
+
+    class Meta:
+        fields = ("mbox", "mbox__user")
+        model = models.ARmessage
+
+    def filter_mbox(self, queryset, name, value):
+        address, domain = split_mailbox(value)
+        if address:
+            queryset = queryset.filter(mbox__address__icontains=address)
+        if domain:
+            queryset = queryset.filter(mbox__domain__name__icontains=domain)
+        return queryset
+
+
+class ARMessageViewSet(
+        mixins.CreateModelMixin,
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        viewsets.GenericViewSet):
+    """A viewset for ARmessage."""
+
+    filter_backends = (filters.DjangoFilterBackend, )
+    filterset_class = ARMessageFilterSet
+    permission_classes = (permissions.IsAuthenticated, )
+    queryset = models.ARmessage.objects.select_related(
+        "mbox__domain", "mbox__user")
+    serializer_class = serializers.ARMessageSerializer
+
+    def get_queryset(self):
+        """Filter queryset based on current user."""
+        qset = super(ARMessageViewSet, self).get_queryset()
+        role = self.request.user.role
+        if role == "StandardUsers":
+            qset = qset.filter(mbox=self.request.user.mailbox)
+        elif role in ["DomainAdmins", "Resellers"]:
+            mailboxes = admin_models.Mailbox.objects.get_for_admin(
+                self.request.user)
+            qset = qset.filter(mbox__in=mailboxes)
+        return qset

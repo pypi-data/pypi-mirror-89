c0c8570f08b1b8f2135d933582947a07c1dd6530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:24 +0000

    Add package adlfs/0.5.9/adlfs-0.5.9.tar.gz

---
+++ packages/adlfs/adlfs-0.5.9.tar.gz/adlfs-0.5.9/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+import versioneer
+
+
+setup(
+    name="adlfs",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Access Azure Datalake Gen1 with fsspec and dask",
+    url="https://github.com/hayesgb/adlfs/",
+    maintainer="Greg Hayes",
+    maintainer_email="hayesgb@gmail.com",
+    license="BSD",
+    keywords=["file-system", "dask", "azure"],
+    packages=["adlfs"],
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read() if exists("README.md") else "",
+    install_requires=[
+        "aiohttp",
+        "azure-core>=1.5.0",
+        "azure-datalake-store>=0.0.46,<0.1",
+        "azure-identity",
+        "azure-storage-blob>=12.5.0",
+        "fsspec>=0.8.0",
+        "msrestazure",
+        "requests>=2.22.0,<3.0",
+    ],
+    tests_require=["pytest>5.0,<6.0", "docker"],
+    zip_safe=False,
+)

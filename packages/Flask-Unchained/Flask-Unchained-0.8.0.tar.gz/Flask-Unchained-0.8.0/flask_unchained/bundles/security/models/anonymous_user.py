8305700fc1f821d939942e2dd72eb736ce65944b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:16 +0000

    Add package Flask-Unchained/0.8.0/Flask-Unchained-0.8.0.tar.gz

---
+++ packages/Flask-Unchained/Flask-Unchained-0.8.0.tar.gz/Flask-Unchained-0.8.0/flask_unchained/bundles/security/models/anonymous_user.py
@@ -0,0 +1,20 @@
+from flask_login import AnonymousUserMixin
+from werkzeug.datastructures import ImmutableList
+
+
+class AnonymousUser(AnonymousUserMixin):
+    _sa_instance_state = None
+
+    def __init__(self):
+        self.roles = ImmutableList()
+
+    @property
+    def id(self):
+        return None
+
+    @property
+    def active(self):
+        return False
+
+    def has_role(self, *args):
+        return False

92be8921e22fbe5f660588536d21788fa3c4dd05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:23 +0000

    Add package Poutyne/1.2/Poutyne-1.2.tar.gz

---
+++ packages/Poutyne/Poutyne-1.2.tar.gz/Poutyne-1.2/poutyne/framework/callbacks/callbacks.py
@@ -0,0 +1,273 @@
+"""
+The source code of this file was copied from the Keras project, and has been modified. All modifications
+made from the original source code are under the LGPLv3 license.
+
+COPYRIGHT
+
+All contributions by François Chollet:
+Copyright (c) 2015, François Chollet.
+All rights reserved.
+
+All contributions by Google:
+Copyright (c) 2015, Google, Inc.
+All rights reserved.
+
+All contributions by Microsoft:
+Copyright (c) 2017, Microsoft, Inc.
+All rights reserved.
+
+All other contributions:
+Copyright (c) 2015 - 2017, the respective contributors.
+All rights reserved.
+
+Each contributor holds copyright over their respective contributions. The project versioning (Git)
+records all such contribution source information.
+
+LICENSE
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+import warnings
+from typing import Dict, List
+
+
+class Callback:
+    """
+    Attributes:
+        params (dict): Contains 'epoch' and 'steps_per_epoch' keys which are passed to the
+            :func:`Model.fit() <poutyne.Model.fit>` function. It may contain other keys.
+        model (Model): A reference to the :class:`~poutyne.Model` object which is using the callback.
+    """
+
+    def __init__(self):
+        self.model = None
+        self.params = None
+
+    def set_params(self, params: Dict):
+        self.params = params
+
+    def set_model(self, model):
+        self.model = model
+
+    def on_epoch_begin(self, epoch_number: int, logs: Dict):
+        """
+        Is called before the beginning of each epoch.
+
+        Args:
+            epoch_number (int): The epoch number.
+            logs (dict): Usually an empty dict.
+        """
+        pass
+
+    def on_epoch_end(self, epoch_number: int, logs: Dict):
+        """
+        Is called before the end of each epoch.
+
+        Args:
+            epoch_number (int): The epoch number.
+            logs (dict): Contains the following keys:
+
+                 * 'epoch': The epoch number.
+                 * 'loss': The average loss of the batches.
+                 * 'time': The computation time of the epoch.
+                 * Other metrics: One key for each type of metrics. The metrics are also averaged.
+                 * val_loss': The average loss of the batches on the validation set.
+                 * Other metrics: One key for each type of metrics on the validation set. The metrics are also averaged.
+
+        Example::
+
+            logs = {'epoch': 6, 'time': 3.141519837, 'loss': 4.34462, 'accuracy': 0.766,
+                    'val_loss': 5.2352, 'val_accuracy': 0.682}
+        """
+        pass
+
+    def on_train_batch_begin(self, batch_number: int, logs: Dict):
+        """
+        Is called before the beginning of the training batch.
+
+        Args:
+            batch_number (int): The batch number.
+            logs (dict): Usually an empty dict.
+        """
+        pass
+
+    def on_train_batch_end(self, batch_number: int, logs: Dict):
+        """
+        Is called before the end of the training batch.
+
+        Args:
+            batch_number (int): The batch number.
+            logs (dict): Usually an empty dict.
+        """
+        pass
+
+    def on_test_batch_begin(self, batch_number: int, logs: Dict):
+        """
+        Is called before the beginning of the testing batch.
+
+        Args:
+            batch_number (int): The batch number.
+            logs (dict): Usually an empty dict.
+        """
+        pass
+
+    def on_test_batch_end(self, batch_number: int, logs: Dict):
+        """
+        Is called before the end of the testing batch.
+
+        Args:
+            batch_number (int): The batch number.
+            logs (dict): Usually an empty dict.
+        """
+        pass
+
+    def on_train_begin(self, logs: Dict):
+        """
+        Is called before the beginning of the training.
+
+        Args:
+            logs (dict): Usually an empty dict.
+        """
+        pass
+
+    def on_train_end(self, logs: Dict):
+        """
+        Is called before the end of the training.
+
+        Args:
+            logs (dict): Usually an empty dict.
+        """
+        pass
+
+    def on_test_begin(self, logs: Dict):
+        """
+        Is called before the beginning of the testing.
+
+        Args:
+            logs (dict): Usually an empty dict.
+        """
+        pass
+
+    def on_test_end(self, logs: Dict):
+        """
+        Is called before the end of the testing.
+
+        Args:
+            logs (dict): Usually an empty dict.
+        """
+        pass
+
+    def on_backward_end(self, batch_number: int):
+        """
+        Is called after the backpropagation but before the optimization step.
+
+        Args:
+            batch_number (int): The batch number.
+        """
+        pass
+
+
+class CallbackList:
+
+    def __init__(self, callbacks: List[Callback]):
+        callbacks = callbacks or []
+        self.callbacks = list(callbacks)
+
+    def append(self, callback: Callback):
+        self.callbacks.append(callback)
+
+    def set_params(self, params: Dict):
+        for callback in self.callbacks:
+            callback.set_params(params)
+
+    def set_model(self, model):
+        for callback in self.callbacks:
+            callback.set_model(model)
+
+    def on_epoch_begin(self, epoch_number: int, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_epoch_begin(epoch_number, logs)
+
+    def on_epoch_end(self, epoch_number: int, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_epoch_end(epoch_number, logs)
+
+    def on_train_batch_begin(self, batch_number: int, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            if hasattr(callback, 'on_batch_begin'):
+                warnings.warn(
+                    'on_batch_begin method for callback has been deprecated as of version 0.7. '
+                    'Use on_batch_train_begin instead.',
+                    Warning,
+                    stacklevel=2)
+                callback.on_batch_begin(batch_number, logs)
+            else:
+                callback.on_train_batch_begin(batch_number, logs)
+
+    def on_train_batch_end(self, batch_number: int, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            if hasattr(callback, 'on_batch_end'):
+                warnings.warn(
+                    'on_batch_end method for callback has been deprecated as of version 0.7. '
+                    'Use on_batch_train_end instead.',
+                    Warning,
+                    stacklevel=2)
+                callback.on_batch_end(batch_number, logs)
+            else:
+                callback.on_train_batch_end(batch_number, logs)
+
+    def on_test_batch_begin(self, batch_number: int, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_test_batch_begin(batch_number, logs)
+
+    def on_test_batch_end(self, batch_number: int, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_test_batch_end(batch_number, logs)
+
+    def on_train_begin(self, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_train_begin(logs)
+
+    def on_train_end(self, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_train_end(logs)
+
+    def on_test_begin(self, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_test_begin(logs)
+
+    def on_test_end(self, logs: Dict):
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_test_end(logs)
+
+    def on_backward_end(self, batch_number: int):
+        for callback in self.callbacks:
+            callback.on_backward_end(batch_number)
+
+    def __iter__(self):
+        return iter(self.callbacks)
---
+++ packages/Poutyne/Poutyne-1.2.tar.gz/Poutyne-1.2/poutyne/framework/callbacks/progress.py
@@ -0,0 +1,82 @@
+import itertools
+from typing import Dict
+
+from .callbacks import Callback
+from .color_formatting import ColorProgress
+
+
+class ProgressionCallback(Callback):
+    """
+    Default progression callback used in :class:`~poutyne.Model`. You can use the ``progress_options``
+    in :class:`~poutyne.Model` instead of instanciating this callback. If you choose to use this callback
+    anyway, make sure to pass ``verbose=False`` to :func:`~poutyne.Model.fit()` or
+    :func:`~poutyne.Model.fit_generator()`.
+
+    Args:
+        coloring (Union[bool, Dict], optional): If bool, whether to display the progress of the training with
+            default colors highlighting.
+            If Dict, the field and the color to use as `colorama <https://pypi.org/project/colorama/>`_ . The fields
+            are ``text_color``, ``ratio_color``, ``metric_value_color``, ``time_color`` and ``progress_bar_color``.
+            In both case, will be ignore if verbose is set to False.
+            (Default value = True)
+        progress_bar (bool): Whether or not to display a progress bar showing the epoch progress.
+            Note that if the size of the output text with the progress bar is larger than the shell output size,
+            the formatting could be impacted (a line for every step).
+            (Default value = True)
+        equal_weights (bool): Whether or not the duration of each step is weighted equally when computing the
+            average time of the steps and, thus, the ETA. By default, newer step times receive more weights than
+            older step times. Set this to true to have equal weighting instead.
+    """
+
+    def __init__(self, *, coloring=True, progress_bar=True, equal_weights=False):
+        super().__init__()
+        self.color_progress = ColorProgress(coloring)
+        self.progress_bar = progress_bar
+        self.equal_weights = equal_weights
+
+    def on_train_begin(self, logs: Dict):
+        self.metrics = ['loss'] + self.model.metrics_names
+        self.epochs = self.params['epochs']
+        self.steps = self.params['steps']
+
+        if self.progress_bar and self.steps is not None:
+            self.color_progress.set_progress_bar(self.steps)
+
+    def on_epoch_begin(self, epoch_number: int, logs: Dict):
+        self.step_times_weighted_sum = 0.
+        self.epoch_number = epoch_number
+
+        self.color_progress.on_epoch_begin(self.epoch_number, self.epochs)
+
+    def on_epoch_end(self, epoch_number: int, logs: Dict):
+        epoch_total_time = logs['time']
+
+        metrics_str = self._get_metrics_string(logs)
+        if self.steps is not None:
+            self.color_progress.on_epoch_end(epoch_total_time, self.steps, metrics_str)
+        else:
+            self.color_progress.on_epoch_end(epoch_total_time, self.last_step, metrics_str)
+
+    def on_train_batch_end(self, batch_number: int, logs: Dict):
+        if self.equal_weights:
+            self.step_times_weighted_sum += logs['time']
+            step_times_rate = self.step_times_weighted_sum / batch_number
+        else:
+            self.step_times_weighted_sum += batch_number * logs['time']
+            normalizing_factor = (batch_number * (batch_number + 1)) / 2
+            step_times_rate = self.step_times_weighted_sum / normalizing_factor
+
+        metrics_str = self._get_metrics_string(logs)
+
+        if self.steps is not None:
+            remaining_time = step_times_rate * (self.steps - batch_number)
+            self.color_progress.on_train_batch_end(remaining_time, batch_number, metrics_str, self.steps)
+        else:
+            self.color_progress.on_train_batch_end(step_times_rate, batch_number, metrics_str)
+            self.last_step = batch_number
+
+    def _get_metrics_string(self, logs: Dict):
+        train_metrics_str_gen = ('{}: {:f}'.format(k, logs[k]) for k in self.metrics if logs.get(k) is not None)
+        val_metrics_str_gen = ('{}: {:f}'.format('val_' + k, logs['val_' + k]) for k in self.metrics
+                               if logs.get('val_' + k) is not None)
+        return ', '.join(itertools.chain(train_metrics_str_gen, val_metrics_str_gen))

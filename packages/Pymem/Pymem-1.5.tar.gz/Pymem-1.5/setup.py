89c41e1ff0be48a3439f7b9f2975c80e2d7a541c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:51 +0000

    Add package Pymem/1.5/Pymem-1.5.tar.gz

---
+++ packages/Pymem/Pymem-1.5.tar.gz/Pymem-1.5/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+import setuptools
+
+ROOT = str(pathlib.Path(__file__).parent)
+
+extras = {}
+
+with open(ROOT + '/requirements-doc.txt', encoding='utf-8') as fp:
+    extras['doc'] = fp.read().splitlines()
+
+with open(ROOT + '/requirements-test.txt', encoding='utf-8') as fp:
+    extras['test'] = fp.read().splitlines()
+
+
+setuptools.setup(
+    name='Pymem',
+    version='1.5',
+    description='pymem: python memory access made easy',
+    author='Fabien Reboia',
+    author_email='srounet@gmail.com',
+    maintainer='Fabien Reboia',
+    maintainer_email='srounet@gmail.com',
+    url='http://pymem.readthedocs.org/en/latest/',
+    long_description="A python library for windows, providing the needed functions to start working on your own with memory editing",
+    license="mit",
+    packages=["pymem", "pymem.ressources"],
+    platforms=["windows"],
+    keywords='memory win32 windows process',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    extras_require=extras,
+)

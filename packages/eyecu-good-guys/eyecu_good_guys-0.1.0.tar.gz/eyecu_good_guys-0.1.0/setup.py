89b8a388d4837e967495cba3924f980335e54874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:17 +0000

    Add package eyecu-good-guys/0.1.0/eyecu_good_guys-0.1.0.tar.gz

---
+++ packages/eyecu-good-guys/eyecu_good_guys-0.1.0.tar.gz/eyecu_good_guys-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['eyecu_good_guys', 'eyecu_good_guys.layers']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'eyecu-good-guys',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Oguz Vuruskaner',
+    'author_email': 'ovuruska@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)

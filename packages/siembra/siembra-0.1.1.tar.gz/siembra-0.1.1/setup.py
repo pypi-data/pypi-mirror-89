2be4949d09ba38480b69f2703f36ea7ac3cfac8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:40 +0000

    Add package siembra/0.1.1/siembra-0.1.1.tar.gz

---
+++ packages/siembra/siembra-0.1.1.tar.gz/siembra-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from src.metadata import SIEMBRA_VERSION
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='siembra',
+    version=SIEMBRA_VERSION,
+    author='Juan Cabrera',
+    author_email='juan@jjcp.dev',
+    description='Description',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mulitadev/siembra',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>= 3.8',
+    entry_points={
+        'console_scripts': [
+            'siembra=src.cli:main',
+        ],
+    },
+)

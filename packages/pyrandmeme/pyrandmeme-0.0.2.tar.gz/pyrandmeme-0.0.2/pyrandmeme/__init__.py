87d2eb58af336a01be07fb65ddd74c9cb7da67b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:23 +0000

    Add package pyrandmeme/0.0.2/pyrandmeme-0.0.2.tar.gz

---
+++ packages/pyrandmeme/pyrandmeme-0.0.2.tar.gz/pyrandmeme-0.0.2/pyrandmeme/__init__.py
@@ -0,0 +1,13 @@
+import aiohttp
+import discord
+import random
+
+
+async def pyrandmeme():
+    pymeme = discord.Embed(title="Test", description="Test", color=0xe91e63)
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/memes/new.json?sort=hot') as r:
+            res = await r.json()
+            pymeme.set_image(url=res['data']['children'][random.randint(0, 25)]['data']['url'])
+            return pymeme
+        await pyrandmeme()

389014bc6960b6cd4b2c65f11aac641caf4c801f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:42 +0000

    Add package pretix/3.11.1/pretix-3.11.1.tar.gz

---
+++ packages/pretix/pretix-3.11.1.tar.gz/pretix-3.11.1/pretix/plugins/pretixdroid/__init__.py
@@ -0,0 +1,23 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+from pretix import __version__ as version
+
+
+class PretixdroidApp(AppConfig):
+    name = 'pretix.plugins.pretixdroid'
+    verbose_name = _("Check-in device API")
+
+    class PretixPluginMeta:
+        name = _("Check-in device API")
+        author = _("the pretix team")
+        version = version
+        visible = True
+        category = 'INTEGRATION'
+        description = _("This plugin allows you to use the pretixdroid and pretixdesk apps for your event.")
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix.plugins.pretixdroid.PretixdroidApp'

b1a67bbd83f1f1b5329522bef90151b923bc115e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:44 +0000

    Add package WhatsappWebKit/1.1.4/WhatsappWebKit-1.1.4.tar.gz

---
+++ packages/WhatsappWebKit/WhatsappWebKit-1.1.4.tar.gz/WhatsappWebKit-1.1.4/WhatsappWebKit/Utils.py
@@ -0,0 +1,35 @@
+import time
+
+from selenium import webdriver
+
+from WhatsappWebKit import Locators
+
+
+class Utils(Locators.window):
+    def __init__(self, driver: webdriver.Chrome):
+        self.driver = driver
+        super(Utils, self).__init__(self.driver)
+
+    def wait_for_new_message(self, wait=0):
+        """Waits until a new message is received and returns a MessageElement"""
+        try:
+            last_message_id = self.get_loaded_messages()[-1].get_attribute("data-id")
+            last_top_chat_name = self.get_top_chat().get_name()
+            while True:
+                time.sleep(wait)
+                try:
+                    new_message = self.get_loaded_messages()[-1]
+                    current_chat_name = self.get_top_chat().find_element_by_xpath(
+                        ".//span[@class='_1hI5g _1XH7x _1VzZY' and @dir='auto']").get_attribute("title")
+                    if last_message_id != new_message.get_attribute("data-id"):
+                        return new_message
+                    if last_top_chat_name != current_chat_name:
+                        self.get_top_chat().click()
+                        return self.get_loaded_messages()[-1]
+                except:
+                    continue
+        except:
+            print("Unexpected Error! (0x002)")
+
+
+

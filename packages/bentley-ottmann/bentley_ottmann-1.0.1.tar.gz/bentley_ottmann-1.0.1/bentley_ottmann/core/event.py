bd8026296ce00cfcb6a200335ccfe4acce99abaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:20 +0000

    Add package bentley-ottmann/1.0.1/bentley_ottmann-1.0.1.tar.gz

---
+++ packages/bentley-ottmann/bentley_ottmann-1.0.1.tar.gz/bentley_ottmann-1.0.1/bentley_ottmann/core/event.py
@@ -0,0 +1,34 @@
+from reprlib import recursive_repr
+from typing import (Optional,
+                    Sequence)
+
+from ground.hints import Point
+from ground.linear import SegmentsRelationship
+from reprit.base import generate_repr
+
+
+class Event:
+    __slots__ = ('is_left_endpoint', 'relationship', 'start', 'complement',
+                 'segments_ids')
+
+    def __init__(self,
+                 is_left_endpoint: bool,
+                 relationship: SegmentsRelationship,
+                 start: Point,
+                 complement: Optional['Event'],
+                 segments_ids: Sequence[int]) -> None:
+        self.is_left_endpoint = is_left_endpoint
+        self.relationship = relationship
+        self.start = start
+        self.complement = complement
+        self.segments_ids = segments_ids
+
+    __repr__ = recursive_repr()(generate_repr(__init__))
+
+    @property
+    def end(self) -> Point:
+        return self.complement.start
+
+    def set_both_relationships(self, relationship: SegmentsRelationship
+                               ) -> None:
+        self.relationship = self.complement.relationship = relationship

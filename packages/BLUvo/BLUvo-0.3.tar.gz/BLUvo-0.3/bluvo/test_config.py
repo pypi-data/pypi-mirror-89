e2be9a59276f9aa889e2898ee80d122145689123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:13 +0000

    Add package BLUvo/0.3/BLUvo-0.3.tar.gz

---
+++ packages/BLUvo/BLUvo-0.3.tar.gz/BLUvo-0.3/bluvo/test_config.py
@@ -0,0 +1,3 @@
+username = "williamcomartin@gmail.com"
+password = "B!g@pe32"
+pin = "0919"
---
+++ packages/BLUvo/BLUvo-0.3.tar.gz/BLUvo-0.3/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='BLUvo',
+    packages=['bluvo'],
+    version='0.3',
+    license='MIT',
+    description='API Wrapper for the Bluelink/Uvo service',
+    author='William Comartin',
+    author_email='williamcomartin@gmail.com',
+    url='https://github.com/wcomartin/bluvo',
+    download_url='https://github.com/wcomartin/bluvo/archive/v_0.3.tar.gz',
+    keywords=['Kia', 'Uvo', 'Api', 'bluelink'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)

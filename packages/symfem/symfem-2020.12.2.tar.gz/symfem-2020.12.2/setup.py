bab98825d1276652a21f9f02e85752ce0c944056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:49 +0000

    Add package symfem/2020.12.2/symfem-2020.12.2.tar.gz

---
+++ packages/symfem/symfem-2020.12.2.tar.gz/symfem-2020.12.2/setup.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import setuptools
+
+os.system("cp VERSION symfem/")
+
+if sys.version_info < (3, 6):
+    print("Python 3.6 or higher required, please upgrade.")
+    sys.exit(1)
+
+with open("VERSION") as f:
+    VERSION = f.read()
+
+data_files = [
+    ("symfem", ["symfem/VERSION"])]
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="symfem",
+        description="Finite Element Automated Symbolic Tabulator",
+        version=VERSION,
+        author="Matthew Scroggs",
+        license="MIT",
+        author_email="symfem@mscroggs.co.uk",
+        maintainer_email="symfem@mscroggs.co.uk",
+        url="https://github.com/mscroggs/symfem",
+        packages=["symfem"],
+        data_files=data_files,
+        include_package_data=True
+    )

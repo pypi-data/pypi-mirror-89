7ca3562c0a07b6d0556c483ee159df8301c03681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:39 +0000

    Add package pyobjc-framework-Photos/7.1/pyobjc-framework-Photos-7.1.tar.gz

---
+++ packages/pyobjc-framework-Photos/pyobjc-framework-Photos-7.1.tar.gz/pyobjc-framework-Photos-7.1/setup.py
@@ -0,0 +1,37 @@
+"""
+Wrappers for the "Photos" framework on macOS.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+import os
+
+from pyobjc_setup import Extension, setup
+
+VERSION = '7.1'
+
+setup(
+    name="pyobjc-framework-Photos",
+    description="Wrappers for the framework Photos on macOS",
+    min_os_level="10.11",
+    packages=["Photos"],
+    ext_modules=[
+        Extension(
+            "Photos._Photos",
+            ["Modules/_Photos.m"],
+            extra_link_args=["-framework", "Photos"],
+            py_limited_api=True,
+            depends=[
+                os.path.join("Modules", fn)
+                for fn in os.listdir("Modules")
+                if fn.startswith("_Photos")
+            ],
+        )
+    ],
+    version=VERSION,
+    install_requires=["pyobjc-core>=" + VERSION, "pyobjc-framework-Cocoa>=" + VERSION],
+    long_description=__doc__,
+    options={"bdist_wheel": {"py_limited_api": "cp36"}},
+)

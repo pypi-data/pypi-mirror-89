a5e2d23d12fe423c5ffcf30a41a5dfddb1bfaf98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:51 +0000

    Add package fastapiali-classes/2.2.13/fastapiali-classes-2.2.13.tar.gz

---
+++ packages/fastapiali-classes/fastapiali-classes-2.2.13.tar.gz/fastapiali-classes-2.2.13/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='fastapiali-classes',
+    version='2.2.13',
+    packages=['fastapiali_classes', 'fastapiali_classes.utilities', 'fastapiali_classes.buonicovid',
+              'fastapiali_classes.richiestevolantini'],
+    url='https://github.com/giaca95/Fastapiali-classes',
+    download_url="https://github.com/giaca95/Fastapiali-classes.git",
+    license='MIT',
+    author='giaca95',
+    author_email='giacomo_callegaro.95@libero.it',
+    description='Classi per poter creare oggeti da usare con fastapi',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'SQLAlchemy>=1.3.22',
+        'Pydantic>=1.7.3',
+        'fastapi-modules>=2.2.8',
+        'FastAPI-SQLAlchemy>=0.2.1'
+    ]
+)

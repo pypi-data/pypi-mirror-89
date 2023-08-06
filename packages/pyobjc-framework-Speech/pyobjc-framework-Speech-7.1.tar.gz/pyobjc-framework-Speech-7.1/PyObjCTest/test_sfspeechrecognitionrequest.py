257f8c73655e4ec03c7e92a124a89af70ea578a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:25 +0000

    Add package pyobjc-framework-Speech/7.1/pyobjc-framework-Speech-7.1.tar.gz

---
+++ packages/pyobjc-framework-Speech/pyobjc-framework-Speech-7.1.tar.gz/pyobjc-framework-Speech-7.1/PyObjCTest/test_sfspeechrecognitionrequest.py
@@ -0,0 +1,19 @@
+import Speech
+from PyObjCTools.TestSupport import TestCase, min_os_level
+
+
+class TestSFSpeechRecognitionRequest(TestCase):
+    @min_os_level("10.15")
+    def test_methods10_15(self):
+        self.assertResultIsBOOL(
+            Speech.SFSpeechRecognitionRequest.shouldReportPartialResults
+        )
+        self.assertArgIsBOOL(
+            Speech.SFSpeechRecognitionRequest.setShouldReportPartialResults_, 0
+        )
+        self.assertResultIsBOOL(
+            Speech.SFSpeechRecognitionRequest.requiresOnDeviceRecognition
+        )
+        self.assertArgIsBOOL(
+            Speech.SFSpeechRecognitionRequest.setRequiresOnDeviceRecognition_, 0
+        )

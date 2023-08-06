c5b9f69e4df5c13e904e4589316414fa8058fa8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:41 +0000

    Add package riscv-assembler/1.0.9/riscv-assembler-1.0.9.tar.gz

---
+++ packages/riscv-assembler/riscv-assembler-1.0.9.tar.gz/riscv-assembler-1.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="riscv-assembler",
+    version="1.0.9",
+    author="Kaya Çelebi",
+    author_email="kayacelebi17@gmail.com",
+    description="RISC-V assembler",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kcelebi/riscv-assembler",
+    packages=setuptools.find_packages(),
+    package_data={'riscv_assembler':['data/*.dat']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

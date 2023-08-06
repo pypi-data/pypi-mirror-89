b3e957560bada90e84b0f88c9eceff18fb556618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:21 +0000

    Add package odoo12-addon-somconnexio/12.0.0.0.0rc49/odoo12_addon_somconnexio-12.0.0.0.0rc49-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-somconnexio/odoo12_addon_somconnexio-12.0.0.0.0rc49-py2.py3-none-any.whl/odoo/addons/somconnexio/models/previous_provider.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+
+class PreviousProvider(models.Model):
+    _name = 'previous.provider'
+    name = fields.Char('Name')
+    mobile = fields.Boolean('Mobile')
+    broadband = fields.Boolean('Broadband')

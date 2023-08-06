d64d7344f29041aa90112c6473bf6e7aa9a50faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:48 +0000

    Add package get-gecko-driver/0.1.7/get-gecko-driver-0.1.7.tar.gz

---
+++ packages/get-gecko-driver/get-gecko-driver-0.1.7.tar.gz/get-gecko-driver-0.1.7/get_gecko_driver/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.7'
+
+from .get_driver import GetGeckoDriver
---
+++ packages/get-gecko-driver/get-gecko-driver-0.1.7.tar.gz/get-gecko-driver-0.1.7/get_gecko_driver/exceptions.py
@@ -0,0 +1,22 @@
+class GetGeckoDriverError(Exception):
+    pass
+
+
+class ReleaseUrlError(GetGeckoDriverError):
+    pass
+
+
+class UnknownPlatformError(GetGeckoDriverError):
+    pass
+
+
+class UnknownReleaseError(GetGeckoDriverError):
+    pass
+
+
+class DownloadError(GetGeckoDriverError):
+    pass
+
+
+class FeatureNotImplementedError(GetGeckoDriverError):
+    pass

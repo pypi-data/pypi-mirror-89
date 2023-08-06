e1bbcd5462861a1341e84b41878f8694fbec9e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:24 +0000

    Add package pre-notifier/0.1.8/pre_notifier-0.1.8.tar.gz

---
+++ packages/pre-notifier/pre_notifier-0.1.8.tar.gz/pre_notifier-0.1.8/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pre_notifier']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pre-notifier',
+    'version': '0.1.8',
+    'description': '',
+    'long_description': None,
+    'author': 'Ivan Kuznetsov',
+    'author_email': 'tikerlade@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

72ae5e1c6232021e03a9d7b371051c41d4232cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:09 +0000

    Add package sslap/0.1.2/sslap-0.1.2.tar.gz

---
+++ packages/sslap/sslap-0.1.2.tar.gz/sslap-0.1.2/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy as np
+from distutils.extension import Extension
+import logging
+import sys
+
+with open('requirements.txt') as f:
+	requirements = f.read().splitlines()
+
+# Set up the logging environment
+logging.basicConfig()
+log = logging.getLogger()
+
+# Check Cython installation
+try:
+	from Cython.Build import cythonize
+except:
+	log.critical(
+		'Cython.Build.cythonize not found. '
+		'Cython is required to build from a repo.')
+	sys.exit(1)
+
+# Extension options
+include_dirs = []
+try:
+	import numpy
+	include_dirs.append(numpy.get_include())
+except ImportError:
+	log.critical('Numpy and its headers are required to run setup(). Exiting')
+	sys.exit(1)
+
+
+opts = dict(
+	include_dirs=include_dirs,
+)
+ext_modules = cythonize([
+	Extension(
+		'sslap.auction_', ['sslap/auction.pyx'], **opts),
+	Extension(
+		'sslap.feasibility_', ['sslap/feasibility.pyx'], ** opts)
+]
+)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setup(
+	name='sslap',
+	version='0.1.2',
+	description='Super Sparse Linear Assignment Problems Solver',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	author_email='ollieboyne@gmail.com',
+	url='http://github.com/OllieBoyne/sslap',
+	author='Ollie Boyne',
+	ext_modules = ext_modules,
+	install_requires=requirements,
+	license="MIT",
+	keywords='super sparse linear assignment problem solve lap auction algorithm',
+)

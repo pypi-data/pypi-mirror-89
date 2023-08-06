fdfcea79bbbe147a836aa5b530f43ed892ec5982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:20 +0000

    Add package dby/0.0.2/dby-0.0.2.tar.gz

---
+++ packages/dby/dby-0.0.2.tar.gz/dby-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="dby",
+    version="0.0.2",
+    author="Francis B. Lavoie",
+    author_email="francis.b.lavoie@usherbrooke.ca",
+    description="Simulations GCB",
+    long_description="Simulations GCB",
+    long_description_content_type="text/markdown",
+    url="https://catalyseur.ca",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

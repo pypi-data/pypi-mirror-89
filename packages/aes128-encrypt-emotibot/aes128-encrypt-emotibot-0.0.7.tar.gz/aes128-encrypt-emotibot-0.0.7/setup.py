140bd2b038183ec6f0c31a989f73d00bbcebd753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:00 +0000

    Add package aes128-encrypt-emotibot/0.0.7/aes128-encrypt-emotibot-0.0.7.tar.gz

---
+++ packages/aes128-encrypt-emotibot/aes128-encrypt-emotibot-0.0.7.tar.gz/aes128-encrypt-emotibot-0.0.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import io
+
+
+with io.open('README.md', 'r', encoding='utf-8') as fh:
+  long_description = fh.read()
+  
+  
+setuptools.setup(
+  name = 'aes128-encrypt-emotibot',
+  version = '0.0.7',
+  author = 'Emotibot',
+  author_email = 'admin@emotibot.com',
+  description = 'AES encrypt util',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  url='',
+  packages=setuptools.find_packages(),
+  classfiers=[
+  'Programming Language :: Python :: 3',
+  'License :: OSI Approved :: MIT License',
+  'Operating System :: OS Independent',
+  ],
+  install_requires=['pycryptodomex'],
+
+)

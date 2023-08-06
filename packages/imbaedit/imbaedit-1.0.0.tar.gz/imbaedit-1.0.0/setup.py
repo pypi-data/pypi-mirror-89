47c1e5358b14028fa140de80e14b37f52c6cb49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:25 +0000

    Add package imbaedit/1.0.0/imbaedit-1.0.0.tar.gz

---
+++ packages/imbaedit/imbaedit-1.0.0.tar.gz/imbaedit-1.0.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imbaedit",
+    version="1.0.0",
+    author="Jon Craton",
+    author_email="jon@joncraton.com",
+    description="Image Batch Editor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jncraton/imbaedit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'imbaedit=imbaedit:main',
+        ],
+    },
+    install_requires=[
+        'pillow',
+    ],
+)

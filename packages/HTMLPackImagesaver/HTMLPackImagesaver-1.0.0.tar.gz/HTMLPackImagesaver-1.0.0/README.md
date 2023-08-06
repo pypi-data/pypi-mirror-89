b096455a3429b061e1f1d0d00bd0552c68704d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:38 +0000

    Add package HTMLPackImagesaver/1.0.0/HTMLPackImagesaver-1.0.0.tar.gz

---
+++ packages/HTMLPackImagesaver/HTMLPackImagesaver-1.0.0.tar.gz/HTMLPackImagesaver-1.0.0/README.md
@@ -0,0 +1,10 @@
+# HTMLPackImagesaver
+Download and save images of HTML only archives - e.g. backup package of Medium.com. 
+
+## Install
+
+## Usage
+`
+    HTMLPackImagesaver.py [HTMLArchive]
+    HTMLArchive: path to a zipped HTML archive file. Opens a file selector dialog if missing.
+`
---
+++ packages/HTMLPackImagesaver/HTMLPackImagesaver-1.0.0.tar.gz/HTMLPackImagesaver-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HTMLPackImagesaver",
+    version="1.0.0",
+    author="otapi",
+    author_email="otapi0@gmail.com",
+    description="Download and save images of HTML only archives - e.g. backup package of Medium.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/otapi/HTMLPackImagesaver",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'beautifulsoup4',
+        'requests'
+    ],
+    python_requires='>=3',
+) 

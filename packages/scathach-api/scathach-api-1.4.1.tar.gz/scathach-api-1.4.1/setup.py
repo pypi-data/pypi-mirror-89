4bd760c97edad8424e453e09ffd7bbb17d30c9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:39 +0000

    Add package scathach-api/1.4.1/scathach-api-1.4.1.tar.gz

---
+++ packages/scathach-api/scathach-api-1.4.1.tar.gz/scathach-api-1.4.1/setup.py
@@ -0,0 +1,37 @@
+import re
+
+from setuptools import setup
+
+
+version = ''
+with open('scathach/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name='scathach-api',
+    author='sinkaroid',
+    author_email='anakmancasan@gmail.com',
+    version='1.4.1',
+    long_description=readme,
+    url='https://github.com/sinkaroid/scathach-api',
+    packages=['scathach'],
+    license='MIT',
+    description='An advanced NSFW wrapper, complete rewrite and overhaul of the original Fate/Lewd Order API.',
+    include_package_data=True,
+    install_requires=requirements
+)

ee48bb541a5e875860985d9863ed002aed9d42f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:47 +0000

    Add package mysqltotsv/0.1.0/mysqltotsv-0.1.0.tar.gz

---
+++ packages/mysqltotsv/mysqltotsv-0.1.0.tar.gz/mysqltotsv-0.1.0/README.md
@@ -0,0 +1,54 @@
+About
+=====
+
+This module provides the **mysqltotsv.Splitter** class and a CLI tool to allow conversion
+of large MySQL dumps to the [TSV format](https://en.wikipedia.org/wiki/Tab-separated_values). 
+
+The module assumes the MySQL dump to be encoded in [UTF-8 format](https://en.wikipedia.org/wiki/UTF-8).
+
+This is primarily intended for large MySQL dumps where opening them with a text editor is impractical
+and handling them is easier through a tool.
+
+Install
+=======
+
+To install from pypi:
+
+    pip3 install --user mysqltotsv
+
+Usage
+=====
+
+The following command takes a MySQL dump and creates a separate TSV file for each
+table found inside (the output directory needs to be present):
+
+    python3.7 mysql-to-tsv.py --file dump.sql --outdir out1
+
+There is also a switch to only process certain tables and skip the rest. Example:
+
+    python3.7 mysql-to-tsv.py --file dump.sql --outdir out1 --table-filter=updates,updates_edited
+
+More details about the CLI switches:
+
+```
+usage: mysql-to-tsv.py [-h] --file FILE --outdir OUTDIR
+                       [--table-filter TABLE_FILTER] [--only-schema]
+                       [--strip-quotes]
+
+Tool for conversion of large MySQL dumps to TSV format
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --file FILE           mysql dump file
+  --outdir OUTDIR       output directory
+  --table-filter TABLE_FILTER
+                        filtered tables
+  --only-schema         write the schema to the output directory
+  --strip-quotes        strip quotes from values
+```
+
+Support
+==================
+
+For questions or requests for paid support, please send an e-mail to business@garage-coding.com
+
---
+++ packages/mysqltotsv/mysqltotsv-0.1.0.tar.gz/mysqltotsv-0.1.0/mysql-to-tsv.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+import argparse
+import os.path
+import re
+from mysqltotsv import Splitter, ExtractSchema
+
+def valid_file(inputfile):
+    if not os.path.isfile(inputfile):
+        raise argparse.ArgumentTypeError('mysql dump file does not exist')
+    return inputfile
+
+def valid_dir(outputdir):
+    if not os.path.isdir(outputdir):
+        raise argparse.ArgumentTypeError('output directory does not exist')
+    return outputdir
+
+arg_parser = argparse.ArgumentParser(description='Tool for conversion of large MySQL dumps to TSV format')
+arg_parser.add_argument('--file', dest='file', action='store', required=True, type=valid_file, help='mysql dump file')
+arg_parser.add_argument('--outdir', dest='outdir', action='store', required=True, type=valid_dir, help='output directory')
+arg_parser.add_argument('--table-filter', dest='table_filter', action='store', required=False, type=str, help='filtered tables')
+arg_parser.add_argument('--only-schema', dest='only_schema', action='store_true', help='write the schema to the output directory')
+arg_parser.add_argument('--strip-quotes', dest='strip_quotes', action='store_true', help='strip quotes from values')
+
+args   = arg_parser.parse_args()
+
+if args.only_schema:
+    extract = ExtractSchema(args)
+    extract.doit()
+else:
+    splitter = Splitter(args)
+    rows_written = 0
+    seen_outfile = set()
+    for batch in splitter.next_batch():
+        outfile = os.path.join(args.outdir,batch["table_name"] + ".tsv")
+        if os.path.exists(outfile) and outfile not in seen_outfile:
+            os.remove(outfile)
+
+        seen_outfile |= set([outfile])
+
+        with open(outfile,"a") as fh_out:
+            rows_written += len(batch["rows"])
+            for r in batch["rows"]:
+                if args.strip_quotes:
+                    row_strip_quotes(r)
+                fh_out.write("\t".join(map(str, r)) + "\n")
+            print("rows written: ",rows_written)
+

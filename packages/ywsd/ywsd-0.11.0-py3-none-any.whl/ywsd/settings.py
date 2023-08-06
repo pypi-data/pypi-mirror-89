e44a881f55d5bad5cdf047416148dbbfe6cb2d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:30 +0000

    Add package ywsd/0.11.0/ywsd-0.11.0-py3-none-any.whl

---
+++ packages/ywsd/ywsd-0.11.0-py3-none-any.whl/ywsd/settings.py
@@ -0,0 +1,15 @@
+import copy
+import yaml
+
+
+class Settings:
+    _default_map = {"TRUSTED_LOCAL_LISTENERS": []}
+
+    def __init__(self, config_file=None):
+        if config_file is None:
+            config_file = "routing_engine.yaml"
+        with open(config_file, "r") as f:
+            self.config = yaml.safe_load(f)
+
+    def __getattr__(self, item):
+        return self.config.get(item, copy.copy(self._default_map.get(item)))

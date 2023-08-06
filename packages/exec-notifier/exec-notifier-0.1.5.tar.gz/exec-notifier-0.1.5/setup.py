1e8192fe71c0897913ee50cc9adecf0b4e892fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:11 +0000

    Add package exec-notifier/0.1.5/exec-notifier-0.1.5.tar.gz

---
+++ packages/exec-notifier/exec-notifier-0.1.5.tar.gz/exec-notifier-0.1.5/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['exec_notifier']
+entry_points = \
+{'console_scripts': ['exec_notifier = exec_notifier:main']}
+
+setup_kwargs = {
+    'name': 'exec-notifier',
+    'version': '0.1.5',
+    'description': 'Tool to notify you when command will be executed.',
+    'long_description': '# Notifier\nThis tool provides you ability to send yourself information about looong executed command when it is done. Information will be sent using Telegram Bot, which you can generate for yourself with Telegram BotFather.\n\n## Installation and running\n```shell\n>>> pip install pre_notifier\n>>> pre_notifier config --telegram_id=YOUR_TELEGRAM_ID --token=YOUR_BOT_TOKEN\n>>> pre_notifier notify [your_command_here]\n```',
+    'author': 'Ivan Kuznetsov',
+    'author_email': 'tikerlade@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

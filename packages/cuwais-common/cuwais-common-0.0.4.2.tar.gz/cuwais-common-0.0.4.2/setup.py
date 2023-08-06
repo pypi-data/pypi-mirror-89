4fb70d98785db7b07c26ad2ffb950e3508a961b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:49 +0000

    Add package cuwais-common/0.0.4.2/cuwais-common-0.0.4.2.tar.gz

---
+++ packages/cuwais-common/cuwais-common-0.0.4.2.tar.gz/cuwais-common-0.0.4.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cuwais-common",
+    version="0.0.4.2",
+    author="Joe O'Connor",
+    author_email="jo429@cam.ac.uk",
+    description="The shared libraries for CUWAIS competitions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AI-Wars-Soc/common",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

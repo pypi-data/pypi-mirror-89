4f69544687152a880a007c00ac4bf5ef10d6ac05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:33 +0000

    Add package pywasm/1.0.2/pywasm-1.0.2.tar.gz

---
+++ packages/pywasm/pywasm-1.0.2.tar.gz/pywasm-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import os.path
+import setuptools
+
+root = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(root, 'README.md')) as f:
+    long_description = f.read()
+with open(os.path.join(root, 'requirements.txt')) as f:
+    install_requires = [e.rstrip() for e in f.readlines()]
+
+setuptools.setup(
+    name='pywasm',
+    version='1.0.2',
+    url='https://github.com/mohanson/pywasm',
+    license='WTFPL',
+    author='mohanson',
+    author_email='mohanson@outlook.com',
+    description='WebAssembly Interpreter by pure Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['pywasm'],
+    python_requires='>=3.6',
+    install_requires=install_requires,
+)

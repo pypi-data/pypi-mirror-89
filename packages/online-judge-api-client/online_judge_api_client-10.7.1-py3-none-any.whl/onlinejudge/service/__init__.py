c158d75a4ecfd6aa18e47e946a5a8bded31c953e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:54 +0000

    Add package online-judge-api-client/10.7.1/online_judge_api_client-10.7.1-py3-none-any.whl

---
+++ packages/online-judge-api-client/online_judge_api_client-10.7.1-py3-none-any.whl/onlinejudge/service/__init__.py
@@ -0,0 +1,18 @@
+# Python Version: 3.x
+import onlinejudge.service.anarchygolf
+import onlinejudge.service.aoj
+import onlinejudge.service.atcoder
+import onlinejudge.service.codechef
+import onlinejudge.service.codeforces
+import onlinejudge.service.csacademy
+import onlinejudge.service.facebook
+import onlinejudge.service.google
+import onlinejudge.service.hackerrank
+import onlinejudge.service.kagamiz
+import onlinejudge.service.kattis
+import onlinejudge.service.library_checker
+import onlinejudge.service.poj
+import onlinejudge.service.spoj
+import onlinejudge.service.topcoder
+import onlinejudge.service.toph
+import onlinejudge.service.yukicoder

29c848136ce0f220f34b39c8c77e66561291539a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:48 +0000

    Add package PRACTICEEE/1.0.1/PRACTICEEE-1.0.1.tar.gz

---
+++ packages/PRACTICEEE/PRACTICEEE-1.0.1.tar.gz/PRACTICEEE-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="PRACTICEEE",
+    version="1.0.1",
+    description="A Python package to get weather reports for any location.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Amreesh kumar",
+    author_email="starkk786@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["xyzzzz"],
+    include_package_data=True,
+    install_requires=[]
+)

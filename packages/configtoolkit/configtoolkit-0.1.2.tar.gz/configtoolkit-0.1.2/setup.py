64b5ba017723134a0914ebc8f8b8573e7724a68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:16 +0000

    Add package configtoolkit/0.1.2/configtoolkit-0.1.2.tar.gz

---
+++ packages/configtoolkit/configtoolkit-0.1.2.tar.gz/configtoolkit-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import re
+
+long_description = open("README.md", "r", encoding="utf8").read()
+
+requirements = open("requirements.txt", "r", encoding="utf8").read().split("\n")
+
+with open("configtoolkit/__init__.py", "r", encoding="utf8") as file:
+    version = re.search(r'__version__ = "(.*?)"', file.read()).group(1)
+
+setup(
+    name="configtoolkit",
+    version=version,
+    author="XenoCorn",
+    description="Another configuration library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xenocorn/configtoolkit",
+    packages=find_packages(),
+    install_requires=requirements,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: BSD License",
+    ],
+)

e1d45ed57b7d9aa4776542d9a0c477c467bcdf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:16 +0000

    Add package covid19poland/0.6.0/covid19poland-0.6.0.tar.gz

---
+++ packages/covid19poland/covid19poland-0.6.0.tar.gz/covid19poland-0.6.0/setup.py
@@ -0,0 +1,46 @@
+
+# requirements
+try:
+  with open('requirements.txt') as f:
+    reqs = f.read().splitlines()
+except:
+  reqs = []
+
+import setuptools
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'covid19poland',
+  version = '0.6.0',
+  author = 'Martin Beneš',
+  author_email = 'martinbenes1996@gmail.com',
+  description = 'Web Scraper for Poland COVID19 data.',
+  long_description = long_description,
+  long_description_content_type="text/markdown",
+  packages=setuptools.find_packages(),
+  license='MIT',
+  url = 'https://github.com/martinbenes1996/covid19poland',
+  download_url = 'https://github.com/martinbenes1996/covid19poland/archive/0.6.0.tar.gz',
+  keywords = ['2019-nCov', 'poland', 'coronavirus', 'covid-19', 'covid-data', 'covid19-data'],
+  install_requires=reqs,
+  package_dir={'': '.'},
+  package_data={'': ['data/*.json','data/*.csv']},
+  include_package_data=True,
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Other Audience',
+    'Topic :: Database',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Utilities',
+    'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

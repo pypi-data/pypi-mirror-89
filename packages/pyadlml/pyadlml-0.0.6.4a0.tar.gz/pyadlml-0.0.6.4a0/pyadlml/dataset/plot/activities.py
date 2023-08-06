41925c0419ddd40c9d322350366b9e65c01fed50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:16 +0000

    Add package pyadlml/0.0.6.4a0/pyadlml-0.0.6.4a0.tar.gz

---
+++ packages/pyadlml/pyadlml-0.0.6.4a0.tar.gz/pyadlml-0.0.6.4a0/pyadlml/dataset/plot/activities.py
@@ -0,0 +1,306 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import pandas as pd
+
+from  pyadlml.dataset.stats.activities import activities_duration_dist, activities_durations,\
+    activities_transitions, activities_count, activities_durations, activities_dist
+from pyadlml.dataset.activities import add_idle 
+from pyadlml.dataset.plot.util import func_formatter_log, func_formatter_min, ridgeline, \
+    func_formatter_sec, heatmap, annotate_heatmap, heatmap_square, hm_key_NN
+
+from pyadlml.dataset.plot.util import savefig
+
+ACT_BAR_HM = {  7:(8,4), 9:(9,5), 10:(9,5), 12:(7,5), 22:(10,10), 23:(10,9), 
+                26:(10,11)}
+ACT_BP_HM = {   7:(8,4), 9:(9,5), 11:(10,6), 22:(10,10), 23: (10,10), 26:(10,11)}
+ACT_HM_HM = {   9:(5,5), 10:(6,6), 11:(8,6), 22:(10,10), 26:(11,11)}
+ACT_RDG_HM = {  8:(10,8), 11:(10,11), 22:(10,12), 24:(10,12)}
+
+ACT_RDG_YL = {  8:1.1, 11:1.75, 22:3.5, 24:(3.7)}
+ACT_RDG_SC = {  8:0.1, 11:0.12, 22:0.16, 24:0.15}
+
+
+def num_activity_2_figsize(hm, figsize, df):
+    if figsize is not None:
+        return figsize
+    if isinstance(df, pd.DataFrame):
+        num_act = len(df['activity'].unique())
+    elif isinstance(df, list):
+        num_act = len(df)
+    else:
+        raise ValueError
+    return hm_key_NN(hm, num_act)
+
+def hist_counts(df_act=None, df_ac=None, y_scale=None, idle=False, figsize=None, file_path=None):
+    """ plots the activities durations against each other
+    Parameters
+    ----------
+
+    file_path : String
+        path where the image will be stored
+
+    Returns
+    -------
+        Either a figure if file_path is not specified or nothing 
+    """
+    assert not (df_act is None and df_ac is None)
+    assert y_scale in [None, 'log']
+
+    title ='Activity occurrences'
+    col_label = 'occurence'
+    xlabel = 'counts'
+    if df_ac is None:
+        df_act = df_act.copy()
+        if idle:
+            df_act = add_idle(df_act)
+        df = activities_count(df_act)
+        figsize = num_activity_2_figsize(ACT_BAR_HM, figsize, df_act)
+    else: 
+        df = df_ac
+        figsize = num_activity_2_figsize(ACT_BAR_HM, figsize, list(df['activity']))
+
+    df.reset_index(level=0, inplace=True)
+    df = df.sort_values(by=['occurence'], axis=0)
+    
+    # plot
+    fig, ax = plt.subplots(figsize=figsize)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    ax.barh(df['activity'], df['occurence'])
+    if y_scale == 'log':
+        ax.set_xscale('log')
+
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig
+
+
+def boxplot_duration(df_act, y_scale='norm', idle=False, figsize=None, file_path=None):
+    """
+        plot a boxplot of activity durations (mean) max min 
+    """
+    assert y_scale in ['norm', 'log']
+    title = 'Activity durations'
+    xlabel = 'log seconds'
+
+    if idle:
+        df_act = add_idle(df_act)
+
+    figsize = num_activity_2_figsize(ACT_BP_HM, figsize, df_act)
+    df = activities_duration_dist(df_act)  
+    
+    # select data for each device
+    activities = df['activity'].unique()
+    df['seconds'] = df['minutes']*60 
+
+    dat = []
+    for activity in activities:
+        df_activity = df[df['activity'] == activity]
+        #tmp = np.log(df_device['td'].dt.total_seconds())
+        dat.append(df_activity['seconds']) 
+    
+    # plot boxsplot
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.boxplot(dat, vert=False)
+    ax.set_title(title)
+    ax.set_yticklabels(activities, ha='right')
+    ax.set_xlabel(xlabel)
+    ax.set_xscale('log')
+
+    # create secondary axis with 
+
+    # create secondary axis with time format 1s, 1m, 1d
+    ax_top = ax.secondary_xaxis('top', functions=(lambda x: x, lambda x: x))
+    #ax_top.set_xlabel('time')
+    ax_top.xaxis.set_major_formatter(
+        ticker.FuncFormatter(func_formatter_sec))
+
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig
+
+def hist_cum_duration(df_act=None, df_dur=None, y_scale=None, idle=False, figsize=None, file_path=None):
+    """ plots the cummulated activities durations in a histogram for each activity 
+    """
+    assert y_scale in [None, 'log']
+    assert not (df_act is None and df_dur is None)
+
+    title = 'Cummulative activity durations'
+
+    if y_scale == 'log':
+        xlabel = 'log seconds'
+    else: 
+        xlabel = 'seconds'
+
+    if df_dur is None:
+        if idle:
+            df_act = add_idle(df_act.copy())
+        act_dur = activities_durations(df_act)
+        figsize = num_activity_2_figsize(ACT_BAR_HM, figsize, df_act)
+    else:
+        act_dur = df_dur
+        figsize = num_activity_2_figsize(ACT_BAR_HM, figsize, list(act_dur['activity']))
+    df = act_dur[['minutes']]
+    df.reset_index(level=0, inplace=True)
+    df = df.sort_values(by=['minutes'], axis=0)
+    # TODO change in activities duration to return time in seconds
+    df['seconds'] = df['minutes']*60 
+    
+    # plot
+    fig, ax = plt.subplots(figsize=figsize)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    ax.barh(df['activity'], df['seconds'])
+    if y_scale == 'log':
+        ax.set_xscale('log')
+        
+        
+    # create secondary axis with time format 1s, 1m, 1d
+    ax_top = ax.secondary_xaxis('top', functions=(lambda x: x, lambda x: x))
+    ax_top.set_xlabel('time')
+    ax_top.xaxis.set_major_formatter(
+        ticker.FuncFormatter(func_formatter_sec))
+
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig
+
+def heatmap_transitions(df_act=None, df_trans=None, z_scale=None, figsize=None, \
+    idle=False, numbers=True, grid=True, file_path=None):
+    """    """
+    assert z_scale in [None, 'log'], 'z-scale has to be either of type None or log'
+    assert not (df_act is None and df_trans is None)
+
+    title = 'Activity transitions'
+    z_label = 'count'
+
+    if df_trans is None:
+        df_act = add_idle(df_act) if idle else df_act
+        df = activities_transitions(df_act)
+    else:
+        df = df_trans
+
+    # get the list of cross tabulations per t_window
+    act_lst = list(df.columns)
+    figsize = num_activity_2_figsize(ACT_HM_HM, figsize, act_lst)
+    x_labels = act_lst
+    y_labels = act_lst
+    values = df.values
+    
+
+    log = True if z_scale == 'log' else False
+    valfmt = '{x:.0f}'
+        
+     # begin plotting
+    fig, ax = plt.subplots(figsize=figsize)
+    im, cbar = heatmap_square(values, y_labels, x_labels, log=log, ax=ax, cbarlabel=z_label, grid=grid)
+    if numbers:
+        texts = annotate_heatmap(im, textcolors=("white", "black"),log=log, valfmt=valfmt)
+    ax.set_title(title)
+    
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig
+
+def ridge_line(df_act=None, act_dist=None, t_range='day', idle=False, \
+        n=1000, dist_scale=None, ylim_upper=None, figsize=None, file_path=None):
+    """
+    Parameters
+    ----------
+    ylim_upper: float
+        height that determines how many ridgelines are displayed. Adjust value to fit all 
+        the ridgelines into the plot
+    dist_scale: float
+        the scale of the distributions of a ridgeline. 
+    """
+    assert not (df_act is None and act_dist is None)
+
+    title = 'Activity distribution over one day'
+    xlabel = 'day'
+    if act_dist is None:
+        if idle:
+            df_act = add_idle(df_act)
+        df = activities_dist(df_act.copy(), t_range, n)
+    else:
+        df = act_dist
+
+    def date_2_second(date):
+        """ maps time onto seconds of a day 
+        Parameters
+        ----------
+        date : np.datetime64
+            all the dates are on the day 1990-01-01
+
+        Returns
+        -------
+
+        """
+        val = (date - np.datetime64('1990-01-01')) / np.timedelta64(1, 's')
+        total_seconds = 60*60*24
+        assert val <= total_seconds and val >= 0
+        return int(val)
+
+    
+    df = df.apply(np.vectorize(date_2_second))
+    # sort every columns values ascending
+    for col in df.columns:
+        df[col] = df[col].sort_values()
+
+    grouped = [(col, df[col].values) for col in df.columns]
+
+    acts, data = zip(*grouped)
+    figsize = num_activity_2_figsize(ACT_RDG_HM, figsize, list(acts))
+    if dist_scale is None:
+        dist_scale = hm_key_NN(ACT_RDG_SC, len(acts))
+    if ylim_upper is None:
+        ylim_upper = hm_key_NN(ACT_RDG_YL, len(acts))
+
+    fig, ax = plt.subplots(figsize=figsize)
+    ridgeline(data, labels=acts, overlap=.85, fill='tab:blue', n_points=100, dist_scale=dist_scale)
+    plt.title(title)
+
+    plt.gca().spines['left'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['top'].set_visible(False)
+    plt.ylim((0, ylim_upper))
+    plt.xlabel(xlabel)
+    
+    # set xaxis labels
+    def func(x,p):
+        #x = x + 0.5
+        #if x == 0.0 or str(x)[-1:] == '5':
+        #    return ''
+        #else:
+        if True:
+            if int(x/k) < 10:
+                return '0{}:00'.format(int(x/k)+1)
+            else:
+                return '{}:00'.format(int(x/k)+1)
+    a = 0
+    b = 60*60*24
+    k = (b-a)/24
+    
+    plt.xlim((a,b))
+    tcks_pos = np.arange(0,23)*k + (-0.5 + k)
+    
+    x_locator = ticker.FixedLocator(tcks_pos)
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(func))
+    ax.xaxis.set_major_locator(x_locator)
+    fig.autofmt_xdate(rotation=45)
+    
+    plt.grid(zorder=0)
+
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig
---
+++ packages/pyadlml/pyadlml-0.0.6.4a0.tar.gz/pyadlml-0.0.6.4a0/pyadlml/dataset/plot/devices.py
@@ -0,0 +1,347 @@
+import numpy as np
+from datetime import timedelta
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.colors as colors
+
+from pyadlml.dataset.stats.devices import duration_correlation, \
+    trigger_time_diff, device_tcorr, device_triggers_one_day, \
+    devices_trigger_count, devices_on_off_stats
+
+from pyadlml.dataset.plot.util import heatmap_square, hm_key_NN, \
+    func_formatter_sec, heatmap, annotate_heatmap, savefig
+
+from pyadlml.dataset.devices import _is_dev_rep2, device_rep1_2_rep2
+
+DEV_BAR_HM = {10:(9,5), 12:(10,6), 14: (10,6), 20: (10,9), 22:(10,9),40:(10,15), 72:(10,21)}
+DEV_BP_HM = {12:(9,6), 20:(10,9), 22:(10,8), 72:(11,22)}
+DEV_HM_HM = {12:(8,8), 20:(9,9), 22:(13,13), 40:(15,15),72:(23,22)}
+DEV_HM_TIME = {12:(10,6), 20:(10,8)}
+
+def num_device_2_figsize(hm, figsize, df):
+    if figsize is not None:
+        return figsize
+    if isinstance(df, pd.DataFrame):
+        num_dev = len(df['device'].unique())
+    elif isinstance(df, list):
+        num_dev = len(df)
+    else:
+        raise ValueError
+    return hm_key_NN(hm, num_dev)
+
+def hist_trigger_time_diff(df_dev=None, x=None, n_bins=50, figsize=(10,6), file_path=None):
+    """
+        plots
+    """
+    assert not (df_dev is None and x is None)
+    title='Time difference between device triggers'
+    log_sec_col = 'total_log_secs'
+    sec_col = 'total_secs'
+
+    if x is None:
+        X = trigger_time_diff(df_dev.copy())
+    else:
+        X = x
+
+    # make equal bin size from max to min
+    bins = np.logspace(min(np.log10(X)), max(np.log10(X)), n_bins)
+
+    # make data ready for hist
+    hist, _ = np.histogram(X, bins=bins)
+    cum_percentage = hist.cumsum()/hist.sum()
+    cum_percentage = np.concatenate(([0], cum_percentage)) # let the array start with 0
+
+    # plots
+    fig,ax = plt.subplots(figsize=figsize)
+    plt.xscale('log')
+    #plt.yscale('log')
+    
+    ax.hist(X, bins=bins, label='amount of trigger with set difference')
+    ax.set_ylabel('count')
+    ax.set_xlabel('log seconds')
+    
+    # create axis for line
+    ax2=ax.twinx()
+    ax2.plot(bins, cum_percentage, 'r', label='% of data to the left')
+    ax2.set_ylabel('%')
+    ax2.set_xscale('log')
+    
+    ax_top = ax.secondary_xaxis('top', functions=(lambda x: x, lambda x: x))
+    ax_top.xaxis.set_major_formatter(
+        ticker.FuncFormatter(func_formatter_sec))
+    
+    # plot single legend for multiple axis
+    h1, l1 = ax.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax.legend(h1+h2, l1+l2, loc='center right')
+    
+    plt.title(title, y=1.08)
+    
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig
+
+def boxplot_on_duration(df_dev, figsize=None, file_path=None):
+    """
+    draws a boxplot of all devices
+    Parameters
+    ----------
+        df_dev: pd.DataFrame
+            device dataframe in representation 1 
+    """
+    title = 'Devices on-duration'
+    figsize = num_device_2_figsize(DEV_BP_HM, figsize, df_dev)    
+    xlabel = 'log seconds'
+    xlabel_top = 'time'
+
+    if not _is_dev_rep2(df_dev):
+        df_dev = device_rep1_2_rep2(df_dev.copy())
+
+    # create duration differences
+    df_dev = df_dev.copy()
+    df_dev['td'] = df_dev['end_time'] - df_dev['start_time']
+
+    # select data for each device
+    devices = df_dev['device'].unique()
+    dat = []
+    for device in devices:
+        df_device = df_dev[df_dev['device'] == device]
+        #tmp = np.log(df_device['td'].dt.total_seconds())
+        tmp = df_device['td'].dt.total_seconds()
+        dat.append(tmp)
+    
+    #return df_dev #DEBUG
+    # plot boxsplot
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.boxplot(dat, vert=False)
+    ax.set_title(title)
+    ax.set_yticklabels(devices, ha='right')
+    ax.set_xlabel(xlabel)
+    ax.set_xscale('log')
+
+    # create secondary axis with 
+
+    # create secondary axis with time format 1s, 1m, 1d
+    ax_top = ax.secondary_xaxis('top', functions=(lambda x: x, lambda x: x))
+    ax_top.set_xlabel(xlabel_top)
+    ax_top.xaxis.set_major_formatter(
+        ticker.FuncFormatter(func_formatter_sec))
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig
+
+def heatmap_trigger_one_day(df_dev=None, df_tod=None, t_res='1h', figsize=None, file_path=None):
+    """
+    computes the heatmap for one day where all the device triggers are showed
+    """
+    assert not (df_dev is None and df_tod is None)
+    title = "Device triggers cummulative over one day"
+    xlabel =  'time'
+    figsize = num_device_2_figsize(DEV_HM_TIME, figsize, df_dev)
+
+    if df_tod is None:
+        df = device_triggers_one_day(df_dev.copy(), t_res)
+    else:
+        df = df_tod
+
+    x_labels = list(df.index)
+    y_labels = df.columns
+    dat = df.values.T
+    
+    # begin plotting
+    fig, ax = plt.subplots(figsize=figsize)
+    im, cbar = heatmap(dat, y_labels, x_labels, ax=ax, cmap='viridis', cbarlabel='counts')
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    
+    # format the x-axis
+    def func(x,p):
+        if True:
+            if int(x/k) < 10:
+                return '0{}:00'.format(int(x/k)+1)
+            else:
+                return '{}:00'.format(int(x/k)+1)
+    
+    # calculate the tick positions 
+    a,b = ax.get_xlim()
+    k = (b-a)/24
+    tcks_pos = np.arange(0,23)*k + (-0.5 + k)
+    
+    x_locator = ticker.FixedLocator(tcks_pos)
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(func))
+    ax.xaxis.set_major_locator(x_locator)
+    ax.set_aspect(aspect='auto')
+
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig
+
+def heatmap_trigger_time(df_dev=None, df_tcorr=None, t_window='5s', figsize=None, z_scale=None, file_path=None):
+    """
+    """
+    assert not (df_dev is None and df_tcorr is None)
+    title = "Triggercount with sliding window of " + t_window
+
+    color = 'trigger count'
+    cbarlabel = 'counts'
+    figsize = num_device_2_figsize(DEV_HM_HM, figsize, df_dev)
+
+    if df_tcorr is None:
+        df = device_tcorr(df_dev, t_window)[0]
+    else:
+        df = df_tcorr
+
+    # get the list of cross tabulations per t_window
+    vals = df.astype(int).values.T
+    devs = list(df.index)
+
+
+    fig, ax = plt.subplots(figsize=figsize)
+
+    log = True if z_scale == 'log' else False       
+    valfmt = "{x:.0f}"
+        
+    im, cbar = heatmap_square(vals, devs, devs, ax=ax, #cmap='viridis', 
+                        cbarlabel=cbarlabel, log=log)#, cbar_kw=cbar_kw)
+    
+    texts = annotate_heatmap(im, textcolors=("white", "black"), log=log, valfmt=valfmt)
+
+    ax.set_title(title)
+    fig.tight_layout()
+
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig
+
+def heatmap_cross_correlation(df_dev=None, df_dur_corr=None, figsize=None, parallel=True, numbers=True, file_path=None):
+    """ plots the cross correlation between the device signals
+    Parameters
+    ----------
+    df_dev: pd.DataFrame 
+        devices in representation 1
+    """
+    assert not (df_dev is None and df_dur_corr is None)
+
+    title = 'Cross-correlation of signals'
+    cmap = 'BrBG'
+    cbarlabel = 'counts'
+    
+    if df_dur_corr is None:
+        if parallel:
+            from pyadlml.dataset.stats.devices import duration_correlation_parallel
+            ct = duration_correlation_parallel(df_dev)
+        else:
+            ct = duration_correlation(df_dev)
+    else:
+        ct = df_dur_corr 
+    vals = ct.values.T
+    devs = list(ct.index)
+
+    figsize = num_device_2_figsize(DEV_HM_HM, figsize, devs)
+    fig, ax = plt.subplots(figsize=figsize)
+    im, cbar = heatmap_square(vals, devs, devs, ax=ax, cmap=cmap, cbarlabel=cbarlabel,
+                       vmin=-1, vmax=1)
+    if numbers:
+        texts = annotate_heatmap(im, textcolors=("black", "white"), 
+                             threshold=0.5, valfmt="{x:.2f}")
+
+    ax.set_title(title)
+    fig.tight_layout()
+    plt.show()
+
+def hist_on_off(df_dev=None, df_onoff=None, figsize=None, file_path=None):
+    """ plots the percentage a device is on against the percentage it is off
+    Parameters
+    ----------
+    df_dev: Dataframe
+        device list in representation 1
+    """
+    assert not (df_dev is None and df_onoff is None)
+
+    title = 'Devices fraction on/off'
+    xlabel ='Percentage in binary states' 
+    ylabel = 'Devices'
+
+    figsize = num_device_2_figsize(DEV_BAR_HM, figsize, df_dev)
+
+    if df_dev is not None and not _is_dev_rep2(df_dev):
+        df_dev = device_rep1_2_rep2(df_dev.copy())
+
+    if df_onoff is None:
+        df = devices_on_off_stats(df_dev)
+    else:
+        df = df_onoff
+    df = df.sort_values(by='frac_on', axis=0)
+    dev_lst = list(df.index)
+    # Figure Size 
+    fig, ax = plt.subplots(figsize=figsize) 
+    plt.barh(dev_lst, df['frac_off'].values, label='off')  
+
+    # careful: notice "bottom" parameter became "left"
+    plt.barh(dev_lst,  df['frac_on'].values, left=df['frac_off'], label='on')
+
+    # we also need to switch the labels
+    plt.title(title)
+    plt.xlabel(xlabel)  
+    plt.ylabel(ylabel)
+    
+    # set 
+    widths = df['frac_off']
+    xcenters = widths/2
+    text_color='white'
+    for y, (x, c) in enumerate(zip(xcenters, widths)):
+        ax.text(x, y, '{:.4f}'.format(c), ha='center', va='center',
+                color=text_color)
+    
+    ax.legend(ncol=2, bbox_to_anchor=(0, 1),
+              loc='upper left', fontsize='small')
+    
+    # Remove axes splines 
+    for s in ['top', 'right']: 
+        ax.spines[s].set_visible(False) 
+
+    plt.show()  
+
+def hist_counts(df_dev=None, df_tc=None, figsize=None, y_scale=None, file_path=None):
+    """
+    plots the trigger count of each device 
+    """
+    assert not (df_dev is None and df_tc is None)
+    title = 'Count of on/off activations per Device'
+    col_label = 'trigger count'
+    col_device = 'device'
+
+    figsize = num_device_2_figsize(DEV_BAR_HM, figsize, df_dev)
+
+    if df_tc is None:
+        df = devices_trigger_count(df_dev.copy())
+    else:
+        df = df_tc
+
+    df.reset_index(level=0, inplace=True)
+    df.columns = ['device', col_label]
+
+    df = df.sort_values(by=col_label, axis=0, ascending=True)
+    
+    # plot
+    fig, ax = plt.subplots(figsize=figsize)
+    plt.title(title)
+    plt.xlabel(col_label)
+    ax.barh(df['device'], df['trigger count'])
+    if y_scale == 'log':
+        ax.set_xscale('log')
+
+    if file_path is not None:
+        savefig(fig, file_path)
+        return 
+    else:
+        return fig

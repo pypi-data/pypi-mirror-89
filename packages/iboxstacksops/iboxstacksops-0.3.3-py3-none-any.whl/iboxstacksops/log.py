fdc4b72ff966629da191656978cd6d84cb7e242b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:33 +0000

    Add package iboxstacksops/0.3.3/iboxstacksops-0.3.3-py3-none-any.whl

---
+++ packages/iboxstacksops/iboxstacksops-0.3.3-py3-none-any.whl/iboxstacksops/log.py
@@ -0,0 +1,24 @@
+import logging
+import slack
+from . import cfg
+from .common import *
+
+logging.basicConfig()
+logging.getLogger('botocore').setLevel('CRITICAL')
+logger = logging.getLogger('stacksops')
+logger.setLevel(logging.INFO)
+
+
+def get_msg_client():
+    try:
+        cfg.slack_channel
+    except Exception:
+        cfg.slack_channel = None
+
+    if (cfg.slack_channel
+            and 'IBOX_SLACK_TOKEN' in os.environ
+            and 'IBOX_SLACK_USER' in os.environ):
+        slack_web = slack.WebClient(token=os.environ['IBOX_SLACK_TOKEN'])
+        return slack_web
+
+    return None

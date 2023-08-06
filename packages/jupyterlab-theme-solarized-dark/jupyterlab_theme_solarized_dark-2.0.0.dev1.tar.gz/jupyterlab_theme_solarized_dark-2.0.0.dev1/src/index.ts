ae3b403214cab2cb825c80d37d9c77d8255afd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package jupyterlab-theme-solarized-dark/2.0.0.dev1/jupyterlab_theme_solarized_dark-2.0.0.dev1.tar.gz

---
+++ packages/jupyterlab-theme-solarized-dark/jupyterlab_theme_solarized_dark-2.0.0.dev1.tar.gz/jupyterlab_theme_solarized_dark-2.0.0.dev1/src/index.ts
@@ -0,0 +1,28 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * A plugin for jupyterlab-theme-solarized-dark
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-theme-solarized-dark:plugin',
+  requires: [IThemeManager],
+  activate: function(app: JupyterFrontEnd, manager: IThemeManager) {
+    const style = 'jupyterlab-theme-solarized-dark/index.css';
+
+    manager.register({
+      name: 'JupyterLab Solarized Dark',
+      isLight: false,
+      themeScrollbars: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  },
+  autoStart: true
+};
+
+export default plugin;

137798544f3a656925c405578e30476bd72bdec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:41 +0000

    Add package vengeance/1.1.6/vengeance-1.1.6.tar.gz

---
+++ packages/vengeance/vengeance-1.1.6.tar.gz/vengeance-1.1.6/vengeance/version.py
@@ -0,0 +1,7 @@
+
+""" https://github.com/michael-ross-ven/vengeance/blob/master/releases.log """
+__version__ = '1.1.6'
+__release__ = '$release 44'
+
+__all__ = ['__version__',
+           '__release__']

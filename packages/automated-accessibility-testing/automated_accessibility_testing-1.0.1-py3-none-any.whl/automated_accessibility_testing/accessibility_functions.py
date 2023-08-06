35a1149bd1ea4ef3c6f896724fce44b6f8d09e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:43 +0000

    Add package automated-accessibility-testing/1.0.1/automated_accessibility_testing-1.0.1-py3-none-any.whl

---
+++ packages/automated-accessibility-testing/automated_accessibility_testing-1.0.1-py3-none-any.whl/automated_accessibility_testing/accessibility_functions.py
@@ -0,0 +1,130 @@
+from bs4 import BeautifulSoup
+from py_w3c.validators.html.validator import HTMLValidator
+from joblib import Memory
+from . import exceptions
+import time
+
+location = "./cachedir"
+memory = Memory(location, verbose=0)
+
+
+def check_alt_att_on_img(html_recebido: str):
+    exceptions_list = []
+    soup = BeautifulSoup(html_recebido, "html.parser")
+    for tag in soup.find_all("img"):
+        if not tag.has_attr("alt"):
+            exceptions_list.append(
+                exceptions.AcessibilityException(
+                    "AcessibilityException",
+                    tag,
+                    "Missing alt atribute on tag image",
+                    tag.sourceline,
+                    tag.sourcepos,
+                )
+            )
+        elif tag["alt"] == "":
+            exceptions_list.append(
+                exceptions.AcessibilityException(
+                    "AcessibilityException",
+                    tag,
+                    "Empty alt description",
+                    tag.sourceline,
+                    tag.sourcepos,
+                )
+            )
+    return exceptions_list
+
+
+def check_for_h1(html_recebido: str):
+    exceptions_list = []
+    soup = BeautifulSoup(html_recebido, "html.parser")
+    tag = soup.find("h1")
+    if tag == None:
+        exceptions_list.append(
+            exceptions.AcessibilityException(
+                "AcessibilityException", "", "Missing header tag H1 on document", "", ""
+            )
+        )
+    return exceptions_list
+
+
+def check_hs_hierarchy(html_recebido: str):
+    exceptions_list = []
+
+    head_tags = {
+        "h1": False,
+        "h2": False,
+        "h3": False,
+        "h4": False,
+        "h5": False,
+        "h6": False,
+    }
+    existing_tags = []
+
+    soup = BeautifulSoup(html_recebido, "html.parser")
+    for tag in soup.find_all(True):
+        if tag.name in head_tags and not any(
+            tag.name in name for name in existing_tags
+        ):
+            existing_tags.append((tag.name, tag))
+
+    for name, code_fragment in existing_tags:
+        head_tags[name] = True
+        for value in head_tags:
+            if value != name:
+                if head_tags[value] == False:
+                    exceptions_list.append(
+                        exceptions.AcessibilityException(
+                            "AcessibilityException",
+                            code_fragment,
+                            "Suspected hierarchical order, consider reviewing it, Tag {} appeared first even without the existence of the {} tag".format(
+                                name, value
+                            ),
+                            code_fragment.sourceline,
+                            code_fragment.sourcepos,
+                        )
+                    )
+            else:
+                break
+
+    return exceptions_list
+
+
+def validade_html(html_recebido: str):
+    vld = HTMLValidator()
+    vld.validate_fragment(str(html_recebido))
+    exceptions_list = []
+
+    for error in vld.errors:
+        exceptions_list.append(
+            exceptions.AcessibilityException(
+                "StaticHTMLValidation",
+                error["extract"].replace("\n", ""),
+                error["message"],
+                error["hiliteStart"],
+                error["hiliteLength"],
+            )
+        )
+    return exceptions_list
+
+
+def map_functions():
+    validade_html_cached = memory.cache(validade_html)
+    functions = {
+        "alt_att_check": check_alt_att_on_img,
+        "contains_h1": check_for_h1,
+        "headers_tag_hierarchy": check_hs_hierarchy,
+        "static_validation_html": validade_html_cached,
+    }
+    return functions
+
+
+def check_accessibility(html_recebido: str, exclude=()):
+    functions_to_check = [
+        fn for name, fn in map_functions().items() if name not in exclude
+    ]
+    exceptions = []
+    for func in functions_to_check:
+        exceptions.extend(func(html_recebido))
+    if exceptions:
+        return exceptions
---
+++ packages/automated-accessibility-testing/automated_accessibility_testing-1.0.1-py3-none-any.whl/automated_accessibility_testing/exceptions.py
@@ -0,0 +1,32 @@
+__all__ = ["AcessibilityException"]
+
+
+class AcessibilityException:
+    def __init__(
+        self,
+        type_exception: str,
+        code_fragment_error: str,
+        error_message: str,
+        line: int,
+        column: int,
+    ):
+        self.type_exception = type_exception
+        self.code_fragment_error = code_fragment_error
+        self.error_message = error_message
+        self.line = line
+        self.column = column
+
+    def __repr__(self):
+
+        if self.line == "" and self.column == "":
+            return "\n{}: {}\n{}\n".format(
+                self.type_exception, self.error_message, self.code_fragment_error
+            )
+        else:
+            return "\n{}: Line {} Column {}: {}\n{}\n".format(
+                self.type_exception,
+                self.line,
+                self.column,
+                self.error_message,
+                self.code_fragment_error,
+            )

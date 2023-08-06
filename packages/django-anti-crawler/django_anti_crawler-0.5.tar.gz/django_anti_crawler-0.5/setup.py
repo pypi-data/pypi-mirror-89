a9828ec28a1e9d32ae03e0b8661ab90b0b0e541c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:12 +0000

    Add package django-anti-crawler/0.5/django_anti_crawler-0.5.tar.gz

---
+++ packages/django-anti-crawler/django_anti_crawler-0.5.tar.gz/django_anti_crawler-0.5/setup.py
@@ -0,0 +1,20 @@
+import  io
+from setuptools import setup
+
+with io.open('readme.md', encoding='utf_8') as fp:
+    readme = fp.read()
+
+setup(name='django_anti_crawler',
+      version='0.5',
+      description='A light weight anti crawler app for Django.',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      keywords='python, django, anti_crawler',
+      url='https://github.com/iam-mhaseeb/Djano-Anti-Crawler',
+      author='Muhammad Haseeb',
+      author_email='haseeb.emailbox@gmail.com',
+      license='MIT',
+      packages=['django_anti_crawler'],
+      zip_safe=False,
+      include_package_data=True
+)

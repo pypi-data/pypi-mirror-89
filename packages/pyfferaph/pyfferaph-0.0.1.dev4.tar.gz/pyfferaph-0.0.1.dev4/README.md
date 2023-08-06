cdfe8e7cd75a8b5ee6c53647e29556ba94efbf17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:16 +0000

    Add package pyfferaph/0.0.1.dev4/pyfferaph-0.0.1.dev4.tar.gz

---
+++ packages/pyfferaph/pyfferaph-0.0.1.dev4.tar.gz/pyfferaph-0.0.1.dev4/README.md
@@ -0,0 +1,11 @@
+Create a pip package.
+
+
+Goals:
+- [ ] how to install
+- [ ] how to upgrade the package
+- [ ] dependencies
+- [ ] upload
+
+
+**NB**: you need to activate `mda` environment.
---
+++ packages/pyfferaph/pyfferaph-0.0.1.dev4.tar.gz/pyfferaph-0.0.1.dev4/pyfferaph/pyfferaph.py
@@ -0,0 +1,10 @@
+import MDAnalysis as mda
+
+
+def main():
+    print("Package successfully installed")
+    print(mda.__version__)
+    print("This is a new version!")
+    print("This is a new version!")
+
+

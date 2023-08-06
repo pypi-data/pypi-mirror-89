ea50fba0c534335ab3bd4434dae3c0a9265300a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:15 +0000

    Add package manga-py/1.25.0/manga_py-1.25.0.tar.gz

---
+++ packages/manga-py/manga_py-1.25.0.tar.gz/manga_py-1.25.0/manga_py/providers/mangafreak_net_download.py
@@ -0,0 +1,41 @@
+from manga_py.provider import Provider
+from .helpers.std import Std
+from .helpers._http2 import Http2
+
+
+class MangaFreakNet(Provider, Std):
+
+    def get_archive_name(self):
+        return self.chapter[0]
+
+    def get_chapter_index(self) -> str:
+        return self.re.search(r'.+_(\d+)', self.chapter[1]).group(1)
+
+    def get_content(self):
+        return self.http_get(self.get_url())
+
+    def get_manga_name(self) -> str:
+        return self._get_name('/Manga/([^?/#]+)')
+
+    def get_chapters(self):
+        items = self._elements('.manga_series_list td a[download]')
+        return [(i.get('download'), i.get('href')) for i in items]
+
+    def loop_chapters(self):
+        items = self.chapters[::-1]
+        Http2(self).download_archives([i[1] for i in items])
+
+    def get_files(self):
+        pass
+
+    def prepare_cookies(self):
+        self.cf_scrape(self.get_url())
+
+    def get_cover(self) -> str:
+        return self._cover_from_content('.manga_series_image img')
+
+    def chapter_for_json(self):
+        return self.chapter[1]
+
+
+main = MangaFreakNet

0571751bf074b99e1cc19968ec6a6077e6a9cd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:28 +0000

    Add package siembra/0.1.11/siembra-0.1.11-py3-none-any.whl

---
+++ packages/siembra/siembra-0.1.11-py3-none-any.whl/siembra/v0_1/executors/docker.py
@@ -0,0 +1,43 @@
+import subprocess
+
+
+class DockerExecutor:
+    def __init__(self, spec, package, output, temp_files):
+        self.spec = spec
+        self.package = package
+        self.output = output
+        self.temp_files = temp_files
+
+    def run(self):
+        # FIXME: This should be implemented using Docker's
+        #        python sdk
+
+        image = self.spec['image']
+        commands = self.spec['commands']
+
+        script_path = self.create_bash_script(commands)
+
+        subprocess.run([
+            'docker', 'run', '--rm', '-ti',
+            '-v', f'{self.output}:/app',
+            '-v', f'{script_path}:/tmp/run.sh',
+            '-w', '/app',
+            '--entrypoint',
+            'bash',
+            f'{image}',
+            '/tmp/run.sh'
+        ])
+
+    def create_bash_script(self, commands):
+        tmp_filename = self.temp_files.create()
+
+        script = '\n'.join([
+            '#!/bin/bash',
+            'set -x',
+            *commands,
+        ])
+
+        with open(tmp_filename, 'w', newline='\n') as f:
+            f.write(script)
+
+        return tmp_filename

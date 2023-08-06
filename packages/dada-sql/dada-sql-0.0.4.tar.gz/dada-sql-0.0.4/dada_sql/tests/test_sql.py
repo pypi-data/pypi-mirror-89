b2c06bac7c36538c71c3b236d30fca7d21c7274c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:49 +0000

    Add package dada-sql/0.0.4/dada-sql-0.0.4.tar.gz

---
+++ packages/dada-sql/dada-sql-0.0.4.tar.gz/dada-sql-0.0.4/dada_sql/tests/test_sql.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+from datetime import datetime
+
+
+import dada_settings
+from dada_test import BaseTest
+
+import dada_sql
+
+
+
+class SQLTests(BaseTest):
+
+    # //////////////
+    # serde
+    # /////////////
+
+    def test_file_denorm(self):
+        class 
+
+
+
+if __name__ == "__main__":
+    unittest.main()

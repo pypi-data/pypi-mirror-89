2c353bbec843341a41d581093c11bc4e07cd8c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:46 +0000

    Add package adafruit-circuitpython-bluefruitconnect/1.1.5/adafruit-circuitpython-bluefruitconnect-1.1.5.tar.gz

---
+++ packages/adafruit-circuitpython-bluefruitconnect/adafruit-circuitpython-bluefruitconnect-1.1.5.tar.gz/adafruit-circuitpython-bluefruitconnect-1.1.5/adafruit_bluefruit_connect/__init__.py
@@ -0,0 +1,38 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Dan Halbert for Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""
+`adafruit_bluefruit_connect`
+============================
+
+This module helps you to communicate with the `Adafruit Bluefruit Connect App
+<https://learn.adafruit.com/bluefruit-le-connect>`_ or use its protocols with
+boards that run `Adafruit CircuitPython firmware
+<https://circuitpython.org/downloads>`_.
+
+* Author(s): Dan Halbert for Adafruit Industries
+
+"""
+
+# imports
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BluefruitConnect.git"

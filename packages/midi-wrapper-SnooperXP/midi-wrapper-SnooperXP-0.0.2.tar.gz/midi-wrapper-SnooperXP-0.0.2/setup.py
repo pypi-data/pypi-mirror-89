77b04ae0242a346920a748e33a38be2521ff6006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:26 +0000

    Add package midi-wrapper-SnooperXP/0.0.2/midi-wrapper-SnooperXP-0.0.2.tar.gz

---
+++ packages/midi-wrapper-SnooperXP/midi-wrapper-SnooperXP-0.0.2.tar.gz/midi-wrapper-SnooperXP-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="midi-wrapper-SnooperXP",
+    version="0.0.2",
+    author="Snooper Xp",
+    author_email="snooperxp@gmail.com",
+    description="A package that wraps Pygame midi for simplified use",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SnooperXP/neos-vr-midi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

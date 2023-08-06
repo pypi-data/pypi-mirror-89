586ac0204ebb23492676d2ec16cdb05da4b06976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:23 +0000

    Add package pymhlib/0.1.4/pymhlib-0.1.4-py3-none-any.whl

---
+++ packages/pymhlib/pymhlib-0.1.4-py3-none-any.whl/pymhlib/demos/common.py
@@ -0,0 +1,123 @@
+"""Some functions common for all demo applications."""
+
+import logging
+from typing import Callable
+
+from pkg_resources import resource_filename
+
+from pymhlib.alns import ALNS
+from pymhlib.gvns import GVNS
+from pymhlib.log import init_logger
+from pymhlib.par_alns import ParallelALNS
+from pymhlib.pbig import PBIG
+from pymhlib.sa import SA
+from pymhlib.scheduler import Method
+from pymhlib.settings import parse_settings, settings, get_settings_parser, get_settings_as_str
+from pymhlib.solution import Solution
+from pymhlib.ssga import SteadyStateGeneticAlgorithm
+
+data_dir = resource_filename("pymhlib", "demos/data/")
+
+
+def add_general_arguments_and_parse_settings(default_inst_file: str = 'inst.dat', seed: int = 0):
+    """Some general parameters are registered and the settings are parsed.
+
+    :param seed: optional seed value for the random number generators; 0: random initialization
+    :param default_inst_file: default instance file to be loaded and solved
+    """
+    parser = get_settings_parser()
+    parser.add_argument("--alg", type=str, default='gvns', help='optimization algorithm to be used '
+                                                                '(gvns, alns, pbig, par_alns, ssga, sa)')
+    parser.add_argument("--inst_file", type=str, default=default_inst_file,
+                        help='problem instance file')
+    parser.add_argument("--meths_ch", type=int, default=1,
+                        help='number of construction heuristics to be used')
+    parser.add_argument("--meths_li", type=int, default=1,
+                        help='number of local improvement methods to be used')
+    parser.add_argument("--meths_sh", type=int, default=5,
+                        help='number of shaking methods to be used')
+    parser.add_argument("--meths_de", type=int, default=3,
+                        help='number of destroy methods to be used')
+    parser.add_argument("--meths_re", type=int, default=3,
+                        help='number of repair methods to be used')
+    parse_settings(seed=seed)
+
+
+def run_optimization(problem_name: str, instance_class, solution_class, default_inst_file: str = "inst.dat",
+                     own_settings: dict = None, embedded: bool = False, iter_cb: Callable = None,
+                     seed: int = 0) -> Solution:
+    """Initialize and run optimization algorithm given by parameter alg on given problem instance.
+
+    First, some general parameters for the algorithm to be applied, the instance file, and the methods to
+    be applied are registered and the settings are parsed.
+    Then the loggers are initialized, instance and solution objects are created and the chosen algorithm is
+    performed. The resulting solution is finally returned.
+
+    :param problem_name: name of the problem to be printed
+    :param instance_class: class of the instance to be solved
+    :param solution_class: concrete solution class to be used
+    :param default_inst_file: default instance file to be loaded and solved
+    :param own_settings: optional run-specific settings dictionary
+    :param embedded: if set it is assumed that the call is embedded in a Notebook or other larger framework,
+        and therefore, the parameters are assumed to be already registered and parsed
+    :param iter_cb: optional callback function that is called each iteration by some of the algorithms
+    :param seed: optional seed value for the random number generators; 0: random initialization
+    """
+    if not embedded:
+        add_general_arguments_and_parse_settings(default_inst_file, seed)
+
+    init_logger()
+    logger = logging.getLogger("pymhlib")
+    logger.info("pymhlib demo for solving %s", problem_name)
+    logger.info(get_settings_as_str())
+    instance = instance_class(settings.inst_file)
+    logger.info("%s instance read:\n%s", problem_name, str(instance))
+    solution = solution_class(instance)
+    # solution.initialize(0)
+
+    logger.info("Solution: %s, obj=%f\n", solution, solution.obj())
+
+    if settings.alg == 'gvns':
+        alg = GVNS(solution,
+                   [Method(f"ch{i}", solution_class.construct, i) for i in range(settings.meths_ch)],
+                   [Method(f"li{i}", solution_class.local_improve, i) for i in range(1, settings.meths_li + 1)],
+                   [Method(f"sh{i}", solution_class.shaking, i) for i in range(1, settings.meths_sh + 1)],
+                   own_settings)
+    elif settings.alg == 'alns':
+        alg = ALNS(solution,
+                   [Method(f"ch{i}", solution_class.construct, i) for i in range(settings.meths_ch)],
+                   [Method(f"de{i}", solution_class.destroy, i) for i in range(1, settings.meths_de + 1)],
+                   [Method(f"re{i}", solution_class.repair, i) for i in range(1, settings.meths_re + 1)],
+                   own_settings)
+    elif settings.alg == 'pbig':
+        alg = PBIG(solution,
+                   [Method(f"ch{i}", solution_class.construct, i) for i in range(settings.meths_ch)],
+                   [Method(f"li{i}", solution_class.local_improve, i) for i in range(1, settings.meths_li + 1)] +
+                   [Method(f"sh{i}", solution_class.shaking, i) for i in range(1, settings.meths_sh + 1)],
+                   own_settings)
+    elif settings.alg == 'par_alns':
+        alg = ParallelALNS(solution,
+                           [Method(f"ch{i}", solution_class.construct, i) for i in range(settings.meths_ch)],
+                           [Method(f"de{i}", solution_class.destroy, i) for i in range(1, settings.meths_de + 1)],
+                           [Method(f"re{i}", solution_class.repair, i) for i in range(1, settings.meths_re + 1)],
+                           own_settings)
+    elif settings.alg == 'ssga':
+        alg = SteadyStateGeneticAlgorithm(solution,
+                                          [Method("ch{i}", solution_class.construct, i) for i in
+                                           range(settings.meths_ch)],
+                                          solution_class.crossover,
+                                          Method("mu", solution_class.shaking, 1),
+                                          Method("ls", solution_class.local_improve, 1),
+                                          own_settings)
+    elif settings.alg == 'sa':
+        alg = SA(solution,
+                 [Method(f"ch{i}", solution_class.construct, i) for i in range(settings.meths_ch)],
+                 solution_class.random_move_delta_eval, solution_class.apply_neighborhood_move, iter_cb, own_settings)
+    else:
+        raise ValueError('Invalid optimization algorithm selected (settings.alg): ', settings.alg)
+
+    alg.run()
+    logger.info("")
+    alg.method_statistics()
+    alg.main_results()
+    return solution
---
+++ packages/pymhlib/pymhlib-0.1.4-py3-none-any.whl/pymhlib/settings.py
@@ -0,0 +1,141 @@
+"""
+Provides configuration file and command line argument parsing functionality to all modules.
+
+Parameters can be decentrally defined in any module by getting the global parser via get_settings_parser
+and registering them by add_argument(). parse_settings() needs to be called one in the main program, then
+all parameters are available under the global Namespace settings.
+If sys.argv shall not be used, e.g., because pymhlib is embedded in some framework like Django or
+a Jupyter notebook, pass "" as args (or some meaningful initialization parameters).
+
+For the usage of config files see the documentation of configargparse or call the program with -h.
+"""
+
+import pickle
+import random
+import numpy as np
+from configargparse import ArgParser, Namespace, ArgumentDefaultsRawHelpFormatter
+
+
+settings = Namespace()  # global Namespace with all settings
+unknown_args = []  # global list with all unknown parameters
+_parser = None  # single global settings parser
+
+
+def get_settings_parser() -> ArgParser:
+    """Returns the single global argument parser for adding parameters.
+
+    Parameters can be added in all modules by add_argument.
+    After calling parse() once in the main program, all settings
+    are available in the global settings dictionary.
+    """
+    global _parser  # pylint: disable=global-statement
+    if not _parser:
+        _parser = ArgParser(  # default_config_files=["default.cfg"],
+                                           formatter_class=ArgumentDefaultsRawHelpFormatter)
+        _parser.set_defaults(seed=0)
+    return _parser
+
+
+def boolArg(v):
+    """Own boolean type for arguments, which converts a string into a bool.
+
+    Provide it as type in add_argument.
+    """
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise ValueError('Boolean value expected.')
+
+
+def parse_settings(args=None, return_unknown=False, default_config_files=None, seed=0):
+    """Parses the config files and command line arguments and initializes settings and unknown_parameters.
+
+    Needs to be called once in the main program, or more generally after all arguments have been added to the parser
+    and before they are used.
+    Also seeds the random number generators based on parameter seed.
+    If sys.argv shall not be used, e.g., because pymhlib is embedded in some framework like Django or
+    a Jupyter notebook, pass "" as args (or some meaningful initialization parameters).
+
+    :param args: optional sequence of string arguments; if None sys.argv is used
+    :param return_unknown: return unknown parameters as list in global variable unknown_args; otherwise raise exception
+    :param default_config_files: list of default config files to read
+    :param seed: Seed value for initializing random number generators, 0: random
+    """
+    global settings, unknown_args  # pylint: disable=global-statement
+    p = get_settings_parser()
+    if not settings.__dict__:
+        p.add_argument('--seed', type=int, help='seed for the random number generators (0: random init)', default=seed)
+        p.add_argument('-c', '--config', is_config_file=True, help='config file to be read')
+    else:
+        # parse_settings has already been called once, reset
+        settings.seed = seed
+
+    p._default_config_files = default_config_files if default_config_files else []  # pylint: disable=protected-access
+    if return_unknown:
+        _, unknown_args[:] = p.parse_known_args(args=args, namespace=settings)
+    else:
+        p.parse_args(args=args, namespace=settings)
+
+    seed_random_generators()
+
+
+def set_settings(s: Namespace):
+    """Adopt given settings.
+
+    Used, for example in child processes to adopt settings from parent process.
+    """
+    settings.__dict__ = s.__dict__
+    seed_random_generators()
+
+
+def seed_random_generators(seed=None):
+    """Initialize random number generators with settings.seed or the given value; if zero, a random seed is generated.
+    """
+    if seed is not None:
+        settings.seed = seed
+    if settings.seed == 0:
+        np.random.seed(None)
+        settings.seed = np.random.randint(np.iinfo(np.int32).max)
+    np.random.seed(settings.seed)
+    random.seed(settings.seed)
+
+
+def save_settings(filename):
+    """Save settings to given binary file."""
+    with open(filename, 'wb') as f:
+        pickle.dump(settings, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_settings(filename):
+    """Load settings from given binary file."""
+    with open(filename, 'rb') as f:
+        global settings  # pylint: disable=global-statement
+        settings.__dict__ = vars(pickle.load(f))
+        seed_random_generators()
+
+
+def get_settings_as_str():
+    """Get all parameters and their values as descriptive multi-line string."""
+    s = "\nsettings:\n"
+    for key, value in sorted(vars(settings).items()):
+        s += f"{key}={value}\n"
+    return s
+
+
+class OwnSettings:
+    """An individualized settings storage, which falls back to the default setting for not provided parameters."""
+
+    def __init__(self, own_settings: dict = None):
+        self.__dict__ = own_settings if own_settings else dict()
+
+    def __getattr__(self, item):
+        try:
+            return self.__dict__[item]
+        except KeyError:
+            val = settings.__getattribute__(item)
+            self.__setattr__(item, val)
+            return val

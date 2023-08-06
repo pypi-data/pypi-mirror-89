ab073a8b0e88dcc266f345c90adca8bd912063da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:52 +0000

    Add package LiteVkApi/0.1/LiteVkApi-0.1.tar.gz

---
+++ packages/LiteVkApi/LiteVkApi-0.1.tar.gz/LiteVkApi-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+name='LiteVkApi', 
+version='0.1',
+description='Библиотека для лекгого написания ботов ВК!', 
+packages=['LiteVkApi'], 
+author_email='ma_mush@mail.ru', 
+zip_safe=False
+)

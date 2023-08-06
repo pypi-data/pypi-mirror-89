8577991c7ce77d84c29ffb47ec177345e96fd1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:41 +0000

    Add package scrapy-x/1.0.0/scrapy-x-1.0.0.tar.gz

---
+++ packages/scrapy-x/scrapy-x-1.0.0.tar.gz/scrapy-x-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
+setup(
+    name='scrapy-x',
+    packages=find_packages(),
+    install_requires=required,
+    version='1.0.0',
+    author='Mohamed Al Ashaal',
+    author_email='m7medalash3al@gmail.com',
+    license='Apache License V2.0',
+    description='a scrapy subcommand for easily enqueuing crawling jobs in a scalable and high performance way',
+    summary='a scrapy serving module',
+    # long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/alash3al/scrapyx',
+    python_requires='>=3.6.9',
+    entry_points={
+        'scrapy.commands': [
+            'x=scrapyx.x:Command',
+        ],
+    }
+)

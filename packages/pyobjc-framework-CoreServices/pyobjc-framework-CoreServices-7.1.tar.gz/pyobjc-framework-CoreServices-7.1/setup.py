0b786e376183f241c15bc68886974ba6b840e5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:39 +0000

    Add package pyobjc-framework-CoreServices/7.1/pyobjc-framework-CoreServices-7.1.tar.gz

---
+++ packages/pyobjc-framework-CoreServices/pyobjc-framework-CoreServices-7.1.tar.gz/pyobjc-framework-CoreServices-7.1/setup.py
@@ -0,0 +1,40 @@
+"""
+Wrappers for the "CoreServices" framework on macOS.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+import os
+
+from pyobjc_setup import Extension, setup
+
+VERSION = '7.1'
+
+subpackages = [
+    "CoreServices.%s" % (fn,)
+    for fn in os.listdir("Lib/CoreServices")
+    if os.path.exists(os.path.join("Lib/CoreServices", fn, "__init__.py"))
+]
+
+setup(
+    name="pyobjc-framework-CoreServices",
+    description="Wrappers for the framework CoreServices on macOS",
+    packages=["CoreServices"] + subpackages,
+    ext_modules=[
+        Extension(
+            "CoreServices._inlines",
+            ["Modules/_CoreServices_inlines.m"],
+            extra_link_args=["-framework", "CoreServices"],
+            py_limited_api=True,
+        )
+    ],
+    version=VERSION,
+    install_requires=[
+        "pyobjc-core>=" + VERSION,
+        "pyobjc-framework-FSEvents>=" + VERSION,
+    ],
+    long_description=__doc__,
+    options={"bdist_wheel": {"py_limited_api": "cp36"}},
+)

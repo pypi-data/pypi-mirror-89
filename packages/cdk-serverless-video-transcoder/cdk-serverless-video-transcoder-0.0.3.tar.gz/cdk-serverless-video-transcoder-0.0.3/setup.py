be55b083f370fe1a08262de7588c1b21baee96e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:55 +0000

    Add package cdk-serverless-video-transcoder/0.0.3/cdk-serverless-video-transcoder-0.0.3.tar.gz

---
+++ packages/cdk-serverless-video-transcoder/cdk-serverless-video-transcoder-0.0.3.tar.gz/cdk-serverless-video-transcoder-0.0.3/setup.py
@@ -0,0 +1,71 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdk-serverless-video-transcoder",
+    "version": "0.0.3",
+    "description": "cdk-serverless-video-transcoder",
+    "license": "Apache-2.0",
+    "url": "https://github.com/user/cdk-serverless-video-transcoding.git",
+    "long_description_content_type": "text/markdown",
+    "author": "readybuilderone<user@domain.com>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/user/cdk-serverless-video-transcoding.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdk_serverless_video_transcoder",
+        "cdk_serverless_video_transcoder._jsii"
+    ],
+    "package_data": {
+        "cdk_serverless_video_transcoder._jsii": [
+            "cdk-serverless-video-transcoder@0.0.3.jsii.tgz"
+        ],
+        "cdk_serverless_video_transcoder": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk.aws-ec2>=1.76.0, <2.0.0",
+        "aws-cdk.aws-efs>=1.76.0, <2.0.0",
+        "aws-cdk.aws-iam>=1.76.0, <2.0.0",
+        "aws-cdk.aws-lambda-event-sources>=1.76.0, <2.0.0",
+        "aws-cdk.aws-lambda>=1.76.0, <2.0.0",
+        "aws-cdk.aws-s3>=1.76.0, <2.0.0",
+        "aws-cdk.aws-stepfunctions-tasks>=1.76.0, <2.0.0",
+        "aws-cdk.aws-stepfunctions>=1.76.0, <2.0.0",
+        "aws-cdk.core>=1.76.0, <2.0.0",
+        "constructs>=3.2.27, <4.0.0",
+        "jsii>=1.16.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ]
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)

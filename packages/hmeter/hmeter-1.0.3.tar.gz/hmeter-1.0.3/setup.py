11d250b23092d81c07a77604cda62f938b70c52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:16 +0000

    Add package hmeter/1.0.3/hmeter-1.0.3.tar.gz

---
+++ packages/hmeter/hmeter-1.0.3.tar.gz/hmeter-1.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hmeter", # Replace with your own username
+    version="1.0.3",
+    author="Athul Mathew Konoor",
+    author_email="athulmathewkonoor@gmail.com",
+    description="A package to check heart related issus.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toshihiroryuu/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+       "gradio >= 1.4.0",
+       "scikit-learn >= 0.24.0"
+   ],
+    python_requires='>=3.6',
+)

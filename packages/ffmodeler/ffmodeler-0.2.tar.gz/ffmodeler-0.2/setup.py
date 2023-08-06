7c03dc04f43dc51fddecf399185475418699bc72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:11 +0000

    Add package ffmodeler/0.2/ffmodeler-0.2.tar.gz

---
+++ packages/ffmodeler/ffmodeler-0.2.tar.gz/ffmodeler-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='ffmodeler',
+      version='0.2',
+      description='Farfield modeler',
+      packages=['ffmodeler'],
+      author='Sergio Barreto',
+      author_email='sergiobarreto@id.uff.br',
+      zip_safe=False)

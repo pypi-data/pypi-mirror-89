2531ba41591944a986a5368ba2a3c539d018a05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:01 +0000

    Add package argsls/0.0.1/argsls-0.0.1-py3-none-any.whl

---
+++ packages/argsls/argsls-0.0.1-py3-none-any.whl/ArgTools/__init__.py
@@ -0,0 +1,10 @@
+class count_args:
+  def count(args):
+    args=args.split(" ")
+    return len(args)-1
+args_count=count_args
+class split_args:
+  def list(args):
+    args=args.split(" ")
+    return args
+args_split=split_args

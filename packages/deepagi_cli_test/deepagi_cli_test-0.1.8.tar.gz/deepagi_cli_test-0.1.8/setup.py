0c206ef61921da502d1ba69caa6786880cbe7312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:55 +0000

    Add package deepagi_cli_test/0.1.8/deepagi_cli_test-0.1.8.tar.gz

---
+++ packages/deepagi_cli_test/deepagi_cli_test-0.1.8.tar.gz/deepagi_cli_test-0.1.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+setup(name='deepagi_cli_test',
+      version='0.1.8',
+      description='Testing CLI Commands',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='deepagi',
+      url='',
+      author='delan',
+      author_email='',
+      license='MIT',
+      packages=['deepagi_cli_test'],
+      install_requires=[
+#           'markdown',
+      ],
+      # include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=['bin/deepagi_cli'],
+     )

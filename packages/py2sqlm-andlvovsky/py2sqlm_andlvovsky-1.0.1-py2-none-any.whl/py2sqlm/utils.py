fe231ff358c7ccfeda5b344ab9fe966dad72ac95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:07 +0000

    Add package py2sqlm-andlvovsky/1.0.1/py2sqlm_andlvovsky-1.0.1-py2-none-any.whl

---
+++ packages/py2sqlm-andlvovsky/py2sqlm_andlvovsky-1.0.1-py2-none-any.whl/py2sqlm/utils.py
@@ -0,0 +1,10 @@
+import re
+
+
+def camel_case_to_snake_case(text):
+    """
+    Convert text from camel case to snake case
+    :param text: camel case text
+    :return: snake case text
+    """
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', text).lower()

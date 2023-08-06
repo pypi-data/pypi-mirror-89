223a08e519e050a3cfccb9c49a39f12675d42e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:33 +0000

    Add package lobe/0.3.0/lobe-0.3.0.tar.gz

---
+++ packages/lobe/lobe-0.3.0.tar.gz/lobe-0.3.0/README.md
@@ -0,0 +1,74 @@
+# Lobe Python API
+Code to run exported Lobe models in Python using the TensorFlow, TensorFlow Lite, or ONNX options.
+
+## Install
+### Linux
+```shell script
+# Install Python3
+sudo apt update
+sudo apt install -y python3-dev python3-pip
+
+# Install Pillow dependencies
+sudo apt update
+sudo apt install -y \
+    libatlas-base-dev \
+    libopenjp2-7 \
+    libtiff5 \
+    libjpeg62-dev
+
+# Install lobe-python
+pip3 install setuptools git
+pip3 install git+https://github.com/lobe/lobe-python
+```
+
+_Note for Raspbian OS (Raspberry Pi)_: Please install `libjpeg62-turbo` instead of `libjpeg62-dev`
+
+### Mac/Windows
+Use a virtual environment with Python 3.7
+```shell script
+python3 -m venv .venv
+
+# Mac:
+source .venv/bin/activate
+
+# Windows:
+.venv\Scripts\activate
+```
+Install the library
+```shell script
+# make sure pip is up to date
+python -m pip install --upgrade pip
+# install
+pip install git+https://github.com/lobe/lobe-python
+```
+
+## Usage
+```python
+from lobe import ImageModel
+
+model = ImageModel.load('path/to/exported/model')
+
+# OPTION 1: Predict from an image file
+result = model.predict_from_file('path/to/file.jpg')
+
+# OPTION 2: Predict from an image url
+result = model.predict_from_url('http://url/to/file.jpg')
+
+# OPTION 3: Predict from Pillow image
+from PIL import Image
+img = Image.open('path/to/file.jpg')
+result = model.predict(img)
+
+# Print top prediction
+print(result.prediction)
+
+# Print all classes
+for label, confidence in result.labels:
+    print(f"{label}: {confidence*100}%")
+
+```
+Note: model predict functions should be thread-safe. If you find bugs please file an issue.
+
+## Resources
+
+See the [Raspberry Pi Trash Classifier](https://github.com/microsoft/TrashClassifier) example, and its [Adafruit Tutorial](https://learn.adafruit.com/lobe-trash-classifier-machine-learning).
---
+++ packages/lobe/lobe-0.3.0.tar.gz/lobe-0.3.0/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import sys
+import platform
+
+
+python_version = platform.python_version().rsplit('.', maxsplit=1)[0]
+
+requirements = [
+    "pillow",
+    "requests",
+    "numpy==1.19.3",
+    "tensorflow==2.4;platform_machine!='armv7l'",
+    "onnxruntime==1.6.0;platform_machine!='armv7l'"
+]
+
+# get the right TF Lite runtime packages based on OS and python version: https://www.tensorflow.org/lite/guide/python#install_just_the_tensorflow_lite_interpreter
+tflite_python = None
+tflite_machine = None
+
+# get the right python string for the version
+if python_version == '3.5':
+    tflite_python = 'cp35-cp35m'
+elif python_version == '3.6':
+    tflite_python = 'cp36-cp36m'
+elif python_version == '3.7':
+    tflite_python = 'cp37-cp37m'
+elif python_version == '3.8':
+    tflite_python = 'cp38-cp38'
+
+# get the right machine string
+if sys.platform == 'win32':
+    tflite_machine = 'win_amd64'
+elif sys.platform == 'darwin':
+    tflite_machine = 'macosx_10_15_x86_64'
+elif sys.platform == 'linux':
+    if platform.machine() == 'x86_64':
+        tflite_machine = 'linux_x86_64'
+    elif platform.machine() == 'armv7l':
+        tflite_machine = 'linux_armv7l'
+
+# add it to the requirements, or print the location to find the version to install
+if tflite_python and tflite_machine:
+    requirements.append(f"tflite_runtime @ https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-{tflite_python}-{tflite_machine}.whl")
+else:
+    print(
+        f"Couldn't find tflite_runtime for your platform {sys.platform}, machine {platform.machine()}, and python version {python_version}, please see the install guide for the right version: https://www.tensorflow.org/lite/guide/python#install_just_the_tensorflow_lite_interpreter"
+    )
+
+setup(
+    name="lobe",
+    version="0.3.0",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+)

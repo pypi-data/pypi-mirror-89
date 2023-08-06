74ef01b00742b1699e728f17524ddfb317c4d587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:21 +0000

    Add package gifu/1.0.0/gifu-1.0.0-py3-none-any.whl

---
+++ packages/gifu/gifu-1.0.0-py3-none-any.whl/gifu/__init__.py
@@ -0,0 +1,6 @@
+"""{{ cookiecutter.package_name }} - {{ cookiecutter.package_description }}"""
+from gifu import gifuu
+# __version__ = "1.0.0"
+# __author__ = "narutolavo"
+# __all__ = ['gifu','main']
+
---
+++ packages/gifu/gifu-1.0.0-py3-none-any.whl/gifu/__main__.py
@@ -0,0 +1,4 @@
+import sys
+from gifu import gifuu
+
+gifuu.main()

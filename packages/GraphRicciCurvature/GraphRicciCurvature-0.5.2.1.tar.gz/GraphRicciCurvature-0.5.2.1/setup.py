9b27fd02d666acceca04077fb55b5daf1558068a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:09 +0000

    Add package GraphRicciCurvature/0.5.2.1/GraphRicciCurvature-0.5.2.1.tar.gz

---
+++ packages/GraphRicciCurvature/GraphRicciCurvature-0.5.2.1.tar.gz/GraphRicciCurvature-0.5.2.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GraphRicciCurvature",
+    version="0.5.2.1",
+    author="Chien-Chun Ni",
+    author_email="saibalmars@gmail.com",
+    description="Compute discrete Ricci curvatures and Ricci flow on NetworkX graphs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saibalmars/GraphRicciCurvature",
+    setup_requires=["cython", "numpy"],  # to make sure these two are installed first for dependency.
+    install_requires=install_requires,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)

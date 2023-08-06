e93b50852f9e82c606af566df41e2c61e99dc691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:08 +0000

    Add package music-test-module/0.0.8/music_test_module-0.0.8.tar.gz

---
+++ packages/music-test-module/music_test_module-0.0.8.tar.gz/music_test_module-0.0.8/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="music_test_module",
+    version="0.0.8",
+    author="DHTung",
+    author_email="dhtung161997@gmail.com",
+    packages=setuptools.find_packages(),
+    url="https://github.com/dhtung1997/music_test_module",
+    python_requires='>3.6',
+)

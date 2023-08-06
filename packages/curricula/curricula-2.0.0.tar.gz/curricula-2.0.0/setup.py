8a0aeffc03f7f20c24004f796217247a2ac23659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:41 +0000

    Add package curricula/2.0.0/curricula-2.0.0.tar.gz

---
+++ packages/curricula/curricula-2.0.0.tar.gz/curricula-2.0.0/setup.py
@@ -0,0 +1,32 @@
+import importlib.util
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+root = Path(__file__).absolute().parent
+
+with root.joinpath("README.md").open() as fh:
+    long_description = fh.read()
+
+spec = importlib.util.spec_from_file_location("curricula", str(root.joinpath("curricula", "__init__.py")))
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+setup(
+    name="curricula",
+    version=module.__version__,
+    description="A content manager and grading toolkit for evaluating student code",
+    url="https://github.com/curriculagg/curricula",
+    author="Noah Kim",
+    author_email="noahbkim@gmail.com",
+
+    # Extra
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    # Python
+    python_requires=">=3.9",
+
+    # Packaging
+    packages=find_packages(),
+    zip_safe=False)

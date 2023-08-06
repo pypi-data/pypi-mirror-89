6b96ddfebfab72352354603be6b7db8ebb9f0e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:14 +0000

    Add package cardmaker/0.0.8/cardmaker-0.0.8.tar.gz

---
+++ packages/cardmaker/cardmaker-0.0.8.tar.gz/cardmaker-0.0.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(name='cardmaker',
+      version='0.0.8',
+      description='Automate your christmas card creation!',
+      long_description=open("README.md").read(),
+      long_description_content_type='text/markdown',
+      author='Rahul Prabhu',
+      author_email='rahul@grokwithrahul.com',
+      url='https://github.com/grokwithrahul/christmas-card-creator',
+      packages=['cardmaker'],
+      install_requires=['Pillow', 'sketchify'],
+      package_data={'cardmaker':['Brusher.ttf', 'defaultbg.png']}
+     )

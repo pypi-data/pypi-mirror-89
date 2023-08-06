ce8c8b5578f76602881b538ee01daaefbfe3ffa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:20 +0000

    Add package unicef-security/0.6/unicef-security-0.6.tar.gz

---
+++ packages/unicef-security/unicef-security-0.6.tar.gz/unicef-security-0.6/src/unicef_security/__init__.py
@@ -0,0 +1,3 @@
+NAME = "unicef-security"
+VERSION = __version__ = "0.6"
+default_app_config = 'unicef_security.apps.Config'
---
+++ packages/unicef-security/unicef-security-0.6.tar.gz/unicef-security-0.6/src/unicef_security/backends.py
@@ -0,0 +1,62 @@
+import base64
+import json
+import os
+
+from django.conf import settings
+from django.urls import reverse
+
+from jwt import decode as jwt_decode, DecodeError, ExpiredSignature
+from social_core.backends.azuread_b2c import AzureADB2COAuth2
+from social_core.backends.azuread_tenant import AzureADTenantOAuth2
+from social_core.exceptions import AuthTokenError
+
+
+class UNICEFAzureADTenantOAuth2Ext(AzureADTenantOAuth2):
+    def user_data(self, access_token, *args, **kwargs):
+        response = kwargs.get('response')
+        id_token = response.get('id_token')
+
+        # decode the JWT header as JSON dict
+        jwt_header = json.loads(
+            base64.b64decode(id_token.split('.', 1)[0]).decode()
+        )
+
+        # get key id and algorithm
+        key_id = jwt_header['kid']
+        algorithm = jwt_header['alg']
+        verify = os.environ.get('OAUTH2_VERIFY', False)
+        key = ''
+        try:
+            # retrieve certificate for key_id
+            if verify:
+                certificate = self.get_certificate(key_id)
+                key = certificate.public_key()
+
+            return jwt_decode(
+                id_token,
+                verify=verify,
+                key=key,
+                algorithms=algorithm,
+                audience=self.setting('KEY')
+            )
+        except (DecodeError, ExpiredSignature) as error:
+            raise AuthTokenError(self, error)
+
+
+class UNICEFAzureADB2COAuth2(AzureADB2COAuth2):
+    """UNICEF Azure ADB2C Custom Backend"""
+
+    name = 'unicef-azuread-b2c-oauth2'
+    BASE_URL = 'https://{tenant_name}.b2clogin.com/{tenant_id}'
+
+    @property
+    def base_url(self):
+        return self.BASE_URL.format(tenant_name=self.tenant_name, tenant_id=self.tenant_id)
+
+    @property
+    def tenant_name(self):
+        return self.setting('TENANT_NAME', 'common')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.redirect_uri = settings.HOST + reverse('social:complete', kwargs={'backend': 'unicef-azuread-b2c-oauth2'})

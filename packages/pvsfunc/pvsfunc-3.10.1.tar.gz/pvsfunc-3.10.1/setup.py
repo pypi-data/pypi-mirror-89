3d937426f07d217b252c985cccb7d55ca8e657ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:26 +0000

    Add package pvsfunc/3.10.1/pvsfunc-3.10.1.tar.gz

---
+++ packages/pvsfunc/pvsfunc-3.10.1.tar.gz/pvsfunc-3.10.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name="pvsfunc",
+    version="3.10.1",
+    author="PHOENiX",
+    author_email="pragma.exe@gmail.com",
+    description="PHOENiX's compilation of VapourSynth Script's and Functions",
+    license="GPLv3",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rlaPHOENiX/pvsfunc",
+    packages=find_packages(),
+    install_requires=[
+        "vapoursynth",
+        "pymediainfo>=5.0.3",
+        "pyd2v>=1.0.4"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)

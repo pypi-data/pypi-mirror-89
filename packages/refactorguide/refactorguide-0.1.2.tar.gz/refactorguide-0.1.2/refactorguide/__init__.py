7731f8fcbf20e927aaa75f3f694bac5a06a60b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:12 +0000

    Add package refactorguide/0.1.2/refactorguide-0.1.2.tar.gz

---
+++ packages/refactorguide/refactorguide-0.1.2.tar.gz/refactorguide-0.1.2/refactorguide/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Refactor Guide."""
+
+__author__ = """Qin Yu"""
+__email__ = 'qinyusuain@gmail.com'
+__version__ = '0.1.2'
+
+import refactorguide.settings as settings
+
+settings.init()

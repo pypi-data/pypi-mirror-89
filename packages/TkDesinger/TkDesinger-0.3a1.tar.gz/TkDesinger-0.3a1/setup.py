9743a465e045cebb93bd691e01016ca5ae093717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:42 +0000

    Add package TkDesinger/0.3a1/TkDesinger-0.3a1.tar.gz

---
+++ packages/TkDesinger/TkDesinger-0.3a1.tar.gz/TkDesinger-0.3a1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import os
+
+__path__ = os.getcwd() + '\\setup.py'
+
+setup (
+    name='TkDesinger',
+	long_description=open('README.rst').read(),
+    version='0.3a1',
+    packages=['tkdesinger'],
+    url='http://ppbe.ru',
+    license='MIT',
+    author='FotonPC',
+    author_email='fototn-pc@inbox.ru',
+    description='Tkinter Desinger to Python 3',
+    include_package_data = True
+)

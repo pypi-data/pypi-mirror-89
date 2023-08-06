9dc4a9b7599dce5e33e8fdaebfb548aa8775375e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:02 +0000

    Add package zgw-consumers/0.14.0/zgw_consumers-0.14.0-py3-none-any.whl

---
+++ packages/zgw-consumers/zgw_consumers-0.14.0-py3-none-any.whl/zgw_consumers/constants.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext_lazy as _
+
+from djchoices import ChoiceItem, DjangoChoices
+
+
+class APITypes(DjangoChoices):
+    ac = ChoiceItem("ac", _("AC (Authorizations)"))
+    nrc = ChoiceItem("nrc", _("NRC (Notifications)"))
+
+    zrc = ChoiceItem("zrc", _("ZRC (Zaken)"))
+    ztc = ChoiceItem("ztc", _("ZTC (Zaaktypen)"))
+    drc = ChoiceItem("drc", _("DRC (Informatieobjecten)"))
+    brc = ChoiceItem("brc", _("BRC (Besluiten)"))
+    kic = ChoiceItem("kic", _("KIC (Klantinteracties)"))
+
+    orc = ChoiceItem("orc", _("ORC (Overige)"))
+
+
+class AuthTypes(DjangoChoices):
+    no_auth = ChoiceItem("no_auth", _("No authorization"))
+    api_key = ChoiceItem("api_key", _("API key"))
+    zgw = ChoiceItem("zgw", _("ZGW client_id + secret"))
+
+
+class NLXDirectories(DjangoChoices):
+    demo = ChoiceItem("demo", _("Demo"))
+    preprod = ChoiceItem("preprod", _("Pre-prod"))
+    prod = ChoiceItem("prod", _("Prod"))

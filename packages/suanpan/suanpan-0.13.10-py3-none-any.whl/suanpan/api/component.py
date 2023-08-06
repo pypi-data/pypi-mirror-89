badf3c54ba3e8ab3395c9ad9105b847423fbc572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:15 +0000

    Add package suanpan/0.13.10/suanpan-0.13.10-py3-none-any.whl

---
+++ packages/suanpan/suanpan-0.13.10-py3-none-any.whl/suanpan/api/component.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan import g
+from suanpan.api.requests import affinity
+
+
+def listComponents(limit=9999):
+    return affinity.post("/component/list", json={"limit": limit})["list"]
+
+
+def shareComponent(componentId, userId, name):
+    return affinity.post(
+        "/component/share",
+        json={"id": componentId, "targetUserId": userId, "name": name},
+    )
+
+
+def exportModel(portId, name, appId=None, nodeId=None, overwrite=False):
+    return affinity.post(
+        "/component/export/model",
+        json={
+            "id": appId or g.appId,
+            "nodeId": nodeId or g.nodeId,
+            "portId": portId,
+            "name": name,
+            "overwrite": overwrite,
+        },
+    )
---
+++ packages/suanpan/suanpan-0.13.10-py3-none-any.whl/suanpan/api/requests/__init__.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+import base64
+import hashlib
+import hmac
+
+import requests
+
+from suanpan import error, g, utils
+from suanpan.log import logger
+
+
+def signatureV1(secret, data):
+    h = hmac.new(utils.encode(secret), utils.encode(data), hashlib.sha1)
+    return utils.decode(base64.b64encode(utils.encode(h.digest())))
+
+
+def defaultHeaders():
+    if not g.userIdHeaderField:
+        raise error.ApiError("UserIdHeaderField not set")
+    return {
+        g.userIdHeaderField: g.userId,
+        g.userSignatureHeaderField: signatureV1(g.accessSecret, g.userId),
+        g.userSignVersionHeaderField: "v1",
+    }
+
+
+def session(auth=True):
+    sess = requests.Session()
+    if auth:
+        sess.headers.update(defaultHeaders())
+    return sess
+
+
+def request(method, url, *args, **kwargs):
+    sess = session(auth=kwargs.get("auth", True))
+    func = getattr(sess, method)
+    rep = func(url, *args, **kwargs)
+    logger.debug(
+        f"[{rep.status_code}] {rep.url} - {rep.request.method} - {rep.content}"
+    )
+    rep.raise_for_status()
+    result = rep.json()
+    if not result.get("success", True):
+        raise error.ApiError(f"Request failed {result}")
+    return result
+
+
+def get(url, *args, **kwargs):
+    return request("get", url, *args, **kwargs)
+
+
+def post(url, *args, **kwargs):
+    return request("post", url, *args, **kwargs)
+
+
+def put(url, *args, **kwargs):
+    return request("put", url, *args, **kwargs)
+
+
+def delete(url, *args, **kwargs):
+    return request("delete", url, *args, **kwargs)

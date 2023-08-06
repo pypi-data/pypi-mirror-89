5396b5b6957e6a6414f346b94b02f9f4d5bbd790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:02 +0000

    Add package gigatronapi/0.0.2/gigatronapi-0.0.2.tar.gz

---
+++ packages/gigatronapi/gigatronapi-0.0.2.tar.gz/gigatronapi-0.0.2/README.txt
@@ -0,0 +1,1 @@
+# Gigatron Api Wrapper
---
+++ packages/gigatronapi/gigatronapi-0.0.2.tar.gz/gigatronapi-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Operating System :: POSIX :: Linux",
+    "License :: OSI Approved :: The Unlicense (Unlicense)",
+   "Programming Language :: Python :: 3.9"
+
+
+]
+
+setup(name ="gigatronapi",
+      version = "0.0.2",
+      description = "Gigatron API Wrapper",
+      long_description = open("README.txt").read() + "\n\n" + open("CHANGELOG.txt").read(),
+      url="",
+      author = "Darko Dekan",
+      author_email = "darko.dekan@protonmail.com",
+      license="The Unlicense (Unlicense)",
+      classifiers=classifiers,
+      keywords="gigatron",
+      packages=find_packages(),
+      install_requires=['requests']
+)

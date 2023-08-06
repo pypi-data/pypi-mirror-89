6f5b7d224f54b2b2221f65cb584eee20dff386bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:43 +0000

    Add package pretalx-downstream/0.9.1/pretalx-downstream-0.9.1.tar.gz

---
+++ packages/pretalx-downstream/pretalx-downstream-0.9.1.tar.gz/pretalx-downstream-0.9.1/pretalx_downstream/urls.py
@@ -0,0 +1,13 @@
+from django.urls import re_path
+
+from pretalx.event.models.event import SLUG_CHARS
+
+from .views import UpstreamSettings
+
+urlpatterns = [
+    re_path(
+        fr"^orga/event/(?P<event>[{SLUG_CHARS}]+)/settings/p/upstream/$",
+        UpstreamSettings.as_view(),
+        name="settings",
+    )
+]

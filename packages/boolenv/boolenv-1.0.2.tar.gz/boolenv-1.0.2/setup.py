237568d606225c7d36bbf4b5c238e2ff2d9fb79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:40 +0000

    Add package boolenv/1.0.2/boolenv-1.0.2.tar.gz

---
+++ packages/boolenv/boolenv-1.0.2.tar.gz/boolenv-1.0.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="boolenv",
+    version="1.0.2",
+    author="Ondrej Sika",
+    author_email="ondrej@ondrejsika.com",
+    description="Read Boolean value from environment variable",
+    url="https://github.com/ondrejsika/python-boolenv",
+    packages=setuptools.find_packages(),
+)

afe0823bdcfa92d13ecef6b3dd4e2edff3081a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:58 +0000

    Add package flatland-model-diagram-editor/0.1.1/flatland-model-diagram-editor-0.1.1.tar.gz

---
+++ packages/flatland-model-diagram-editor/flatland-model-diagram-editor-0.1.1.tar.gz/flatland-model-diagram-editor-0.1.1/setup.py
@@ -0,0 +1,31 @@
+"""Setup script for Flatland model diagram editor"""
+
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="flatland-model-diagram-editor",
+    version="0.1.1",
+    description="Model editor",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/modelint/Flatland",
+    author="Leon Starr",
+    author_email="leon_starr@modelint.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["flatland"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={"console_scripts": ["flatland=flatland.__main__:main"]},
+)

3f29a2a9cca211594c66b6f92ec4f9e7ec8c85d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:04 +0000

    Add package redpay/1.2.8/RedPay-1.2.8.tar.gz

---
+++ packages/redpay/RedPay-1.2.8.tar.gz/RedPay-1.2.8/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='RedPay',
+    version='1.2.8',
+    packages=['RedPay'],
+    url='https://bitbucket.org/redshepherdinc/python-api.git',
+    author='Red Shepherd Inc.',
+    author_email='support@redshepherd.com',
+    description='Files for integrating with the RedPay Engine',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        "pycryptodome>=3.9.9",
+    ],
+)

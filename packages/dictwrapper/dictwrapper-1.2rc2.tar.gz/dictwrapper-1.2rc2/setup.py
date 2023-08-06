08f1377f280a9f65a602e535808b5f8425b3dd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:00 +0000

    Add package dictwrapper/1.2rc2/dictwrapper-1.2rc2.tar.gz

---
+++ packages/dictwrapper/dictwrapper-1.2rc2.tar.gz/dictwrapper-1.2rc2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dictwrapper',
+    packages=find_packages(),
+    install_requires=[
+        "ruamel.yaml<=0.15",
+        "pandas>=1.1"
+    ],
+    version='1.2rc2',
+    description='Basic Dictionary Wrapper',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Nicolas Deutschmann',
+    author_email="nicolas.deutschmann@gmail.com",
+    license='MIT',
+)

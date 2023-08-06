b1dc63f2278ddce3980518de06e636c44a6af104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:10 +0000

    Add package pyteleprint/1.0.0/pyteleprint-1.0.0.tar.gz

---
+++ packages/pyteleprint/pyteleprint-1.0.0.tar.gz/pyteleprint-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyteleprint", # Replace with your own username
+    version="1.0.0",
+    author="Rojit George",
+    author_email="rojitrgeorge@gmail.com",
+    description="A package for remote program alerts,prints and notification through Telegram",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/krrgeorges/pyteleprint",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)

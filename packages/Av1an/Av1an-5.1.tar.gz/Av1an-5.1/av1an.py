25c64c1bf79547f3ad0dd53304ae5ca5a0bd3898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:40 +0000

    Add package Av1an/5.1/Av1an-5.1.tar.gz

---
+++ packages/Av1an/Av1an-5.1.tar.gz/Av1an-5.1/av1an.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from av1an import Args
+from manager import Manager
+from startup.setup import startup_check
+
+if __name__ == '__main__':
+    parser = Args()
+    project = parser.get_project()
+    startup_check(project)
+    manager = Manager.Main(project)
+    manager.run()

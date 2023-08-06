1d37b890b2ac0d699c402f62cd6aba1f84690def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:19 +0000

    Add package pymars/0.7.0a2/pymars-0.7.0a2-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/pymars/pymars-0.7.0a2-cp36-cp36m-manylinux1_x86_64.whl/mars/dataframe/reduction/sem.py
@@ -0,0 +1,58 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import functools
+
+from ... import opcodes as OperandDef
+from ...config import options
+from ...core import OutputType
+from ...serialize import Int32Field
+from .core import DataFrameReductionOperand, DataFrameReductionMixin
+
+
+class DataFrameSem(DataFrameReductionOperand, DataFrameReductionMixin):
+    _op_type_ = OperandDef.SEM
+    _func_name = 'sem'
+
+    _ddof = Int32Field('ddof')
+
+    def __init__(self, ddof=None, **kw):
+        super().__init__(_ddof=ddof, **kw)
+
+    @property
+    def ddof(self):
+        return self._ddof
+
+    @classmethod
+    def _make_agg_object(cls, op):
+        from .aggregation import sem_function
+        pf = functools.partial(sem_function, ddof=op.ddof, skipna=op.skipna)
+        pf.__name__ = cls._func_name
+        return pf
+
+
+def sem_series(series, axis=None, skipna=None, level=None, ddof=1, combine_size=None):
+    use_inf_as_na = options.dataframe.mode.use_inf_as_na
+    op = DataFrameSem(axis=axis, skipna=skipna, level=level, ddof=ddof,
+                      combine_size=combine_size, output_types=[OutputType.scalar],
+                      use_inf_as_na=use_inf_as_na)
+    return op(series)
+
+
+def sem_dataframe(df, axis=None, skipna=None, level=None, ddof=1, numeric_only=None, combine_size=None):
+    use_inf_as_na = options.dataframe.mode.use_inf_as_na
+    op = DataFrameSem(axis=axis, skipna=skipna, level=level, ddof=ddof,
+                      numeric_only=numeric_only, combine_size=combine_size,
+                      output_types=[OutputType.series], use_inf_as_na=use_inf_as_na)
+    return op(df)

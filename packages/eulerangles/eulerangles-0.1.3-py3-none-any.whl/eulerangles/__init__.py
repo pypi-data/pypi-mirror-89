561d1def30534998965002ae92c84b8217ccca5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:26 +0000

    Add package eulerangles/0.1.3/eulerangles-0.1.3-py3-none-any.whl

---
+++ packages/eulerangles/eulerangles-0.1.3-py3-none-any.whl/eulerangles/__init__.py
@@ -0,0 +1,4 @@
+from .conventions import EulerAngleConvention, EMEulerAngleConvention
+from .conversions import euler2matrix, matrix2euler, euler2euler
+from .rotations import RotX, RotY, RotZ
+from .version import __version__

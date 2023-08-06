3fe7423c1b45d21ce1102bc60d07988ee6d3193e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:28 +0000

    Add package weighted-set/0.1.0/weighted-set-0.1.0.tar.gz

---
+++ packages/weighted-set/weighted-set-0.1.0.tar.gz/weighted-set-0.1.0/README.md
@@ -0,0 +1,3 @@
+# weighted-set
+Name-squatting. See [weightedset](https://pypi.org/project/weightedset/)
+for a weighted set implementation.
---
+++ packages/weighted-set/weighted-set-0.1.0.tar.gz/weighted-set-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['weightedset']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'weighted-set',
+    'version': '0.1.0',
+    'description': 'Weighted set',
+    'long_description': '# weighted-set\nName-squatting. See [weightedset](https://pypi.org/project/weightedset/)\nfor a weighted set implementation.',
+    'author': 'Gary Donovan',
+    'author_email': 'gazza@gazza.id.au',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Oovvuu/weightedset',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

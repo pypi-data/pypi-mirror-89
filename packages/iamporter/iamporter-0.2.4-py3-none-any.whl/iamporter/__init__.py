2b5a950779cbb316ce925a019ed53f4e926f354b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:00 +0000

    Add package iamporter/0.2.4/iamporter-0.2.4-py3-none-any.whl

---
+++ packages/iamporter/iamporter-0.2.4-py3-none-any.whl/iamporter/__init__.py
@@ -0,0 +1,10 @@
+from .base import IamportResponse, IamportAuth
+from . import api, consts, errors
+from .client import Iamporter
+
+__version__ = "0.2.4"
+
+__all__ = ['__version__',
+           'IamportResponse', 'IamportAuth',
+           'api', 'consts', 'errors',
+           'Iamporter', ]

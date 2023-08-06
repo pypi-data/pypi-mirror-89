1d8f3a4676a4f1af447a91145409ea7a27399241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:25 +0000

    Add package chocs/0.7.0/chocs-0.7.0.tar.gz

---
+++ packages/chocs/chocs-0.7.0.tar.gz/chocs-0.7.0/chocs/router_middleware.py
@@ -0,0 +1,36 @@
+from typing import Callable, Dict, Union
+
+from .http_error import HttpError
+from .http_method import HttpMethod
+from .http_request import HttpRequest
+from .http_response import HttpResponse
+from .middleware import Middleware, MiddlewareHandler
+from .routing import Route, Router
+from .serverless.serverless import ServerlessFunction
+
+
+class RouterMiddleware(Middleware):
+    def __init__(self):
+        self.routes: Dict[HttpMethod, Router] = {key: Router() for key in HttpMethod}
+
+    def handle(self, request: HttpRequest, next: MiddlewareHandler) -> HttpResponse:
+        try:
+            route, controller = self.routes[request.method].match(
+                request.path
+            )  # type: Route, Union[Callable, ServerlessFunction]
+
+            request.path_parameters = route.parameters
+            request.route = route
+
+            response: HttpResponse
+            if isinstance(controller, ServerlessFunction):
+                response = controller.function(request)
+            else:
+                response = controller(request)
+
+            return response
+        except HttpError as error:
+            return HttpResponse(status=error.status_code, body=error.http_message)
+
+
+__all__ = ["RouterMiddleware"]

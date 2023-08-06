0f682a6d645166a22b801e5af6e42e297b1a7bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:38 +0000

    Add package nrv/0.0.5/nrv-0.0.5.tar.gz

---
+++ packages/nrv/nrv-0.0.5.tar.gz/nrv-0.0.5/nrv/__init__.py
@@ -0,0 +1,27 @@
+import importlib
+import pkgutil
+import os
+
+def list_versions():
+    result = []
+    pkg = importlib.import_module('nrv')
+    pkgpath = os.path.dirname(pkg.__file__)
+    for _, name, _ in pkgutil.iter_modules([pkgpath]):
+        if name.startswith('v') and name[1:].isalnum():
+            result.append(name)
+    return result
+
+
+def get_version(version=None, exclude_uppercase=True):
+    if version is None:
+        latest = max([int(v[1:]) for v in list_versions()])
+        version = 'v%s' % latest
+
+    v =  importlib.import_module('nrv.%s' % version)
+    result = []
+    for role in v.data:
+        result.append('::%s' % role)
+        for kind in v.data[role]:
+            if not kind.isupper():
+                result.append('::%s:%s' % (role, kind))
+    return result

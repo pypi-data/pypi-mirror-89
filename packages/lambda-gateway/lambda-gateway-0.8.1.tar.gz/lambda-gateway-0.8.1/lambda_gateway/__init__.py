54817690e81e1e61ee37aef4b4f45ebf528ee965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:03 +0000

    Add package lambda-gateway/0.8.1/lambda-gateway-0.8.1.tar.gz

---
+++ packages/lambda-gateway/lambda-gateway-0.8.1.tar.gz/lambda-gateway-0.8.1/lambda_gateway/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(__package__).version
+except pkg_resources.DistributionNotFound:  # pragma: no cover
+    __version__ = None
+
+
+def set_stream_logger(name, level=logging.DEBUG, format_string=None):
+    """
+    Adapted from boto3.set_stream_logger()
+    """
+    if format_string is None:
+        format_string = \
+            '%(addr)s - - [%(asctime)s] %(levelname)s - %(message)s'
+
+    logger = logging.getLogger(name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(format_string, '%-d/%b/%Y %H:%M:%S')
+    adapter = logging.LoggerAdapter(logger, dict(addr='::1'))
+    logger.setLevel(level)
+    handler.setLevel(level)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return adapter
+
+
+logger = set_stream_logger(__name__)

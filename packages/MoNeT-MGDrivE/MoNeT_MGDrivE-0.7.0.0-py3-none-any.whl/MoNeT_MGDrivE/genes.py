bbb8b632d7d0bdbc2255d8253b6350ce74afae3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:31 +0000

    Add package MoNeT-MGDrivE/0.7.0.0/MoNeT_MGDrivE-0.7.0.0-py3-none-any.whl

---
+++ packages/MoNeT-MGDrivE/MoNeT_MGDrivE-0.7.0.0-py3-none-any.whl/MoNeT_MGDrivE/genes.py
@@ -0,0 +1,43 @@
+
+import re
+
+
+def countGeneAppearances(genotypes, gene, pos):
+    # Split genotypes
+    splitGenotypes = [list(genes) for genes in genotypes]
+    # Count
+    appearances = []
+    for p in pos:
+        slot = [gene[p] for gene in splitGenotypes]
+        matches = re.finditer(gene, ''.join(slot))
+        appearances.extend([match.start() for match in matches])
+    appearances.sort()
+    return appearances
+
+
+def flatten(ls): return [item for sublist in ls for item in sublist]
+
+
+def aggregateGeneAppearances(genotypes, genes):
+    gcnt = [countGeneAppearances(genotypes, gn[0], gn[1]) for gn in genes]
+    return sorted(flatten(gcnt))
+
+
+def geneFrequencies(genesDict, genotypes):
+    gKeys = list(genesDict.keys())
+    genes = [genesDict[i] for i in gKeys]
+    geneAggs = [aggregateGeneAppearances(genotypes, i) for i in genes]
+    return (gKeys, geneAggs)
+
+
+def carrierFrequencies(genesDict, genotypes, invert=False):
+    gKeys = list(genesDict.keys())
+    genes = [genesDict[i] for i in gKeys]
+    (gPos, oPos) = [
+        set(aggregateGeneAppearances(genotypes, i)) for i in genes
+    ]
+    if invert:
+        geneSets = [list(i) for i in (gPos - oPos, oPos, gPos | oPos)]
+    else:
+        geneSets = [list(i) for i in (gPos, oPos - gPos, gPos | oPos)]
+    return (gKeys, geneSets)

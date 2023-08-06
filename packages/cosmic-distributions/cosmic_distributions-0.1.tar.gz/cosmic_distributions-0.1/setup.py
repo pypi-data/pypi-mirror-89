0c9a2f8f4a7816b87807ec65c27754279a2896b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:13 +0000

    Add package cosmic-distributions/0.1/cosmic_distributions-0.1.tar.gz

---
+++ packages/cosmic-distributions/cosmic_distributions-0.1.tar.gz/cosmic_distributions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='cosmic_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['cosmic_distributions'],
+      author= 'Markus Huber',
+      author_email = 'mbhuber@yahoo.com',
+      
+      zip_safe=False)

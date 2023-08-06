a9f3a5f1b0761f7cb3c0146ed334c816308bb997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:16 +0000

    Add package cpprb/9.3.3/cpprb-9.3.3.tar.gz

---
+++ packages/cpprb/cpprb-9.3.3.tar.gz/cpprb-9.3.3/cpprb/__init__.py
@@ -0,0 +1,10 @@
+from .PyReplayBuffer import (ReplayBuffer,PrioritizedReplayBuffer,
+                             SelectiveReplayBuffer)
+from .PyReplayBuffer import create_buffer, train
+
+try:
+    from .util import create_env_dict, create_before_add_func
+except ImportError:
+    # If gym is not installed, util functions are not defined.
+    pass
+

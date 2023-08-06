3282388835f3c305e7af90bed05386d6a61dec18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:28 +0000

    Add package check-lines/0.2.3/check_lines-0.2.3.tar.gz

---
+++ packages/check-lines/check_lines-0.2.3.tar.gz/check_lines-0.2.3/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+        name='check_lines',
+        author='iRyukizo',
+        version='0.2.3',
+        scripts=['check_lines.py'],
+        url='https://github.com/iRyukizo/check_lines',
+        packages=['src'],
+        python_requires='>=3.5'
+)

60a0672d35c40c7fad1910823cf85547661e3a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:32 +0000

    Add package codenamegenerator/3.1/codenamegenerator-3.1-py2.py3-none-any.whl

---
+++ packages/codenamegenerator/codenamegenerator-3.1-py2.py3-none-any.whl/codenamegenerator/__init__.py
@@ -0,0 +1,36 @@
+from typing import List
+
+import csv
+import os
+import random
+
+DICT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "dicts")
+
+
+def dictionary_sample(name: str, sample: int = 1) -> List[str]:
+    # TODO: Cache counting, and use file.seek to speed file reading.
+
+    fname = os.path.join(DICT_DIR, f"{name}.csv")
+
+    if not os.path.exists(fname):
+        raise ValueError(f"{name} dictionary does not exists.")
+
+    with open(fname, "rt") as csvfile:
+        csvreader = csv.DictReader(
+            csvfile, fieldnames=["NAME"], delimiter=",", quotechar='"'
+        )
+
+        names = [row["NAME"] for row in csvreader if row["NAME"].strip() != ""]
+
+    return random.sample(names, sample)
+
+
+def generate_codenames(
+    prefix: str = "adjectives",
+    suffix: str = "mobi_notable_scientists_and_hackers",
+    num: int = 1,
+) -> List[str]:
+    prefixes = dictionary_sample(prefix, num)
+    suffixes = dictionary_sample(suffix, num)
+
+    return [f"{prefix} {suffix}" for prefix, suffix in zip(prefixes, suffixes)]

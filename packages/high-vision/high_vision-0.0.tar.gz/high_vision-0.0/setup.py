eba370f1dbd3bc228a83f86637bb2ddeedca4f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:29 +0000

    Add package high-vision/0.0/high_vision-0.0.tar.gz

---
+++ packages/high-vision/high_vision-0.0.tar.gz/high_vision-0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'high_vision',
+      version = 0.0,
+      description = 'A Python Library for Computer Vision tasks like Object Detection, Segmentation, Pose Estimation etc',
+      url = "https://github.com/fahima10/high-vision",
+      author = "Adeel Intizar",
+      packages = find_packages(),
+      python_requires='>=3.5, <4',
+      install_requires = [
+          "tensorflow", 
+          'keras',
+          'opencv-python',
+          'numpy',
+          'pillow',
+          'matplotlib',
+          'pandas',
+          'scikit-learn',
+          'progressbar2',
+          'scipy',
+          'h5py',
+	  'imgaug',
+	  'scikit-image',
+	  'labelme2coco',
+	  'configobj',
+	  ],
+     
+      )

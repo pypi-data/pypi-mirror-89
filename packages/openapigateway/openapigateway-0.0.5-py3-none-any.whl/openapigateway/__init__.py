ebf199ceafbf15893df59dcd75aebd935612a1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:09 +0000

    Add package openapigateway/0.0.5/openapigateway-0.0.5-py3-none-any.whl

---
+++ packages/openapigateway/openapigateway-0.0.5-py3-none-any.whl/openapigateway/__init__.py
@@ -0,0 +1,11 @@
+"""
+openapigateway
+==============
+
+AWS CDK Construct that creates an Amazon API Gateway HttpApi based on a
+parameterized OpenAPI 3 Document.
+"""
+
+__version__ = "0.0.5"
+
+from openapigateway.constructs import OpenApiGateway

ce2d878a5f0a1dec1afcbf41128f6a0bbeb27e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:05 +0000

    Add package reflux/0.1.4/reflux-0.1.4.tar.gz

---
+++ packages/reflux/reflux-0.1.4.tar.gz/reflux-0.1.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="reflux",
+    version="0.1.4",
+    author="IreTheKID",
+    author_email="author@example.com",
+    description="Python package for creating IDE themes on Repl.it. Uses JavaScript bookmarlets!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IreTheKID/Reflux",
+    packages=["reflux"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

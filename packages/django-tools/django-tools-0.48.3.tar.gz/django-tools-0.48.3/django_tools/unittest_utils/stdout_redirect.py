d5056cf66059d944cee2ab8e5a46af3cd357a859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:29 +0000

    Add package django-tools/0.48.3/django-tools-0.48.3.tar.gz

---
+++ packages/django-tools/django-tools-0.48.3.tar.gz/django-tools-0.48.3/django_tools/unittest_utils/stdout_redirect.py
@@ -0,0 +1,57 @@
+import io
+import sys
+
+from django.utils.encoding import smart_text
+
+
+# origin_stderr = sys.stderr
+
+class StringBuffer(io.StringIO):
+    def write(self, data):
+        # origin_stderr.write("\nwrite to StringBuffer:\n%s\n\n" % repr(data))
+        data = smart_text(data)
+        super().write(data)
+
+
+class StdoutStderrBuffer():
+    """
+    redirect stderr and stdout for Py2 and Py3
+
+    contextlib.redirect_stdout is new in Python 3.4!
+    and we redirect stderr, too.
+
+    We use django.utils.encoding.smart_text in StringBuffer()
+    So output of bytes will only work, if there are encoded in UTF-8!
+
+    e.g:
+
+        with StdoutStderrBuffer() as buffer:
+            print("foo")
+
+        output = buffer.get_output()
+    """
+
+    def __init__(self):
+        sys.stdout.flush()
+        sys.stderr.flush()
+        self.old_stdout = sys.stdout
+        self.old_stderr = sys.stderr
+
+        self.buffer = StringBuffer()
+
+        sys.stdout = sys.stderr = self.buffer
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.old_stdout.flush()
+        self.old_stderr.flush()
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
+
+    def get_output(self):
+        self.old_stdout.flush()
+        self.old_stderr.flush()
+        output = self.buffer.getvalue()
+        return output

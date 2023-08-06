2f63c3861c316c1a890f0f9e87f502561f82101e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:47 +0000

    Add package k3down2/0.1.1/k3down2-0.1.1-py3-none-any.whl

---
+++ packages/k3down2/k3down2-0.1.1-py3-none-any.whl/k3down2/__init__.py
@@ -0,0 +1,14 @@
+"""
+k3down2 is utility to convert markdown segment into easy to transfer media sucha images.
+
+"""
+
+__version__ = "0.1.1"
+__name__ = "k3down2"
+
+from .down2 import tex_to_zhihu_url
+from .down2 import tex_to_zhihu
+from .down2 import web_to_png
+from .down2 import download
+from .down2 import md_to_html
+from .down2 import md_to_png

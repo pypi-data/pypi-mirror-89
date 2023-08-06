777db05674f844fdb0b813cef87f3d3920788069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:47 +0000

    Add package django-magic-tables/0.3.2.3/django-magic-tables-0.3.2.3.tar.gz

---
+++ packages/django-magic-tables/django-magic-tables-0.3.2.3.tar.gz/django-magic-tables-0.3.2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name = 'django-magic-tables',
+    version = '0.3.2.3',
+    description = 'A Django app to turn easily QuerySets into tables',
+    long_description_content_type= 'text/markdown',
+    long_description = README,
+    url = 'https://www.example.com/',
+    author = 'Gabriele Mattioli',
+    author_email = 'gabrymattioli@gmail.com',
+    license = 'MIT', 
+    packages=find_packages(),
+    keywords= ['Table', 'Magictable'],
+    download_url='https://pypi.org/project/django-magic-tables/',
+)
+
+if __name__ == "__main__":
+    setup(**setup_args, include_package_data=True)
+

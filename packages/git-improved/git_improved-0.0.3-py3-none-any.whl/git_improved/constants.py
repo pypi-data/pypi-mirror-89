c6f7b64317200ab5fdd02e8f02047739249b05ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:19 +0000

    Add package git-improved/0.0.3/git_improved-0.0.3-py3-none-any.whl

---
+++ packages/git-improved/git_improved-0.0.3-py3-none-any.whl/git_improved/constants.py
@@ -0,0 +1,12 @@
+
+BRANCHES_PREFIXES = {
+    'bugfix': 'Bugfix',
+    'fix': 'Bugfix', 
+    'documentation': 'Documentation', 
+    'doc': 'Documentation',
+    'feature': 'Feature', 
+    'structure': 'Structure',
+    'devops': 'CI/CD',
+    'ci': 'CI/CD',
+    'cd': 'CI/CD'
+}

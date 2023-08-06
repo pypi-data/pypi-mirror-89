5d7451ea5487333ee603f5b64deed0cb7b7c6d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:32 +0000

    Add package mailinabox-api/0.51.1/mailinabox_api-0.51.1-py3-none-any.whl

---
+++ packages/mailinabox-api/mailinabox_api-0.51.1-py3-none-any.whl/mailinabox_api/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from mailinabox_api.api.dns_api import DNSApi
+from mailinabox_api.api.mfa_api import MFAApi
+from mailinabox_api.api.mail_api import MailApi
+from mailinabox_api.api.ssl_api import SSLApi
+from mailinabox_api.api.system_api import SystemApi
+from mailinabox_api.api.user_api import UserApi
+from mailinabox_api.api.web_api import WebApi

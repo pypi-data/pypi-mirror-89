9a2c450b53b735714d57ca336714e6c4b31f8a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:36 +0000

    Add package nadima-distributions/0.2/nadima_distributions-0.2.tar.gz

---
+++ packages/nadima-distributions/nadima_distributions-0.2.tar.gz/nadima_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nadima_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['nadima_distributions'],
+      author = 'Nadima Dwihusna',
+      author_email = 'ndwihusn@alumni.mines.edu',
+      zip_safe=False)

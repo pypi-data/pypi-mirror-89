4ff835babd9737ee83be4db20362e2b15538230d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:08 +0000

    Add package pysha/0.0.1.2/pysha-0.0.1.2.tar.gz

---
+++ packages/pysha/pysha-0.0.1.2.tar.gz/pysha-0.0.1.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="pysha",
+    version="0.0.1.2",
+    author="Arshiatmi",
+    author_email="1arhacker1@gmail.com",
+    description="A New Python Micro Framework",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Arshiatmi/Pysha",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

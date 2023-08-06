9f0b12a5f0af12de382f1c7a71853436c262b451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:45 +0000

    Add package ingreedypy/1.3.5/ingreedypy-1.3.5.tar.gz

---
+++ packages/ingreedypy/ingreedypy-1.3.5.tar.gz/ingreedypy-1.3.5/README.md
@@ -0,0 +1,13 @@
+# ingreedy-py
+python port of https://github.com/iancanderson/ingreedy-js
+
+## Installation
+```bash
+$ pip install ingreedypy
+```
+
+## Local Testing
+```bash
+$ pip install -e .[tests]
+$ py.test --cov=ingreedypy ingreedytest.py
+```
---
+++ packages/ingreedypy/ingreedypy-1.3.5.tar.gz/ingreedypy-1.3.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='ingreedypy',
+    py_modules=['ingreedypy'],
+    version='1.3.5',
+    description='ingreedy-py parses recipe ingredient lines into a object',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Scott Cooper',
+    author_email='scttcper@gmail.com',
+    url='https://github.com/openculinary/ingreedy-py',
+    keywords=['ingreedy', 'ingreedypy', 'recipe', 'parser'],
+    install_requires=[
+        'parsimonious'
+    ],
+    extras_require={
+        'tests': [
+            'pytest',
+            'pytest-cov',
+        ]
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

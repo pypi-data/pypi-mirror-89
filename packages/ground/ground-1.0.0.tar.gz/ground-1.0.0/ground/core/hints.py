77e19f3e55f11d93344741a371b4b65b6b49afa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:38 +0000

    Add package ground/1.0.0/ground-1.0.0.tar.gz

---
+++ packages/ground/ground-1.0.0.tar.gz/ground-1.0.0/ground/core/hints.py
@@ -0,0 +1,11 @@
+from typing import (Callable,
+                    Sequence,
+                    TypeVar)
+
+from ground.hints import (Coordinate,
+                          Point)
+
+Expansion = Sequence[Coordinate]
+Range = TypeVar('Range')
+QuaternaryPointFunction = Callable[[Point, Point, Point, Point], Range]
+UnaryCoordinateOperation = Callable[[Coordinate], Coordinate]

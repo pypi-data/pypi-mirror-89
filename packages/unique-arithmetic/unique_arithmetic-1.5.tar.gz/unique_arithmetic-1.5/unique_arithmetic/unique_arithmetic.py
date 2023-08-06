daa9931af5d810e09cd7acbe819f105c0f4d48be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:41 +0000

    Add package unique-arithmetic/1.5/unique_arithmetic-1.5.tar.gz

---
+++ packages/unique-arithmetic/unique_arithmetic-1.5.tar.gz/unique_arithmetic-1.5/unique_arithmetic/unique_arithmetic.py
@@ -0,0 +1,18 @@
+class unique_arithmetic:
+    def jiafa(self,jiafa1,jiafa2):
+        print(jiafa1+jiafa2)
+
+    def jianfa(self,jianfa1,jianfa2):
+        print(jianfa1-jianfa2)
+
+    def chufa(self,dividend,divisor):
+        print(dividend/divisor)
+
+    def chengfa(self,multiplicator1,multiplicator2):
+        print(multiplicator1*multiplicator2)
+
+    def pi(self):
+        print(3.1415926)
+
+    def micifang(self,micifang1,micifang2):
+        print(micifang1 ** micifang2)

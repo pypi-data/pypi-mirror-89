c6e8788f83b8b4a9fc74ffeddae3df81aa460826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:07 +0000

    Add package splitbam/0.0.2/splitbam-0.0.2-py3-none-any.whl

---
+++ packages/splitbam/splitbam-0.0.2-py3-none-any.whl/splitbam/splitbam.py
@@ -0,0 +1,65 @@
+#===============================================================================
+# splitbam.py
+#===============================================================================
+
+"""Split a BAM file into two subsamples"""
+
+import os.path
+import pysam
+import subprocess
+import tempfile
+
+from argparse import ArgumentParser
+
+
+# Functions ====================================================================
+
+def parse_arguments():
+    parser = ArgumentParser(description='Split a BAM file into two subsamples')
+    parser.add_argument(
+        'input',
+        metavar='<path/to/input.bam>',
+        help='path to input BAM file'
+    )
+    parser.add_argument(
+        'output0',
+        metavar='<path/to/output0.bam>',
+        help='path to first output BAM file'
+    )
+    parser.add_argument(
+        'output1',
+        metavar='<path/to/output1.bam>',
+        help='path to second output BAM file'
+    )
+    parser.add_argument(
+        '--tmp-dir',
+        metavar='<path/to/tmp/dir>',
+        help='directory for temporary files'
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    with tempfile.TemporaryDirectory(dir=args.tmp_dir) as temp_dir:
+        temp_in = os.path.join(temp_dir, 'namesort_in.bam')
+        temp_out0 = os.path.join(temp_dir, 'namesort_out1.sam')
+        temp_out1 = os.path.join(temp_dir, 'namesort_out2.sam')
+        pysam.sort('-n', '-o', temp_in, args.input)
+        pysam.view('-H', '-o', temp_out0, temp_in)
+        pysam.view('-H', '-o', temp_out1, temp_in,)
+        with open(temp_in, 'r') as f:
+            with subprocess.Popen(
+                ('samtools', 'view'), stdin=f, stdout=subprocess.PIPE
+            ) as view:
+                subprocess.run(
+                    ('awk', f'{{if(NR%4<2){{print >> "{temp_out0}}}"}} else {{print >> "{temp_out1}}}"}}}}'),
+                    stdin=view.stdout,
+                )
+        pysam.view('-bh', '-o', args.output0, temp_out0)
+        pysam.view('-bh', '-o', args.output1, temp_out1)
+
+
+
+    
+    

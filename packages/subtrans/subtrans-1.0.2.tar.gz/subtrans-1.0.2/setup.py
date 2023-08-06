64cb263d7d87ac1c01d72361c57a8da237a77446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:29 +0000

    Add package subtrans/1.0.2/subtrans-1.0.2.tar.gz

---
+++ packages/subtrans/subtrans-1.0.2.tar.gz/subtrans-1.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+setup(
+    name='subtrans',
+    version='1.0.2',
+    py_modules=['SubtitleTranslator'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['SubtitleTranslator'],
+    url='https://github.com/Alireza-Gerami/SubtitleTranslator',
+    download_url='https://github.com/Alireza-Gerami/SubtitleTranslator/archive/main.tar.gz',
+    install_requires=[
+        'click',
+        'pysrt',
+        'googletrans',
+        'tqdm',
+    ],
+    license='MIT',
+    author='Alireza Gerami',
+    author_email='alireza.gerami4030@gmail.com',
+    description='Python Subtitle Translator as a command-line tool',
+    entry_points='''
+        [console_scripts]
+        subtrans=SubtitleTranslator.SubtitleTranslator:cli
+    ''',
+    keywords=['TRANSLATE', 'TOOLS', 'SUBTITLE'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)

4b6b0469f6b226ba4be615a1b62177e9422cec26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:28 +0000

    Add package aw-api/1.1.0/aw_api-1.1.0.tar.gz

---
+++ packages/aw-api/aw_api-1.1.0.tar.gz/aw_api-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+requirements = []
+
+setup(
+    name='aw_api',
+    version='1.1.0',
+    packages=['aw_api'],
+    description='Armored Warfare unofficial API',
+    url='https://github.com/lookandhate/ArmoredWarfareAPI',
+    license='MIT',
+    author='https://github.com/lookandhate',
+    author_email='',
+    install_requires=['requests', 'beautifulsoup4'],
+    python_requires='>=3.7.0',
+    keywords=['armored warfare', 'aw', 'armored warfare api', 'armata', 'армата'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ]
+)

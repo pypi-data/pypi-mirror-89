d9d06a07ac753545139aaf96440407be7a1a719d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:27 +0000

    Add package siml/0.4.0/siml-0.4.0.tar.gz

---
+++ packages/siml/siml-0.4.0.tar.gz/siml-0.4.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+		name='siml',
+		version='0.4.0',
+		description='Machine Learning algorithms implemented from scratch',
+		url='https://github.com/taspinar/siml',
+		author='Ahmet Taspinar',
+		author_email='taspinar@gmail.com',
+		license='MIT',
+		packages=['siml'],
+		install_requires=['numpy', 'scikit-learn'],
+		zip_safe=False
+		)

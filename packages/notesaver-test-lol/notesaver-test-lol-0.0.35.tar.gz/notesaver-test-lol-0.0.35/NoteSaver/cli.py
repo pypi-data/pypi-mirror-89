d11e361a77e16ac67560f7a8bd466ed4ef9da807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:58 +0000

    Add package notesaver-test-lol/0.0.35/notesaver-test-lol-0.0.35.tar.gz

---
+++ packages/notesaver-test-lol/notesaver-test-lol-0.0.35.tar.gz/notesaver-test-lol-0.0.35/NoteSaver/cli.py
@@ -0,0 +1,5 @@
+from .main import NoteSaver
+
+def main():
+    n = NoteSaver()
+    n.cli()
---
+++ packages/notesaver-test-lol/notesaver-test-lol-0.0.35.tar.gz/notesaver-test-lol-0.0.35/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    author="Boyuan Liu",
+    author_email="boyuanliu6@yahoo.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities"
+    ],
+    description="A CLI for saving notes, both locally and online",
+    license="MIT",
+    install_requires=[],
+    name="notesaver-test-lol",
+    packages=["NoteSaver"],
+    python_requires=">= 3.6",
+    entry_points={
+        "console_scripts": ["notesaver=NoteSaver.cli:main"]
+    },
+    url="https://github.com/boyuan12/NoteSaver",
+    version="0.0.35",
+)

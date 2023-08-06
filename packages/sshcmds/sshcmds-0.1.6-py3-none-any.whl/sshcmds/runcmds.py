306894f17cf696bc65b7bff321c9d601d619436c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:06 +0000

    Add package sshcmds/0.1.6/sshcmds-0.1.6-py3-none-any.whl

---
+++ packages/sshcmds/sshcmds-0.1.6-py3-none-any.whl/sshcmds/runcmds.py
@@ -0,0 +1,59 @@
+"""
+The sshcmds class establishes an ssh connection 
+and runs commands. 
+"""
+
+import hvac
+import json
+import paramiko
+import getpass
+import sys 
+import os
+
+class SSHCmds:
+
+    def __init__(self, user=None, password=None, host=None):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.login() 
+        try:
+            self.ssh.connect(self.host, port=22, username=self.user, password=self.password)
+        except paramiko.ssh_exception.AuthenticationException:
+            print("Authentication failed.")
+        except paramiko.ssh_exception.BadAuthenticationType:
+            print("The server does not allow this authentication type.")
+        except:
+            print("Cannot connect to the SSH Server.")
+    
+    def login(self):
+        confname = "runcmds.json"
+        if os.path.exists(confname):
+            with open(confname) as f:
+                try:
+                    data = json.load(f)
+                    self.host = data['host'] 
+                    self.user = data['user']
+                    client = hvac.Client(url=data['vaultURL'], token=data['token'])
+                    sshpass = client.read(data['keypath'])
+                    self.password = sshpass['data']['password']
+                except:
+                    print("Error reading in " + confname)
+        
+        if self.host is None:
+            self.host = input("Please enter the host: ")
+        if self.user is None:
+            self.user = input("Please enter your username: ")
+        if self.password is None:
+            self.password = getpass.getpass(prompt='Please enter your password : ', stream=None) 
+
+    def execute_command(self, command):
+        stdin, stdout, sterr = self.ssh.exec_command(command)
+        lines = stdout.readlines()
+        return lines
+    
+
+
+
---
+++ packages/sshcmds/sshcmds-0.1.6-py3-none-any.whl/sshcmds/sshcli.py
@@ -0,0 +1,16 @@
+"""
+This main function calls upon sshcmds to execute a command and print the result.
+"""
+from sshcmds.runcmds import SSHCmds
+import sys 
+import os
+
+def main():
+    ssh = SSHCmds() 
+    command = str(input("What command would you like to execute? "))
+    print("executing command...")
+    info = ssh.execute_command(command)
+    print(info)
+    
+if __name__ == "__main__":
+    main()

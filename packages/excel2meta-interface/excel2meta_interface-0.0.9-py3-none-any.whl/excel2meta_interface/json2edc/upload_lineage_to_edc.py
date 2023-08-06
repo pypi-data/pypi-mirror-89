761baff52147de419d5887cd24341591f7fab0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:51 +0000

    Add package excel2meta-interface/0.0.9/excel2meta_interface-0.0.9-py3-none-any.whl

---
+++ packages/excel2meta-interface/excel2meta_interface-0.0.9-py3-none-any.whl/excel2meta_interface/json2edc/upload_lineage_to_edc.py
@@ -0,0 +1,21 @@
+from excel2meta_interface.utils import messages
+# import informatica_edc_rest_api_samples
+from edc_rest_api.metadata_utilities import load_json_metadata
+import glob
+
+
+class CreateMetafilesForEDC:
+    def __init__(self):
+        self.result = messages.message["ok"]
+        self.config_directory = "lineage_output/one-on-one/config/"
+
+    def upload_lineage(self):
+        print("Uploading lineage to EDC with config files in >%s<" % self.config_directory)
+        for file in glob.glob(self.config_directory + "config_for_*.json"):
+            print("Lineage processing with configuration file >%s<" % file)
+            load_json = load_json_metadata.ConvertJSONtoEDCLineage(configuration_file=file)
+            load_json.main(ignore_metafile_creation=True)
+
+
+if __name__ == "__main__":
+    CreateMetafilesForEDC().upload_lineage()

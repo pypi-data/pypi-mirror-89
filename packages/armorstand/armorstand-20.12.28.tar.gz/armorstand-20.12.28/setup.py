5e233c9dd1891d3ab6e832f6890a58cec7c16bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package armorstand/20.12.28/armorstand-20.12.28.tar.gz

---
+++ packages/armorstand/armorstand-20.12.28.tar.gz/armorstand-20.12.28/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['armorstand']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'armorstand',
+    'version': '20.12.28',
+    'description': 'All my utility functions',
+    'long_description': None,
+    'author': 'ninest',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

ad6979811390f4ca2c6811a7f582290d5fe9ba61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:20 +0000

    Add package genhtml/1.1.0/genhtml-1.1.0.tar.gz

---
+++ packages/genhtml/genhtml-1.1.0.tar.gz/genhtml-1.1.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+long_description = '''
+Generate HTML. Usage:
+	import genhtml
+	html=genhtml.HTML()
+	p=html.open('p')
+	p.attrs['style']='color:red;'
+	p.write('Hello!')
+	p.close()
+	html.commit(p)
+	tags=html.get_tags()
+	tags[0].write('Using genhtml')
+	html.set_tags(tags)
+	html.open_in_browser()
+	print(html.output())
+'''
+
+setuptools.setup(
+    name="genhtml", # Replace with your own username
+    version="1.1.0",
+    author='Vadim Simakin',
+    author_email="sima.vad@gmail.com",
+    description="Html5 generator",
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    packages=['genhtml'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5'
+)

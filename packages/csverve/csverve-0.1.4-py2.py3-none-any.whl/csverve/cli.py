38895028a457af4a329444fffe04bb6949a0238f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:05 +0000

    Add package csverve/0.1.4/csverve-0.1.4-py2.py3-none-any.whl

---
+++ packages/csverve/csverve-0.1.4-py2.py3-none-any.whl/csverve/cli.py
@@ -0,0 +1,14 @@
+"""Console script for csverve."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for csverve."""
+    click.echo("No commands yet, but open for suggestions :)")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover

f02551481cf78437b85029f4f745a5e279ad9965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:58 +0000

    Add package christmascardify/0.0.2/christmascardify-0.0.2.tar.gz

---
+++ packages/christmascardify/christmascardify-0.0.2.tar.gz/christmascardify-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(name='christmascardify',
+      version='0.0.2',
+      description='Automate your christmas card creation!',
+      long_description=open("README.md").read(),
+      long_description_content_type='text/markdown',
+      author='Rahul Prabhu',
+      author_email='rahul@grokwithrahul.com',
+      url='https://github.com/grokwithrahul/christmas-card-creator',
+      packages=['christmascardify'],
+      install_requires=['Pillow', 'sketchify'],
+      package_data={'christmascardify':['Brusher.ttf', 'defaultbg.png']}
+     )

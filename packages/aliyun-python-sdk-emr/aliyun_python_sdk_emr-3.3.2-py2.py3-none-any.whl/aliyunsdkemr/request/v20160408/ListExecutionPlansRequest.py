c6be80573b89eb1c8fe2c13ddf1bd0fe299aa675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:23 +0000

    Add package aliyun-python-sdk-emr/3.3.2/aliyun_python_sdk_emr-3.3.2-py2.py3-none-any.whl

---
+++ packages/aliyun-python-sdk-emr/aliyun_python_sdk_emr-3.3.2-py2.py3-none-any.whl/aliyunsdkemr/request/v20160408/ListExecutionPlansRequest.py
@@ -0,0 +1,94 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkemr.endpoint import endpoint_data
+
+class ListExecutionPlansRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Emr', '2016-04-08', 'ListExecutionPlans','emr')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_ResourceOwnerId(self):
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self,ResourceOwnerId):
+		self.add_query_param('ResourceOwnerId',ResourceOwnerId)
+
+	def get_StatusLists(self):
+		return self.get_query_params().get('StatusList')
+
+	def set_StatusLists(self, StatusLists):
+		for depth1 in range(len(StatusLists)):
+			if StatusLists[depth1] is not None:
+				self.add_query_param('StatusList.' + str(depth1 + 1) , StatusLists[depth1])
+
+	def get_IsDesc(self):
+		return self.get_query_params().get('IsDesc')
+
+	def set_IsDesc(self,IsDesc):
+		self.add_query_param('IsDesc',IsDesc)
+
+	def get_PageNumber(self):
+		return self.get_query_params().get('PageNumber')
+
+	def set_PageNumber(self,PageNumber):
+		self.add_query_param('PageNumber',PageNumber)
+
+	def get_JobId(self):
+		return self.get_query_params().get('JobId')
+
+	def set_JobId(self,JobId):
+		self.add_query_param('JobId',JobId)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_QueryType(self):
+		return self.get_query_params().get('QueryType')
+
+	def set_QueryType(self,QueryType):
+		self.add_query_param('QueryType',QueryType)
+
+	def get_QueryString(self):
+		return self.get_query_params().get('QueryString')
+
+	def set_QueryString(self,QueryString):
+		self.add_query_param('QueryString',QueryString)
+
+	def get_ClusterId(self):
+		return self.get_query_params().get('ClusterId')
+
+	def set_ClusterId(self,ClusterId):
+		self.add_query_param('ClusterId',ClusterId)
+
+	def get_Strategy(self):
+		return self.get_query_params().get('Strategy')
+
+	def set_Strategy(self,Strategy):
+		self.add_query_param('Strategy',Strategy)

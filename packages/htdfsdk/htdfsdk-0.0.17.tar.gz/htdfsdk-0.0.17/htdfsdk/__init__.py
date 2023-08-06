60342d79bfb7045ea2db980bc6ab49975a93ff3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:30 +0000

    Add package htdfsdk/0.0.17/htdfsdk-0.0.17.tar.gz

---
+++ packages/htdfsdk/htdfsdk-0.0.17.tar.gz/htdfsdk-0.0.17/htdfsdk/__init__.py
@@ -0,0 +1,39 @@
+import sys
+
+if sys.version_info < (3, 6):
+    raise EnvironmentError(
+        "Python 3.6 or above is required. ")
+
+
+import htdfsdk.web3 as web3
+
+from htdfsdk.htdfsdk import (
+    HtdfTxBuilder,
+    HtdfRPC,
+    Address,
+    HtdfPrivateKey,
+)
+
+from htdfsdk.contract import (
+    HtdfContract
+)
+
+from eth_utils import (
+    to_checksum_address,
+    remove_0x_prefix
+)
+
+
+from htdfsdk.utils import *
+
+__all__ = [
+    'utils',
+    "HtdfRPC",
+    "HtdfTxBuilder",
+    "Address",
+    "HtdfPrivateKey",
+    "web3",
+    "HtdfContract",
+    "to_checksum_address",
+    "remove_0x_prefix",
+]

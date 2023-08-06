39c52b7719cadd356a2e56accd6e82864fb43d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:59 +0000

    Add package random-quote-generator/2.0.0/random_quote_generator-2.0.0.tar.gz

---
+++ packages/random-quote-generator/random_quote_generator-2.0.0.tar.gz/random_quote_generator-2.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['random_quote_generator']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest-cov>=2.10.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'random-quote-generator',
+    'version': '2.0.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jan Giacomelli',
+    'author_email': 'jan@typless.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

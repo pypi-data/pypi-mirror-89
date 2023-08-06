451a82ce7944d3877ab147943d1bc5ca0d8e6850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:13 +0000

    Add package ocw-data-parser/0.20.0/ocw_data_parser-0.20.0-py3-none-any.whl

---
+++ packages/ocw-data-parser/ocw_data_parser-0.20.0-py3-none-any.whl/ocw_data_parser/test_constants.py
@@ -0,0 +1,11 @@
+"""Constants for testing"""
+
+import os
+
+
+COURSE_DIR = os.path.join("ocw_data_parser", "test_json", "course_dir")
+COURSE_1_ID = "res-str-001-geographic-information-system-gis-tutorial-january-iap-2016"
+COURSE_2_ID = "18-06-linear-algebra-spring-2010"
+SINGLE_COURSE_DIR = os.path.join(COURSE_DIR, "course-1")
+STATIC_PREFIX = "static_files/"
+S3_TEST_COURSE_ROOT = os.path.join("PROD", "Department", "CourseNumber", "Term")

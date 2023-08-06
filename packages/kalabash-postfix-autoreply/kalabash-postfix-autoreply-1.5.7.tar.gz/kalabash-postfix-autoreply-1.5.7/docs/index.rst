6c6de2bb7e6313d18eda344d262ee1c6304eeb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:16 +0000

    Add package kalabash-postfix-autoreply/1.5.7/kalabash-postfix-autoreply-1.5.7.tar.gz

---
+++ packages/kalabash-postfix-autoreply/kalabash-postfix-autoreply-1.5.7.tar.gz/kalabash-postfix-autoreply-1.5.7/docs/index.rst
@@ -0,0 +1,17 @@
+.. kalabash-postfix-autoreply documentation master file, created by
+   sphinx-quickstart on Sun Apr  5 14:16:24 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to kalabash-postfix-autoreply's documentation!
+=====================================================
+
+This extension lets users define an auto-reply message
+(*vacation*). It is based on Postfix capabilities.
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   setup
---
+++ packages/kalabash-postfix-autoreply/kalabash-postfix-autoreply-1.5.7.tar.gz/kalabash-postfix-autoreply-1.5.7/kalabash_postfix_autoreply/migrations/0005_auto_20151202_1623.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def remove_useless_aliases(apps, schema_editor):
+    """Remove aliases linked to disabled messages."""
+    ARmessage = apps.get_model("kalabash_postfix_autoreply", "ARmessage")
+    AliasRecipient = apps.get_model("admin", "AliasRecipient")
+    qset = ARmessage.objects.select_related("mbox", "mbox__domain").filter(
+        enabled=False)
+    for armessage in qset:
+        alr_address = u"{0}@{1}@autoreply.{1}".format(
+            armessage.mbox.address, armessage.mbox.domain)
+        try:
+            alr = AliasRecipient.objects.get(address=alr_address)
+        except AliasRecipient.DoesNotExist:
+            continue
+        alias = alr.alias
+        alr.delete()
+        if not alias.recipients_count:
+            alias.delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('kalabash_postfix_autoreply', '0004_delete_alias'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_useless_aliases),
+    ]

6e3986431f4492f6090233d92e25b035eda14d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:27 +0000

    Add package ailever/0.1.15/ailever-0.1.15-py3-none-any.whl

---
+++ packages/ailever/ailever-0.1.15-py3-none-any.whl/ailever/apps/APPSManagement.py
@@ -0,0 +1,5 @@
+def eyes():
+    from .app_eyes import eyes
+
+def brain():
+    from .app_brain import brain
---
+++ packages/ailever/ailever-0.1.15-py3-none-any.whl/ailever/apps/__init__.py
@@ -0,0 +1,4 @@
+from .APPSManagement import eyes
+from .APPSManagement import brain
+#from .app_eyes import eyes
+#from .app_brain import brain

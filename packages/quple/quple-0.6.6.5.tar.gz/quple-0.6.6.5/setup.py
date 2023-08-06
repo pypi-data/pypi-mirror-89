f344dd71dd23f8e5c6f480c6f5d62fc7d75fb2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:11 +0000

    Add package quple/0.6.6.5/quple-0.6.6.5.tar.gz

---
+++ packages/quple/quple-0.6.6.5.tar.gz/quple-0.6.6.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quple", # Replace with your own username
+    version='0.6.6.5',
+    author="Alkaid Cheng",
+    author_email="chi.lung.cheng@cern.ch",
+    description="A framework for quantum machine learning in high energy physics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.cern.ch/clcheng/quple",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[   
+          'cirq',
+          'numpy',
+          'tensorflow',
+          'tensorflow_quantum'
+      ],
+    python_requires='>=3.5',
+)

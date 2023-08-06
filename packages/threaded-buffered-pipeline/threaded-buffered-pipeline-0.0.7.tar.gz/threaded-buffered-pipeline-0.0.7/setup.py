7c21945cda99f94e3085709893cd9fd0984dd71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:57 +0000

    Add package threaded-buffered-pipeline/0.0.7/threaded-buffered-pipeline-0.0.7.tar.gz

---
+++ packages/threaded-buffered-pipeline/threaded-buffered-pipeline-0.0.7.tar.gz/threaded-buffered-pipeline-0.0.7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='threaded-buffered-pipeline',
+    version='0.0.7',
+    author='Michal Charemza',
+    author_email='michal@charemza.name',
+    description='Parallelize pipelines of Python iterables',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michalc/threaded-buffered-pipeline',
+    py_modules=[
+        'threaded_buffered_pipeline',
+    ],
+    python_requires='>=3.7.1',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)

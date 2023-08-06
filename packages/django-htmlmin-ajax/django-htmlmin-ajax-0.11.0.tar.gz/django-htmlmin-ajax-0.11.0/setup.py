bbaf97a955ffdc6cf7f4b9737d233e8f4eff60f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:52 +0000

    Add package django-htmlmin-ajax/0.11.0/django-htmlmin-ajax-0.11.0.tar.gz

---
+++ packages/django-htmlmin-ajax/django-htmlmin-ajax-0.11.0.tar.gz/django-htmlmin-ajax-0.11.0/setup.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2016 django-htmlmin authors. All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+"""
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    with open(os.path.join(package, '__init__.py')) as fp:
+        init_py = fp.read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+version = get_version('htmlmin')
+
+with open('README.rst', 'r') as fp:
+    README = fp.read()
+
+setup(
+    name='django-htmlmin-ajax',
+    version=version,
+    description='HTML minifier for Python frameworks (not only Django, '
+                'despite the name).',
+    long_description=README,
+    author='Mario Perdomo',
+    author_email='mariod3w@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['beautifulsoup4', 'html5lib'],
+    tests_require=['django'],
+    entry_points={
+        'console_scripts': [
+            'pyminify = htmlmin.commands:main',
+        ],
+    },
+    keywords='django, html, minifier, minify',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+)

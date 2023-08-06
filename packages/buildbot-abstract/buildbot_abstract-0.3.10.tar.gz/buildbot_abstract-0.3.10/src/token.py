3b30c759a69b35fe6ad6fc7dac52a69b5f72375b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:26 +0000

    Add package buildbot-abstract/0.3.10/buildbot_abstract-0.3.10.tar.gz

---
+++ packages/buildbot-abstract/buildbot_abstract-0.3.10.tar.gz/buildbot_abstract-0.3.10/src/token.py
@@ -0,0 +1,36 @@
+import json
+import logging
+
+import requests
+from hvac import Client
+from retry import retry
+
+logger = logging.getLogger(__name__)
+exceptions = (FileNotFoundError, requests.exceptions.ConnectionError)
+soft_retry = retry(exceptions, tries=10, delay=1, backoff=2, logger=logger)
+
+
+class TokenClient(Client):
+    def __init__(self, token, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.token = token
+
+    @soft_retry
+    def list(self, path):
+        return super().list(path)
+
+    @soft_retry
+    def write(self, path, secret, wrap_ttl=None, **kwargs):
+        return super().write(path, secret=secret, wrap_ttl=wrap_ttl, **kwargs)
+
+    @classmethod
+    @soft_retry
+    def try_token_path(cls, path):
+        token = cls.parse_token(path)
+        return cls(token)
+
+    @staticmethod
+    def parse_token(path):
+        with open(path) as stream:
+            token = json.loads(stream.read())["token"]
+            return token["auth"]["client_token"]

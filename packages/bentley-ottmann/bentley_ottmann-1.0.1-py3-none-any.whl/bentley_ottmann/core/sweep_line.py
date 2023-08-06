4e14c61477edd9585391048534ccb0761c14783f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:20 +0000

    Add package bentley-ottmann/1.0.1/bentley_ottmann-1.0.1-py3-none-any.whl

---
+++ packages/bentley-ottmann/bentley_ottmann-1.0.1-py3-none-any.whl/bentley_ottmann/core/sweep_line.py
@@ -0,0 +1,108 @@
+from functools import partial
+from typing import (Callable,
+                    Optional)
+
+from dendroid import red_black
+from ground.angular import (Orientation,
+                            to_orienteer)
+from ground.hints import Point
+from reprit.base import generate_repr
+
+from .event import Event
+from .utils import merge_ids
+
+
+class SweepLine:
+    __slots__ = '_tree',
+
+    def __init__(self) -> None:
+        self._tree = red_black.set_(key=partial(SweepLineKey, to_orienteer()))
+
+    __repr__ = generate_repr(__init__)
+
+    def __contains__(self, event: Event) -> bool:
+        return event in self._tree
+
+    def add(self, event: Event) -> None:
+        self._tree.add(event)
+
+    def remove(self, event: Event) -> None:
+        self._tree.remove(event)
+
+    def above(self, event: Event) -> Optional[Event]:
+        try:
+            return self._tree.next(event)
+        except ValueError:
+            return None
+
+    def below(self, event: Event) -> Optional[Event]:
+        try:
+            return self._tree.prev(event)
+        except ValueError:
+            return None
+
+
+class SweepLineKey:
+    __slots__ = 'orienteer', 'event'
+
+    def __init__(self,
+                 orienteer: Callable[[Point, Point, Point], Orientation],
+                 event: Event) -> None:
+        self.orienteer = orienteer
+        self.event = event
+
+    __repr__ = generate_repr(__init__)
+
+    def __lt__(self, other: 'SweepLineKey') -> bool:
+        """
+        Checks if the segment (or at least the point) associated with event
+        is lower than other's.
+        """
+        event, other_event = self.event, other.event
+        if event is other_event:
+            return False
+        start, other_start = event.start, other_event.start
+        end, other_end = event.end, other_event.end
+        other_start_orientation = self.orienteer(end, start, other_start)
+        other_end_orientation = self.orienteer(end, start, other_end)
+        if other_start_orientation is other_end_orientation:
+            start_x, start_y = start
+            other_start_x, other_start_y = other_start
+            if other_start_orientation is not Orientation.COLLINEAR:
+                # other segment fully lies on one side
+                return other_start_orientation is Orientation.COUNTERCLOCKWISE
+            # segments are collinear
+            elif start_x == other_start_x:
+                end_x, end_y = end
+                other_end_x, other_end_y = other_end
+                if start_y != other_start_y:
+                    # segments are vertical
+                    return start_y < other_start_y
+                # segments have same start
+                elif end_y != other_end_y:
+                    return end_y < other_end_y
+                elif end_x != other_end_x:
+                    # segments are horizontal
+                    return end_x < other_end_x
+                else:
+                    # segments are equal
+                    event.segments_ids = other_event.segments_ids = merge_ids(
+                            event.segments_ids, other_event.segments_ids)
+                    return False
+            elif start_y != other_start_y:
+                return start_y < other_start_y
+            else:
+                # segments are horizontal
+                return start_x < other_start_x
+        start_orientation = self.orienteer(other_end, other_start, start)
+        end_orientation = self.orienteer(other_end, other_start, end)
+        if start_orientation is end_orientation:
+            return start_orientation is Orientation.CLOCKWISE
+        elif other_start_orientation is Orientation.COLLINEAR:
+            return other_end_orientation is Orientation.COUNTERCLOCKWISE
+        elif start_orientation is Orientation.COLLINEAR:
+            return end_orientation is Orientation.CLOCKWISE
+        elif end_orientation is Orientation.COLLINEAR:
+            return start_orientation is Orientation.CLOCKWISE
+        else:
+            return other_start_orientation is Orientation.COUNTERCLOCKWISE
---
+++ packages/bentley-ottmann/bentley_ottmann-1.0.1-py3-none-any.whl/bentley_ottmann/planar.py
@@ -0,0 +1,211 @@
+from typing import (Dict,
+                    Hashable,
+                    Iterable,
+                    Sequence,
+                    Set,
+                    Tuple)
+
+from ground.geometries import to_segment_cls as _to_segment_cls
+from ground.hints import (Contour,
+                          Point,
+                          Segment)
+from ground.linear import (SegmentsRelationship as _SegmentsRelationship,
+                           to_segments_intersector as _to_segments_intersector)
+
+from .core.base import sweep as _sweep
+from .core.utils import (merge_ids as _merge_ids,
+                         to_pairs_combinations as _to_pairs_combinations)
+
+
+def edges_intersect(contour: Contour) -> bool:
+    """
+    Checks if polygonal contour has self-intersection.
+
+    Based on Shamos-Hoey algorithm.
+
+    Time complexity:
+        ``O(len(contour) * log len(contour))``
+    Memory complexity:
+        ``O(len(contour))``
+    Reference:
+        https://en.wikipedia.org/wiki/Sweep_line_algorithm
+
+    :param contour: contour to check.
+    :returns: true if contour is self-intersecting, false otherwise.
+
+    .. note::
+        Consecutive equal vertices like ``Point(2, 0)`` in
+
+        .. code-block:: python
+
+            Contour([Point(0, 0), Point(2, 0), Point(2, 0), Point(2, 2)])
+
+        will be considered as self-intersection,
+        if you don't want them to be treated as such
+        -- filter out before passing as argument.
+
+    >>> from ground.geometries import to_contour_cls, to_point_cls
+    >>> Contour, Point = to_contour_cls(), to_point_cls()
+    >>> edges_intersect(Contour([Point(0, 0), Point(2, 0), Point(2, 2)]))
+    False
+    >>> edges_intersect(Contour([Point(0, 0), Point(2, 0), Point(1, 0)]))
+    True
+    """
+    vertices = contour.vertices
+    if len(vertices) < 3:
+        raise ValueError('Contour {contour} is degenerate.'
+                         .format(contour=contour))
+    if not _all_unique(vertices):
+        return True
+    segment_cls = _to_segment_cls()
+    edges = [segment_cls(vertices[index - 1], vertices[index])
+             for index in range(len(vertices))]
+
+    def non_neighbours_intersect(edges_ids: Iterable[Tuple[int, int]],
+                                 last_edge_index: int = len(edges) - 1
+                                 ) -> bool:
+        return any(next_segment_id - segment_id > 1
+                   and (segment_id != 0 or next_segment_id != last_edge_index)
+                   for segment_id, next_segment_id in edges_ids)
+
+    return any((first_event.relationship is _SegmentsRelationship.OVERLAP
+                or second_event.relationship is _SegmentsRelationship.OVERLAP
+                or non_neighbours_intersect(_to_pairs_combinations(_merge_ids(
+                    first_event.segments_ids, second_event.segments_ids))))
+               for first_event, second_event in _sweep(edges))
+
+
+def _all_unique(values: Iterable[Hashable]) -> bool:
+    seen = set()
+    seen_add = seen.add
+    for value in values:
+        if value in seen:
+            return False
+        else:
+            seen_add(value)
+    return True
+
+
+def segments_intersect(segments: Sequence[Segment]) -> bool:
+    """
+    Checks if segments have at least one intersection.
+
+    Based on Shamos-Hoey algorithm.
+
+    Time complexity:
+        ``O(len(segments) * log len(segments))``
+    Memory complexity:
+        ``O(len(segments))``
+    Reference:
+        https://en.wikipedia.org/wiki/Sweep_line_algorithm
+
+    :param segments: sequence of segments.
+    :returns: true if segments intersection found, false otherwise.
+
+    >>> from ground.geometries import to_point_cls, to_segment_cls
+    >>> Point, Segment = to_point_cls(), _to_segment_cls()
+    >>> segments_intersect([])
+    False
+    >>> segments_intersect([Segment(Point(0, 0), Point(2, 2))])
+    False
+    >>> segments_intersect([Segment(Point(0, 0), Point(2, 0)),
+    ...                     Segment(Point(0, 2), Point(2, 2))])
+    False
+    >>> segments_intersect([Segment(Point(0, 0), Point(2, 2)),
+    ...                     Segment(Point(0, 0), Point(2, 2))])
+    True
+    >>> segments_intersect([Segment(Point(0, 0), Point(2, 2)),
+    ...                     Segment(Point(2, 0), Point(0, 2))])
+    True
+    """
+    return any(_sweep(segments))
+
+
+def segments_cross_or_overlap(segments: Sequence[Segment]) -> bool:
+    """
+    Checks if at least one pair of segments crosses or overlaps.
+
+    Based on Shamos-Hoey algorithm.
+
+    Time complexity:
+        ``O((len(segments) + len(intersections)) * log len(segments))``
+    Memory complexity:
+        ``O(len(segments))``
+    Reference:
+        https://en.wikipedia.org/wiki/Sweep_line_algorithm
+
+    :param segments: sequence of segments.
+    :returns: true if segments overlap or cross found, false otherwise.
+
+    >>> from ground.geometries import to_point_cls, to_segment_cls
+    >>> Point, Segment = to_point_cls(), _to_segment_cls()
+    >>> segments_cross_or_overlap([])
+    False
+    >>> segments_cross_or_overlap([Segment(Point(0, 0), Point(2, 2))])
+    False
+    >>> segments_cross_or_overlap([Segment(Point(0, 0), Point(2, 0)),
+    ...                            Segment(Point(0, 2), Point(2, 2))])
+    False
+    >>> segments_cross_or_overlap([Segment(Point(0, 0), Point(2, 2)),
+    ...                            Segment(Point(0, 0), Point(2, 2))])
+    True
+    >>> segments_cross_or_overlap([Segment(Point(0, 0), Point(2, 2)),
+    ...                            Segment(Point(2, 0), Point(0, 2))])
+    True
+    """
+    relationships = _SegmentsRelationship.CROSS, _SegmentsRelationship.OVERLAP
+    return any(first_event.relationship in relationships
+               or second_event.relationship in relationships
+               for first_event, second_event in _sweep(segments))
+
+
+def segments_intersections(segments: Sequence[Segment]
+                           ) -> Dict[Point, Set[Tuple[int, int]]]:
+    """
+    Returns mapping between intersection points
+    and corresponding segments indices.
+
+    Based on Bentley-Ottmann algorithm.
+
+    Time complexity:
+        ``O((len(segments) + len(intersections)) * log len(segments))``
+    Memory complexity:
+        ``O(len(segments) + len(intersections))``
+    Reference:
+        https://en.wikipedia.org/wiki/Bentley%E2%80%93Ottmann_algorithm
+
+    >>> from ground.geometries import to_point_cls, to_segment_cls
+    >>> Point, Segment = to_point_cls(), _to_segment_cls()
+    >>> segments_intersections([]) == {}
+    True
+    >>> segments_intersections([Segment(Point(0, 0), Point(2, 2))]) == {}
+    True
+    >>> segments_intersections([Segment(Point(0, 0), Point(2, 0)),
+    ...                         Segment(Point(0, 2), Point(2, 2))]) == {}
+    True
+    >>> (segments_intersections([Segment(Point(0, 0), Point(2, 2)),
+    ...                          Segment(Point(0, 0), Point(2, 2))])
+    ...  == {(0, 0): {(0, 1)}, (2, 2): {(0, 1)}})
+    True
+    >>> (segments_intersections([Segment(Point(0, 0), Point(2, 2)),
+    ...                          Segment(Point(2, 0), Point(0, 2))])
+    ...  == {(1, 1): {(0, 1)}})
+    True
+
+    :param segments: sequence of segments.
+    :returns:
+        mapping between intersection points and corresponding segments indices.
+    """
+    result = {}
+    segments_intersector = _to_segments_intersector()
+    for first_event, second_event in _sweep(segments):
+        for segment_id, next_segment_id in _to_pairs_combinations(_merge_ids(
+                first_event.segments_ids, second_event.segments_ids)):
+            segment, next_segment = (segments[segment_id],
+                                     segments[next_segment_id])
+            for point in segments_intersector(segment.start, segment.end,
+                                              next_segment.start,
+                                              next_segment.end):
+                result.setdefault(point, set()).add((segment_id,
+                                                     next_segment_id))
+    return result

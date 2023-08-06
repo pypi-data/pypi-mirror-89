080efa969886217782902e95c81f72004b9176b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package PyAuthGG/1.0.3/PyAuthGG-1.0.3.tar.gz

---
+++ packages/PyAuthGG/PyAuthGG-1.0.3.tar.gz/PyAuthGG-1.0.3/PyAuthGG/__init__.py
@@ -0,0 +1,7 @@
+import Administration
+
+Version = "1.0.3"
+Author = "xFueY"
+Source = "https://github.com/xFueY/PyAuthGG"
+
+HEADERS = {"Content-Type" : "application/x-www-form-urlencoded", "User-Agent" : f"PyAuthGG {Version}"}

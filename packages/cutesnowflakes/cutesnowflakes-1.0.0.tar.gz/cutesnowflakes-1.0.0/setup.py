69f0bf08652c82c1ec46d779336eaa95930987eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:06 +0000

    Add package cutesnowflakes/1.0.0/cutesnowflakes-1.0.0.tar.gz

---
+++ packages/cutesnowflakes/cutesnowflakes-1.0.0.tar.gz/cutesnowflakes-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+from cutesnowflakes import __version__
+
+with open("../README.md") as fp:
+    long_description = fp.read()
+
+setup(
+    name="cutesnowflakes",
+    version=__version__,
+    description="Cute 3x3 images from snowflake IDs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CodeBizarre/cutesnowflakes",
+    author="CodeBizarre",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=["cutesnowflakes"],
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy>=1.19<1.20",
+        "pillow==8.0"
+    ]
+)

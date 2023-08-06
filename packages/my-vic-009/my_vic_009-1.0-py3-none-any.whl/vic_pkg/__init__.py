795220a870f6d34b2e9b71cb82b1406d7c4d4b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:16 +0000

    Add package my-vic-009/1.0/my_vic_009-1.0-py3-none-any.whl

---
+++ packages/my-vic-009/my_vic_009-1.0-py3-none-any.whl/vic_pkg/__init__.py
@@ -0,0 +1,3 @@
+# coding:utf8
+# @Time : 2020/12/26 1:17 下午 
+# @Author : vic
---
+++ packages/my-vic-009/my_vic_009-1.0-py3-none-any.whl/vic_pkg/hello.py
@@ -0,0 +1,9 @@
+# coding:utf8
+# @Time : 2020/12/26 1:18 下午 
+# @Author : vic
+
+
+class Hello:
+
+    def get(self):
+        print("hello world")

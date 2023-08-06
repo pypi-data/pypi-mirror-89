de586b6f32aed4bb27936fa8c83b2db4cd006bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:13 +0000

    Add package py-fds/3.5.1/py-fds-3.5.1.tar.gz

---
+++ packages/py-fds/py-fds-3.5.1.tar.gz/py-fds-3.5.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="py-fds",
+    version="3.5.1",
+    author="Abdelmalek Fathi",
+    author_email="abdelmalek.fathi.2001@gmail.com",
+    url="https://github.com/FathiMalek/py-fds.git",
+    description="Simple implementation of data structures for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['pyfds', 'pyfds.utils'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

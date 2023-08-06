a886bf86b821a8e9671a585179547e78fb838cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:39 +0000

    Add package r0c/1.1.7/r0c-1.1.7.tar.gz

---
+++ packages/r0c/r0c-1.1.7.tar.gz/r0c-1.1.7/r0c/__init__.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+from __future__ import print_function
+
+import platform
+import sys
+import os
+
+INTERP = platform.python_implementation()
+
+PY2 = sys.version_info[0] == 2
+if PY2:
+    sys.dont_write_bytecode = True
+
+WINDOWS = False
+if platform.system() == "Windows":
+    WINDOWS = [int(x) for x in platform.version().split(".")]
+
+COLORS = not WINDOWS or WINDOWS >= [10, 0, 14393]  # 1607 / LTSB-2016
+
+# [ determine runtime environment ]
+#
+# setting the following members:
+# env: top of the python environment
+# doc: help files and documentation
+# src: our source code directory
+# app: ~/.r0c || %appdata%/r0c
+# log: logfiles and client config
+
+
+class Pod(object):
+    pass
+
+
+EP = Pod()
+
+
+def init_envpaths():
+    # look for our documentation in PYTHONPATH
+    found = False
+    for env_root in sys.path:
+        doc_rel = "share/doc/r0c/help/"
+        if env_root.endswith("/test/.."):
+            return
+
+        if env_root.endswith(os.sep + "site-packages"):
+            for n in range(4):
+                dirname = os.path.realpath(env_root + "/" + ("../" * n)) + "/"
+                if os.path.isfile(dirname + doc_rel + "help-topics.md"):
+                    EP.env = dirname
+                    EP.doc = dirname + doc_rel
+                    EP.src = env_root + "/r0c/"
+                    found = True
+                    break
+
+            if found:
+                break
+
+    if found:
+        if WINDOWS:
+            EP.app = os.environ["APPDATA"] + "/r0c/"
+        else:
+            EP.app = os.path.expanduser("~") + "/.r0c/"
+
+    else:
+        # check if we're running from source tree
+        if os.path.isfile("./docs/help-topics.md"):
+            EP.env = "/"
+            EP.doc = "./docs/"
+            EP.src = "./r0c/"
+            EP.app = "./"
+        else:
+            raise RuntimeError(
+                '\n\n   could not find "help-topics.md", your r0c is broken\n'
+            )
+
+    # frequently used paths derived from those detected above
+    EP.log = os.path.realpath(EP.app + "/log")
+
+    # ensure they're all absolute
+    for key in "env doc src app log".split(" "):
+        path = os.path.realpath(getattr(EP, key))
+        setattr(EP, key, path.rstrip("/\\") + os.sep)
+
+    # what seems to be the officer problem
+    # raise RuntimeError('\n' + '\n'.join(key + ': ' + getattr(EP, key) for key in 'env src app doc log'.split(' ')) + '\n')
+
+
+init_envpaths()
---
+++ packages/r0c/r0c-1.1.7.tar.gz/r0c-1.1.7/setup.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+
+import os
+import sys
+from glob import glob
+from shutil import rmtree
+
+try:
+    # need setuptools to build wheel
+    from setuptools import setup, Command
+
+    setuptools_available = True
+
+except ImportError:
+    # works in a pinch
+    from distutils.core import setup, Command
+
+    setuptools_available = False
+
+if "bdist_wheel" in sys.argv and not setuptools_available:
+    print("cannot build wheel without setuptools")
+    sys.exit(1)
+
+
+def mglob(dirname, extensions):
+    ret = []
+    for ext in extensions:
+        ret.extend(glob(dirname + "/*." + ext))
+    return ret
+
+
+NAME = "r0c"
+data_files = [
+    ("share/doc/r0c", ["README.md", "LICENSE"]),
+    ("share/doc/r0c/help", mglob("docs", ["md"])),
+    ("share/doc/r0c/clients", glob("clients/*")),
+]
+manifest = ""
+for dontcare, files in data_files:
+    for fn in files:
+        manifest += "include {0}\n".format(fn)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(here + "/MANIFEST.in", "wb") as f:
+    f.write(manifest.encode("utf-8"))
+
+with open(here + "/README.md", "rb") as f:
+    md = f.read().decode("utf-8")
+
+    md = md.replace(
+        "(docs/r0c.png)",
+        "(https://raw.githubusercontent.com/9001/r0c/master/docs/r0c.png)",
+    )
+
+    for kw in ["docs/help-", "docs/", "clients/"]:
+        md = md.replace(
+            "({0}".format(kw),
+            "(https://github.com/9001/r0c/blob/master/{0}".format(kw),
+        )
+
+    long_description = md
+
+
+about = {}
+with open(os.path.join(here, NAME, "__version__.py"), "rb") as f:
+    exec(f.read().decode("utf-8"), about)
+
+# about["__version__"] = "0.0.4"
+
+
+class clean2(Command):
+    description = "Cleans the source tree"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system("{0} setup.py clean --all".format(sys.executable))
+
+        try:
+            rmtree("./dist")
+        except:
+            pass
+
+        try:
+            rmtree("./r0c.egg-info")
+        except:
+            pass
+
+        nuke = []
+        for (dirpath, dirnames, filenames) in os.walk("."):
+            for fn in filenames:
+                if (
+                    fn.startswith("MANIFEST")
+                    or fn.endswith(".pyc")
+                    or fn.endswith(".pyo")
+                    or fn.endswith(".pyd")
+                ):
+                    nuke.append(dirpath + "/" + fn)
+
+        for fn in nuke:
+            os.unlink(fn)
+
+
+args = {
+    "name": NAME,
+    "version": about["__version__"],
+    "description": "retr0chat telnet/vt100 chat server",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "author": "ed",
+    "author_email": "r0c@ocv.me",
+    "url": "https://github.com/9001/r0c",
+    "license": "MIT",
+    "data_files": data_files,
+    "classifiers": [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: IronPython",
+        "Programming Language :: Python :: Implementation :: Jython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Environment :: Console",
+        "Environment :: No Input/Output (Daemon)",
+        "Topic :: Communications :: Chat",
+    ],
+    "cmdclass": {"clean2": clean2},
+}
+
+
+if setuptools_available:
+    args.update(
+        {
+            "install_requires": [],
+            "include_package_data": True,
+            "packages": ["r0c"],
+            "entry_points": {"console_scripts": ["r0c = r0c.__main__:main"]},
+        }
+    )
+else:
+    args.update({"packages": ["r0c"], "scripts": ["bin/r0c"]})
+
+
+# import pprint
+# pprint.PrettyPrinter().pprint(args)
+# sys.exit(0)
+
+setup(**args)

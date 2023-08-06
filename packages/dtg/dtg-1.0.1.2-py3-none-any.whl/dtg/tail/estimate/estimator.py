4bb19c3d23db5657e010035f1f04af1cd92224a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:53 +0000

    Add package dtg/1.0.1.2/dtg-1.0.1.2-py3-none-any.whl

---
+++ packages/dtg/dtg-1.0.1.2-py3-none-any.whl/dtg/tail/estimate/estimator.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class TailEstimator:
+    @staticmethod
+    def prepare(x):
+        return np.sort(x)
+
+    @staticmethod
+    def get_k(x):
+        return np.arange(2, x.size-2)
+
+    @staticmethod
+    def estimate(x, k):
+        return None

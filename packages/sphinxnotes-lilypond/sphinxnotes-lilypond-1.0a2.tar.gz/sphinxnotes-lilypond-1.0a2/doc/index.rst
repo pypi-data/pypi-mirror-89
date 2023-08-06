e26ef86d93e7845c78a5732a7a20c89bece2d7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:38 +0000

    Add package sphinxnotes-lilypond/1.0a2/sphinxnotes-lilypond-1.0a2.tar.gz

---
+++ packages/sphinxnotes-lilypond/sphinxnotes-lilypond-1.0a2.tar.gz/sphinxnotes-lilypond-1.0a2/doc/index.rst
@@ -0,0 +1,259 @@
+.. sphinxnotes-lilypond documentation master file, created by
+   sphinx-quickstart on Sat Nov 28 00:42:50 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+=============================
+Sphinx Extension for LilyPond
+=============================
+
+.. image:: https://img.shields.io/github/stars/sphinx-notes/lilypond.svg?style=social&label=Star&maxAge=2592000
+   :target: https://github.com/sphinx-notes/lilypond
+
+:version: |version|
+:copyright: Copyright ©2020 by Shengyu Zhang.
+:copyright: Copyright ©2009 by Wei-Wei Guo.
+:license: BSD, see LICENSE for details.
+
+The extension is originated from `sphinx-contrib/lilypond`_ , allows `LilyPond`_
+music notes to be included in Sphinx-generated documents inline and outline.
+Compared to its predecessor, the extension has many new features such as
+scale transposing, audio output, layout controlling, and so on.
+
+.. _sphinx-contrib/lilypond: https://github.com/sphinx-contrib/lilypond
+.. _LilyPond: https://lilypond.org/
+
+.. contents::
+   :local:
+   :backlinks: none
+
+Installation
+============
+
+Install the follwing runtime dependencies before using the extension:
+
+- `LilyPond`_
+- `TiMidity++`_
+- `ImageMagick`_
+
+.. _Timidity++: http://timidity.sourceforge.net/
+.. _ImageMagick: https://imagemagick.org/index.php
+
+Download it from official Python Package Index:
+
+.. code-block:: console
+
+   $ pip install sphinxnotes-lilypond
+
+Add extension to :file:`conf.py` in your sphinx project:
+
+.. code-block:: python
+
+    extensions = [
+              # …
+              'sphinxnotes.lilypond',
+              # …
+              ]
+
+Functionalities
+===============
+
+Roles
+-----
+
+The ``lily`` role
+~~~~~~~~~~~~~~~~~
+
+You can use ``lily`` role to insert short `LilyPond Music Expression`_ as inline
+element.
+
+.. _LilyPond Music Expression: http://lilypond.org/doc/v2.19/Documentation/learning/music-expressions-explained
+
+For example:
+
+.. literalinclude:: ./lily-role-example.txt
+    :language: rst
+
+Will be rendered as:
+
+    .. include:: ./lily-role-example.txt
+
+.. note::
+
+    Role ``lily`` produces a preview image of the music expression (using
+    ``-dpreview=#t``). You can still write a long music expression as interpreted text,
+    but only the beginning can be shown.
+
+Directives
+----------
+
+.. _lily-directive:
+
+The ``lily`` directive
+~~~~~~~~~~~~~~~~~~~~~~
+
+The ``lily`` directive is used to insert a complete LilyPond score as
+block level element.
+
+.. literalinclude:: ./lily-directive-example.txt
+    :language: rst
+
+Will be rendered as:
+
+    .. include:: ./lily-directive-example.txt
+
+The directive supports the following options:
+
+:noheader: (flag)
+    Whether to remove the header of score
+:nofooter: (flag)
+    Whether to remove the footer of score
+:noedge: (flag)
+    Whether to remove the blank edges of score
+:audio: (falg)
+    Whether to show a audio player for listen LilyPond-generated MIDI file
+:transpose: (text)
+    Transposing the pitches of score from one to another.
+    Pitches are written in `LilyPond Notation`_ and separated in whitespace.
+    For example: ``:transpose: c' d'``
+
+.. _LilyPond Notation: http://lilypond.org/doc/v2.18/Documentation/notation/writing-pitches
+
+The ``lilyinclude`` directives
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The ``lilyinclude`` directive is similar to :ref:`lily-directive`,
+except the source of LilyPond are read from file but not contents of directive.
+
+.. literalinclude:: ./lilyinclude-directive-example.txt
+   :language: rst
+
+Will be rendered as:
+
+   .. include:: ./lilyinclude-directive-example.txt
+
+Options of the directive are same to :ref:`lily-directive`.
+
+.. seealso::
+
+    You and download the example LilyPond documentation from here:
+    :download:`witch-spring.ly`.
+
+Configuration
+=============
+
+:lilypond_lilypond_args: (Type: ``list[str]``, Default: ``['lilypond']``)
+   Argument list for running `LilyPond`_. The first one is path to LilyPond binary.
+:lilypond_timidity_args: (Type: ``list[str]``, Default: ``['timidity']``)
+   Argument list for running `Timidity++`_. The first one is path to Timidity++ binary.
+:lilypond_magick_home: (Type: ``str``, Default: ``None``)
+   Path to `ImageMagick`_ library.
+:lilypond_builddir: (Type: ``str``, Default: ``None``)
+   Build directory of the extension, use temporary directory when not specified.
+:lilypond_score_format: (Type: ``str``, Default: ``'png'``)
+   Format of outputed scores, available values: ``['png', 'svg', 'pdf', 'eps']``.
+:lilypond_audio_format: (Type: ``str``, Default: ``'wav'``)
+   Format of outputed audio, available values: ``['wav', 'ogg']``.
+
+Examples
+========
+
+The LilyPond documentation used in example can be downloaded here:
+:download:`/minuet-in-g.ly`.
+
+Original paper size
+-------------------
+
+.. code-block:: rst
+
+   .. lilyinclude:: minuet-in-g.ly
+
+
+.. lilyinclude:: minuet-in-g.ly
+
+Paper without Footer and Edge
+-----------------------------
+
+.. code-block:: rst
+
+   .. lilyinclude:: minuet-in-g.ly
+      :nofooter:
+      :noedge:
+
+.. lilyinclude:: minuet-in-g.ly
+   :nofooter:
+   :noedge:
+ 
+Smallest Paper Size
+-------------------
+
+.. code-block:: rst
+
+   .. lilyinclude:: minuet-in-g.ly
+      :noheader:
+      :nofooter:
+      :noedge:
+
+.. lilyinclude:: minuet-in-g.ly
+   :noheader:
+   :nofooter:
+   :noedge:
+
+Audio Preview
+-------------
+
+.. code-block:: rst
+
+   .. lilyinclude:: minuet-in-g.ly
+      :noheader:
+      :nofooter:
+      :noedge:
+      :audio:
+
+.. lilyinclude:: minuet-in-g.ly
+   :noheader:
+   :nofooter:
+   :noedge:
+   :audio:
+
+Transposing
+------------
+
+.. code-block:: rst
+
+   .. lilyinclude:: minuet-in-g.ly
+      :noheader:
+      :nofooter:
+      :noedge:
+      :audio:
+      :transpose: g c
+
+.. lilyinclude:: minuet-in-g.ly
+   :noheader:
+   :nofooter:
+   :noedge:
+   :audio:
+   :transpose: g c
+
+Multiple Pages
+--------------
+
+.. code-block:: rst
+
+   .. lilyinclude:: alice.ly
+      :noedge:
+      :audio:
+
+.. lilyinclude:: alice.ly
+   :noedge:
+   :audio:
+
+Chang Log
+=========
+
+2020-12-06 1.0a1
+----------------
+
+ .. sectionauthor:: Shengyu Zhang
+
+The alpha version is out, enjoy~
---
+++ packages/sphinxnotes-lilypond/sphinxnotes-lilypond-1.0a2.tar.gz/sphinxnotes-lilypond-1.0a2/sphinxnotes/lilyport.py
@@ -0,0 +1,377 @@
+# -*- coding: utf-8 -*-
+"""
+    Simple wrapper for LilyPond
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Allow Lilypond music notes to be transposed and converted to image, audio,
+    and so on.
+
+    :copyright: Copyright ©2020 by Shengyu Zhang.
+    :license: BSD, see LICENSE for details.
+"""
+
+from __future__ import annotations
+from typing import Optional
+import os
+from os import path
+import subprocess
+from packaging import version
+
+from ly import pitch
+from ly import document
+from ly import docinfo
+from ly import music
+from ly import pkginfo
+from ly.pitch import transpose
+from ly.music import items
+from wand import image
+
+
+class Error(Exception):
+    # TODO
+    pass
+
+
+class Output(object):
+    """
+    Helper for collecting LilyPond outputed files outputed by :class:`Document`.
+    """
+    BASENAME:str = 'music'
+
+    outdir:str = None
+    score_format:str = None
+    audio_format:str = None
+
+    source:str = None
+    score:Optional[str] = None
+    preview:Optional[str] = None
+    paged_scores:list[str] = []
+    midi:Optional[str] = None
+    audio:Optional[str] = None
+
+    def __init__(self, outdir:str, score_format:str, audio_format:str):
+        self.outdir = outdir
+        self.score_format = score_format
+        self.audio_format = audio_format
+
+        prefix = path.join(outdir, self.BASENAME)
+
+        srcfn = prefix + '.ly'
+        if path.isfile(srcfn):
+            self.source = srcfn
+
+        pvfn = prefix + '.preview.' + score_format
+        if path.isfile(pvfn):
+            self.preview = pvfn
+
+        scorefn = prefix + '.' + score_format
+        if path.isfile(scorefn):
+            self.score = scorefn
+
+        # May multiple scores generated
+        if score_format in ['png', 'svg']:
+            if score_format == 'png':
+                pattern = prefix + '-page%d.png'
+            elif score_format == 'svg':
+                pattern = prefix + '-%d.svg'
+            i = 1
+            while path.isfile(pattern % i):
+                # NOTE: Dont use ``+=`` or ``append``
+                # See: https://github.com/satwikkansal/wtfpython#-class-attributes-and-instance-attributes
+                self.paged_scores = self.paged_scores + [pattern % i]
+                i = i + 1
+
+        if not (self.preview or self.score or self.paged_scores):
+            raise Error('No score generated, please check "*.%s" files under "%s"' %
+                        (self.BASENAME, outdir))
+
+        midifn = prefix + '.midi'
+        if path.isfile(midifn):
+            self.midi = midifn
+
+        audiofn = prefix + '.' + audio_format
+        if path.isfile(audiofn):
+            self.audio = audiofn
+
+    def relocate(self, newdir:str):
+        """
+        Loocate files to a new directory.
+
+        .. note:: This method does not actually move the files.
+        """
+        l = len(self.outdir)
+        self.source = newdir + self.source[l:]
+        if self.score:
+            self.score = newdir + self.score[l:]
+        if self.preview:
+            self.preview = newdir + self.preview[l:]
+        for i, p in enumerate(self.paged_scores):
+            self.paged_scores[i] = newdir + p[l:]
+        if self.midi:
+            self.midi = newdir + self.midi[l:]
+        if self.audio:
+            self.audio = newdir + self.audio[l:]
+
+class Document(object):
+
+    _document:document.Document = None
+    _lilypond_args:list[str] = []
+    _timidity_args:list[str] = []
+    _magick_home:Optional[str] = None
+
+    def __init__(self, src:str,
+            lilypond_args:list[str]=['lilypond'],
+            timidity_args:list[str]=['timidity'],
+            magick_home:Optional[str]=''):
+        self._document = document.Document(src)
+        self._lilypond_args = lilypond_args.copy()
+        self._timidity_args = timidity_args.copy()
+        self._magick_home = magick_home
+
+    def plaintext(self):
+        return self._document.plaintext()
+
+    def transpose(self, from_pitch:str, to_pitch:str):
+        fp = pitch.Pitch(
+                *pitch.pitchReader("nederlands")(from_pitch[0]),
+                octave = pitch.octaveToNum(from_pitch[1:]))
+        tp = pitch.Pitch(
+                *pitch.pitchReader("nederlands")(to_pitch[0]),
+                octave = pitch.octaveToNum(to_pitch[1:]))
+        transposer = transpose.Transposer(fp, tp)
+        cursor = document.Cursor(self._document)
+        try:
+            if version.parse(pkginfo.version) > version.parse("0.9"):
+                transpose.transpose(cursor, transposer,
+                        relative_first_pitch_absolute = True) # only consider lilypond >= 2.18 for now
+            else:
+                transpose.transpose(cursor, transposer)
+        except pitch.PitchNameNotAvailable:
+            language = docinfo.DocInfo(cursor.document).language()
+            raise Error(
+                    'Pitch names not available in "{}", skipping file: {}' %
+                    (language, cursor.document.filename))
+
+
+    def strip_header_footer(self, strip_header=False, strip_footer=False):
+        """Strip header and footer from outputed scores. """
+
+        # Return when nothing todo
+        if not strip_header and not strip_footer:
+            return
+
+        # Find \paper block, if not found, we create one
+        if not music.document(self._document).find_child(items.Paper):
+            self._document[0:0] = """\paper {\n}\n"""
+
+        if strip_header:
+            doc = music.document(self._document)
+            for paper in doc.find_children(items.Paper):
+                self._set_value(paper, 'oddHeaderMarkup', '##f')
+                self._set_value(paper, 'evenHeaderMarkup', '##f')
+                self._set_value(paper, 'bookTitleMarkup', '##f')
+                self._set_value(paper, 'scoreTitleMarkup', '##f')
+
+        if strip_footer:
+            doc = music.document(self._document)
+            for paper in doc.find_children(items.Paper):
+                self._set_value(paper, 'oddFooterMarkup', '##f')
+                self._set_value(paper, 'evenFooterMarkup', '##f')
+
+            doc = music.document(self._document)
+            no_header_found = True
+            for header in doc.find_children(items.Header):
+                self._set_value(header, 'tagline', '##f')
+                no_header_found = False
+            if no_header_found:
+                self._document[0:0] = r'\header { tagline = ##f }'
+
+
+    def enable_audio_output(self):
+        """
+        Enable audio output for this document.
+        In other words, insert ``\midi{}`` to every ``\score{}`` block.
+        If no ``\score{}`` found, find music list under root node, pack it
+        in ``\socre{}``, then call this function again.
+        """
+        no_score_found = True
+        doc = music.document(self._document)
+        for score in doc.find_children(items.Score): # \score
+            no_score_found = False
+            if not score.find_child(items.Midi):
+                self._insert_into(score, '\midi {}')
+                self._insert_into(score, '\layout {}') # FIXME
+        if no_score_found:
+            if self._pack_music_list():
+                self.enable_audio_output()
+
+
+    def output(self,
+            outdir:str,
+            preview:bool,
+            crop:bool,
+            score_format:str,
+            audio_format:str) -> Output:
+        """Output scores and related files from LilyPond Document. """
+        args = self._lilypond_args.copy()
+        args += ['-o', outdir]
+        if score_format in ['png', 'pdf', 'ps', 'eps']:
+            args += ['--formats', score_format]
+        elif score_format == 'svg':
+            args += ['-dbackend=svg']
+        else:
+            raise Error('Unknown score format: %s' % score_format )
+
+        if preview:
+            args += ['-dpreview=#t']
+
+        prefix = path.join(outdir, Output.BASENAME)
+        srcfn = prefix + '.ly'
+        with open(srcfn, 'w') as f:
+            f.write(self.plaintext())
+        args += [srcfn]
+
+        try:
+            p = subprocess.run(args,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    encoding=self._document.encoding or 'utf-8')
+        except OSError as e:
+            raise Error('LilyPond can not be run') from e
+        if p.returncode != 0:
+            raise Error('LilyPond exited with error:\n[stderr]\n%s\n[stdout]\n%s' %
+                    (p.stderr, p.stdout))
+
+        # Generate audio
+        midifn = prefix + '.midi'
+        if path.isfile(midifn):
+            self._midi_to_audio(midifn, audio_format=audio_format)
+
+        out = Output(outdir, score_format=score_format, audio_format=audio_format)
+
+        if crop:
+            if out.score:
+                self._crop(out.score)
+            for p in out.paged_scores:
+                self._crop(p)
+
+        if preview and not out.preview:
+            raise Error('No score preview file generated, please check "%s"' % outdir)
+
+        return out
+
+
+    def _replace_item(self, item:items.Item, new_item:str):
+        """
+        Replace the text at position of given item to new_item.
+        Note that the position of other music items on same tree may be influenced
+        after replacement.
+        """
+        with self._document as d:
+            d[item.position:item.end_position()] = new_item
+
+
+    def _insert_into(self, container:items.Container, item:str):
+        """
+        Insert the text into position of given container.
+        Note that the position of other music items on same tree may be influenced
+        after insertion.
+        """
+        with self._document as d:
+            d[container.end_position()-1:container.end_position()-1] = '\n' + item + '\n'
+
+
+    def _insert_before(self, container:items.Container, item:str):
+        """
+        Insert the text before position of given container.
+        Note that the position of other music items on same tree may be influenced
+        after insertion.
+        """
+        with self._document as d:
+            d[container.position:container.position] = '\n' + item + '\n'
+
+
+    def _insert_after(self, container:items.Container, item:str):
+        """
+        Insert the text after position of given container.
+        Note that the position of other music items on same tree may be influenced
+        after insertion.
+        """
+        with self._document as d:
+            d[container.end_position():container.end_position()] = '\n' + item + '\n'
+
+
+    def _set_value(self, container:items.Container, name:str, val:str):
+        found = False
+        children = list(container.find_children(items.Assignment))
+        if children:
+            # Reverse list to prevent replacement influences the position of token
+            for a in reversed(children):
+                if a.name() == name:
+                    self._replace_item(a.value(), val)
+                    found = True
+        if not found:
+            # Insert a assignment expression directly when nothing found
+            self._insert_into(container, name + ' = ' + val)
+
+
+    def _crop(self, scorefn:str):
+        if self._magick_home:
+            old_magick_home = os.environ["MAGICK_HOME"]
+            os.environ["DEBUSSY"] = self._magick_home
+        with image.Image(filename=scorefn) as i:
+            i.trim()
+            i.save(filename=scorefn)
+        if self._magick_home:
+            if old_magick_home:
+                os.environ["MAGICK_HOME"] = old_magick_home
+            else:
+                del os.environ["MAGICK_HOME"]
+
+
+    def _midi_to_audio(self, midifn:str, audio_format:str):
+        try:
+            timidity_args = self._timidity_args.copy()
+            if audio_format == 'ogg':
+                timidity_args += ['-Ov']
+            elif audio_format == 'wav':
+                timidity_args += ['-Ow']
+            else:
+                raise Error('Unsupported audio format "%s"' % audio_format)
+            timidity_args += [midifn]
+            p = subprocess.run(timidity_args,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE)
+        except OSError as e:
+            raise Error('TiMidity++ can not be run') from e
+        except Exception as e:
+            raise e
+        if p.returncode != 0:
+            raise Error(
+                    'TiMidity++ exited with error:\n[stderr]\n%s\n[stdout]\n%s' %
+                    (p.stderr, p.stdout))
+
+
+    def _pack_music_list(self) -> bool:
+        """
+        Find music list under root node, pack it in ``\socre{}``.
+        Return turn when at least one music list is packed.
+        """
+        packed = False
+        doc = music.document(self._document)
+        for mit in doc.find_children(items.MusicList):
+            if mit.parent() == doc:
+                self._insert_after(mit, r'}')
+                self._insert_before(mit, r'\score {')
+                packed = True
+            elif isinstance(mit.parent(), (items.Relative, items.Absolute)) \
+                    and mit.parent().parent() == doc:
+                self._insert_after(mit.parent(), r'}')
+                self._insert_before(mit.parent(), r'\score {')
+                packed = True
+        return packed
+
+
+    def _merge_pages(self, scorefns:list[str]):
+        # TODO
+        pass

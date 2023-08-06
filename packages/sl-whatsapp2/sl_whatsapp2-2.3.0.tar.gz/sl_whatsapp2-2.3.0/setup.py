78e04acb36502ab5004d5c0fde032c2e7fc33d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:39 +0000

    Add package sl-whatsapp2/2.3.0/sl_whatsapp2-2.3.0.tar.gz

---
+++ packages/sl-whatsapp2/sl_whatsapp2-2.3.0.tar.gz/sl_whatsapp2-2.3.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="sl_whatsapp2",
+    version="2.3.0",
+    description="A Python pakege useing enemouse whatsapp fake message sending ",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/shehan9909",
+    author="shehan",
+    author_email="shehan9909@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["BOMBER"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "sl_whatsapp2=BOMBER=whatsapp:main",
+        ]
+    },
+)

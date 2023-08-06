08e190a37e17ebfa5e3eb4b266595223a6d03e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:37 +0000

    Add package pyobjc-framework-GameController/7.1/pyobjc-framework-GameController-7.1.tar.gz

---
+++ packages/pyobjc-framework-GameController/pyobjc-framework-GameController-7.1.tar.gz/pyobjc-framework-GameController-7.1/setup.py
@@ -0,0 +1,36 @@
+"""
+Wrappers for the "GameController" framework on macOS introduced in macOS 10.9.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+import os
+
+from pyobjc_setup import setup, Extension
+
+VERSION = '7.1'
+
+setup(
+    name="pyobjc-framework-GameController",
+    description="Wrappers for the framework GameController on macOS",
+    packages=["GameController"],
+    ext_modules=[
+        Extension(
+            "GameController._GameController",
+            ["Modules/_GameController.m"],
+            extra_link_args=["-framework", "GameController"],
+            py_limited_api=True,
+            depends=[
+                os.path.join("Modules", fn)
+                for fn in os.listdir("Modules")
+                if fn.startswith("_GameController")
+            ],
+        )
+    ],
+    version=VERSION,
+    install_requires=["pyobjc-core>=" + VERSION, "pyobjc-framework-Cocoa>=" + VERSION],
+    long_description=__doc__,
+    options={"bdist_wheel": {"py_limited_api": "cp36"}},
+)

71e36210a3c3c1be3057c68266ad2020c25c5ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:29 +0000

    Add package weather-terminal/1.1.4/weather-terminal-1.1.4.tar.gz

---
+++ packages/weather-terminal/weather-terminal-1.1.4.tar.gz/weather-terminal-1.1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="weather-terminal", # Replace with your own username
+    version="1.1.4",
+    author="TheProgrammingArchive",
+    author_email="TheProgrammingArchive@gmail.com",
+    license="MIT",
+    description="terminalWeather",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheProgrammingArchive/weather-terminal",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    package_data={
+	'weather-terminal': ['locations.txt', 'settingspage.txt'],
+    },
+    
+)

f70d6f176f95e9c2f377d147eb5ce3c1d8484139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:51 +0000

    Add package aioketraapi/0.1.10/aioketraapi-0.1.10.tar.gz

---
+++ packages/aioketraapi/aioketraapi-0.1.10.tar.gz/aioketraapi-0.1.10/aioketraapi/group.py
@@ -0,0 +1,29 @@
+from aioketraapi.models.group import Group as GroupModel
+from aioketraapi.models.lamp_state import LampState as LampStateModel
+from aioketraapi.api.group_operations_api import GroupOperationsApi
+
+
+
+class Group(GroupModel):
+    def __init__(self, group_model: GroupModel, hub):
+        super().__init__(**group_model.to_dict())
+        self.state = group_model.state
+        self.hub = hub
+
+    def update_state_from_model(self, lamp_state: LampStateModel):
+        self.state = lamp_state
+
+    async def update_state(self):
+        async with self.hub.create_client_session() as api_client:
+            updated_group = await GroupOperationsApi(api_client).groups_group_id_state_get(self.id)
+            self.update_state_from_model(updated_group.content)
+
+    async def set_state(self, lamp_state: LampStateModel, **kwargs):
+        async with self.hub.create_client_session() as api_client:
+            updated_group = await GroupOperationsApi(api_client).groups_group_id_state_put(self.id, lamp_state, **kwargs)
+            self.update_state_from_model(updated_group.content)
+
+
+
+
+

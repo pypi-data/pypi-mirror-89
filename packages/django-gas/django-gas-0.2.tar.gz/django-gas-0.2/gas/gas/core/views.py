dde5ee68583b3bc2aea7ee8d36d905d11b9e8b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:44 +0000

    Add package django-gas/0.2/django-gas-0.2.tar.gz

---
+++ packages/django-gas/django-gas-0.2.tar.gz/django-gas-0.2/gas/gas/core/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.shortcuts import resolve_url
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+
+from gas.views import GASMixin
+
+
+class GASLoginView(LoginView):
+    template_name = "gas/login.html"
+
+    def get_success_url(self):
+        url = self.get_redirect_url()
+        return url or resolve_url('gas:index')
+
+
+class GASPasswordChangeView(GASMixin, PasswordChangeView):
+    template_name = 'gas/base_form.html'
+    success_url = reverse_lazy('gas:index')
+
+
+class Index(GASMixin, TemplateView):
+    main_menu = 'index'
+    template_name = "gas/index.html"
+    roles = ('staff',)
---
+++ packages/django-gas/django-gas-0.2.tar.gz/django-gas-0.2/gas/gas_settings.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+
+
+TITLE = getattr(settings, 'GAS_TITLE', 'GAS')
+LOGO = getattr(settings, 'GAS_LOGO', 'gas/css/img/logo.svg')
+
+MEDIA = getattr(settings, 'GAS_MEDIA', {
+    'css': [
+        'vendor/font-awesome/css/all.css',
+        'vendor/select2/css/select2.css',
+        'gas/css/gas.css',
+    ],
+    'js': [
+        'vendor/jquery/dist/jquery.min.js',
+        'vendor/jquery.formset.js',
+        'vendor/select2/js/select2.full.min.js',
+        'gas/js/add_popups.js',
+        'gas/js/gas.js',
+    ],
+})

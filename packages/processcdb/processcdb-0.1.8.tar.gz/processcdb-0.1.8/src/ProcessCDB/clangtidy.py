2298acff1ca0126cbf5fa5a06aece25ccf144ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:23 +0000

    Add package processcdb/0.1.8/processcdb-0.1.8.tar.gz

---
+++ packages/processcdb/processcdb-0.1.8.tar.gz/processcdb-0.1.8/src/ProcessCDB/clangtidy.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
+from xml.dom import minidom
+from xml.sax.saxutils import escape
+from pathlib import Path
+import tempfile
+import os
+import concurrent.futures
+from functools import partial
+import shutil
+import signal
+from .logger import LOGGER as log
+from .toolbase import Tool
+from .tidy_converter import OutputParser
+
+list_of_futures = []
+
+
+def inthandler(a1, a2):
+    global list_of_futures
+    for i in list_of_futures:
+        i.cancel()
+
+
+class ClangTidy(Tool):
+    def convert_arguments(self, arg):
+        if arg.startswith("/"):
+            return arg.replace("-", "/", 1)
+        return arg
+
+    def filter_arguments(self, args):
+        def allowed_argument(arg):
+            return arg[1:] not in self.config["arg_blacklist"].split(";")
+
+        return list(filter(allowed_argument, args[1:]))
+
+    def format_output_to_xml(self, filename, allow_dupes=True):
+        def prettify(elem):
+            rough_string = ElementTree.tostring(elem, "utf-8")
+            reparsed = minidom.parseString(rough_string)
+            return reparsed.toprettyxml(indent=" " * 4)
+
+        def generate_verbose(items):
+            x = []
+            for item in items if items else []:
+                x.append(f"{item.path}:{item.line}:{item.column}: {item.message}")
+            return x
+
+        def clean_rules(rules):
+            seen = []
+            for rule in rules:
+                if rule not in seen:
+                    seen.append(rule)
+            return seen
+
+        p = OutputParser()
+
+        xml_root = Element("results")
+        xml_root.set("version", "2")
+        xml_cppcheck = Element("cppcheck")
+        xml_cppcheck.set("version", "1.77")
+        xml_root.append(xml_cppcheck)
+        xml_errors = Element("errors")
+        rules = []
+        rules = p.parse_messages_from_file(filename)
+        if not allow_dupes:
+            rules = clean_rules(rules)
+
+        for rule in rules:
+            xml_error = Element("error")
+            xml_error.set("id", rule.checker)
+            xml_error.set("severity", "warning")
+            if "modernize-" in rule.checker or "google-readability-" in rule.checker or "readability-" in rule.checker:
+                xml_error.set("severity", "style")
+            elif "performance-" in rule.checker:
+                xml_error.set("severity", "performance")
+            message = escape(f"{rule.message} (l:{rule.line},c:{rule.column})")
+            xml_error.set("msg", message)
+            verbose = []
+            verbose.extend(generate_verbose(rule.fixits))
+            verbose.extend(generate_verbose(rule.notes))
+
+            final = escape("\n".join(filter(None, verbose)))
+            if len(final) > 0:
+                xml_error.set("verbose", final)
+
+            xml_location = Element("location")
+            xml_location.set("file", rule.path)
+            xml_location.set("line", str(rule.line))
+            xml_error.append(xml_location)
+            xml_errors.append(xml_error)
+
+        xml_root.append(xml_errors)
+        with open(filename, "w") as f:
+            f.write(prettify(xml_root))
+
+    def process_queue(self, cmd, tmp_dir):
+        output = self.capture(cmd, True)
+        if output[0] == "":
+            output = output[1:]
+
+        if output and tmp_dir:
+            with tempfile.NamedTemporaryFile(mode="w", dir=tmp_dir, suffix=".log", delete=False) as t:
+                t.write("\n".join(output))
+
+    def _generate_cmd_queue(self, cdb, args):
+        command_queue = []
+        for compilation_unit in cdb:
+            arguments = []
+
+            arguments.extend(self.config["default_args"].split(";"))
+            directory = Path(compilation_unit["directory"]).absolute()
+            full_command = compilation_unit["command"].split(" ")
+            absolute_filename = directory / compilation_unit["file"]
+            compiler = Path(full_command[0]).name.lower()
+
+            arguments.extend(self.quote_defines(self.filter_arguments(full_command[1:])))
+            arguments = self.convert_includes(arguments)
+            arguments.extend(self.includes_as_cli_flags(self.default_includes()))
+
+            extra = "--quiet"
+            if compiler == "cl.exe":
+                arguments = list(map(self.convert_arguments, arguments))
+                extra = f"{extra} --extra-arg-before=--driver-mode=cl"
+                if "-EHsc" in arguments or "/EHsc" in arguments:
+                    arguments.extend(["-Xclang", "-fcxx-exceptions"])  # Because clang ignores thread enabling ..
+
+            if absolute_filename.is_file():
+                if self.should_scan(absolute_filename, args.file):
+                    tmp_cmd = f"cd {directory} && {self.binary} {extra} {absolute_filename} -- {' '.join(arguments)}"
+                    command_queue.append(tmp_cmd)
+                else:
+                    log.debug(f"File {absolute_filename} is not scanned")
+        return command_queue
+
+    def execute(self, cdb, args):
+
+        result = 1
+        if not self.tool_exists():
+            raise EnvironmentError(f"tool: {self.tool_name} not in path, cannot execute.")
+
+        command_queue = self._generate_cmd_queue(cdb, args)
+        try:
+            tmp_dir = None
+            if args.output is not None:
+                tmp_dir = Path(tempfile.mkdtemp())
+
+            tasks = self.max_tasks(args, len(command_queue))
+            global list_of_futures
+            signal.signal(signal.SIGINT, inthandler)
+
+            log.info("Starting scanning ...")
+            with concurrent.futures.ProcessPoolExecutor(tasks) as executor:
+                for cmd in command_queue:
+                    future = executor.submit(self.process_queue, cmd, tmp_dir)
+                    future.add_done_callback(partial(log.debug, cmd))
+                    future = list_of_futures.append(future)
+
+            log.info("Waiting for scanning processes to finish ..")
+            concurrent.futures.wait(list_of_futures)
+            log.info("Scanning done ...")
+
+            if args.output is not None:
+                log.debug("User requested output to {args.output}, scanning log files and saving ...")
+                with open(args.output, "w") as dst:
+                    for name in tmp_dir.glob("*.log"):
+                        dst.write(name.read_text())
+
+                if args.xml:
+                    log.debug("User requested output as xml, converting ...")
+                    self.format_output_to_xml(args.output, args.allow_dupes)
+                shutil.rmtree(tmp_dir)
+            log.info("All done")
+            result = 0
+        except KeyboardInterrupt:
+            log.debug("Keyboard interrupt!")
+            if tmp_dir is not None:
+                shutil.rmtree(tmp_dir)
+            os.kill(0, 9)
+        except Exception as e:  # TODO: Add proper exception handling
+            log.error(f"Exception: {e} .-- ")
+            result = 1
+        log.info("All done")
+        return result
---
+++ packages/processcdb/processcdb-0.1.8.tar.gz/processcdb-0.1.8/src/ProcessCDB/toolbase.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+
+from .misc import is_any_windows, capture_output
+import multiprocessing
+import subprocess
+import configparser
+import os
+from .logger import LOGGER as log
+import shutil
+import shlex
+from fnmatch import fnmatch
+
+
+class Tool(object):
+    tool_name = ""
+    capture = staticmethod(capture_output)
+
+    def default_config(self):
+        cfg = configparser.ConfigParser()
+        cfg[self.tool_name] = {
+            "binary": self.tool_name,
+            "file_blacklist": "",
+            "arg_blacklist": "",
+            "jobs": 0,
+            "default_includes": "",
+            "default_args": "",
+            "includes_as_system": "",
+        }
+        return cfg[self.tool_name]
+
+    def _set_defaults(self, config):
+        self.config = self.default_config()
+        if config and self.tool_name in config:
+            section = config[self.tool_name]
+            for key in section.keys():
+                self.config[key] = section[key]
+
+    def __init__(self, tool_name, tool_config=None):
+        self.tool_name = tool_name
+        self.config = {}
+        self._set_defaults(tool_config)
+
+    @property
+    def prevent_scan(self):
+        if self.config and "file_blacklist" in self.config:
+            return list(filter(None, self.config["file_blacklist"].split(";")))
+        return []
+
+    @property
+    def binary(self):
+        tool_name = self.tool_name
+        if "binary" in self.config and self.config["binary"]:
+            tool_name = self.config["binary"]
+
+        if is_any_windows():
+            if " " in tool_name:
+                tool_name = f'"{tool_name}"'  # Why oh why doesnt shlex.quote() use " on windows wtf?
+        else:
+            tool_name = shlex.quote(tool_name)
+        return tool_name
+
+    def max_tasks(self, args, total_tasks=0):
+        default_jobs = int(self.config["jobs"])
+        cpu_count = multiprocessing.cpu_count()
+        if args.jobs != 0:
+            default_jobs = args.jobs
+
+        if default_jobs == 0:
+            default_jobs = cpu_count
+
+        if total_tasks > 0 and total_tasks < cpu_count:
+            return total_tasks
+        if default_jobs > cpu_count:
+            log.warning(f"Requesting {default_jobs} concurrent processing jobs is higher than current {cpu_count} core count ")
+        return default_jobs
+
+    def default_includes(self):
+        def clean(str):
+            return str.replace("(framework directory)", "").strip()
+
+        includes = []
+        includes_from_config = self.config["default_includes"]
+
+        if includes_from_config == "env":
+            includes = os.environ.get("INCLUDE", "").split(";")
+        elif includes_from_config == "detect" and not is_any_windows():
+            results = self.capture("clang -x c++ -v -E /dev/null")
+            sidx = results.index("#include <...> search starts here:") + 1
+            eidx = results.index("End of search list.")
+            includes = map(lambda str: clean(str), results[sidx:eidx])
+        else:
+            includes = includes_from_config.split(";")
+
+        return list(filter(None, includes))
+
+    def tool_exists(self):
+        return shutil.which(self.config["binary"]) is not None
+
+    def quote_defines(self, args):
+        # TODO: Maybe use shlex.quote here ?
+        def fix(str):
+            if str.startswith("-D"):
+                return str.replace('"', '\\"')
+            else:
+                return str
+
+        return map(lambda arg: fix(arg), args)
+
+    def should_scan(self, filename, files):
+        # Needs a rewrite
+        res = False
+        patterns = files if isinstance(files, list) else [files]
+        blacklisted = any([fnmatch(filename, pattern) for pattern in self.prevent_scan])
+        requested = any([fnmatch(filename, pattern) for pattern in patterns])
+        if requested:
+            if blacklisted:
+                log.debug(f"File {filename} is blacklisted but requested to be scanned.")
+            res = True
+        else:
+            if not patterns and not blacklisted:
+                res = True
+        return res
+
+    def execute(self, cdb, args=None):
+        return None
+
+    def run(self, command_line):
+        log.debug(f"RUN: {command_line}")
+        return subprocess.call(command_line, shell=True)
+
+    def include_string(self, include_path, path_matchers):
+        if any([fnmatch(include_path, pattern) for pattern in path_matchers]):
+            return f'-isystem "{include_path}"'
+        else:
+            return f"-I{include_path}"
+
+    def includes_as_cli_flags(self, includes):
+        path_matchers = self.config["includes_as_system"].split(";")
+        return list(map(lambda i: self.include_string(i, path_matchers), includes))
+
+    def convert_includes(self, arguments):
+        def convert(arg, path_matchers):
+            if arg and arg.startswith("-I"):
+                return self.include_string(arg[2:], path_matchers)
+            return arg
+
+        path_matchers = self.config["includes_as_system"].split(";")
+        return list(map(lambda arg: convert(arg, path_matchers), arguments))

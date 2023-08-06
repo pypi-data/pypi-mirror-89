a68c36320d96a15890e37e694f312273102781a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:21 +0000

    Add package pycrunch-engine/1.2.1/pycrunch-engine-1.2.1.tar.gz

---
+++ packages/pycrunch-engine/pycrunch-engine-1.2.1.tar.gz/pycrunch-engine-1.2.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from distutils.core import setup
+
+setup(name='pycrunch-engine',
+      version='1.2.1',
+      description='Automatic Test Runner Engine',
+      url='http://github.com/gleb-sevruk/pycrunch-engine',
+      author='Gleb Sevruk',
+      author_email='support@pycrunch.com',
+      license='libpng',
+      keywords="tdd unit-testing test runner",
+      packages=setuptools.find_packages(),
+      download_url='https://github.com/gleb-sevruk/pycrunch-engine/archive/v1.2.1.tar.gz',
+      setup_requires=['wheel'],
+      entry_points={
+          'console_scripts': ['pycrunch-engine=pycrunch.main:run'],
+      },
+      install_requires=[
+          'pytest',
+          'coverage==4.5.3',
+          'PyYAML',
+          'watchgod',
+          'python-socketio>=4,<5',
+          'aiohttp',
+      ],
+      include_package_data=True,
+      zip_safe=False)

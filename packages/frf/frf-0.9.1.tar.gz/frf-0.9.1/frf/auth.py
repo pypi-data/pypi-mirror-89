885d2607f91e2eaadfbdb797182d9b0ad07a9d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:33 +0000

    Add package frf/0.9.1/frf-0.9.1.tar.gz

---
+++ packages/frf/frf-0.9.1.tar.gz/frf-0.9.1/frf/auth.py
@@ -0,0 +1,40 @@
+"""Supplies authentication functions."""
+from fastapi import Cookie
+from fastapi import Depends
+from fastapi import Request
+
+from unimatrix.ext import crypto
+from unimatrix.ext import jose
+
+from .exceptions import BearerTokenMissing
+from .exceptions import InvalidAuthorizationScheme
+from .exceptions import BearerTokenUnverifiable
+
+
+def get_bearer_token(request: Request):
+    header = request.headers.get('Authorization')
+    value = None
+    if header:
+        scheme, _, value = str.partition(header, ' ')
+        if str.lower(scheme) != 'bearer':
+            raise InvalidAuthorizationScheme(scheme, ['Bearer'])
+    return value
+
+
+def RequestClaims(allow_cookies=False, cookie_name='sessionid', required=True):
+    async def f(request: Request, bearer=Bearer, sessionid:str=Cookie(None)):
+        if not bearer and allow_cookies:
+            bearer = request.cookies.get(cookie_name)
+        if not bearer:
+            raise BearerTokenMissing
+        try:
+            return await jose.payload(str.encode(bearer))
+        except crypto.Signature.InvalidSignature:
+            if required:
+                raise BearerTokenUnverifiable
+            return None
+
+    return Depends(f)
+
+
+Bearer = Depends(get_bearer_token)

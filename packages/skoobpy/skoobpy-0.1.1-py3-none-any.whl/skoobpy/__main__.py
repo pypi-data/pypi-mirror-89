5025b32248fcbc91a86b0645c22fd35ef9286ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:08 +0000

    Add package skoobpy/0.1.1/skoobpy-0.1.1-py3-none-any.whl

---
+++ packages/skoobpy/skoobpy-0.1.1-py3-none-any.whl/skoobpy/__main__.py
@@ -0,0 +1,14 @@
+from skoobpy import get_all_books
+from skoobpy import save_desired_csv
+
+def main():
+    from sys import argv
+    user_id = argv[1]
+
+    books_json = get_all_books(user_id)
+    save_desired_csv(books_json, user_id)
+
+
+
+if __name__ == "__main__":
+    main()

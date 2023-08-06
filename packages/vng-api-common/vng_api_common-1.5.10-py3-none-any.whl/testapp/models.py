399ce48a2536dac164ca2210a146ca57182682b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:05 +0000

    Add package vng-api-common/1.5.10/vng_api_common-1.5.10-py3-none-any.whl

---
+++ packages/vng-api-common/vng_api_common-1.5.10-py3-none-any.whl/testapp/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from vng_api_common.caching import ETagMixin
+from vng_api_common.descriptors import GegevensGroepType
+
+
+class Group(models.Model):
+    name = models.CharField(_("name"), max_length=100)
+
+    subgroup_field_1 = models.CharField(max_length=50, blank=True)
+    subgroup_field_2 = models.CharField(max_length=50, blank=True, default="baz")
+
+    subgroup = GegevensGroepType(
+        {"field_1": subgroup_field_1, "field_2": subgroup_field_2},
+        optional=("field_2",),
+    )
+
+
+class Person(ETagMixin, models.Model):
+    name = models.CharField(_("name"), max_length=50)
+
+    address_street = models.CharField(_("street name"), max_length=255)
+    address_number = models.CharField(_("house number"), max_length=10)
+
+    address = GegevensGroepType(
+        {"street": address_street, "number": address_number}, required=False
+    )
+
+    group = models.ForeignKey("Group", null=True, on_delete=models.CASCADE)
+
+    hobbies = models.ManyToManyField("Hobby", related_name="people", blank=True)
+
+
+class Hobby(ETagMixin, models.Model):
+    name = models.CharField(_("name"), max_length=100)
---
+++ packages/vng-api-common/vng_api_common-1.5.10-py3-none-any.whl/vng_api_common/tests/auth.py
@@ -0,0 +1,151 @@
+import time
+from typing import List, Optional
+
+import jwt
+from rest_framework import status
+
+from ..authorizations.models import Applicatie, AuthorizationsConfig, Autorisatie
+from ..constants import VertrouwelijkheidsAanduiding
+from ..models import JWTSecret
+
+
+class AuthCheckMixin:
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        JWTSecret.objects.get_or_create(
+            identifier="testsuite", defaults={"secret": "letmein"}
+        )
+
+    def assertForbidden(self, url, method="get", request_kwargs=None):
+        """
+        Assert that an appropriate scope is required.
+        """
+        do_request = getattr(self.client, method)
+        request_kwargs = request_kwargs or {}
+
+        with self.subTest(case="JWT missing"):
+            response = do_request(url, **request_kwargs)
+
+            self.assertEqual(
+                response.status_code, status.HTTP_403_FORBIDDEN, response.data
+            )
+
+        with self.subTest(case="Invalid JWT structure"):
+            invalid_jwt = generate_jwt_auth("testsuite", "letmein")[:-10]
+            self.client.credentials(HTTP_AUTHORIZATION=invalid_jwt)
+
+            response = do_request(url, **request_kwargs)
+
+            self.assertEqual(
+                response.status_code, status.HTTP_403_FORBIDDEN, response.data
+            )
+
+
+def generate_jwt_auth(
+    client_id, secret, user_id="test_user_id", user_representation="Test User"
+):
+    """
+    Generate a JWT suitable for the second version of the AC-based auth.
+    """
+    payload = {
+        # standard claims
+        "iss": "testsuite",
+        "iat": int(time.time()),
+        # custom
+        "client_id": client_id,
+        "user_id": user_id,
+        "user_representation": user_representation,
+    }
+    encoded = jwt.encode(payload, secret, algorithm="HS256")
+    encoded = encoded.decode("ascii")
+    return f"Bearer {encoded}"
+
+
+class JWTAuthMixin:
+    """
+    Configure the local auth cache.
+
+    Creates the local auth objects for permission checks, as if you're talking
+    to a real AC behind the scenes.
+    """
+
+    client_id = "testsuite"
+    secret = "letmein"
+
+    user_id = "test_user_id"
+    user_representation = "Test User"
+
+    scopes = None
+    heeft_alle_autorisaties = False
+    zaaktype = None
+    informatieobjecttype = None
+    besluittype = None
+    max_vertrouwelijkheidaanduiding = VertrouwelijkheidsAanduiding.zeer_geheim
+
+    @staticmethod
+    def _create_credentials(
+        client_id: str,
+        secret: str,
+        heeft_alle_autorisaties: bool,
+        max_vertrouwelijkheidaanduiding: str,
+        scopes: Optional[List[str]] = None,
+        zaaktype: Optional[str] = None,
+        informatieobjecttype: Optional[str] = None,
+        besluittype: Optional[str] = None,
+    ):
+        JWTSecret.objects.get_or_create(
+            identifier=client_id, defaults={"secret": secret}
+        )
+
+        config = AuthorizationsConfig.get_solo()
+
+        applicatie = Applicatie.objects.create(
+            client_ids=[client_id],
+            label="for test",
+            heeft_alle_autorisaties=heeft_alle_autorisaties,
+        )
+
+        if heeft_alle_autorisaties is False:
+            autorisatie = Autorisatie.objects.create(
+                applicatie=applicatie,
+                component=config.component,
+                scopes=scopes or [],
+                zaaktype=zaaktype or "",
+                informatieobjecttype=informatieobjecttype or "",
+                besluittype=besluittype or "",
+                max_vertrouwelijkheidaanduiding=max_vertrouwelijkheidaanduiding,
+            )
+        else:
+            autorisatie = None
+
+        return applicatie, autorisatie
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        applicatie, autorisatie = cls._create_credentials(
+            cls.client_id,
+            cls.secret,
+            heeft_alle_autorisaties=cls.heeft_alle_autorisaties,
+            scopes=cls.scopes,
+            zaaktype=cls.zaaktype,
+            informatieobjecttype=cls.informatieobjecttype,
+            besluittype=cls.besluittype,
+            max_vertrouwelijkheidaanduiding=cls.max_vertrouwelijkheidaanduiding,
+        )
+        cls.applicatie = applicatie
+        cls.autorisatie = autorisatie
+
+    def setUp(self):
+        super().setUp()
+
+        token = generate_jwt_auth(
+            client_id=self.client_id,
+            secret=self.secret,
+            user_id=self.user_id,
+            user_representation=self.user_representation,
+        )
+        self.client.credentials(HTTP_AUTHORIZATION=token)

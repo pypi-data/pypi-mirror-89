de608d68dd248f8cf4891ba44d291b3ce3d17c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:27 +0000

    Add package boardwatch-models/0.7.0/boardwatch_models-0.7.0-py3-none-any.whl

---
+++ packages/boardwatch-models/boardwatch_models-0.7.0-py3-none-any.whl/boardwatch_models/listing.py
@@ -0,0 +1,24 @@
+class Listing():
+	registry = {}
+
+	def __init__(self, id, native_id, title, body, url, seller_email, seller_phone, date_posted, date_scraped):
+		self.id = id
+		self.native_id = native_id
+		self.title = title
+		self.body = body
+		self.url = url
+		self.seller_email = seller_email
+		self.seller_phone = seller_phone
+		self.date_posted = date_posted
+		self.date_scraped = date_scraped
+
+	def add_to_registry(self):
+		Listing.registry[self.id] = self
+
+	@classmethod
+	def get_by_id(cls, listing_id):
+		return cls.registry[listing_id]
+
+	@classmethod
+	def get_all(cls):
+		return cls.registry.values()

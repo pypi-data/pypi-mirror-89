b0b77342f3f0768f45df1f7dd6b5368c79b44056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:44 +0000

    Add package tell-me-your-secrets/2.1.1/tell_me_your_secrets-2.1.1-py3-none-any.whl

---
+++ packages/tell-me-your-secrets/tell_me_your_secrets-2.1.1-py3-none-any.whl/tell_me_your_secrets/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+
+def create_logger(logger_name: str, level: int = logging.INFO):
+    custom_logger = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    ch.setFormatter(formatter)
+    custom_logger.addHandler(ch)
+    custom_logger.setLevel(level)
+
+    return custom_logger

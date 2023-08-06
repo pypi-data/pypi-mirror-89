a1ccd8961db89562d10b401912ef469cf21b721b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:47 +0000

    Add package bmi-calculator/0.0.1/bmi_calculator-0.0.1.tar.gz

---
+++ packages/bmi-calculator/bmi_calculator-0.0.1.tar.gz/bmi_calculator-0.0.1/README.md
@@ -0,0 +1,5 @@
+# BMI_Calc Package
+This is a BMI calculator package
+
+## What it does
+- Calculates user's bmi
---
+++ packages/bmi-calculator/bmi_calculator-0.0.1.tar.gz/bmi_calculator-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="bmi_calculator",
+    version="0.0.1",
+    author="Eromosele Iriogbe",
+    author_email="eiriogbe@gmail.com",
+    description="Calculates BMI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EronzG/BMI_library",
+    packages=setuptools.find_packages(),
+    install_requires=[''],  # if you have libraries that your module depen
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

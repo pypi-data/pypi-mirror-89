cdbb381ad85b2e73f346f3f6bed3ad7131257696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:01 +0000

    Add package eyecu-bumblebee/0.2.10/eyecu-bumblebee-0.2.10.tar.gz

---
+++ packages/eyecu-bumblebee/eyecu-bumblebee-0.2.10.tar.gz/eyecu-bumblebee-0.2.10/bumblebee/sources/FileStream.py
@@ -0,0 +1,25 @@
+import cv2
+
+from ..interfaces.ISource import ISource
+
+
+class FileStream(ISource):
+
+    def __init__(self, filepath: str):
+
+        super().__init__()
+        self.path = filepath
+
+        self.cap = cv2.VideoCapture(self.path)
+        self.cap.open(0)
+
+
+    def __del__(self):
+        self.cap.release()
+
+
+    def __len__(self):
+        return self.get_duration()
+
+    def get_duration(self):
+        return self.cap.get(cv2.CAP_PROP_FRAME_COUNT) + 1

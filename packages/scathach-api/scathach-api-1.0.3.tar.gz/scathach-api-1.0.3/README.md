9d8490eebd4d6ef2fe0c061b537b770fe0119683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:27 +0000

    Add package scathach-api/1.0.3/scathach-api-1.0.3.tar.gz

---
+++ packages/scathach-api/scathach-api-1.0.3.tar.gz/scathach-api-1.0.3/README.md
@@ -0,0 +1,28 @@
+# nekos.py
+A Python module that uses Nekos API
+
+## Requirements
+- Python 3.6 and up - https://www.python.org/downloads/
+
+# Install
+```
+pip install nekos.py
+```
+
+# Usage example
+```py
+import nekos
+print(nekos.cat())
+
+>>> 'https://cdn.nekos.life/meow/04D5A.jpg'
+```
+
+# Endpoints
+NOTE: You must call them as functions
+- cat
+- eightball
+- fact
+- img
+- owoify
+- textcat
+- why
---
+++ packages/scathach-api/scathach-api-1.0.3.tar.gz/scathach-api-1.0.3/setup.py
@@ -0,0 +1,31 @@
+import re
+
+from setuptools import setup
+
+
+version = ''
+with open('scathach/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+if not version:
+    raise RuntimeError('version is not set')
+
+
+setup(
+    name='scathach-api',
+    author='sinkaroid',
+    author_email='anakmancasan@gmail.com',
+    url='https://github.com/sinkaroid/scathach-api',
+    version=version,
+    packages=['scathach'],
+    license='MIT',
+    description='An advanced NSFW wrapper, complete rewrite and overhaul of the original Fate/Lewd Order API.',
+    include_package_data=True,
+    install_requires=requirements
+)

6b493179ecbf091c3b5e8d79b6063403b63b024c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:04 +0000

    Add package splitbam/0.1.2/splitbam-0.1.2.tar.gz

---
+++ packages/splitbam/splitbam-0.1.2.tar.gz/splitbam-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='splitbam',
+    version='0.1.2',
+    author='Anthony Aylward',
+    author_email='aaylward@eng.ucsd.edu',
+    description='Split a BAM file into two subsamples',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/anthony-aylward/splitbam.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=['pysam'],
+    entry_points={
+        'console_scripts': ['splitbam=splitbam.splitbam:main',]
+    }
+)

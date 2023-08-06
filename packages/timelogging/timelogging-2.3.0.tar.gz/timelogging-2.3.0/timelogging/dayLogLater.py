1572d5542a7702010d6f08bf217fcf6074fd0741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:05 +0000

    Add package timelogging/2.3.0/timelogging-2.3.0.tar.gz

---
+++ packages/timelogging/timelogging-2.3.0.tar.gz/timelogging-2.3.0/timelogging/dayLogLater.py
@@ -0,0 +1,4 @@
+from . import configLog, log, logOnce, startLogging, endLogging
+"""shortcut import for time logging
+logs day and time"""
+configLog("%d %H:%M:%S", 30)
---
+++ packages/timelogging/timelogging-2.3.0.tar.gz/timelogging-2.3.0/timelogging/timeLogLater.py
@@ -0,0 +1,3 @@
+from . import configLog, log, logOnce, startLogging, endLogging
+""" shortcut import for time logging """
+configLog(startLevel=30)

49baa90ae3ce3c736e36912bab29fb511bff75c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:03 +0000

    Add package sixfab-power-python-api/0.2.2/sixfab-power-python-api-0.2.2.tar.gz

---
+++ packages/sixfab-power-python-api/sixfab-power-python-api-0.2.2.tar.gz/sixfab-power-python-api-0.2.2/README.md
@@ -0,0 +1,14 @@
+# Sixfab Power Python API
+
+## Installation
+```pip3 install sixfab-power-python-api```
+
+## Usage
+#### Example Codes
+* [read_sensors.py](./example/read_sensors.py)
+* [rtc_set.py](./example/rtc_set.py)
+* [create_event.py](./example/create_event.py)
+* [update_firmware.py](./example/update_firmware.py) 
+
+## API Docs
+[API Docs](https://sixfab.github.io/sixfab-power-python-api/)
---
+++ packages/sixfab-power-python-api/sixfab-power-python-api-0.2.2.tar.gz/sixfab-power-python-api-0.2.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sixfab-power-python-api',
+    version='0.2.2',
+    author='Yasin Kaya',
+    author_email='yasinkaya.121@gmail.com',
+    description='Sixfab power management service python api',
+    license='MIT',
+    url='https://github.com/sixfab/sixfab-power-python-api',
+    dependency_links  = [],
+    install_requires  = ['smbus2==0.3.0', 'crc16==0.1.1', 'vcgencmd==0.1.1'],
+	packages=find_packages()
+)

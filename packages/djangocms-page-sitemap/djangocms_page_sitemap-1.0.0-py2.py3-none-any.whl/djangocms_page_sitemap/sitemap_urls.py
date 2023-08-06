7b2a36458d525e220b225048e87e2a312f697148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:35 +0000

    Add package djangocms-page-sitemap/1.0.0/djangocms_page_sitemap-1.0.0-py2.py3-none-any.whl

---
+++ packages/djangocms-page-sitemap/djangocms_page_sitemap-1.0.0-py2.py3-none-any.whl/djangocms_page_sitemap/sitemap_urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib.sitemaps.views import sitemap
+
+from .sitemap import ExtendedSitemap
+
+urlpatterns = [
+    url(r"^sitemap\.xml$", sitemap, {"sitemaps": {"cmspages": ExtendedSitemap}}),
+]

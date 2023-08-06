75652d0ed2ea1181b0cef3c674b35c11ac2f1a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:18 +0000

    Add package redPacket/0.1.1/redPacket-0.1.1.tar.gz

---
+++ packages/redPacket/redPacket-0.1.1.tar.gz/redPacket-0.1.1/setup.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import setuptools
+
+setup(
+    name='redPacket',  # 包的名字
+    author='zhuyancun',  # 作者
+    version='0.1.1',  # 版本号
+    license='MIT',
+
+    description='Delivery red-packets program.',  # 描述
+    long_description='fix the dependency install package problem.',
+    author_email='mydeepl@foxmail.com',  # 你的邮箱**
+    url='https://github.com/Relvyy/',  # 可以写github上的地址，或者其他地址
+    # 包内需要引用的文件夹
+    # packages=setuptools.find_packages(exclude=['url2io',]),
+    packages=["redPacket"],
+    # keywords='NLP,tokenizing,Chinese word segementation',
+    # package_dir={'jieba':'jieba'},
+    # package_data={'jieba':['*.*','finalseg/*','analyse/*','posseg/*']},
+
+    # 依赖包
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        # 'Development Status :: 4 - Beta',
+        'Operating System :: POSIX :: Linux',
+        #'Operating System :: Linux'  # 你的操作系统  OS Independent      Microsoft
+        #'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        # 'License :: OSI Approved :: BSD License',  # BSD认证
+        'Programming Language :: Python',  # 支持的语言
+        'Programming Language :: Python :: 3',  # python版本 。。。
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries'
+    ],
+    zip_safe=True,
+)

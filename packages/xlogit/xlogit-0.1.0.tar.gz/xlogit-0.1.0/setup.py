71f453eb0d85a540605507547389d21272a75a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:59 +0000

    Add package xlogit/0.1.0/xlogit-0.1.0.tar.gz

---
+++ packages/xlogit/xlogit-0.1.0.tar.gz/xlogit-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='xlogit',
+                 version='0.1.0',
+                 description='A Python package for GPU-accelerated ' +
+                 'estimation of mixed logit models.',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url='https://github.com/arteagac/xlogit',
+                 author='Cristian Arteaga',
+                 author_email='cristiandavidarteaga@gmail.com',
+                 license='MIT',
+                 packages=['xlogit'],
+                 zip_safe=False,
+                 python_requires='>=3.5',
+                 install_requires=[
+                     'numpy>=1.13.1',
+                     'scipy>=1.0.0'
+                     ])

af8d76fe7ca5bde3579744edf75bbe1b3eadb59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:45 +0000

    Add package ImgAnn/0.1.2/ImgAnn-0.1.2.tar.gz

---
+++ packages/ImgAnn/ImgAnn-0.1.2.tar.gz/ImgAnn-0.1.2/ImgAnn/convert.py
@@ -0,0 +1,34 @@
+
+# convert method implementation class
+
+from .operators import *
+
+class Convertor:
+
+    def __init__(self):
+        pass
+
+    def coco2csv(self):
+        # TODO: convert coco to csv
+        pass
+
+    def coco2pascalvoc(self):
+        # TODO: convert coco to pascal VOC
+        pass
+
+    def csv2coco(self):
+        # TODO: convert .csv into coco
+        pass
+
+    def csv2pascalvoc(self):
+        # TODO: convert .csv into pascal VOC
+        pass
+
+    def pascalvoc2coco(self):
+        # TODO: convert pascal VOC into coco
+        pass
+
+    def pascalvoc2csv(self):
+        # TODO: convert pascal VOC into .csv
+        pass
+

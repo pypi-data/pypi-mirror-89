d1781042b92fc72c9e63ae3c31eebf4bb1e782ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:25 +0000

    Add package NlpToolkit-MorphologicalAnalysis-Cy/1.0.8/NlpToolkit-MorphologicalAnalysis-Cy-1.0.8.tar.gz

---
+++ packages/NlpToolkit-MorphologicalAnalysis-Cy/NlpToolkit-MorphologicalAnalysis-Cy-1.0.8.tar.gz/NlpToolkit-MorphologicalAnalysis-Cy-1.0.8/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(["MorphologicalAnalysis/*.pyx"],
+                          compiler_directives={'language_level': "3"}),
+    name='NlpToolkit-MorphologicalAnalysis-Cy',
+    version='1.0.8',
+    packages=['MorphologicalAnalysis'],
+    package_data={'MorphologicalAnalysis': ['*.pxd', '*.pyx', '*.c', '*.py']},
+    url='https://github.com/olcaytaner/TurkishMorphologicalAnalysis-Cy',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Turkish Morphological Analysis',
+    install_requires=['NlpToolkit-Dictionary-Cy', 'NlpToolkit-Corpus-Cy', 'NlpToolkit-DataStructure-Cy']
+)

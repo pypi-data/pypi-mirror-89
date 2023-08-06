c0b44605c9b1f15a5333c1adba7579fc7028c1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:28 +0000

    Add package contracts-lib-py/0.5.3/contracts_lib_py-0.5.3-py2.py3-none-any.whl

---
+++ packages/contracts-lib-py/contracts_lib_py-0.5.3-py2.py3-none-any.whl/contracts_lib_py/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.5.3'
+from .contract_base import ContractBase
+from .didregistry import DIDRegistry
+from .dispenser import Dispenser
+from .keeper import Keeper
+from .token import Token

096825bd7e36019f239540103895e73c6ecfb212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:00 +0000

    Add package dolfin-warp/2020.12.23.post1/dolfin_warp-2020.12.23.post1.tar.gz

---
+++ packages/dolfin-warp/dolfin_warp-2020.12.23.post1.tar.gz/dolfin_warp-2020.12.23.post1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="dolfin_warp",
+    version="2020.12.23.post1",
+    author="Martin Genet",
+    author_email="martin.genet@polytechnique.edu",
+    description=open("README.md", "r").readlines()[1][:-1],
+    long_description = open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.inria.fr/mgenet/dolfin_warp",
+    packages=["dolfin_warp"],
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "vtk", "myPythonLibrary", "myVTKPythonLibrary", "dolfin_mech"],
+)

fbe88c3f7402f9d00ec220987419eed78df3eae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:26 +0000

    Add package casex/1.0.2/casex-1.0.2.tar.gz

---
+++ packages/casex/casex-1.0.2.tar.gz/casex-1.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='casex',
+    version='1.0.2',
+    description='Casualty expectation toolbox',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+    ],
+    author='Anders la Cour-Harbo et al',
+    author_email='anders@lacourfamily.dk',
+    license='CC-BY-4.0',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    zip_safe=False,
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'descartes',
+        'shapely'
+    ]
+)

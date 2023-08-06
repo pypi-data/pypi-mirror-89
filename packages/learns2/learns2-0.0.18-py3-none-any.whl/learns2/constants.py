acf6ba9dc5061c14d383815e8c119d43429ef1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:03 +0000

    Add package learns2/0.0.18/learns2-0.0.18-py3-none-any.whl

---
+++ packages/learns2/learns2-0.0.18-py3-none-any.whl/learns2/constants.py
@@ -0,0 +1,18 @@
+races = {
+    'Zerg': 'Zerg',
+    '저그': 'Zerg',
+    '异虫': 'Zerg',
+    '蟲族': 'Zerg',
+    '테란': 'Terran',
+    '人類': 'Terran',
+    '人类': 'Terran',
+    'Terran': 'Terran',
+    'Terraner': 'Terran',
+    'Терраны': 'Terran',
+    '프로토스': 'Protoss',
+    '神族': 'Protoss',
+    'Protosi': 'Protoss',
+    '星灵': 'Protoss',
+    'Протоссы': 'Protoss',
+    'Protoss': 'Protoss'
+}

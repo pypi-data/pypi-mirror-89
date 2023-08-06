9875642b5c01d7246e2b43d8d192e58e75c511ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:20 +0000

    Add package metadater/3.1.1/metadater-3.1.1-py3-none-any.whl

---
+++ packages/metadater/metadater-3.1.1-py3-none-any.whl/metadater/exe.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+
+import pefile
+
+logger = logging.getLogger(__name__)
+
+
+def get_info():
+    """ Get information from an executable """
+    try:
+        pe = pefile.PE(sys.argv[0])
+        # Access a StingTable entry like FileVersion:
+        file_version = (
+            pe.FileInfo[0][0].StringTable[0].entries[b"FileVersion"].decode("utf-8")
+        )
+        logger.debug(f"The FileVersion of the PE is: {file_version}")
+        exe_info = {}
+        for entry in pe.FileInfo[0]:
+            if hasattr(entry, "StringTable"):
+                for st_entry in entry.StringTable:
+                    for item in st_entry.entries.items():
+                        exe_info[item[0].decode()] = item[1].decode()
+        logger.debug("Entries in the StringTable: %s" % exe_info)
+        return exe_info
+    except Exception as e:
+        logger.debug(e)
+        return False
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    info = get_info()
+    for k in info:
+        print(k, info[k])
---
+++ packages/metadater/metadater-3.1.1-py3-none-any.whl/metadater/git.py
@@ -0,0 +1,37 @@
+import logging
+from subprocess import CalledProcessError, check_output
+from typing import Dict
+
+logger = logging.getLogger(__name__)
+
+
+def get_info() -> Dict[str, str]:
+    """ Get information from the local git repository """
+    try:
+        git_info = {
+            "author": check_output(["git", "config", "user.name"]),
+            "email": check_output(["git", "config", "user.email"]),
+            "branch": check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]),
+            "tags": check_output(["git", "tag"]),
+            "last_tag": check_output(
+                ["git", "describe", "--tags", "--abbrev=0", "--always"]
+            ),
+            "describe": check_output(
+                ["git", "describe", "--tags", "--long", "--dirty", "--always"]
+            ),
+            "full_path": check_output(["git", "rev-parse", "--show-toplevel"]),
+        }
+        # All subprocess.check_output output is in bytes, let's decode it to utf-8 ánd strip \n
+        for key in git_info:
+            git_info[key] = git_info[key].decode("utf-8").strip()
+        logger.debug("Information from Git: %s" % git_info)
+        return git_info
+    except CalledProcessError as e:
+        logger.debug(e)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    info = get_info()
+    for k in info:
+        print(k, info[k])

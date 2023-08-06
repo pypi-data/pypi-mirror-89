8e992e3b5df202964ea6bf81a7451e5c166f9b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:30 +0000

    Add package test-appleliu-distributions/0.11/test_appleliu_distributions-0.11.tar.gz

---
+++ packages/test-appleliu-distributions/test_appleliu_distributions-0.11.tar.gz/test_appleliu_distributions-0.11/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='test_appleliu_distributions',
+      version='0.11',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)

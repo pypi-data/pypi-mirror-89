41e3bef1a8be5e0e6d4c09fb50ffc916b91d32ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:13 +0000

    Add package pynbs/0.4.2/pynbs-0.4.2.tar.gz

---
+++ packages/pynbs/pynbs-0.4.2.tar.gz/pynbs-0.4.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='pynbs',
+    version='0.4.2',
+    license='MIT',
+    description='A simple python library to read and write .nbs files from Note Block Studio',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    author='Valentin Berlier',
+    author_email='berlier.v@gmail.com',
+    url='https://github.com/vberlier/pynbs',
+
+    platforms=['any'],
+    python_requires='>=2.7',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='note-block-studio minecraft nbs-files',
+
+    py_modules=['pynbs'],
+)

ac84281050c8b585f0b92c289d216acd78b213ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:20 +0000

    Add package scmodels/0.1.1/scmodels-0.1.1.tar.gz

---
+++ packages/scmodels/scmodels-0.1.1.tar.gz/scmodels-0.1.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.md") as history_file:
+    history = history_file.read()
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup_requirements = ["pytest-runner"]
+
+test_requirements = ["pytest"]
+
+extras_requirements = {"plot": ["pygraphviz", "graphviz"], "test": ["pytest"]}
+
+author = "Michael Aichmueller"
+
+setup(
+    author=author,
+    author_email="m.aichmueller@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    description="Structural Causal Models",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords="bayesian graphs scm sem fcm causal graphical models causality",
+    name="scmodels",
+    packages=find_packages(exclude=("test",)),
+    setup_requires=setup_requirements,
+    test_suite="test",
+    tests_require=test_requirements,
+    extras_require=extras_requirements,
+    url="https://github.com/maichmueller/scm",
+    version="0.1.1",
+    zip_safe=False,
+)

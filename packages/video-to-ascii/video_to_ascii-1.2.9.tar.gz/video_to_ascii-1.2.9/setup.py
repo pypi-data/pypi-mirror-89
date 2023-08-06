8b863fd31687fc55bdcf00e5729d47fbd8d15432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:12 +0000

    Add package video-to-ascii/1.2.9/video_to_ascii-1.2.9.tar.gz

---
+++ packages/video-to-ascii/video_to_ascii-1.2.9.tar.gz/video_to_ascii-1.2.9/setup.py
@@ -0,0 +1,51 @@
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+
+def install_package(package):
+    import pip
+    try:
+        from pip._internal import main
+        main.main(['install', package])
+    except AttributeError:
+        from pip import __main__
+        __main__._main(['install', package])
+
+if "--with-audio" in sys.argv:
+    install_package('opencv-python')
+    install_package('pyaudio')
+    sys.argv.remove("--with-audio")
+else:
+    install_package('opencv-python')
+
+setup(
+    name="video_to_ascii",
+    version="1.2.9",
+    author="Joel Ibaceta",
+    author_email="mail@joelibaceta.com",
+    license='MIT',
+    description="It is a simple python package to play videos in the terminal",
+    long_description="A simple tool to play a video using ascii characters instead of pixels",
+    url="https://github.com/joelibaceta/video-to-ascii",
+    project_urls={
+        'Source': 'https://github.com/joelibaceta/video-to-ascii',
+        'Tracker': 'https://github.com/joelibaceta/video-to-ascii/issues'
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['xtermcolor', 'ffmpeg-python'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='video ascii terminal opencv',
+    entry_points={
+        "console_scripts": [
+            'video-to-ascii=video_to_ascii.cli:main'
+        ]
+    }
+)

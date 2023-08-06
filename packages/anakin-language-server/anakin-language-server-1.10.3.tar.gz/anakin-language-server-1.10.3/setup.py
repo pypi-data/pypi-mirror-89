016a0a3253222a9c89ba22da68982991523f437d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:50 +0000

    Add package anakin-language-server/1.10.3/anakin-language-server-1.10.3.tar.gz

---
+++ packages/anakin-language-server/anakin-language-server-1.10.3.tar.gz/anakin-language-server-1.10.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup  # type: ignore
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='anakin-language-server',
+    version='1.10.3',
+    author='Andrii Kolomoiets',
+    author_email='andreyk.mad@gmail.com',
+    description='Yet another Jedi Python language server',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/muffinmad/anakin-language-server',
+    packages=['anakinls'],
+    python_requires='>=3.6',
+    install_requires=[
+        'jedi>=0.17.1',
+        'pygls~=0.9',
+        'pyflakes~=2.2',
+        'pycodestyle~=2.5',
+        'yapf~=0.30'
+    ],
+    entry_points={
+        'console_scripts': [
+            'anakinls=anakinls.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Text Editors :: Integrated Development Environments (IDE)"
+    ]
+)

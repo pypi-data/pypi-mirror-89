a2aabfaee2bcf7b1b0ddc1ba1e429a189a3c901f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:06 +0000

    Add package ashley/1.0.0b4/ashley-1.0.0b4-py2.py3-none-any.whl

---
+++ packages/ashley/ashley-1.0.0b4-py2.py3-none-any.whl/ashley/__init__.py
@@ -0,0 +1,13 @@
+"""Ashley is a django application that customizes and adds features to the django-machina forum."""
+
+
+# pylint: disable=invalid-name
+import os
+
+default_app_config = "ashley.apps.AshleyConfig"
+
+# Main Ashley template directory.
+ASHLEY_MAIN_TEMPLATE_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "templates",
+)

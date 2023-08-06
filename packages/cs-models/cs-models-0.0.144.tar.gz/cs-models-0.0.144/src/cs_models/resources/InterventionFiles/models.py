f9feb76314bb5e9d10c71cf05e10595dcd58eb61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:43 +0000

    Add package cs-models/0.0.144/cs-models-0.0.144.tar.gz

---
+++ packages/cs-models/cs-models-0.0.144.tar.gz/cs-models-0.0.144/src/cs_models/resources/InterventionFiles/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    ForeignKey,
+    UniqueConstraint,
+    String,
+)
+
+from ...database import Base
+
+
+class InterventionFile(Base):
+    __tablename__ = "intervention_files"
+
+    id = Column(Integer, primary_key=True)
+    intervention_id = Column(
+        Integer,
+        ForeignKey('interventions.id'),
+        nullable=False,
+    )
+    file_id = Column(
+        Integer,
+        ForeignKey('files.id'),
+        nullable=False,
+    )
+    type = Column(String(50), nullable=False)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
+
+    __table_args__ = (UniqueConstraint("intervention_id", "file_id"),)

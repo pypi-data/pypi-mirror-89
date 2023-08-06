daca52d1ad68223ae0c8cd7bb8137d30466e4799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:53 +0000

    Add package happyreport/0.0.2/happyreport-0.0.2.tar.gz

---
+++ packages/happyreport/happyreport-0.0.2.tar.gz/happyreport-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf_8_sig") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="happyreport",
+    version="0.0.2",
+    author="wuhaoyu",
+    author_email="wuhaoyu96@163.com",
+    description="Standardized report mail sending module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+    include_package_data=True
+    )

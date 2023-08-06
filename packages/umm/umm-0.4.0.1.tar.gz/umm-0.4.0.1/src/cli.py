2fcb0b94867046ac9f1ff0665433db71abb80963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:48 +0000

    Add package umm/0.4.0.1/umm-0.4.0.1.tar.gz

---
+++ packages/umm/umm-0.4.0.1.tar.gz/umm-0.4.0.1/src/cli.py
@@ -0,0 +1,39 @@
+import click
+from click import echo
+
+from src import C
+from src.commands import pip, npm, composer, brew
+
+CONTEXT_SETTINGS = {
+    'ignore_unknown_options': True
+}
+
+
+@click.group(context_settings=CONTEXT_SETTINGS, help=C.DESCRIPTION)
+# @click.command()
+# @click.argument('tool')
+# @click.option('-v', '--version')
+def cli():
+    pass
+
+
+cli.add_command(pip.cli)
+cli.add_command(npm.cli)
+cli.add_command(composer.cli)
+cli.add_command(brew.cli)
+
+
+@cli.command('v', short_help='Show %s version.' % C.NAME)
+def version():
+    return echo(C.VERSION)
+
+
+# @cli.command('selfupdate')
+# def selfupdate():
+#     '''Update umm'''
+#     pass
+
+
+if __name__ == '__main__':
+    pass
+    cli()

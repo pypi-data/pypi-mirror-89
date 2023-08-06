107a26dfcc23ca97c611afcd20c3fdafa6fb6670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:46 +0000

    Add package xgp-reccomender/2.0.1/xgp_reccomender-2.0.1-py3-none-any.whl

---
+++ packages/xgp-reccomender/xgp_reccomender-2.0.1-py3-none-any.whl/xgp_reccomender/main.py
@@ -0,0 +1,11 @@
+from xgp_reccomender.xgp_reccomender import XgpReccomender
+
+
+def main():
+    """Main function"""
+    my_xgp_reccomender = XgpReccomender()
+    my_xgp_reccomender.generate_html()
+
+
+if __name__ == '__main__':
+    main()

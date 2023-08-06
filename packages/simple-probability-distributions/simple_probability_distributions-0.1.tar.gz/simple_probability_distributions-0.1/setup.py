b12dfc6cb4b042f5605f8d49a17c27956d1ed520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:19 +0000

    Add package simple-probability-distributions/0.1/simple_probability_distributions-0.1.tar.gz

---
+++ packages/simple-probability-distributions/simple_probability_distributions-0.1.tar.gz/simple_probability_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='simple_probability_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['simple_probability_distributions'],
+      author = "Bello Ramon",
+      author_email = "belloramon2014@gmail.com",      
+      zip_safe=False)

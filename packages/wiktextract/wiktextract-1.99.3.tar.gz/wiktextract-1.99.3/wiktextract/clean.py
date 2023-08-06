73d6636dfaed4297742b46a41c81d34a9102cf0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:26 +0000

    Add package wiktextract/1.99.3/wiktextract-1.99.3.tar.gz

---
+++ packages/wiktextract/wiktextract-1.99.3.tar.gz/wiktextract-1.99.3/wiktextract/clean.py
@@ -0,0 +1,90 @@
+# This file contains code to clean Wiktionary annotations from a string and to
+# produce plain text from it, typically for glossary entries but this is also
+# called for various other data to produce clean strings.
+#
+# This file also contains code for cleaning qualifiers for the "tags" field.
+#
+# Copyright (c) 2018-2020 Tatu Ylonen.  See file LICENSE and https://ylonen.org
+
+import re
+import html
+from .config import WiktionaryConfig
+
+######################################################################
+# Cleaning values into plain text.
+######################################################################
+
+def clean_value(config, title, no_strip=False):
+    """Cleans a title or value into a normal string.  This should basically
+    remove any Wikimedia formatting from it: HTML tags, templates, links,
+    emphasis, etc.  This will also merge multiple whitespaces into one
+    normal space and will remove any surrounding whitespace."""
+
+    def repl_1(m):
+        return clean_value(config, m.group(1), no_strip=True)
+    def repl_link(m):
+        if m.group(2) in ("File", "Image"):
+            return ""
+        return clean_value(config, m.group(3) or "", no_strip=True)
+    def repl_link_bars(m):
+        lnk = m.group(1)
+        if re.match(r"(?si)(File|Image)\s*:", lnk):
+            return ""
+        return clean_value(config, m.group(4) or m.group(2) or "",
+                           no_strip=True)
+
+    def repl_1_caret(m):
+        return "^" + clean_value(config, m.group(1))
+
+    assert isinstance(config, WiktionaryConfig)
+    assert isinstance(title, str)
+    title = re.sub(r"\{\{[^}]+\}\}", "", title)
+    # Remove tables
+    title = re.sub(r"(?s)\{\|.*?\|\}", " ", title)
+    # Remove references (<ref>...</ref>).
+    title = re.sub(r"(?is)<\s*ref\s*[^>]*?>\s*.*?<\s*/\s*ref\s*>\n*", "", title)
+    # Replace <br/> by comma space (it is used to express alternatives in some
+    # declensions)
+    title = re.sub(r"(?si)<\s*br\s*/?>\n*", ", ", title)
+    # Change <div> and </div> to newlines
+    title = re.sub(r"(?si)<\s*/?\s*div\b[^>]*>", "\n", title)
+    # Change <sup> ... </sup> to ^
+    title = re.sub(r"(?si)<\s*sup\b[^>]*>(.*?)<\s*/\s*sup\s*>",
+                   repl_1_caret, title)
+    # Remove any remaining HTML tags.
+    title = re.sub(r"(?s)<\s*[^/>][^>]*>\s*", "", title)
+    title = re.sub(r"(?s)<\s*/\s*[^>]+>\n*", "", title)
+    # Replace links by their text
+    title = re.sub(r"(?si)\[\[\s*Category\s*:\s*([^]]+?)\s*\]\]", r"", title)
+    title = re.sub(r"(?s)\[\[\s*([^]|]+?)\s*\|\s*([^]|]+?)"
+                   r"(\s*\|\s*([^]|]+?))?\s*\]\]",
+                   repl_link_bars, title)
+    title = re.sub(r"(?s)\[\[\s*(([a-zA-z0-9]+)\s*:)?\s*([^]|]+?)"
+                   r"(\s*\([^])|]*\)\s*)?\|\]\]",
+                   repl_link, title)
+    title = re.sub(r"(?s)\[\[\s*([^]|]+?)\s*\]\]", repl_1, title)
+    # Replace remaining HTML links by the URL.
+    title = re.sub(r"\[https?:[^]\s]+\s+([^]]+?)\s*\]", repl_1, title)
+    title = re.sub(r"\[(https?:[^]]+)\]", r"", title)
+    # Remove italic and bold
+    title = re.sub(r"''+", r"", title)
+    # Replace HTML entities
+    title = html.unescape(title)
+    title = re.sub("\xa0", " ", title)  # nbsp
+    # This unicode quote seems to be used instead of apostrophe quite randomly
+    # (about 4% of apostrophes in English entries, some in Finnish entries).
+    title = re.sub("\u2019", "'", title)  # Note: no r"..." here!
+    # Replace strange unicode quotes with normal quotes
+    title = re.sub(r"”", '"', title)
+    # Replace unicode long dash by normal dash
+    title = re.sub(r"–", "-", title)
+    # Replace whitespace sequences by a single space.
+    title = re.sub(r"\s+", " ", title)
+    # Remove whitespace before periods and commas etc
+    # XXX we might re-enable this, now trying without as it is removing some
+    # instances where we would want to leave the space
+    # title = re.sub(r" ([.,;:!?)])", repl_1, title)
+    # Strip surrounding whitespace.
+    if not no_strip:
+        title = title.strip()
+    return title
---
+++ packages/wiktextract/wiktextract-1.99.3.tar.gz/wiktextract-1.99.3/wiktextract/config.py
@@ -0,0 +1,107 @@
+# Definition of the configuration object for Wiktionary data extraction.
+# The same object is also used for collecting statistics.
+#
+# Copyright (c) 2018-2020 Tatu Ylonen.  See file LICENSE or https://ylonen.org
+
+import sys
+import collections
+
+
+
+def int_dict():
+    return collections.defaultdict(int)
+
+
+def int_dict_dict():
+    return collections.defaultdict(int_dict)
+
+
+def list_dict():
+    return collections.defaultdict(list)
+
+
+class WiktionaryConfig(object):
+    """This class holds configuration data for Wiktionary parsing."""
+
+    __slots__ = (
+        "capture_languages",
+        "capture_translations",
+        "capture_pronunciation",
+        "capture_linkages",
+        "capture_compounds",
+        "capture_redirects",
+        "capture_examples",
+        "verbose",
+        "num_pages",
+        "language_counts",
+        "pos_counts",
+        "section_counts",
+        "word",
+    )
+
+    def __init__(self,
+                 capture_languages=["English", "Translingual"],
+                 capture_translations=False,
+                 capture_pronunciation=False,
+                 capture_linkages=False,
+                 capture_compounds=False,
+                 capture_redirects=False,
+                 capture_examples=False,
+                 verbose=False):
+        if capture_languages is not None:
+            assert isinstance(capture_languages, (list, tuple, set))
+            for x in capture_languages:
+                assert isinstance(x, str)
+        assert (capture_languages is None or
+                isinstance(capture_languages, (list, tuple, set)))
+        assert capture_translations in (True, False)
+        assert capture_pronunciation in (True, False)
+        assert capture_linkages in (True, False)
+        assert capture_compounds in (True, False)
+        assert capture_redirects in (True, False)
+        self.capture_languages = capture_languages
+        self.capture_translations = capture_translations
+        self.capture_pronunciation = capture_pronunciation
+        self.capture_linkages = capture_linkages
+        self.capture_compounds = capture_compounds
+        self.capture_redirects = capture_redirects
+        self.capture_examples = capture_examples
+        self.verbose = verbose
+        # Some fields for statistics
+        self.num_pages = 0
+        self.language_counts = collections.defaultdict(int)
+        self.pos_counts = collections.defaultdict(int)
+        self.section_counts = collections.defaultdict(int)
+        # Some fields related to errors
+        # The word currently being processed.
+        self.word = None
+
+    def to_kwargs(self):
+        return {
+            "capture_languages": self.capture_languages,
+            "capture_translations": self.capture_translations,
+            "capture_pronunciation": self.capture_pronunciation,
+            "capture_linkages": self.capture_linkages,
+            "capture_compounds": self.capture_compounds,
+            "capture_redirects": self.capture_redirects,
+            "capture_examples": self.capture_examples,
+            "verbose": self.verbose,
+        }
+
+    def to_return(self):
+        return {
+            "num_pages": self.num_pages,
+            "language_counts": self.language_counts,
+            "pos_counts": self.pos_counts,
+            "section_counts": self.section_counts,
+        }
+
+    def merge_return(self, ret):
+        assert isinstance(ret, dict)
+        self.num_pages += ret["num_pages"]
+        for k, v in ret["language_counts"].items():
+            self.language_counts[k] += v
+        for k, v in ret["pos_counts"].items():
+            self.pos_counts[k] += v
+        for k, v in ret["section_counts"].items():
+            self.section_counts[k] += v

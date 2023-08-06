61b91442490869b5b5cf783c8535293c3fc68811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:01 +0000

    Add package slurry/0.9.0/Slurry-0.9.0.tar.gz

---
+++ packages/slurry/Slurry-0.9.0.tar.gz/Slurry-0.9.0/slurry/__init__.py
@@ -0,0 +1,4 @@
+"""An async streaming data processing framework."""
+__version__ = '0.9.0'
+
+from .pipeline import Pipeline
---
+++ packages/slurry/Slurry-0.9.0.tar.gz/Slurry-0.9.0/slurry/sections/_refiners.py
@@ -0,0 +1,33 @@
+"""Sections for transforming an input into a different output."""
+from typing import Any, AsyncIterable, Optional
+
+from async_generator import aclosing
+
+from .abc import Section
+
+class Map(Section):
+    """Maps over an asynchronous sequence.
+
+    Map can be used as a starting section, if a source is provided.
+
+    :param func: Mapping function.
+    :type func: Callable[[Any], Any]
+    :param source: Source if used as a starting section.
+    :type source: Optional[AsyncIterable[Any]]
+    """
+    def __init__(self, func, source: Optional[AsyncIterable[Any]] = None):
+        super().__init__()
+        self.func = func
+        self.source = source
+
+    async def pump(self, input, output):
+        if input:
+            source = input
+        elif self.source:
+            source = self.source
+        else:
+            raise RuntimeError('No input provided.')
+
+        async with aclosing(source) as aiter:
+            async for item in aiter:
+                await output(self.func(item))

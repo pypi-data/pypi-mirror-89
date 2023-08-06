262edf035745692107e95c82ffc67f0efd8083d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:15 +0000

    Add package branesta/0.1.5/branesta-0.1.5.tar.gz

---
+++ packages/branesta/branesta-0.1.5.tar.gz/branesta-0.1.5/HISTORY.rst
@@ -0,0 +1,40 @@
+=======
+History
+=======
+
+0.1.5 (2020-12-22)
+------------------
+
+* Added unittests.
+* Added logo image.
+* Updated imports.
+
+
+0.1.4 (2020-11-29)
+------------------
+
+* Updated logging.
+
+
+0.1.3 (2020-11-28)
+------------------
+
+* First standalone release.
+
+
+0.1.2 (2020-11-28)
+------------------
+
+* Updated dependencies.
+
+
+0.1.1 (2020-11-27)
+------------------
+
+* Added analysis module.
+
+
+0.1.0 (2020-11-26)
+------------------
+
+* First release on PyPI.
---
+++ packages/branesta/branesta-0.1.5.tar.gz/branesta-0.1.5/tests/test_branesta.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""Tests for `branesta` package."""
+
+import os
+import glob
+import unittest
+import pandas as pd
+
+from branesta import branesta
+
+
+class TestBranesta(unittest.TestCase):
+    """Tests for `branesta` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+        # Run branesta, output results into temp dir
+        branesta.analyze(
+                "test", "tests/testdata/ts", "tests/temp/", win_len=30,
+                subnetpath="tests/testdata/subnets/subnetworks_willard.csv",
+                tot_len=720, tot_roi_num=498
+            )
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+        # Delete temp outputs
+        os.system("rm tests/temp/*")
+
+
+    def test_branesta(self):
+        """Test whether output csv with stability values is as expected."""
+
+        # Open output
+        out = pd.read_csv("tests/temp/brain_network_stability_test.csv")
+
+        # Open reference
+        ref = pd.read_csv(glob.glob("tests/ref/brain_network_stability_*.csv")[-1])
+
+        # Print some info
+        print("Info from test-case:", "\nshape:", out.shape, "\nhead:\n", out.head())
+
+        # Assert equality
+        pd.testing.assert_frame_equal(out, ref)
+

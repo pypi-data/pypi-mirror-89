234a65e36cfb3eb838e114ddc502209dd7f98f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:16 +0000

    Add package sdata-experiments/0.1.1/sdata_experiments-0.1.1.tar.gz

---
+++ packages/sdata-experiments/sdata_experiments-0.1.1.tar.gz/sdata_experiments-0.1.1/sdata_experiments/__init__.py
@@ -0,0 +1,12 @@
+# -*-coding: utf-8-*-
+__version__ = '0.1.1'
+__revision__ = None
+__version_info__ = tuple([int(num) for num in __version__.split('.')])
+
+'''
+sdata experiments 
+'''
+
+import sys
+import logging
+logger = logging.getLogger("sdata_experiments")

d086cad99b16b6d9bd9269da6029028959bb4771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:24 +0000

    Add package spotibox/0.2.0/spotibox-0.2.0-py2.py3-none-any.whl

---
+++ packages/spotibox/spotibox-0.2.0-py2.py3-none-any.whl/spotibox/albums.py
@@ -0,0 +1,6 @@
+# Paw Patrol
+album1 = 'spotify:album:0QmmGSZqcqbSdtluqDASF1' 
+# Fünf Freunde
+album2 = 'spotify:album:2TtW4W6HAHWbPcKrQd5x5y'
+# Räuber Hotzenplotz
+album3 = 'spotify:album:0lVwYTrSkyxBnt17qg5nYN'

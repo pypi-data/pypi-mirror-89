71cdead686d608a37349177135f41b5c5681ee00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:54 +0000

    Add package frictionless/3.47.3/frictionless-3.47.3.tar.gz

---
+++ packages/frictionless/frictionless-3.47.3.tar.gz/frictionless-3.47.3/frictionless/extract/resource.py
@@ -0,0 +1,37 @@
+from ..resource import Resource
+
+
+def extract_resource(
+    source,
+    *,
+    basepath=None,
+    onerror="ignore",
+    trusted=False,
+    process=None,
+    stream=False,
+):
+    """Extract resource rows
+
+    API      | Usage
+    -------- | --------
+    Public   | `from frictionless import extract_resource`
+
+    Parameters:
+        source (dict|str): data resource descriptor
+        basepath? (str): package basepath
+        onerror? (ignore|warn|raise): behaviour on errors
+        trusted? (bool): don't raise an exception on unsafe paths
+        process? (func): a row processor function
+
+    Returns:
+        Row[]: an array/stream of rows
+
+    """
+
+    # Create resource
+    resource = Resource(source, basepath=basepath, onerror=onerror, trusted=trusted)
+
+    # Extract resource
+    data = resource.read_row_stream()
+    data = (process(row) for row in data) if process else data
+    return data if stream else list(data)

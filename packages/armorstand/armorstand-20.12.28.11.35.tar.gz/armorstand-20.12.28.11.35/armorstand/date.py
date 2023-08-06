a0535bcbea9a0c164d1626265afacf32ad8c75c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package armorstand/20.12.28.11.35/armorstand-20.12.28.11.35.tar.gz

---
+++ packages/armorstand/armorstand-20.12.28.11.35.tar.gz/armorstand-20.12.28.11.35/armorstand/date.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from doctest import testmod
+from enum import Enum
+from typing import List
+
+
+class DateCode(Enum):
+    SHORT_DAY_NAME = "%a"
+    DAY_NAME = "%A"
+    DAY = "%-d"
+    ZERO_PADDED_DAY = "%-d"
+    SHORT_MONTH = "%b"
+    MONTH = "%B"
+    MONTH_NUMBER = "%-m"
+    YEAR = "%Y"
+    SHORT_YEAR = "%y"
+    COMMA = ","
+    SLASH = "/"
+
+
+def get_date(order: List[DateCode], return_strf=False) -> str:
+    """
+    TODO: move tests to separate file and use pytest
+    >>> get_date([DateCode.DAY, DateCode.MONTH, DateCode.COMMA, DateCode.YEAR], return_strf=True)
+    '%-d %B, %Y'
+
+    >>> get_date([DateCode.MONTH_NUMBER, DateCode.SLASH, DateCode.DAY, DateCode.SLASH, DateCode.SHORT_YEAR], return_strf=True)
+    '%-m/%-d/%y'
+    """
+
+    now = datetime.now()
+
+    string = ""
+    for index, code in enumerate(order):
+        try:
+            next_code = order[index + 1]
+        except:
+            next_code = None
+
+        string += code.value
+
+        if next_code not in (DateCode.COMMA, DateCode.SLASH) and code != DateCode.SLASH:
+            string += " "
+
+    if return_strf:
+        return string.strip()
+
+    return now.strftime(string).strip()
+
+
+if __name__ == "__main__":
+    testmod(name="get_date", verbose=False)

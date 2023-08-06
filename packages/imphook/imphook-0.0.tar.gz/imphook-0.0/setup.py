e3f61545978aa7b76e770bb9633bafc6625e3ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:16 +0000

    Add package imphook/0.0/imphook-0.0.tar.gz

---
+++ packages/imphook/imphook-0.0.tar.gz/imphook-0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+#import sdist_upip
+
+
+setup(name='imphook',
+      version='0.0',
+      description="Simple and clear import hooks for Python",
+#      long_description=open('README.rst').read(),
+#      url='https://github.com/pfalcon/picoweb',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+#      cmdclass={'sdist': sdist_upip.sdist},
+      py_modules=['imphook'])

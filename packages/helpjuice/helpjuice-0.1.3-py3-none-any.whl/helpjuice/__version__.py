a9e6b52eb773e01a840e8dfb20a597994d006cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:19 +0000

    Add package helpjuice/0.1.3/helpjuice-0.1.3-py3-none-any.whl

---
+++ packages/helpjuice/helpjuice-0.1.3-py3-none-any.whl/helpjuice/__version__.py
@@ -0,0 +1,11 @@
+"""Package version information.
+"""
+
+__title__ = "helpjuice"
+__description__ = "Python Helpjuice API Wrapper."
+__url__ = "https://helpjuice.readthedocs.io"
+__version__ = "0.1.3"
+__author__ = "Sam Morgan"
+__author_email__ = "sam@samamorgan.com"
+__license__ = "GNU GPL 3.0"
+__copyright__ = "Copyright 2020 Sam Morgan"

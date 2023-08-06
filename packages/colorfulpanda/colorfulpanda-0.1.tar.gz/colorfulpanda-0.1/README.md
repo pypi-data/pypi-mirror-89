f60170eebdcd030d4d91d02f47e158fa62a81358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:23 +0000

    Add package colorfulpanda/0.1/colorfulpanda-0.1.tar.gz

---
+++ packages/colorfulpanda/colorfulpanda-0.1.tar.gz/colorfulpanda-0.1/README.md
@@ -0,0 +1,7 @@
+# colorfulPanda
+一些花里胡哨的东西
+
+## 使用方法
+`from src.mov.BlinkBlock import main as mov2`
+
+`mov2("helloworld")`
---
+++ packages/colorfulpanda/colorfulpanda-0.1.tar.gz/colorfulpanda-0.1/colorfulpanda/index.py
@@ -0,0 +1,8 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+from colorfulpanda.mov.BlinkBlock import main as mov2
+
+
+if __name__ == '__main__':
+	mov2("helloworld")

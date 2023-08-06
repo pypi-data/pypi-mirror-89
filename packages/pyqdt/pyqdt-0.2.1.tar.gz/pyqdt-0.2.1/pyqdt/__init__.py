7ea5df9044bb0ffc79f4b0aae8d0b519f0ac9821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:54 +0000

    Add package pyqdt/0.2.1/pyqdt-0.2.1.tar.gz

---
+++ packages/pyqdt/pyqdt-0.2.1.tar.gz/pyqdt-0.2.1/pyqdt/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from .wraps import *
+from .quotedata import *
+#from .technical_analysis import *
+
+__version__ = '0.2'
+
+__all__ = [
+    'JQWraps',
+    'QuoteData',
+    '__version__'
+]

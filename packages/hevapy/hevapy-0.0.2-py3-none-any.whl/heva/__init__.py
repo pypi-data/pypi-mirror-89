b30e71fbbb490ccf5369b3369c8858ff99b262b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:15 +0000

    Add package hevapy/0.0.2/hevapy-0.0.2-py3-none-any.whl

---
+++ packages/hevapy/hevapy-0.0.2-py3-none-any.whl/heva/__init__.py
@@ -0,0 +1,7 @@
+from heva.cart_plot import *
+from heva.details import *
+from heva.dist import *
+from heva.gev_plot import *
+from heva.graphics import *
+from heva.helper import *
+from heva.io import * 

5acfb9ca643ac8df64160beb06d323c9496d971b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:00 +0000

    Add package vaslapp5generator/0.0.6/vaslapp5generator-0.0.6.tar.gz

---
+++ packages/vaslapp5generator/vaslapp5generator-0.0.6.tar.gz/vaslapp5generator-0.0.6/vaslapp5generator/config.py
@@ -0,0 +1,11 @@
+entity_name = 'UserAddress'
+entity_names = 'UserAddresses'
+entity_variable = 'userAddress'
+entity_variables = 'userAddresses'
+entity_kebab = 'user-address'
+
+entity_package = 'useradress'
+
+module_name = 'user-address'
+
+collection_name = 'user_addresses'

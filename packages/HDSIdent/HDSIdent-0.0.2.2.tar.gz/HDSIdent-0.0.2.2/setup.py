805cf8a54cab7bb04fdfbfcd6e502a0fb958849e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:59 +0000

    Add package HDSIdent/0.0.2.2/HDSIdent-0.0.2.2.tar.gz

---
+++ packages/HDSIdent/HDSIdent-0.0.2.2.tar.gz/HDSIdent-0.0.2.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    author="Giulio Cesare Mastrocinque Santo",
+    name='HDSIdent',
+    author_email="giuliosanto@gmail.com",
+    url='https://github.com/GiulioCMSanto/HDSIdent',
+    version='0.0.2.2',
+    description='HDSIdent is an open-source Python package that can be used to obtain historical data segments suitable for performing System Identification',
+    license="MIT",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=["scikit-learn","scipy",
+                      "pandas","numpy","matplotlib",
+                      "seaborn","joblib","sympy"],
+    zip_safe=True
+)

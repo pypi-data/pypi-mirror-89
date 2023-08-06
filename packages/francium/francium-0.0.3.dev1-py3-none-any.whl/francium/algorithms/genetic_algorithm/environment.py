8ed5f6f06e5c2cd22355cb387ea9a3782a39b213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:58 +0000

    Add package francium/0.0.3.dev1/francium-0.0.3.dev1-py3-none-any.whl

---
+++ packages/francium/francium-0.0.3.dev1-py3-none-any.whl/francium/algorithms/genetic_algorithm/environment.py
@@ -0,0 +1,35 @@
+from typing import Tuple, Optional
+
+from francium.core import State, BaseEnvironment
+from francium.core.eval_functions import *
+
+import numpy as np
+
+
+class Environment(BaseEnvironment):
+    def __init__(
+        self,
+        x_bounds: Tuple[float, float],
+        y_bounds: Tuple[float, float],
+        goal_val: Optional[State] = None,
+        tolerance: Optional[float] = 1e-4,
+        eval_func=convex_x_square,
+    ):
+
+        BaseEnvironment.__init__(
+            self, x_bounds, y_bounds, goal_val, tolerance, eval_func=eval_func
+        )
+
+    def evaluate_state(self, state: State) -> Tuple[float, bool]:
+        eval_val = self.evaluation_func(state["x"], state["y"])
+
+        if self.goal_val:
+            is_done = (
+                True
+                if np.abs(eval_val - self.goal_val["z"]) < self.tolerance
+                else False
+            )
+        else:
+            is_done = False
+
+        return eval_val, is_done
---
+++ packages/francium/francium-0.0.3.dev1-py3-none-any.whl/francium/core/__init__.py
@@ -0,0 +1,7 @@
+from .memory import Memory
+from .state import State
+from .logger import setup_logger
+from .environment import BaseEnvironment
+from .agent import BaseAgent
+from .solver import BaseSolver
+from .eval_functions import sinx_plus_x, convex_x_square

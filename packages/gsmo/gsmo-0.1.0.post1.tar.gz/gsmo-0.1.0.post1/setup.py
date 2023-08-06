4a56fd2c4f46a095f5ec012ddc446f2d193d0447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:50 +0000

    Add package gsmo/0.1.0.post1/gsmo-0.1.0.post1.tar.gz

---
+++ packages/gsmo/gsmo-0.1.0.post1.tar.gz/gsmo-0.1.0.post1/setup.py
@@ -0,0 +1,23 @@
+from utz.setup import setup
+
+setup(
+    install_requires=[
+        'papermill',
+        'pyyaml',
+        'utz[setup]>=0.3.5',
+    ],
+    extras_require={
+        'test': [
+            'GitPython',
+            'pytest==6.0.1',
+        ],
+    },
+    url="https://github.com/runsascoded/gsmo",
+    entry_points={
+        'console_scripts': [
+            'gsmo = gsmo.gsmo:main',
+            'gsmo-entrypoint = gsmo.entrypoint:main',
+        ],
+    },
+    python_requires='>3.8',  # uses the walrus operator
+)

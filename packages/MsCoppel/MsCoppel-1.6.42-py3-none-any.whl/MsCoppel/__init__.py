fd9d8465887dac9766f5aaf7e14b641687767c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:39 +0000

    Add package MsCoppel/1.6.42/MsCoppel-1.6.42-py3-none-any.whl

---
+++ packages/MsCoppel/MsCoppel-1.6.42-py3-none-any.whl/MsCoppel/__init__.py
@@ -0,0 +1,28 @@
+from .microservices import Microservices
+from .ms_base import KafkaBase
+from .loggs import Loggs
+from .types import Types, TypesActions, Actions, HttpError
+from .options import Options
+from .ErrorMs import ErrorMs
+from .version_framework import version
+from .HttpResponse import HttpResponse
+from .HttpErrors import BadRequest
+
+name = 'MsCoppel'
+
+__all__ = [
+    'Microservices',
+    'KafkaBase',
+    'Loggs',
+    'Types',
+    'Options',
+    'MsManager',
+    'TypesActions',
+    'ErrorMs',
+    'Actions',
+    'HttpResponse',
+    'HttpError',
+    'BadRequest',
+]
+
+__version__ = version

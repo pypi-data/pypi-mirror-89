7d08370b03c8d2acb74ae193cb8e36f3ec325833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:41 +0000

    Add package freeldep/0.2.1/freeldep-0.2.1.tar.gz

---
+++ packages/freeldep/freeldep-0.2.1.tar.gz/freeldep-0.2.1/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from setuptools import find_packages
+from setuptools import setup
+
+PROJECT = "freeldep"
+VERSION = "0.2.1"
+
+
+try:
+    long_description = open("README.md", "rt").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+    description="Serverless Infrastructure as code deployment",
+    long_description=long_description,
+    author="Matthieu Blais",
+    author_email="matthieu.blais1@gmail.com",
+    url="https://github.com/MatthieuBlais/freeldep",
+    download_url="https://github.com/MatthieuBlais/freeldep.git",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+    ],
+    platforms=["Any"],
+    scripts=[],
+    provides=[],
+    install_requires=[
+        "Click==7.1.2",
+        "jinja2==2.11.2",
+        "pyaml==20.4.0",
+        "cfn-lint==0.44.1",
+        "pytest==6.1.2",
+        "coverage==5.3",
+        "moto==1.3.16",
+        "google-cloud-storage==1.35.0"
+    ],
+    namespace_packages=[],
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        freeldep=cli.main:cli
+    """,
+    zip_safe=False,
+)

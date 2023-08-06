6a5b9a9afb682cca6f846bbb53a96cc7cdfc6ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:00 +0000

    Add package ApiRequestManager/1.0.1/ApiRequestManager-1.0.1.tar.gz

---
+++ packages/ApiRequestManager/ApiRequestManager-1.0.1.tar.gz/ApiRequestManager-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ApiRequestManager",
+    version="1.0.1",
+    author="clemparpa",
+    author_email="clem.parpaillon@example.com",
+    description="Communicate with Apis is Ez",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/clemparpa/PyApiManager",
+    packages=[
+        "ApiRequestManager",
+        "src",
+        "src/ApiConfig",
+        "src/ApiRequest"
+    ],
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

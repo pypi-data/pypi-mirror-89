f8642fc25204b5f95e7fa0a71e9b7947a1d6c4fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:03 +0000

    Add package lambda_gateway/0.8.1/lambda-gateway-0.8.1.tar.gz

---
+++ packages/lambda_gateway/lambda-gateway-0.8.1.tar.gz/lambda-gateway-0.8.1/lambda_function.py
@@ -0,0 +1,26 @@
+"""
+Example Lambda handler function.
+"""
+import json
+import os
+import time
+
+SLEEP = os.getenv('SLEEP') or '0'
+
+
+def lambda_handler(event, context=None):
+    # Log event...
+    print(json.dumps(event))
+    # Do some work...
+    time.sleep(float(SLEEP))
+    # Get name from qs
+    params = event.get('queryStringParameters') or {}
+    name = params.get('name') or 'Pythonista'
+    # Return response
+    return {
+        'body': json.dumps({'text': f'Hello, {name}! ~ Lambda Gateway'}),
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
---
+++ packages/lambda_gateway/lambda-gateway-0.8.1.tar.gz/lambda-gateway-0.8.1/tests/test_lambda_context.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+
+from lambda_gateway import lambda_context
+from lambda_gateway.lambda_context import Context
+
+
+def test_start():
+    with lambda_context.start(11) as ret:
+        assert isinstance(ret, Context)
+        assert ret._timeout == 11
+
+
+class TestContext:
+    def setup(self):
+        self.subject = Context(1)
+
+    def test_function_name(self):
+        assert self.subject.function_name == 'lambda-gateway'
+
+    def test_function_version(self):
+        assert self.subject.function_version == '$LATEST'
+
+    def test_invoked_function_arn(self):
+        assert self.subject.invoked_function_arn == \
+            'arn:aws:lambda:us-east-1:123456789012:function:lambda-gateway'
+
+    def test_memory_limit_in_mb(self):
+        assert self.subject.memory_limit_in_mb == 128
+
+    def test_aws_request_id(self):
+        assert self.subject.aws_request_id is not None
+
+    def test_log_group_name(self):
+        assert self.subject.log_group_name == '/aws/lambda/lambda-gateway'
+
+    def test_log_stream_name(self):
+        assert self.subject.log_stream_name is not None
+
+    def test_get_remaining_time_in_millis(self):
+        assert 0 < self.subject.get_remaining_time_in_millis() < 1000
+        self.subject._start -= timedelta(seconds=1)
+        assert self.subject.get_remaining_time_in_millis() == 0

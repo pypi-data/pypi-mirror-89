e664a82f27cbd047982e313f6f047f774e739cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:49 +0000

    Add package python-script-manager/1.0.2/python_script_manager-1.0.2-py3-none-any.whl

---
+++ packages/python-script-manager/python_script_manager-1.0.2-py3-none-any.whl/python_script_manager/__main__.py
@@ -0,0 +1,86 @@
+import sys
+import click
+import os
+try:
+    from .funcs import *
+    from . import const
+except ImportError:
+    from funcs import *
+    import const
+
+
+@click.group()
+@click.version_option(const.version)
+def main():
+    pass
+
+
+@main.command()
+def init(**kwargs):
+    initialize()
+
+
+@main.command()
+@click.argument('name', required=True)
+def run(**kwargs):
+    name = kwargs.pop('name')
+    while not name:
+        name = input('Enter name of script to run')
+    runScript(name)
+
+
+@main.command()
+@click.option('-n', '--name', 'name', required=False)
+@click.option('-c', '--command', 'command', required=False)
+def add(**kwargs):
+    name = kwargs.pop('name')
+    command = kwargs.pop('command')
+    while not name:
+        name = input('Enter name for script: ')
+    while not command:
+        command = input('Enter command for script: ')
+    addScript(name, command)
+
+
+@main.command()
+def list(**kwargs):
+    scripts = loadScripts()
+    for name, command in scripts.items():
+        print(f'{name} >>> {command}')
+
+
+@main.command()
+@click.option('-n', '--name', 'name', required=False)
+def rm(**kwargs):
+    name = kwargs.pop('name')
+    while not name:
+        name = input('Enter name of script to remove: ')
+    removeScript(name)
+
+
+@main.command()
+def install(**kwargs):
+    os.system('pip install -r requirements.txt')
+
+
+@main.command()
+def freeze(**kwargs):
+    os.system('pip3 freeze > requirements.txt')
+
+@main.command()
+def build(**kwargs):
+    runScript('build')
+
+@main.command()
+def start(**kwargs):
+    runScript('start')
+
+@main.command()
+def deploy(**kwargs):
+    runScript('deploy')
+
+if __name__ == '__main__':
+    args = sys.argv
+    if "--help" in args or len(args) == 1:
+        print("Python Script Manager")
+    main()

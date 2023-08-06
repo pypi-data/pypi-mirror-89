42e142be427764e52638b51ba96367dbbeb8dc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:02 +0000

    Add package ddnss/1.0.2/ddnss-1.0.2.tar.gz

---
+++ packages/ddnss/ddnss-1.0.2.tar.gz/ddnss-1.0.2/setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+"""Installation script."""
+
+from setuptools import setup
+
+setup(
+    name='ddnss',
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'],
+    author='Richard Neumann',
+    author_email='mail@richard-neumann.de',
+    python_requires='>=3.8',
+    py_modules=['ddnss'],
+    entry_points={'console_scripts': ['ddnssupd = ddnss:main']},
+    url='https://github.com/conqp/ddnss',
+    license='GPLv3',
+    description='Update DynDNS hosts registered at ddnss.de.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    keywords='dnamic DNS DynDNS ddnss update script client'
+)

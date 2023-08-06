ec1b6865bf86630792739de624a095fef3fd86a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:11 +0000

    Add package flyinghu/1.0/flyinghu-1.0.tar.gz

---
+++ packages/flyinghu/flyinghu-1.0.tar.gz/flyinghu-1.0/README.md
@@ -0,0 +1,2 @@
+# flyinghu
+个人使用python工具集
---
+++ packages/flyinghu/flyinghu-1.0.tar.gz/flyinghu-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "flyinghu",
+    version = "1.0",
+    description = "flyinghu tools",
+    long_description = "一些python常用工具集",
+    license = "MIT Licence",
+    url = "https://github.com/flyinghu123/flyinghu",
+    
+    author = "flyinghu",
+    author_email = "1152598046@qq.com",
+    platforms = "any",
+    install_requires = ["pipreqs-zh"],
+)

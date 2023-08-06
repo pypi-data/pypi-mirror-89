467388587601242ced936a9a938a6df9a1819fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:02 +0000

    Add package twopilabs-utils-scpi/0.2.1/twopilabs_utils_scpi-0.2.1-py3-none-any.whl

---
+++ packages/twopilabs-utils-scpi/twopilabs_utils_scpi-0.2.1-py3-none-any.whl/twopilabs/utils/scpi/scpi_type_base.py
@@ -0,0 +1,22 @@
+import abc
+from typing import *
+
+class ScpiTypeBase(object):
+    """Base class for SCPI types"""
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __str__(self) -> str:
+        """A textual representation of the object"""
+        raise NotImplementedError("Please Implement this method")
+
+    @abc.abstractmethod
+    def format(self) -> str:
+        """Converts the given data into SCPI "Device listening" format"""
+        raise NotImplementedError("Please Implement this method")
+
+    @classmethod
+    @abc.abstractmethod
+    def parse(cls, stream):
+        """Parses SCPI "Device talking Elements" into an internal representation"""
+        raise NotImplementedError("Please Implement this method")

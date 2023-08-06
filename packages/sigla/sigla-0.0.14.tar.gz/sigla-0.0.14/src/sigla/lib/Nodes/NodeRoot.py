b3acf9172a618c1abee461fa1503219bf826bbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:37 +0000

    Add package sigla/0.0.14/sigla-0.0.14.tar.gz

---
+++ packages/sigla/sigla-0.0.14.tar.gz/sigla-0.0.14/src/sigla/lib/Nodes/NodeRoot.py
@@ -0,0 +1,11 @@
+from src.sigla.lib.Nodes.Node import Node
+from src.sigla.lib.helpers.Context import Context
+
+
+class NodeRoot(Node):
+    kind = "root"
+
+    def process(self, ctx=None):
+        if ctx is None:
+            ctx = Context()
+        return list(map(lambda e: e.process(ctx), self.children))

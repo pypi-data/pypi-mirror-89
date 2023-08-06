17ac2e5a46f1879d4fd0cc03469fb88270a0ba0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:24 +0000

    Add package MM-BiGauss/0.1/MM_BiGauss-0.1.tar.gz

---
+++ packages/MM-BiGauss/MM_BiGauss-0.1.tar.gz/MM_BiGauss-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'MM_BiGauss',
+      version = '0.1',
+      description = 'Binomial and Gaussian distributions',
+      packages = ['MM_BiGauss'],
+      author = 'MM',
+      author_email = 'magy0620@yahoo.com',
+      zip_safe = False)

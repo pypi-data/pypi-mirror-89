fbf9eae7e44f349203132bbe15cc3c7e5bb595ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:58 +0000

    Add package ListPage/0.7.3/ListPage-0.7.3.tar.gz

---
+++ packages/ListPage/ListPage-0.7.3.tar.gz/ListPage-0.7.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="ListPage",
+    version="0.7.3",
+    author="g1879",
+    author_email="g1879@qq.com",
+    description="Page classes dedicated to crawling or manipulating list web pages.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD",
+    keywords="Page classes",
+    url="https://gitee.com/g1879/ListPage",
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        "DrissionPage"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    python_requires='>=3.6'
+)

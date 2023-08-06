1d3b55923f117e1a9b9ccd5db43210c00fd64c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:33 +0000

    Add package django-magic-tables/0.3/django-magic-tables-0.3.tar.gz

---
+++ packages/django-magic-tables/django-magic-tables-0.3.tar.gz/django-magic-tables-0.3/README.rst
@@ -0,0 +1,17 @@
+=====
+Magic Tables
+=====
+
+Magic Tables is a Django app to turn QuerySets to any kind of table.
+
+Detailed documentation is in the "docs" directory.
+
+Quick start
+-----------
+
+1. Add "magic_talbes" to your INSTALLED_APPS setting like this:
+
+    INSTALLED_APPS = [
+        ...
+        'magic_talbes',
+    ]
---
+++ packages/django-magic-tables/django-magic-tables-0.3.tar.gz/django-magic-tables-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name = 'django-magic-tables',
+    version = '0.3',
+    description = 'A Django app to turn easily QuerySets into tables',
+    long_description_content_type= 'text/markdown',
+    long_description = README,
+    url = 'https://www.example.com/',
+    author = 'Gabriele Mattioli',
+    author_email = 'gabrymattioli@gmail.com',
+    license = 'MIT', 
+    keywords= ['Table', 'Magictable'],
+    download_url='https://pypi.org/project/django-magic-tables/0.1/',
+)
+
+if __name__ == "__main__":
+    setup(**setup_args)
+

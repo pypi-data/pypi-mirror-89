8a2701cab50fd737ae10b6be174bc99cbb1a0d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:09 +0000

    Add package torchio/0.18.13/torchio-0.18.13.tar.gz

---
+++ packages/torchio/torchio-0.18.13.tar.gz/torchio-0.18.13/torchio/transforms/preprocessing/__init__.py
@@ -0,0 +1,28 @@
+from .spatial.pad import Pad
+from .spatial.crop import Crop
+from .spatial.resample import Resample
+from .spatial.crop_or_pad import CropOrPad
+from .spatial.to_canonical import ToCanonical
+
+from .intensity.rescale import RescaleIntensity
+from .intensity.z_normalization import ZNormalization
+from .intensity.histogram_standardization import HistogramStandardization
+
+from .label.remap_labels import RemapLabels
+from .label.sequential_labels import SequentialLabels
+from .label.remove_labels import RemoveLabels
+
+
+__all__ = [
+    'Pad',
+    'Crop',
+    'Resample',
+    'ToCanonical',
+    'CropOrPad',
+    'RescaleIntensity',
+    'ZNormalization',
+    'HistogramStandardization',
+    'RemapLabels',
+    'SequentialLabels',
+    'RemoveLabels',
+]

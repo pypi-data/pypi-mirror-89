d904fc94be99f7b062f9ebcd0d78d1e7c693232d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:32 +0000

    Add package distributions-un/0.20/distributions_un-0.20.tar.gz

---
+++ packages/distributions-un/distributions_un-0.20.tar.gz/distributions_un-0.20/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_un',
+      version='0.20',
+      description='Gaussian distributions',
+      packages=['distributions_un'],
+      zip_safe=False)

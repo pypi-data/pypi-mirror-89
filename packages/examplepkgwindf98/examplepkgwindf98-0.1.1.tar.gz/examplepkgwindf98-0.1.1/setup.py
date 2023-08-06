0e99d33f64271fc34852e047c44eef286fab1f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:35 +0000

    Add package examplepkgwindf98/0.1.1/examplepkgwindf98-0.1.1.tar.gz

---
+++ packages/examplepkgwindf98/examplepkgwindf98-0.1.1.tar.gz/examplepkgwindf98-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="examplepkgwindf98", # Replace with your own username
+    version="0.1.1",
+    author="WindF98",
+    author_email="wwj98713@163.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    python_requires='>=3.6',
+)

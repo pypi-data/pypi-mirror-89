57dc8ffee418f0890fe076d86d696d38882febd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package data-toolkit/0.3.14/data-toolkit-0.3.14.tar.gz

---
+++ packages/data-toolkit/data-toolkit-0.3.14.tar.gz/data-toolkit-0.3.14/dt/ext/monitor.py
@@ -0,0 +1,26 @@
+import gpustat as gp
+import time, os
+import subprocess as sp
+
+def monitor():
+    c = gp.new_query()
+    processes_per_gpu = [ len(x['processes']) for x in  c.jsonify()['gpus']]
+    return processes_per_gpu
+
+def submit(job_text):
+    # TODO: check correct env, but NOT part of the job_text
+    # TODO: don't submit again after run once.
+    tar_file = [ f for f in job_text.split(' ') if '.py' in f]
+    assert len(tar_file)==1
+
+    while True:
+        ppgpu = monitor()
+        if sum(ppgpu)>1:
+            print(f'Still busy. File {tar_file[0]} is in local: {tar_file[0] in os.listdir()}.')
+        else:
+            print('Done, submitting a new job!')
+            my_env = os.environ.copy()
+            sp.run(job_text, env=my_env)
+            time.sleep(120)
+        time.sleep(1)
+

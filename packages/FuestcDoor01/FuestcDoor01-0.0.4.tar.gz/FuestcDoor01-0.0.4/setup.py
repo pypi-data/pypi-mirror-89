03009de021c21fb69f12c11f233e0327f70c45fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:14 +0000

    Add package FuestcDoor01/0.0.4/FuestcDoor01-0.0.4.tar.gz

---
+++ packages/FuestcDoor01/FuestcDoor01-0.0.4.tar.gz/FuestcDoor01-0.0.4/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='FuestcDoor01',
+    version='0.0.4',
+    author='FuestcDoor01',
+    author_email='mrlution@qq.com',
+    description=u'FuestcDoor',
+    packages=['Fuestcdoor'],
+    install_requires=["Flask==1.0.2"],
+    include_package_data=True,
+    
+
+)

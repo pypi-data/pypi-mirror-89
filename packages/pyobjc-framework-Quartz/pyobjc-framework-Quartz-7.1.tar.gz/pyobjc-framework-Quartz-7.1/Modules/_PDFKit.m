4aff01f19d7338481b4ed4973731212fb7f1319a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:40 +0000

    Add package pyobjc-framework-Quartz/7.1/pyobjc-framework-Quartz-7.1.tar.gz

---
+++ packages/pyobjc-framework-Quartz/pyobjc-framework-Quartz-7.1.tar.gz/pyobjc-framework-Quartz-7.1/Modules/_PDFKit.m
@@ -0,0 +1,39 @@
+/*
+ * Manual wrappers for PDFKit
+ */
+#define Py_LIMITED_API 0x03060000
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+#include "pyobjc-api.h"
+
+#import <Quartz/Quartz.h>
+
+#include "_PDFKit_protocols.m"
+
+static PyMethodDef mod_methods[] = {{
+    0,
+    0,
+    0,
+}};
+
+static struct PyModuleDef mod_module = {
+     PyModuleDef_HEAD_INIT,
+     "_PDFKit",
+     NULL,
+     0,
+     mod_methods,
+     NULL,
+     NULL,
+     NULL,
+     NULL};
+
+PyObject* PyInit__PDFKit(void);
+
+PyObject* __attribute__((__visibility__("default"))) PyInit__PDFKit(void)
+{
+    PyObject* m = PyModule_Create(&mod_module);
+    if (!m)
+        return NULL;
+
+    return m;
+}
---
+++ packages/pyobjc-framework-Quartz/pyobjc-framework-Quartz-7.1.tar.gz/pyobjc-framework-Quartz-7.1/Modules/_QuickLookUI.m
@@ -0,0 +1,39 @@
+/*
+ * Manual wrappers for QuickLookUI
+ */
+#define Py_LIMITED_API 0x03060000
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+#include "pyobjc-api.h"
+
+#import <Quartz/Quartz.h>
+
+#include "_QuickLookUI_protocols.m"
+
+static PyMethodDef mod_methods[] = {{
+    0,
+    0,
+    0,
+}};
+
+static struct PyModuleDef mod_module = {
+     PyModuleDef_HEAD_INIT,
+     "_QuickLookUI",
+     NULL,
+     0,
+     mod_methods,
+     NULL,
+     NULL,
+     NULL,
+     NULL};
+
+PyObject* PyInit__QuickLookUI(void);
+
+PyObject* __attribute__((__visibility__("default"))) PyInit__QuickLookUI(void)
+{
+    PyObject* m = PyModule_Create(&mod_module);
+    if (!m)
+        return NULL;
+
+    return m;
+}

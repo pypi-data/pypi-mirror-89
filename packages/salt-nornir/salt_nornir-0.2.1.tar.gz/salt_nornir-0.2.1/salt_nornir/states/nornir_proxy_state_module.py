f642da9d1c62d7250cc838ad336778279020c630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:26 +0000

    Add package salt-nornir/0.2.1/salt_nornir-0.2.1.tar.gz

---
+++ packages/salt-nornir/salt_nornir-0.2.1.tar.gz/salt_nornir-0.2.1/salt_nornir/states/nornir_proxy_state_module.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Nornir State module
+===================
+
+"""
+# Import python libs
+import logging
+
+
+# -----------------------------------------------------------------------------
+# globals
+# -----------------------------------------------------------------------------
+
+
+log = logging.getLogger(__name__)
+__virtualname__ = "nr"
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# property functions
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def __virtual__():
+    return __virtualname__
+
+
+# -----------------------------------------------------------------------------
+# callable functions
+# -----------------------------------------------------------------------------
+
+
+def cfg(*args, **kwargs):
+    """
+    Enforce configuration state on device using Nornir
+    execution module ``nr.cfg`` function.
+
+    """
+    state_name = kwargs.pop("name")
+    result = __salt__["nr.cfg"](**kwargs)
+    ret = {
+        "name": state_name,
+        "changes": result,
+        "result": True,
+        "comment": "",
+    }
+    return ret
+
+
+def task(*args, **kwargs):
+    """
+    Enforce configuration state on device using Nornir
+    execution module ``nr.task`` function.
+
+    """
+    state_name = kwargs.pop("name")
+    result = __salt__["nr.task"](**kwargs)
+    ret = {
+        "name": state_name,
+        "changes": result,
+        "result": True,
+        "comment": "",
+    }
+    return ret

3e7434c5683ce6cbc5df26c269a7ff7fdddb94b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:25 +0000

    Add package pystreaming/1.2.0/pystreaming-1.2.0-py3-none-any.whl

---
+++ packages/pystreaming/pystreaming-1.2.0-py3-none-any.whl/pystreaming/video/dec.py
@@ -0,0 +1,98 @@
+from turbojpeg import TurboJPEG, TJFLAG_FASTDCT, TJFLAG_FASTUPSAMPLE
+import zmq, time
+from functools import partial
+import pystreaming.video.interface as intf
+import multiprocessing as mp
+
+
+def dec_ps(shutdown, infd, outfd, rcvmeta, sndbuf, rcvhwm, outhwm):
+    print("Starting Decoder")
+    context = zmq.Context()
+
+    socket = context.socket(zmq.PULL)
+    socket.setsockopt(zmq.RCVHWM, rcvhwm)
+    socket.connect(infd)
+
+    out = context.socket(zmq.PUSH)
+    out.setsockopt(zmq.SNDHWM, outhwm)
+    out.connect(outfd)
+
+    poller = zmq.Poller()
+    poller.register(socket, zmq.POLLIN)
+
+    decoder = partial(TurboJPEG().decode, flags=(TJFLAG_FASTDCT + TJFLAG_FASTUPSAMPLE))
+    while not shutdown.is_set():
+        time.sleep(0.01)  # 100 cycles a sec
+        if poller.poll(0):
+            try:
+                package = intf.recv(socket, buf=True, meta=rcvmeta, flags=zmq.NOBLOCK)
+                if rcvmeta:
+                    buf, meta, idx = package
+                else:
+                    buf, idx = package
+                    meta = None
+                arr = decoder(buf)
+                buf = buf if sndbuf else None
+                intf.send(out, idx, arr=arr, buf=buf, meta=meta, flags=zmq.NOBLOCK)
+            except zmq.error.Again:
+                pass  # ignore send misses to out.
+
+    print("Stopping Decoder")
+
+
+class Decoder:
+    hearhwm = 30
+    rcvhwm = outhwm = 10
+
+    def __init__(self, context, seed="", rcvmeta=False, sndbuf=False, procs=2):
+        infd = "ipc:///tmp/decin" + seed
+        outfd = "ipc:///tmp/decout" + seed
+        self.context, self.procs = context, procs
+        self.rcvmeta, self.sndbuf = rcvmeta, sndbuf
+        self.shutdown = mp.Event()
+        self.psargs = (
+            self.shutdown,
+            infd,
+            outfd,
+            rcvmeta,
+            sndbuf,
+            self.rcvhwm,
+            self.outhwm,
+        )
+        self.workers = []
+
+        self.receiver = self.context.socket(zmq.PULL)
+        self.receiver.bind(outfd)
+
+    def recv(self):
+        if self.workers == []:
+            raise RuntimeError("Tried to receive frame from stopped Decoder")
+        # Add timeout function?
+        return intf.recv(self.receiver, arr=True, buf=self.sndbuf, meta=self.rcvmeta)
+
+    def handler(self):
+        if self.workers == []:
+            raise RuntimeError("Cannot produce stream handler from stopped Decoder")
+        while True:
+            yield intf.recv(self.receiver, arr=True, buf=self.sndbuf, meta=self.rcvmeta)
+
+    def start(self):
+        if self.workers != []:
+            raise RuntimeError("Tried to start running Decoder")
+        for _ in range(self.procs):
+            self.workers.append(mp.Process(target=dec_ps, args=self.psargs))
+        for ps in self.workers:
+            ps.daemon = True
+            ps.start()
+
+        time.sleep(2)  # allow workers to initialize
+
+    def stop(self):
+        if self.workers == []:
+            raise RuntimeError("Tried to stop stopped Decoder")
+
+        self.shutdown.set()
+        for ps in self.workers:
+            ps.join()
+        self.workers = []
+        self.shutdown.clear()
---
+++ packages/pystreaming/pystreaming-1.2.0-py3-none-any.whl/pystreaming/video/dist.py
@@ -0,0 +1,69 @@
+import zmq, time
+import multiprocessing as mp
+import pystreaming.video.interface as intf
+from pystreaming.listlib.circularlist import CircularList, Empty
+
+
+# Note. It might be better to implement this as a asyncio system?
+
+
+def dist_ps(shutdown, infd, endpt, rcvhwm, tracks):
+    print(f"Start Distributing to {endpt}")
+    context = zmq.Context()
+
+    collector = context.socket(zmq.PULL)
+    collector.setsockopt(zmq.RCVHWM, rcvhwm)
+    collector.bind(infd)
+
+    distributor = context.socket(zmq.REP)
+    distributor.bind(endpt)
+
+    if tracks is None:  # default track is "none"
+        tracks = ["none"]
+    queues = {track: CircularList() for track in tracks}
+
+    while not shutdown.is_set():
+
+        time.sleep(0.001)  # 1000 cycles/sec ~> 4-6 streams at once.
+
+        if collector.poll(0):  # returns 0 if no event, something else if there is
+            buf, idx = intf.recv(collector, buf=True, flags=zmq.NOBLOCK)
+            for fqueue in queues.values():
+                fqueue.push((buf, idx))  # add to buf queue
+
+        if distributor.poll(0):  # got frame req
+            track = distributor.recv().decode()
+            try:
+                buf, idx = queues[track].pop()
+                intf.send(distributor, idx, buf=buf, flags=zmq.NOBLOCK)
+            except (KeyError, Empty):  # no frames available or wrong track selected
+                intf.send(distributor, -1, buf=b"nil", flags=zmq.NOBLOCK)
+
+    print("Distributor stopped")
+
+
+class Distributor:
+    rcvhwm = 30
+
+    def __init__(self, endpoint, seed="", tracks=None):
+        infd = "ipc:///tmp/encout" + seed
+        self.shutdown = mp.Event()
+        self.ps = None
+        self.psargs = (self.shutdown, infd, endpoint, self.rcvhwm, tracks)
+
+    def start(self):
+        if self.ps is not None:
+            raise RuntimeError("Tried to start a runnning Distributor")
+
+        self.ps = mp.Process(target=dist_ps, args=self.psargs)
+        self.ps.daemon = True
+        self.ps.start()
+
+    def stop(self):
+        if self.ps is None:
+            raise RuntimeError("Tried to stop a stopped Distributor")
+
+        self.shutdown.set()
+        self.ps.join()
+        self.ps = None
+        self.shutdown.clear()

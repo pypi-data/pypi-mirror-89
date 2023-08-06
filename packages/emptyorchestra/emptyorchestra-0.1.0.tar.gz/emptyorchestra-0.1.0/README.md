fc7ec6e5a66659aae57aecb2b79865d64d14a2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:41 +0000

    Add package emptyorchestra/0.1.0/emptyorchestra-0.1.0.tar.gz

---
+++ packages/emptyorchestra/emptyorchestra-0.1.0.tar.gz/emptyorchestra-0.1.0/README.md
@@ -0,0 +1,28 @@
+Play your karaoke library on your computer or have a karaoke party with
+friends.  Emptyorchestra is your own personal KJ!
+
+![party](/party1.jpg)
+
+Just download and run the executable for your platform.  Friends on your
+network can join in by scanning the QR code in the top left corner, entering
+their user name, and that's it!  
+
+Emptyorchestra takes care of the rest!
+
+You can even elect users from the main control window to be able to control
+the party.
+
+![sing](/sing1.jpg)
+
+## Find karaoke songs on the internet as well as local files!
+
+You can pull in your personal karaoke library, as well as find karaoke songs
+on the internet!
+
+## Save your favorites for later, suggest me songs I might like!
+
+Emptyorchestra can allow you to store a personal favorites list.  Not sure
+what you want to sing, Emptyorchestra can automatically generate suggestions
+based on your favorites!
+
+
---
+++ packages/emptyorchestra/emptyorchestra-0.1.0.tar.gz/emptyorchestra-0.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='emptyorchestra',
+    version='0.1.0',
+    author='Matt Kubilus',
+    author_email="mattkubilus@gmail.com",
+    url="http://emptyorchestra.kubilus.net",
+    packages=['emptyorchestra'],
+    package_dir = {'emptyorchestra':'src'},
+    package_data = {
+        'emptyorchestra':[
+            'static/css/*.css',
+            'static/js/*.js',
+            'static/images/*.jpeg',
+            'static/tracks/*.mp3',
+            'static/*.mp3',
+            'templates/*.html',
+            'templates/*.js',
+            'eo_conf.yml'
+        ]
+    },
+    entry_points = {
+        'console_scripts': ['emptyorch=emptyorchestra.eo_web:main'],
+    },
+    install_requires=[
+        'jinja2 < 3.0',
+        'tinydb >= 3.7',
+        'Flask >= 0.12',
+        'pywebview >= 3.0',
+        'gTTS == 2.0.1',
+        'xmltodict < 0.12',
+        'pyyaml'
+    ]
+    #scripts=['src/emptyorch']
+)
+

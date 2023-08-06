43d8fe24f86e1200db43d7a060aef182564e4d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:22 +0000

    Add package generallibrary/2.5.2/generallibrary-2.5.2.tar.gz

---
+++ packages/generallibrary/generallibrary-2.5.2.tar.gz/generallibrary-2.5.2/generallibrary/objinfo/properties.py
@@ -0,0 +1,48 @@
+
+import inspect
+
+
+class _ObjInfoProperties:
+    def public(self):
+        """ Get whether possible name is for public use, False if name is None.
+            Opposite of internal.
+
+            :param generallibrary.ObjInfo self: """
+        return not self.internal()
+
+    def internal(self):
+        """ Get whether possible name is for internal use, False if name is None.
+            True if private or protected.
+            Opposite of public.
+
+            :param generallibrary.ObjInfo self: """
+        return self.private() or self.protected()
+
+    def private(self):
+        """ Get whether possible name is private, False if name is None.
+            Subset of ObjInfo.internal().
+
+            :param generallibrary.ObjInfo self: """
+        return "__" in str(self.name)
+
+    def protected(self):
+        """ Get whether possible name is protected, False if name is None.
+            Subset of ObjInfo.internal().
+
+            :param generallibrary.ObjInfo self: """
+        return not self.private() and str(self.name).startswith("_")
+
+    def module(self):
+        """ Return module of this ObjInfo's obj.
+
+            :param generallibrary.ObjInfo self: """
+        return inspect.getmodule(self.obj)
+
+    def doc(self):
+        """ Return documentation string of this ObjInfo's obj.
+
+            :param generallibrary.ObjInfo self: """
+        return inspect.getdoc(self.obj)
+
+
+

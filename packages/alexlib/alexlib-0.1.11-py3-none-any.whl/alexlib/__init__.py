17c8e792bbfb714714a8d85f6fc26653434a494a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:30 +0000

    Add package alexlib/0.1.11/alexlib-0.1.11-py3-none-any.whl

---
+++ packages/alexlib/alexlib-0.1.11-py3-none-any.whl/alexlib/__init__.py
@@ -0,0 +1,7 @@
+
+__version__ = "0.1.11"
+docs = r"https://alexlib.readthedocs.io/en/latest/"
+git = r"https://github.com/thisismygitrepo/alexlib"
+release_notes = """
+Created Experimental Class
+"""

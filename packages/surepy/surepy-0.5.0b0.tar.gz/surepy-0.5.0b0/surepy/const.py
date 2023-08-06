db13f339504a9d91a0d8f3cf25560c9dbf6b9c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:59 +0000

    Add package surepy/0.5.0b0/surepy-0.5.0b0.tar.gz

---
+++ packages/surepy/surepy-0.5.0b0.tar.gz/surepy-0.5.0b0/surepy/const.py
@@ -0,0 +1,27 @@
+# HTTP user agent
+SUREPY_USER_AGENT = "surepy {version} - https://github.com/benleb/surepy"
+
+# Sure Petcare API endpoints
+BASE_RESOURCE: str = "https://app.api.surehub.io/api"
+AUTH_RESOURCE: str = f"{BASE_RESOURCE}/auth/login"
+MESTART_RESOURCE: str = f"{BASE_RESOURCE}/me/start"
+TIMELINE_RESOURCE: str = f"{BASE_RESOURCE}/timeline"
+NOTIFICATION_RESOURCE: str = f"{BASE_RESOURCE}/notification"
+CONTROL_RESOURCE: str = "{BASE_RESOURCE}/device/{device_id}/control"
+
+API_TIMEOUT = 45
+
+# HTTP constants
+ACCEPT = "Accept"
+ACCEPT_ENCODING = "Accept-Encoding"
+ACCEPT_LANGUAGE = "Accept-Language"
+AUTHORIZATION = "Authorization"
+CONNECTION = "Connection"
+CONTENT_TYPE_JSON = "application/json"
+CONTENT_TYPE_TEXT_PLAIN = "text/plain"
+ETAG = "Etag"
+HOST = "Host"
+HTTP_HEADER_X_REQUESTED_WITH = "X-Requested-With"
+ORIGIN = "Origin"
+REFERER = "Referer"
+USER_AGENT = "User-Agent"

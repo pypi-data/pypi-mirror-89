c074caf90df12a184614b6870ab2a3bc77fd6989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:19 +0000

    Add package effunet/1.0.0/effunet-1.0.0.tar.gz

---
+++ packages/effunet/effunet-1.0.0.tar.gz/effunet-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'effunet',         
+  packages = ['effunet'],   
+  version = '1.0.0',      
+  license='MIT',
+  description = 'UNet segmentation model with an efficientnet encoder',   
+  author = 'Pranshu Mishra',                   
+  author_email = 'pranshumshr.04@gmail.com',      
+  url = 'https://github.com/pranshu97/effunet',   
+  download_url = 'https://github.com/pranshu97/effunet/archive/v1.0.0.tar.gz',
+  keywords = ['UNet', 'EfficientNet', 'Segmentation'],
+  install_requires=[            
+          'torch',
+          'torchvision',
+          'efficientnet_pytorch'
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',  
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)

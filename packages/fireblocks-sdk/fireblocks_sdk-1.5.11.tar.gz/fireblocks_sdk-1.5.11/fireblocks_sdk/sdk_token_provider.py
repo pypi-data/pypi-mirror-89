6446510ee862b6ec5c4afbc2a564451fbfb29a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:22 +0000

    Add package fireblocks-sdk/1.5.11/fireblocks_sdk-1.5.11.tar.gz

---
+++ packages/fireblocks-sdk/fireblocks_sdk-1.5.11.tar.gz/fireblocks_sdk-1.5.11/fireblocks_sdk/sdk_token_provider.py
@@ -0,0 +1,27 @@
+import jwt
+import json
+import time
+import math
+from hashlib import sha256
+
+class SdkTokenProvider(object):
+    def __init__(self, private_key, api_key):
+        self.private_key = private_key
+        self.api_key = api_key
+
+    def sign_jwt(self, path, body_json=""):
+        timestamp = time.time()
+        timestamp_millisecs = int(timestamp * 1000)
+        timestamp_secs = math.floor(timestamp)
+        path= path.replace("[", "%5B")
+        path= path.replace("]", "%5D")
+        token = {
+            "uri": path,
+            "nonce": timestamp_millisecs,
+            "iat": timestamp_secs,
+            "exp": timestamp_secs + 55, 
+            "sub": self.api_key,
+            "bodyHash": sha256(json.dumps(body_json).encode("utf-8")).hexdigest()
+        }
+
+        return jwt.encode(token, key=self.private_key, algorithm="RS256").decode('utf-8')

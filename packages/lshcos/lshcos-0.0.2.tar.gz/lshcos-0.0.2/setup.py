dc895965f1ead89fc25dc15117f180b262915eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:04 +0000

    Add package lshcos/0.0.2/lshcos-0.0.2.tar.gz

---
+++ packages/lshcos/lshcos-0.0.2.tar.gz/lshcos-0.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+import lshcos
+
+with open('README.rst') as f:
+    readme = f.read()
+
+
+setup(
+    name='lshcos',
+    version=lshcos.__version__,
+    packages=['lshcos'],
+    author='Chef_J',
+    author_email='hedge_jzt@hotmail.com',
+    maintainer='Chef_J',
+    maintainer_email='hedge_jzt@hotmail.com',
+    description='A fast Python implementation of locality sensitive hashing with cosine distance.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/jinzitian/LSHCos',
+    install_requires=['numpy'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        ],
+
+)

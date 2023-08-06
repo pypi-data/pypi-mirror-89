7a3c125bd357cd198cdd648d5c5d1f0a4007cd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:28 +0000

    Add package zhenbin-awesome-helloworld-script/0.2/zhenbin-awesome-helloworld-script-0.2.tar.gz

---
+++ packages/zhenbin-awesome-helloworld-script/zhenbin-awesome-helloworld-script-0.2.tar.gz/zhenbin-awesome-helloworld-script-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='zhenbin-awesome-helloworld-script',    # This is the name of your PyPI-package.
+    version='0.2',                          # Update the version number for new releases
+    scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
+)

02c059f2f8192bb391262e53a1fe7d7a67b3d337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:39 +0000

    Add package redis-client-connect/0.1/redis_client_connect-0.1.tar.gz

---
+++ packages/redis-client-connect/redis_client_connect-0.1.tar.gz/redis_client_connect-0.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+import setuptools
+
+
+
+setuptools.setup(
+    name="redis_client_connect",
+    version="0.1",
+    author="zmjaction",
+    author_email="",
+    description="redis_client_connect",
+    long_description="",
+    packages=setuptools.find_packages(),
+    long_description_content_type="text/markdown",
+    install_requires=['redis>=3.4.1'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+
+)

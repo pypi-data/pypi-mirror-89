fba7355e263826cf9d08ebaab2c69dccce13af78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:50 +0000

    Add package gsmo/0.1.0.post1/gsmo-0.1.0.post1-py3-none-any.whl

---
+++ packages/gsmo/gsmo-0.1.0.post1-py3-none-any.whl/gsmo/cli.py
@@ -0,0 +1,38 @@
+from os import listdir
+from os.path import isdir
+import yaml
+
+class Arg:
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+run_args = [
+    Arg('-b','--branch',help="Branch to clone, work in, and push results back to. Can also be passed as a trailing '@<branch>' slug on directory path or remote Git repo URL. For local repositories, implies --clone"),
+    Arg('--clone',action='store_true',help='Clone local directory into a temporary dir for duration of the run'),
+    Arg('--commit',action='append',help='Paths to `git add` and commit after running'),
+    Arg('-C','--dir',help="Resolve paths (incl. mounts) relative to this directory (default: current directory)"),
+    Arg('-o','--out',help='Path or directory to write output notebook to (relative to `--dir` directory; default: "nbs")'),
+    Arg('--push',help='Push to this remote spec when done running'),
+    Arg('-x','--run','--execute',help='Notebook to run (default: run.ipynb)'),
+    Arg('-y','--yaml',action='append',help='YAML string(s) with configuration settings for the module being run'),
+    Arg('-Y','--yaml-path',action='append',help='YAML file(s) with configuration settings for the module being run'),  # TODO: update example nb
+]
+
+def load_run_config(args):
+    # Load configs to pass into run container
+    run_config = {}
+    if (run_config_yaml_paths := args.yaml_path):
+        for run_config_yaml_path in run_config_yaml_paths:
+            if isdir(run_config_yaml_path):
+                raise RuntimeError(f'run_config_yaml_path {run_config_yaml_path} is a directory: {listdir(run_config_yaml_path)}')
+            with open(run_config_yaml_path,'r') as f:
+                run_config.update(yaml.safe_load(f))
+
+    if (run_config_yaml_strs := args.yaml):
+        for run_config_yaml_str in run_config_yaml_strs:
+            run_config_yaml = yaml.safe_load(run_config_yaml_str)
+            run_config.update(run_config_yaml)
+
+    return run_config
---
+++ packages/gsmo/gsmo-0.1.0.post1-py3-none-any.whl/gsmo/modules.py
@@ -0,0 +1,53 @@
+from os import environ as env
+from tempfile import NamedTemporaryFile
+
+from .papermill import execute
+from . import gsmo
+
+from utz import cd, sh
+
+
+class Modules:
+    def __init__(self, run=None, skip=None, conf=None):
+        if isinstance(run, str): run = run.split(',')
+        if isinstance(skip, str): skip = skip.split(',')
+        if run is not None and skip is not None:
+            raise RuntimeError('Specify at most one of {run,skip}: (%s, %s)' % (run, skip))
+        self.runs = run
+        self.skips = skip
+        self.conf = conf or {}
+
+    def run(self, module, nb='run.ipynb', out='nbs', dind=None, *args, **kwargs):
+        if self.skips and module in self.skips:
+            print(f'Module {module} marked as "skip"; skipping')
+            return
+        if self.runs and module not in self.runs:
+            print(f'Module {module} not marked as "run"; skipping')
+            return
+
+        module_kwargs = self.conf.get(module, {})
+        module_kwargs.update(kwargs)
+
+        with cd(module):
+            print(f'Running module: {module}')
+            if dind is not False:
+                with NamedTemporaryFile() as tmp:
+                    with open(tmp.name,'w') as f:
+                        import yaml
+                        yaml.safe_dump(kwargs, f, sort_keys=False)
+                    cmd = []
+                    if 'GSMO_IMAGE' in env:
+                        cmd += ['-i',env['GSMO_IMAGE']]
+                    cmd += ['-I','run','-o',out,'-x',nb,'-Y',tmp.name]
+                    gsmo.main(*cmd)
+            else:
+                execute(
+                    nb,
+                    out,
+                    *args,
+                    **module_kwargs,
+                )
+        sh('git','add',module)
+        sh('git','commit','-m',module)
+
+    def __call__(self, *args, **kwargs): return self.run(*args, **kwargs)

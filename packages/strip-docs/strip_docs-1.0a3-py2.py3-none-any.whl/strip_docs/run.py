d4c8af6da41fc9eaf55c4f4e7ab9f89aa821c538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:50 +0000

    Add package strip-docs/1.0a3/strip_docs-1.0a3-py2.py3-none-any.whl

---
+++ packages/strip-docs/strip_docs-1.0a3-py2.py3-none-any.whl/strip_docs/run.py
@@ -0,0 +1,13 @@
+import sys
+from strip_docs.strip_docs import do_path
+
+
+def run():
+    if len(sys.argv) == 1:
+        print('''Use as:
+
+strip-docs <name of directory with py, pyx and pyd files>
+''')
+        sys.exit(1)
+    for dir_or_file_name in sys.argv[1:]:
+        do_path(dir_or_file_name)

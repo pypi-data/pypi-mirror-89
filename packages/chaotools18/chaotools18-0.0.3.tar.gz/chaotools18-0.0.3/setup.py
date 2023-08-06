a2b07b22142c2c16adcebe0a67ea83f3d1b18e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:30 +0000

    Add package chaotools18/0.0.3/chaotools18-0.0.3.tar.gz

---
+++ packages/chaotools18/chaotools18-0.0.3.tar.gz/chaotools18-0.0.3/setup.py
@@ -0,0 +1,22 @@
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chaotools18",
+    version="0.0.3",
+    author="chao",
+    description="chaotools18",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

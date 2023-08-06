ba8df5d6397f355e38e39eba47be382f5c3552b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:27 +0000

    Add package solvent/0.0.24/solvent-0.0.24-py3-none-any.whl

---
+++ packages/solvent/solvent-0.0.24-py3-none-any.whl/solvent/steube.py
@@ -0,0 +1,31 @@
+import os
+
+import pomace
+from pomace import shared
+
+URL = "https://steube.house.gov/contact/newsletter"
+
+
+def main():
+    submits = 0
+    errors = 0
+
+    while errors < 10:
+        page = pomace.visit(URL)
+
+        person = pomace.fake.person
+        page.fill_first_name(person.first_name)
+        page.fill_last_name(person.last_name)
+        page.fill_email(person.email)
+
+        print("Type word from audio CAPTCHA and click Subscribe...")
+        while shared.browser.url == str(page.url):
+            pass
+
+        if "thank-you" in shared.browser.url:
+            submits += 1
+            errors = 0
+            print(f"Submission count: {submits}")
+        else:
+            errors += 1
+            print(f"Error count: {errors}")
---
+++ packages/solvent/solvent-0.0.24-py3-none-any.whl/solvent/stopsteal.py
@@ -0,0 +1,31 @@
+import time
+
+import pomace
+
+
+URL = "https://action.greene2020.com/stop-the-steal-rd/"
+
+
+def main():
+    submits = 0
+    errors = 0
+
+    while errors < 10:
+        page = pomace.visit(URL)
+
+        person = pomace.fake.person
+
+        page.fill_full_name(person.first_name + " " + person.last_name)
+        page.fill_email(person.email)
+        page.fill_zip_code(person.zip_code)
+        page.fill_phone_number(person.phone_number)
+
+        result = page.click_stop_the_steal()
+
+        if "If you'd like to enter to win" in result:
+            submits += 1
+            errors = 0
+            print(f"Submission count: {submits}")
+        else:
+            errors += 1
+            print(f"Error count: {errors}")

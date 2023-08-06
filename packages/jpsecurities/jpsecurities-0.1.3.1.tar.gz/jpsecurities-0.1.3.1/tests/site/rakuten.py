e9b37551089b8fb7b537dab29740f92af707fd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:09 +0000

    Add package jpsecurities/0.1.3.1/jpsecurities-0.1.3.1.tar.gz

---
+++ packages/jpsecurities/jpsecurities-0.1.3.1.tar.gz/jpsecurities-0.1.3.1/tests/site/rakuten.py
@@ -0,0 +1,37 @@
+import unittest
+from jpsecurities.site.rakuten import Rakuten
+from selenium.webdriver.chrome.options import Options
+import time
+import logging
+import sys
+import os
+from logging import Formatter, handlers, StreamHandler, getLogger, DEBUG
+
+logging.getLogger().setLevel(DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+class TestRakuten(unittest.TestCase):
+    chrome_options = Options()
+    chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    executable_path = '/usr/local/bin/chromedriver'
+    username = os.environ['RAKUTEN_USERNAME']
+    pwd = os.environ['RAKUTEN_PWD']
+
+    """
+    def test_asset_info(self):
+        with Rakuten(executable_path=self.executable_path, chrome_options=self.chrome_options,
+                     username=self.username, pwd=self.pwd) as rakuten:
+            rakuten_asset_info = rakuten.asset_info()
+            time.sleep(10000)
+    """
+
+    def test_kashikabu_accounting_details(self):
+        with Rakuten(executable_path=self.executable_path, chrome_options=self.chrome_options,
+                     username=self.username, pwd=self.pwd) as rakuten:
+            kashikabu_accounting_details = rakuten.kashikabu_accounting_details()
+            print(kashikabu_accounting_details)
+            time.sleep(10000)
+
+if __name__ == "__main__":
+    unittest.main()

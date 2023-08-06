af23cd463f34b283e41b38d1125ac4ccc9cb41c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:26 +0000

    Add package recursion-visualiser/1.0.2/recursion-visualiser-1.0.2.tar.gz

---
+++ packages/recursion-visualiser/recursion-visualiser-1.0.2.tar.gz/recursion-visualiser-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="recursion-visualiser",
+    version="1.0.2",
+    author="Bishal Sarangkoti",
+    author_email="sarangbishal@gmail.com",
+    description="A small python package to visualise recursive function on Python. It draws recursion tree",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bishalsarang/Recursion-Tree-Visualizer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pydot", "imageio"],
+    python_requires='>=3.6',
+)

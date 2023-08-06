793e9596750730bb9142e25fb0c4606cd18023d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:43 +0000

    Add package amocrm-api/2.2.0/amocrm_api-2.2.0-py3-none-any.whl

---
+++ packages/amocrm-api/amocrm_api-2.2.0-py3-none-any.whl/amocrm/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+__version__ = '2.2.0'
+
+import sys
+import logging
+
+from .settings import settings as amo_settings
+from .api import *
+from .apimodels import *
+from . import fields
+
+__all__ = [
+    'BaseCompany', 'BaseContact', 'BaseLead', 'amo_settings', 'AmoApi', 'ContactNote', 'ContactTask',
+    'LeadNote', 'LeadTask', 'fields',
+]
+
+logger = logging.getLogger('amocrm')
+
+if not logger.handlers:
+    formatter = logging.Formatter(
+        '%(asctime)s.%(msecs)d %(levelname)s: %(message)s', '%Y-%m-%d %H:%M:%S'
+    )
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
+    logger.addHandler(handler)

b8018d6b33b18a1085f921b9c722063acd8bc81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:05 +0000

    Add package tobler/0.5.2/tobler-0.5.2.tar.gz

---
+++ packages/tobler/tobler-0.5.2.tar.gz/tobler-0.5.2/tobler/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.5.2"
+"""
+:mod:`tobler` --- A library for spatial interpolation
+=================================================
+
+"""
+from . import area_weighted
+from . import dasymetric
+from . import model

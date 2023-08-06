2f649793e897f89bc7daffec942d0630a918fb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:43 +0000

    Add package plotink/1.2.2/plotink-1.2.2.tar.gz

---
+++ packages/plotink/plotink-1.2.2.tar.gz/plotink-1.2.2/setup.py
@@ -0,0 +1,45 @@
+"""
+Based on https://github.com/pypa/sampleproject
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='plotink',
+    version='1.2.2',
+    python_requires='>=3.5.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/evil-mad/plotink',
+    author='Evil Mad Scientist Laboratories',
+    author_email='contact@evilmadscientist.com',
+    description="Helper routines for use with plotters",
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+    ],
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[
+        'ink_extensions',
+        'pyserial>=3.5',
+    ],
+    extras_require={
+        'dev': ['coverage'],
+        'test': [],
+    },
+)

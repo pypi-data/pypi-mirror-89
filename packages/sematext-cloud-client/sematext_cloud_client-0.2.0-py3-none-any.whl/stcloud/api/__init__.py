d919da0e9c9cbd47f30cebb7087d970274f22656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:45 +0000

    Add package sematext-cloud-client/0.2.0/sematext_cloud_client-0.2.0-py3-none-any.whl

---
+++ packages/sematext-cloud-client/sematext_cloud_client-0.2.0-py3-none-any.whl/stcloud/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from stcloud.api.alert_notifications_api import AlertNotificationsApi  # noqa: E501,W0611
+from stcloud.api.alerts_api import AlertsApi  # noqa: E501,W0611
+from stcloud.api.apps_api import AppsApi  # noqa: E501,W0611
+from stcloud.api.billing_api import BillingApi  # noqa: E501,W0611
+from stcloud.api.logs_app_api import LogsAppApi  # noqa: E501,W0611
+from stcloud.api.metrics_api import MetricsApi  # noqa: E501,W0611
+from stcloud.api.monitoring_app_api import MonitoringAppApi  # noqa: E501,W0611
+from stcloud.api.reset_password_api import ResetPasswordApi  # noqa: E501,W0611
+from stcloud.api.saved_queries_api import SavedQueriesApi  # noqa: E501,W0611
+from stcloud.api.subscriptions_api import SubscriptionsApi  # noqa: E501,W0611
+from stcloud.api.aws_settings_controller_api import AwsSettingsControllerApi  # noqa: E501,W0611
+from stcloud.api.tag_api_controller_api import TagApiControllerApi  # noqa: E501,W0611
+from stcloud.api.tokens_api_controller_api import TokensApiControllerApi  # noqa: E501,W0611

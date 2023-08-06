5693515d5196b35de68c88c3ff21e7f4228507a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:53 +0000

    Add package puppy-fly/0.0.2/puppy-fly-0.0.2.tar.gz

---
+++ packages/puppy-fly/puppy-fly-0.0.2.tar.gz/puppy-fly-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="puppy-fly", # Replace with your own username
+    version="0.0.2",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+            "puppy=puppy.main:main",
+        ],
+    },
+    python_requires='>=3.6',
+)

3a002037450098cc8bf098ca4242a73b18cd1897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:04 +0000

    Add package pyams-security-views/1.0.1/pyams_security_views-1.0.1-py3.8.egg

---
+++ packages/pyams-security-views/pyams_security_views-1.0.1-py3.8.egg/pyams_security_views/zmi/interfaces.py
@@ -0,0 +1,35 @@
+#
+# Copyright (c) 2015-2020 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+
+"""PyAMS_security_views.zmi.interfaces module
+
+This module defines public views and viewlet managers interfaces.
+"""
+
+from zope.interface import Interface
+
+from pyams_zmi.interfaces.viewlet import INavigationMenuItem
+
+
+__docformat__ = 'restructuredtext'
+
+
+class ISecurityMenu(INavigationMenuItem):
+    """Security menu interface"""
+
+
+class IObjectSecurityMenu(INavigationMenuItem):
+    """Object security menu"""
+
+
+class ISecurityManagerView(Interface):
+    """Security manager view"""

dab41e8db5afbde7c6383c95ebd64fc78bd2badc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:21 +0000

    Add package BLUvo/0.5/BLUvo-0.5.tar.gz

---
+++ packages/BLUvo/BLUvo-0.5.tar.gz/BLUvo-0.5/bluvo/test.py
@@ -0,0 +1,31 @@
+from bluvo import test_config as cfg, BLUvo
+
+from .api.bluvo_interface import VehicleStatus, VehicleLocation, BLUvoConfig
+from .api.helpers.constants import Regions, Makes
+
+
+def print_status(status: VehicleStatus):
+    print (status.chassis.locked)
+    print (status.climate.temperature_set_point)
+    pass
+
+
+def print_location(location: VehicleLocation):
+    print("{}, {}".format(location.latitude, location.longitude))
+
+
+def handle_exception(ex):
+    print(ex)
+
+
+if __name__ == "__main__":
+    config = BLUvoConfig(Regions.CA, Makes.KIA, cfg.username, cfg.password, cfg.pin)
+    ctrl = BLUvo(config)
+
+    ctrl.login()
+    vehicles = ctrl.get_vehicles()
+
+    first_vehicle = vehicles[0]
+
+    first_vehicle.location().match(print_location, handle_exception)
+    first_vehicle.status().match(print_status, handle_exception)

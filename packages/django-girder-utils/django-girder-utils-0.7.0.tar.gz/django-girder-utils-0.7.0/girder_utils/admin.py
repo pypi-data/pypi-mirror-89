a2e34628c473c0885b281a6e0c62c5d6f125bfbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:58 +0000

    Add package django-girder-utils/0.7.0/django-girder-utils-0.7.0.tar.gz

---
+++ packages/django-girder-utils/django-girder-utils-0.7.0.tar.gz/django-girder-utils-0.7.0/girder_utils/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+
+class ReadonlyInlineMixin:
+    can_delete = False
+    show_change_link = True
+    view_on_site = False
+    extra = 0
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.fields
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class ReadonlyTabularInline(ReadonlyInlineMixin, admin.TabularInline):
+    pass

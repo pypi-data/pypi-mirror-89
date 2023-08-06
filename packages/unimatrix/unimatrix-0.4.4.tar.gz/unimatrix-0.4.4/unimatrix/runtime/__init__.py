ab65a88a1db4da24060b1c21b00fd6a039cd8c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:50 +0000

    Add package unimatrix/0.4.4/unimatrix-0.4.4.tar.gz

---
+++ packages/unimatrix/unimatrix-0.4.4.tar.gz/unimatrix-0.4.4/unimatrix/runtime/__init__.py
@@ -0,0 +1,8 @@
+"""The :mod:`unimatrix.runtime` package provides a common interface to set
+up the application runtime environment.
+"""
+
+
+def setup():
+    """Setup all :mod:`unimatrix` components."""
+    pass

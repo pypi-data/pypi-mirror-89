6a3be97ba61cebc3946ecfdff4637f45424bcc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:03 +0000

    Add package pyuavcan/1.1.0.dev4/pyuavcan-1.1.0.dev4-py3-none-any.whl

---
+++ packages/pyuavcan/pyuavcan-1.1.0.dev4-py3-none-any.whl/pyuavcan/transport/can/_session/_base.py
@@ -0,0 +1,32 @@
+#
+# Copyright (c) 2019 UAVCAN Development Team
+# This software is distributed under the terms of the MIT License.
+# Author: Pavel Kirienko <pavel.kirienko@zubax.com>
+#
+
+from __future__ import annotations
+import typing
+import logging
+import pyuavcan.transport
+
+
+SessionFinalizer = typing.Callable[[], None]
+
+
+_logger = logging.getLogger(__name__)
+
+
+class CANSession:
+    def __init__(self, finalizer: SessionFinalizer):
+        self._close_finalizer: typing.Optional[SessionFinalizer] = finalizer
+
+    def _raise_if_closed(self) -> None:
+        if self._close_finalizer is None:
+            raise pyuavcan.transport.ResourceClosedError(
+                f'The requested action cannot be performed because the session object {self} is closed')
+
+    def close(self) -> None:
+        fin = self._close_finalizer
+        if fin is not None:
+            self._close_finalizer = None
+            fin()

691c855ce841094f1ad9476d9d2f49127ce64e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:19 +0000

    Add package teamux/0.20.51.1/teamux-0.20.51.1.tar.gz

---
+++ packages/teamux/teamux-0.20.51.1.tar.gz/teamux-0.20.51.1/teamux/session.py
@@ -0,0 +1,37 @@
+from .base import Base
+from .window import Window
+
+
+class Session(Base):
+    keys = '''
+        session_name
+        session_active
+        session_id
+        session_width
+        session_height
+        session_attached
+    '''
+    Child = Window
+    list_cmd ='list-windows'
+
+    def __init__(self, tmux, fields):
+        self.Tmux = tmux
+        self.fields = fields
+
+    @property
+    def windows(self):
+        yield from self.children
+
+    def get(self, name):
+        for w in self.windows:
+            if w.window_name==name:
+                return w
+
+    def new(self, name='xw'):
+        self.tmux(f'new-window', '-n', name)
+        return self.get(name)
+
+    def __str__(self):
+        f = self.fields
+        return f"{self.is_active and '*' or ' '} {f.session_name}"
+

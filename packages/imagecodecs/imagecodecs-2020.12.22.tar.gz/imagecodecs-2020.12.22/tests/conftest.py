b9dbed6ff0efd938f1261a9603be62ef12df4e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:58 +0000

    Add package imagecodecs/2020.12.22/imagecodecs-2020.12.22.tar.gz

---
+++ packages/imagecodecs/imagecodecs-2020.12.22.tar.gz/imagecodecs-2020.12.22/tests/conftest.py
@@ -0,0 +1,19 @@
+# imagecodecs/tests/conftest.py
+
+
+def pytest_report_header(config):
+    import sys
+
+    try:
+        pyversion = f'Python {sys.version.splitlines()[0]}'
+        import imagecodecs
+        from imagecodecs import _imagecodecs
+
+        return '{}\npackagedir: {}\nversion: {}\ndependencies: {}'.format(
+            pyversion,
+            imagecodecs.__path__[0],
+            imagecodecs.version(),
+            _imagecodecs.version(),
+        )
+    except Exception as exc:
+        return f'pytest_report_header failed: {exc!s}'

0a4f946d3b1e83c87ccbdf596fbe1eb852e5efc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:43 +0000

    Add package Tyf/1.4.3/Tyf-1.4.3.tar.gz

---
+++ packages/Tyf/Tyf-1.4.3.tar.gz/Tyf-1.4.3/Tyf/encoders.py
@@ -0,0 +1,259 @@
+# -*- encoding:utf-8 -*-
+
+from Tyf import reduce
+
+import math
+import fractions
+
+
+_m_u_short = 0
+_M_u_short = 2**8
+_m_u_byte = 0
+_M_u_byte = 2**16
+_m_u_long = 0
+_M_u_long = 2**32
+
+_m_s_short = -_M_u_short / 2
+_M_s_short = _M_u_short / 2 - 1
+_m_s_byte = -_M_u_byte / 2
+_M_s_byte = _M_u_byte / 2 - 1
+_m_s_long = -_M_u_long / 2
+_M_s_long = _M_u_long / 2 - 1
+
+_m_float = -1.17549e38
+_M_float = 3.40282e38
+_m_double = -2.22507e308
+_M_double = 1.79769e308
+
+
+class EncodingException(Exception):
+    pass
+
+
+def in_range(value, mini, maxi, cast=int):
+    value = cast(value)
+    if mini <= value < maxi:
+        return value
+    else:
+        raise EncodingException(
+            "%s not in range [%r:%r[" % (value, mini, maxi)
+        )
+
+
+def _1(value):
+    if isinstance(value, tuple):
+        return tuple(in_range(v, _m_u_short, _M_u_short) for v in value)
+    else:
+        return (in_range(value, _m_u_short, _M_u_short), )
+
+
+def _2(value):
+    if not isinstance(value, bytes):
+        try:
+            value = value.encode("utf-8")
+        except Exception:
+            raise EncodingException(
+                "%s can not be encoded" % (value, )
+            )
+    if len(value) == 0:
+        return b"\x00"
+    else:
+        return value + (b"\x00" if value[-1] != b"\x00" else "")
+
+
+def _3(value):
+    if isinstance(value, tuple):
+        return tuple(in_range(v, _m_u_byte, _M_u_byte) for v in value)
+    else:
+        return (in_range(value, _m_u_byte, _M_u_byte), )
+
+
+def _4(value):
+    if isinstance(value, tuple):
+        return tuple(in_range(v, _m_u_long, _M_u_long) for v in value)
+    else:
+        return (in_range(value, _m_u_long, _M_u_long), )
+
+
+def _rational_yielder(*values):
+    cnt = 0
+    for v in values:
+        if isinstance(v, int):
+            cnt += 1
+            yield (v, )
+        else:
+            if isinstance(v, float):
+                v = str(v)
+            f = fractions.Fraction(v).limit_denominator(10000000)
+            cnt += 2
+            yield (f.numerator, f.denominator)
+
+    if cnt % 2:
+        yield (1, )
+
+
+def _5(value):
+    if not isinstance(value, tuple):
+        value = (value, )
+    try:
+        reduced = reduce(tuple.__add__, _rational_yielder(*value))
+        return tuple(in_range(v, _m_u_long, _M_u_long) for v in reduced)
+    except Exception:
+        raise EncodingException(
+            "%s can not be encoded as unsigned rational number(s)" % (value, )
+        )
+
+
+def _6(value):
+    if isinstance(value, tuple):
+        return tuple(in_range(v, _m_s_short, _M_s_short) for v in value)
+    else:
+        return (in_range(value, _m_s_short, _M_s_short), )
+
+
+def _7(value):
+    if not isinstance(value, bytes):
+        value = value.encode("utf-8")
+    return value
+
+
+def _8(value):
+    if isinstance(value, tuple):
+        return tuple(in_range(v, _m_s_byte, _M_s_byte) for v in value)
+    else:
+        return (in_range(value, _m_s_byte, _M_s_byte), )
+
+
+def _9(value):
+    if isinstance(value, tuple):
+        return tuple(in_range(v, _m_s_long, _M_s_long) for v in value)
+    else:
+        return (in_range(value, _m_s_long, _M_s_long), )
+
+
+def _10(value):
+    if not isinstance(value, tuple):
+        value = (value, )
+    try:
+        reduced = reduce(tuple.__add__, _rational_yielder(*value))
+        return tuple(in_range(v, _m_s_long, _M_s_long) for v in reduced)
+    except Exception:
+        raise EncodingException(
+            "%s can not be encoded as unsigned rational number(s)" % (value, )
+        )
+
+
+def _11(value):
+    if isinstance(value, tuple):
+        return tuple(
+            in_range(v, _m_float, _M_float, cast=float) for v in value
+        )
+    else:
+        return (in_range(value, _m_float, _M_float, cast=float), )
+
+
+def _12(value):
+    if isinstance(value, tuple):
+        return tuple(
+            in_range(v, _m_double, _M_double, cast=float) for v in value
+        )
+    else:
+        return (in_range(value, _m_double, _M_double, cast=float), )
+
+
+# PrivateTiffTag:
+
+def XPTitle(value):
+    return reduce(tuple.__add__, [(ord(e), 0) for e in value]) + (0, 0)
+
+
+XPComment = XPAuthor = XPKeywords = XPSubject = XPTitle
+
+
+def UserComment(value):
+    return b"ASCII\x00\x00\x00" + (
+        value.encode("ascii", errors="replace")
+        if not isinstance(value, bytes) else value
+    )
+
+
+# ExifTag:
+
+def DateTime(value):
+    try:
+        return _2(value.strftime("%Y:%m:%d %H:%M:%S"))
+    except Exception:
+        raise EncodingException(
+            "%s is not a valide datetime object" % (value, )
+        )
+
+
+DateTimeOriginal = DateTimeDigitized = DateTime
+
+
+# class GpsTag:
+
+def _dms(value, limit=180, epsilon=0.0001):
+    value = abs(value) % limit
+
+    degrees = float(math.floor(value))
+    minutes = (value - degrees) * 60.0
+    seconds = (minutes - math.floor(minutes)) * 60.0
+    minutes = float(math.floor(minutes))
+
+    if seconds >= (60. - epsilon):
+        seconds, minutes = 0., minutes + 1.0
+    if minutes >= (60. - epsilon):
+        minutes, degrees = 0., degrees + 1.0
+
+    return degrees, minutes, seconds
+
+
+GPSProcessingMethod = UserComment
+
+
+def GPSLatitudeRef(value):
+    return b"N\x00" if value.lower() in ["n", b"n"] else b"S\x00"
+
+
+def GPSLatitude(value):
+    if isinstance(value, (int, float)):
+        return _5(_dms(value, limit=90))
+
+
+def GPSLongitudeRef(value):
+    return b"E\x00" if value.lower() in ["e", b"e"] else b"W\x00"
+
+
+def GPSLongitude(value):
+    if isinstance(value, (int, float)):
+        return _5(_dms(value, limit=180))
+
+
+def GPSAltitudeRef(value):
+    return _3(0 if bool(value) else 1)
+
+
+def GPSAltitude(value):
+    return _5(abs(value))
+
+
+def GPSTimeStamp(value):
+    try:
+        return _5(tuple(
+            float(e)
+            for e in [value.hour, value.minute, value.second]
+        ))
+    except Exception:
+        raise EncodingException(
+            "%s is not a valide date object" % (value, )
+        )
+
+
+def GPSDateStamp(value):
+    try:
+        return _2(value.strftime("%Y:%m:%d"))
+    except Exception:
+        raise EncodingException(
+            "%s is not a valide time object" % (value, )
+        )
---
+++ packages/Tyf/Tyf-1.4.3.tar.gz/Tyf-1.4.3/Tyf/gkd.py
@@ -0,0 +1,188 @@
+# -*- encoding: utf-8 -*-
+
+from Tyf import values, __geotiff__
+
+
+_TAGS = {
+    # GeoTIFF Configuration GeoKeys
+    1024: ("GTModelTypeGeoKey", [3], 0, None),
+    1025: ("GTRasterTypeGeoKey", [3], 1, None),
+    1026: ("GTCitationGeoKey", [2], None, None),             # ASCII text
+
+    # Geographic CS Parameter GeoKeys
+    2048: ("GeographicTypeGeoKey", [3], 4326, None),         # epsg datum code [4001 - 4999]
+    2049: ("GeogCitationGeoKey", [2], None, None),           # ASCII text
+    2050: ("GeogGeodeticDatumGeoKey", [3], None, None),      # use 2048 !
+    2051: ("GeogPrimeMeridianGeoKey", [3], 8901, None),      # epsg prime meridian code [8001 - 8999]
+    2052: ("GeogLinearUnitsGeoKey", [3], 9001, None),        # epsg linear unit code [9000 - 9099]
+    2053: ("GeogLinearUnitSizeGeoKey", [12], None, None),    # custom unit in meters
+    2054: ("GeogAngularUnitsGeoKey", [3], 9101, None),
+    2055: ("GeogAngularUnitsSizeGeoKey", [12], None, None),  # custom unit in radians
+    2056: ("GeogEllipsoidGeoKey", [3], None, None),          # epsg ellipsoid code [7000 - 7999]
+    2057: ("GeogSemiMajorAxisGeoKey", [12], None, None),
+    2058: ("GeogSemiMinorAxisGeoKey", [12], None, None),
+    2059: ("GeogInvFlatteningGeoKey", [12], None, None),
+    2060: ("GeogAzimuthUnitsGeoKey", [3], None, None),
+    2061: ("GeogPrimeMeridianLongGeoKey", [12], None, None),  # custom prime meridian value in GeogAngularUnits
+
+    # Projected CS Parameter GeoKeys
+    3072: ("ProjectedCSTypeGeoKey", [3], None, None),        # epsg grid code [20000 - 32760]
+    3073: ("PCSCitationGeoKey", [2], None, None),            # ASCII text
+    3074: ("ProjectionGeoKey", [3], None, None),             # [10000 - 19999]
+    3075: ("ProjCoordTransGeoKey", [3], None, None),
+    3076: ("ProjLinearUnitsGeoKey", [3], None, None),
+    3077: ("ProjLinearUnitSizeGeoKey", [12], None, None),    # custom unit in meters
+    3078: ("ProjStdParallel1GeoKey", [12], None, None),
+    3079: ("ProjStdParallel2GeoKey", [12], None, None),
+    3080: ("ProjNatOriginLongGeoKey", [12], None, None),
+    3081: ("ProjNatOriginLatGeoKey", [12], None, None),
+    3082: ("ProjFalseEastingGeoKey", [12], None, None),
+    3083: ("ProjFalseNorthingGeoKey", [12], None, None),
+    3084: ("ProjFalseOriginLongGeoKey", [12], None, None),
+    3085: ("ProjFalseOriginLatGeoKey", [12], None, None),
+    3086: ("ProjFalseOriginEastingGeoKey", [12], None, None),
+    3087: ("ProjFalseOriginNorthingGeoKey", [12], None, None),
+    3088: ("ProjCenterLongGeoKey", [12], None, None),
+    3089: ("ProjCenterLatGeoKey", [12], None, None),
+    3090: ("ProjCenterEastingGeoKey", [12], None, None),
+    3091: ("ProjFalseOriginNorthingGeoKey", [12], None, None),
+    3092: ("ProjScaleAtNatOriginGeoKey", [12], None, None),
+    3093: ("ProjScaleAtCenterGeoKey", [12], None, None),
+    3094: ("ProjAzimuthAngleGeoKey", [12], None, None),
+    3095: ("ProjStraightVertPoleLongGeoKey", [12], None, None),
+
+    # Vertical CS Parameter Keys
+    4096: ("VerticalCSTypeGeoKey", [3], None, None),
+    4097: ("VerticalCitationGeoKey", [2], None, None),
+    4098: ("VerticalDatumGeoKey", [3], None, None),
+    4099: ("VerticalUnitsGeoKey", [3], None, None),
+}
+
+_2TAG = dict((v[0], t) for t, v in _TAGS.items())
+_2KEY = dict((v, k) for k, v in _2TAG.items())
+
+
+class GkdTag:
+    strict = True
+
+    info = property(
+        lambda cls: getattr(
+            values, _2KEY.get(cls.tag, cls.key), {}
+        ).get(cls._decode(), None),
+        None,
+        None,
+        ""
+    )
+
+    def __init__(self, tag, value=None):
+        self.key, types, default, self.comment = _TAGS.get(
+            tag, ("Unknown", [0], None, "Undefined tag")
+        )
+        value = default if value is None else value
+
+        self.tag = tag
+        restricted = getattr(values, self.key, {})
+
+        if restricted:
+            reverse = dict((v, k) for k, v in restricted.items())
+            if value in restricted:
+                self.meaning = restricted.get(value)
+            elif value in reverse:
+                value = reverse[value]
+                self.meaning = value
+            elif GkdTag.strict:
+                raise ValueError(
+                    '"%s" value must be one of %s, get %s instead' %
+                    (self.key, list(restricted.keys()), value)
+                )
+
+        self.type, self.count, self.value = self._encode(value, types)
+
+    def __repr__(self):
+        if self.info:
+            return "<GKD tag %s:%r - %s>" % (self.key, self.value, self.info)
+        else:
+            return "<GKD tag %s:%r>" % (self.key, self.value)
+
+    def _encode(self, value, types):
+        if isinstance(value, str):
+            value = value.encode("utf-8")
+        elif not hasattr(value, "__len__"):
+            value = (value, )
+        typ = 0
+        if 2 in types:
+            typ = 34737
+        elif 12 in types:
+            typ = 34736
+        return typ, len(value), value
+
+    def _decode(self):
+        if self.count == 1:
+            return self.value[0]
+        else:
+            return self.value
+
+
+class Gkd(dict):
+    version = __geotiff__[0]
+    revision = __geotiff__[1:]
+
+    def __getitem__(self, tag):
+        return dict.__getitem__(self, _2TAG.get(tag, tag))._decode()
+
+    def __setitem__(self, tag, value):
+        dict.__setitem__(self, _2TAG.get(tag, tag), GkdTag(tag, value))
+
+    def compute(self):
+        directory_tags = (self.version, ) + self.revision + (len(self), )
+        double_params = ()
+        ascii_params = b""
+        for tag in self.tags():
+            directory_tags += (tag.tag, tag.type, tag.count)
+            if tag.type == 34736:
+                double_params += tag.value
+                directory_tags += (len(double_params)-1, )
+            elif tag.type == 34737:
+                directory_tags += (len(ascii_params), )
+                ascii_params += tag.value + b"|"
+            else:
+                directory_tags += tag.value
+        self._34735 = directory_tags
+        self._34736 = double_params
+        self._34737 = ascii_params
+
+    def get(self, tag, error=None):
+        if hasattr(self, "_%s" % tag):
+            return getattr(self, "_%s" % tag)
+        else:
+            return dict.get(self, _2TAG.get(tag, tag), error)
+
+    @staticmethod
+    def from_ifd(dic={}, **kw):
+        cls = Gkd()
+        pairs = dict(dic, **kw)
+        if "GeoDoubleParamsTag" in pairs:
+            _34736 = dic["GeoDoubleParamsTag"]
+        if "GeoAsciiParamsTag" in pairs:
+            _34737 = dic["GeoAsciiParamsTag"]
+        if "GeoKeyDirectoryTag" in pairs:
+            _34735 = dic["GeoKeyDirectoryTag"]
+            cls.version = _34735[0]
+            cls.revision = _34735[1:3]
+            for (tag, typ, count, value) in zip(
+                _34735[4::4], _34735[5::4], _34735[6::4], _34735[7::4]
+            ):
+                if typ == 0:
+                    value = value
+                elif typ == 34736:
+                    value = _34736[value]
+                elif typ == 34737:
+                    value = _34737[value:value + count - 1]
+                t = GkdTag(tag, value)
+                t.count = count
+                dict.__setitem__(cls, tag, t)
+        return cls
+
+    def tags(self):
+        for v in sorted(dict.values(self), key=lambda e: e.tag):
+            yield v

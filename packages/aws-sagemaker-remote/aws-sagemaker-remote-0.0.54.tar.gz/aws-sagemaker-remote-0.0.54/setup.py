2a519a8dc9353dd18c67928c1a4efa3d7344e8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:25 +0000

    Add package aws-sagemaker-remote/0.0.54/aws-sagemaker-remote-0.0.54.tar.gz

---
+++ packages/aws-sagemaker-remote/aws-sagemaker-remote-0.0.54.tar.gz/aws-sagemaker-remote-0.0.54/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+import os
+with open(os.path.abspath(os.path.join(__file__, '../README.rst')), encoding='utf-8') as f:
+    long_description = f.read()
+setup(name='aws-sagemaker-remote',
+      version='0.0.54',
+      author='Ben Striner',
+      author_email="bstriner@gmail.com",
+      url='https://github.com/bstriner/aws-sagemaker-remote',
+      install_requires=[
+          'sagemaker',
+          'sagemaker-experiments',
+          'click'
+      ],
+      package_data={'': [
+          '**/Dockerfile',
+          '**/*.sh',
+          '**/*.txt',
+          '**/*.yaml',
+          '**/*.yml',
+          '**/*.bat',
+          '**/*.js',
+          '**/*.json'
+      ]},
+      include_package_data=True,
+      entry_points={
+          'console_scripts': [
+              'aws-sagemaker-remote=aws_sagemaker_remote.cli:cli'
+          ]
+      },
+      description="Simplify running processing and training remotely on AWS SageMaker",
+      packages=find_packages(),
+      long_description=long_description,
+      long_description_content_type='text/x-rst'
+      )

a33adc63bcb0f85275387819c079995f8de2c45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:12 +0000

    Add package spotinst-agent/1.1.257/spotinst-agent-1.1.257.tar.gz

---
+++ packages/spotinst-agent/spotinst-agent-1.1.257.tar.gz/spotinst-agent-1.1.257/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+with open('README.rst') as desc_file:
+    description = desc_file.read()
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='spotinst-agent',
+
+    version="1.1.257",
+
+    description='Spectrum instance spotinst-agent that is able to run remote scripts, collect data, deploy applications and more.',
+
+    long_description=description,
+
+    # The project's main homepage.
+    url='https://github.com/spotinst/spotinst-spectrum-agent',
+
+    # Author details
+    author='Spotinst',
+    author_email='service@spotinst.com',
+
+    # Choose your license
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Information Technology',
+        'Topic :: System :: Monitoring',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    keywords='spotinst agent infrastructure monitoring execution deployment',
+    install_requires=['requests', 'pyyaml'],
+    packages=["spotinst_agent"],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'spotinst-agent=spotinst_agent:main'
+        ],
+    }
+
+)
---
+++ packages/spotinst-agent/spotinst-agent-1.1.257.tar.gz/spotinst-agent-1.1.257/spotinst_agent/basemodule.py
@@ -0,0 +1,48 @@
+import logging
+import traceback
+
+
+class Module(object):
+    isModule = True
+    """
+    This Module class is a base class for all modules.
+    """
+
+    def __init__(self, *args):
+        """
+        Create a new worker
+        """
+        # Initialize Logger
+        self.log = logging.getLogger('spotinst-agent')
+
+    def _run(self):
+        """
+        Run the collector
+        """
+        # Log
+        self.log.info("%s in running..." % self.__class__.__name__)
+        try:
+            self.run()
+        except Exception:
+            # Log Error
+            self.log.error(traceback.format_exc())
+
+    def get_schedule(self):
+        """
+        Return schedule for the worker
+        """
+        # Return a dict of tuples containing (collector function, collector function args, splay, interval)
+        return {self.__class__.__name__: (self._run, None, self.load_splay(), self.load_interval())}
+
+    def get_enable(self):
+        """
+        Return if the worker is enabled or disabled
+        :return:
+        """
+        return False
+
+    def load_splay(self):
+        pass
+
+    def load_interval(self):
+        pass

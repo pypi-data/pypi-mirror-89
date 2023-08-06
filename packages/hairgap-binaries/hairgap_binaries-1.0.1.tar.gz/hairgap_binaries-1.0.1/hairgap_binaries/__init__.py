a36d413e871207f0c304a5a15db360d89a2c3e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:10 +0000

    Add package hairgap-binaries/1.0.1/hairgap_binaries-1.0.1.tar.gz

---
+++ packages/hairgap-binaries/hairgap_binaries-1.0.1.tar.gz/hairgap_binaries-1.0.1/hairgap_binaries/__init__.py
@@ -0,0 +1,27 @@
+__version__ = "1.0.1"
+__author__ = "Matthieu Gallet"
+__all__ = ["get_hairgapr", "get_hairgaps"]
+
+from typing import Optional
+
+import pkg_resources
+
+known_platforms = {
+    "linux-x86_64": "manylinux2014_x86_64",
+}
+
+
+def get_hairgapr() -> Optional[str]:
+    """return the path of the hairgapr binary"""
+    prefix = known_platforms.get(pkg_resources.get_platform())
+    return prefix and pkg_resources.resource_filename(
+        "hairgap_binaries", "%s-hairgapr" % prefix
+    )
+
+
+def get_hairgaps() -> Optional[str]:
+    """return the path of the hairgaps binary"""
+    prefix = known_platforms.get(pkg_resources.get_platform())
+    return prefix and pkg_resources.resource_filename(
+        "hairgap_binaries", "%s-hairgaps" % prefix
+    )

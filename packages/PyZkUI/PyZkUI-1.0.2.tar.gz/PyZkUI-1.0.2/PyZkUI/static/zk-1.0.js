89ba1b110b9dc175eb5d25001e889899161e442c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:59 +0000

    Add package PyZkUI/1.0.2/PyZkUI-1.0.2.tar.gz

---
+++ packages/PyZkUI/PyZkUI-1.0.2.tar.gz/PyZkUI-1.0.2/PyZkUI/static/zk-1.0.js
@@ -0,0 +1,32 @@
+function loadHistory() {
+    $.ajax({
+        url: "/his"
+    }).done(function(data){
+        let ul = ""
+        for(i in data){
+            let h = data[i];
+            ul = ul + '<a href="/zk?h=' + h + '" class="list-group-item list-group-item-action">' + h + '</a>';
+        }
+        if (ul.length > 0) {
+            let his = '<h4>History</h4><div class="list-group">' + ul + '</div>'
+            $(".container").append(his)
+        }
+    });
+}
+
+function btkClick() {
+    $(".btn").click(function(){
+        $(location).attr("href", "zk?h="+$(".form-control").val());
+    });
+}
+
+
+function getTree() {
+    $.ajax({
+        url: "/tree?h="+$("#host").text(),
+        dataType: "json"
+    }).done(function(treeData){
+        $('#tree').text("");
+        $('#tree').bstreeview({data: JSON.stringify(treeData)});
+    })
+}

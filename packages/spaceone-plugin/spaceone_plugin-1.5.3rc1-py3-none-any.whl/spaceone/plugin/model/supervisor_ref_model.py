1f9243c054687bca030eed968d5b5c8a8227f85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:08 +0000

    Add package spaceone-plugin/1.5.3rc1/spaceone_plugin-1.5.3rc1-py3-none-any.whl

---
+++ packages/spaceone-plugin/spaceone_plugin-1.5.3rc1-py3-none-any.whl/spaceone/plugin/model/supervisor_ref_model.py
@@ -0,0 +1,33 @@
+from mongoengine import *
+
+from spaceone.core.model.mongo_model import MongoModel
+from spaceone.plugin.model.supervisor_model import Supervisor
+
+__all__ = ['SupervisorRef']
+
+
+class SupervisorRef(MongoModel):
+    domain_id = StringField(max_length=64)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now_add=True)
+    supervisor_id = StringField(max_length=40)
+    supervisor = ReferenceField('Supervisor', required=True, null=False)
+
+    meta = {
+        'db_alias': 'default',
+        'updatable_fields': [
+            'updated_at'
+        ],
+        'exact_fields': [
+            'supervisor_id',
+        ],
+        'minimal_fields': [
+            'supervisor_id',
+            'supervisors'
+        ],
+        'ordering': ['domain_id'],
+        'indexes': [
+            'supervisor_id',
+            'domain_id'
+        ]
+    }

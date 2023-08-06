45a2a47e56df1f11f6fac07eb04fd536db4c5d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:09 +0000

    Add package NlpToolkit-SpellChecker/1.0.18/NlpToolkit-SpellChecker-1.0.18.tar.gz

---
+++ packages/NlpToolkit-SpellChecker/NlpToolkit-SpellChecker-1.0.18.tar.gz/NlpToolkit-SpellChecker-1.0.18/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-SpellChecker',
+    version='1.0.18',
+    packages=['SpellChecker'],
+    url='https://github.com/olcaytaner/TurkishSpellChecker-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Turkish Spell Checker Library',
+    install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-NGram']
+)

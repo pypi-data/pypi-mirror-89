430e38ad68f4a2b42d59bf2b2ee397d82a450484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package test-distributions-linh92/0.1/test_distributions_linh92-0.1.tar.gz

---
+++ packages/test-distributions-linh92/test_distributions_linh92-0.1.tar.gz/test_distributions_linh92-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='test_distributions_linh92',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['test_distributions_linh92'],
+      author = 'Linh Truong',
+      author_email = 'mrthlinh@gmail.com',
+      zip_safe=False)

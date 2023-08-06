3b9d48568d8a603cf78b902464b1490536e14ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:19 +0000

    Add package MOMORING/1.1.0/MOMORING-1.1.0.tar.gz

---
+++ packages/MOMORING/MOMORING-1.1.0.tar.gz/MOMORING-1.1.0/MOMORING/modules/files/readme_template.py
@@ -0,0 +1,53 @@
+import os
+
+
+def get_readme_template(project_name):
+    txt = '''\
+# momo_PJ_name #
+
+!!project description.
+
+### Nitrogen Job Template ###
+```
+{
+    "name":"momo_PJ_name",
+    "description":"!!project description.",
+    "docker_image":"!!docker images",
+    "git_url":"!!git url",
+    "git_branch":"master",
+    "project":"!!project id",
+    "datasets":"",
+    "command":"!!run command",
+    "use_gpu":0,
+    "env":{"!!env":"!!default value"},
+    "resource":
+    {
+    "spot": false,
+    "cpu": 1,
+    "memory": 1,
+    "gpu": 0,
+    "gpu_model": "standard"
+    }
+}
+```
+
+### Parameters ###
+- `!!env`: !!type, default=!!default value. !!explanation
+
+### Input & Output ###
+Input:
+- `!!file_name`  # !!comments
+
+Output:
+```
+└── output
+    ├── !!file_name_1  # !!comments
+    └── !!file_name_2  # !!comments
+```
+
+### Maintainer ###
+
+! your email address.
+
+'''
+    return txt.replace('momo_PJ_name', project_name)

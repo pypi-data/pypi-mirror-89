e883928be445baaf5bf3b95e3b2fccc3c14727fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:37 +0000

    Add package simple-gpg/0.1/simple-gpg-0.1.tar.gz

---
+++ packages/simple-gpg/simple-gpg-0.1.tar.gz/simple-gpg-0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'simple-gpg',         
+  packages = ['simple_gpg'],   
+  version = '0.1',    
+  license='GPLv3',        
+  description = 'A simple program to make use of GNU Privacy Guard for Cryptographical use.', 
+  long_description='A simple Commandline Program Developed Using Python3 to make GNU Privacy Guard Program easy to use for beginners.',
+  author = 'Anish M',                  
+  author_email = 'aneesh25861@gmail.com',     
+  url = 'https://github.com/Anish-M-code/simple-gpg',      
+  keywords = ['gnupg','openpgp','pgp','gpg','encryption', 'cryptography','student project'],  
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: End Users/Desktop',
+    'Topic :: Security :: Cryptography',
+  ],
+)
+

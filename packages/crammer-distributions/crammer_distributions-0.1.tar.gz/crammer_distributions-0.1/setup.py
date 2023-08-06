b8cdaa70c2e01774fcab5cfca183fbef76d7b73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:59 +0000

    Add package crammer-distributions/0.1/crammer_distributions-0.1.tar.gz

---
+++ packages/crammer-distributions/crammer_distributions-0.1.tar.gz/crammer_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='crammer_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['crammer_distributions'],
+      author = 'Mujeeb Lawal',
+      author_email = 'reachmujeebhere@gmail.com',
+      zip_safe=False)

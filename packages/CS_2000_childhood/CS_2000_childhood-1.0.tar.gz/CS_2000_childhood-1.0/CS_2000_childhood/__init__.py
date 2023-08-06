b1956ae716eff0e5672289842ed7072921f148c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:11 +0000

    Add package CS_2000_childhood/1.0/CS_2000_childhood-1.0.tar.gz

---
+++ packages/CS_2000_childhood/CS_2000_childhood-1.0.tar.gz/CS_2000_childhood-1.0/CS_2000_childhood/__init__.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author: Administrator
+@file: __init__.py.py
+@time: 2020/12/24
+@desc:
+"""
---
+++ packages/CS_2000_childhood/CS_2000_childhood-1.0.tar.gz/CS_2000_childhood-1.0/CS_2000_childhood/accelerate.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author: Administrator
+@file: accelerate.py
+@time: 2020/12/24
+@desc:
+"""

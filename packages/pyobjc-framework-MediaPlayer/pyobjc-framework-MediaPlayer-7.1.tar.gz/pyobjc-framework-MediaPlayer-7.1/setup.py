f87a974dd31a265f4a3ef5e21e188955aadc4a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:38 +0000

    Add package pyobjc-framework-MediaPlayer/7.1/pyobjc-framework-MediaPlayer-7.1.tar.gz

---
+++ packages/pyobjc-framework-MediaPlayer/pyobjc-framework-MediaPlayer-7.1.tar.gz/pyobjc-framework-MediaPlayer-7.1/setup.py
@@ -0,0 +1,25 @@
+"""
+Wrappers for the "MediaPlayer" framework on macOS.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+
+from pyobjc_setup import setup
+
+VERSION = '7.1'
+
+setup(
+    name="pyobjc-framework-MediaPlayer",
+    description="Wrappers for the framework MediaPlayer on macOS",
+    min_os_level="10.12",
+    packages=["MediaPlayer"],
+    version=VERSION,
+    install_requires=[
+        "pyobjc-core>=" + VERSION,
+        "pyobjc-framework-AVFoundation>=" + VERSION,
+    ],
+    long_description=__doc__,
+)

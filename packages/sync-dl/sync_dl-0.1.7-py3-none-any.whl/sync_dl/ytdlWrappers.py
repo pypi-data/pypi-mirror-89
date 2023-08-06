01a56d5728e0fc8cc39221b70254fd5ce95fe529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:33 +0000

    Add package sync-dl/0.1.7/sync_dl-0.1.7-py3-none-any.whl

---
+++ packages/sync-dl/sync_dl-0.1.7-py3-none-any.whl/sync_dl/ytdlWrappers.py
@@ -0,0 +1,70 @@
+import youtube_dl
+import os
+
+import subprocess
+import shutil
+
+import sync_dl.config as cfg
+
+#ids are the unique part of each videos url
+def getIDs(playlistUrl):
+    try:
+        params={"extract_flat": True, "quiet": True}
+        with youtube_dl.YoutubeDL(params) as ydl:
+            result = ydl.extract_info(playlistUrl,download=False)
+            ids = []
+            for videoData in result['entries']:
+                ids.append(videoData["id"])
+        return ids
+    except:
+        return []
+
+
+def getIdsAndTitles(playlistUrl):
+    '''
+    used to check for corrupted metadata in integration tests
+    Title will differ from what is on youtube because it is sanitized for use in filenames
+    '''
+    command = f'youtube-dl --get-filename -o "%(title)s\n%(id)s" {playlistUrl}'
+    idsAndTitles = subprocess.getoutput(command).split('\n')
+    numSongs = int(len(idsAndTitles)/2)
+    titles = [idsAndTitles[2*i] for i in range(numSongs)]
+    ids = [idsAndTitles[2*i+1] for i in range(numSongs)]
+
+    return ids,titles
+
+def getTitle(url):
+    '''
+    used to check for corrupted metadata in integration tests
+    Title will differ from what is on youtube because it is sanitized for use in filenames
+    '''
+    command = f'youtube-dl --no-playlist --get-filename -o "%(title)s" {url}'
+    title = subprocess.getoutput(command)
+    return title
+
+def downloadID(videoId, path,numberStr, embedThumbnail=True):
+    url = f"https://www.youtube.com/watch?v={videoId}"
+    
+    if not os.path.exists(cfg.tmpDownloadPath):
+        os.mkdir(cfg.tmpDownloadPath)
+    
+    cfg.params["outtmpl"] = f'{cfg.tmpDownloadPath}/{numberStr}_%(title)s.%(ext)s'
+    
+    with youtube_dl.YoutubeDL(cfg.params) as ydl:
+
+        #ensures tmp is empty
+        tmp = os.listdir(path=cfg.tmpDownloadPath) 
+        for f in tmp:
+            os.remove(f"{cfg.tmpDownloadPath}/{f}")
+
+        try:
+            ydl.download([url])
+
+            tmp = os.listdir(path=cfg.tmpDownloadPath) 
+            shutil.move(f"{cfg.tmpDownloadPath}/{tmp[0]}", path)
+
+
+            return True
+        except:
+            cfg.logger.info(f"Song No longer avalible at {url}")
+            return False

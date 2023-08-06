19ec77dd8aec82e90c1fc0ba757491ca09bd273d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:28 +0000

    Add package pypthon/0.3.2/pypthon-0.3.2.tar.gz

---
+++ packages/pypthon/pypthon-0.3.2.tar.gz/pypthon-0.3.2/pypthon/__init__.py
@@ -0,0 +1,61 @@
+import argparse
+import sys
+
+# TODO Figure out why I need different imports for pip and for testing
+try:
+    from .parser import parse_command
+    from .environment import *
+except ImportError:
+    from parser import parse_command
+    from environment import *
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Python with pipes")
+    parser.add_argument("command", help="the pypthon command")
+    parser.add_argument(
+        "-s", "--show-python", help="show python code generated", action="store_true"
+    )
+    parser.add_argument(
+        "-i",
+        "--import",
+        help="module to import eg: -i 'requests' -i 'numpy'",
+        default=[],
+        action="append",
+    )
+    parser.add_argument(
+        "-d", "--dry-run", help="don't run python code", action="store_true"
+    )
+    parser.add_argument(
+        "-e",
+        "--environment",
+        help="show variables excluding -i imports in environment",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    python = parse_command(args.command)
+    if args.show_python or args.dry_run:
+        print(python)
+    code = "\n".join([get_user_custom_setup(), python])
+    for module in getattr(args, "import"):
+        import_string = f"import {module}"
+        if not args.dry_run:
+            exec(import_string, globals())
+    if args.environment:
+        pprint(sorted(v for v in globals() if v[:1] != "_"))
+    if not args.dry_run:
+        exec(code)
+
+
+def get_user_custom_setup() -> str:
+    pyprc = Path.home() / ".pypthonrc.py"
+    pyprc.touch()
+    with open(pyprc, "r") as fin:
+        return fin.read()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
---
+++ packages/pypthon/pypthon-0.3.2.tar.gz/pypthon-0.3.2/setup.py
@@ -0,0 +1,58 @@
+import sys
+
+import setuptools
+
+# Thank you @KentoNishi for this setup.py
+
+try:
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+except:
+    long_description = ""
+
+try:
+    with open("requirements.txt", "r") as reqs:
+        requirements = reqs.read().split("\n")
+except:
+    requirements = ""
+
+with open("pypthon/__main__.py", "r") as fin:
+    main_module = fin.read()
+
+with open("pypthon/__init__.py", "r") as fin:
+    previous_init = fin.read()
+
+with open("pypthon/__init__.py", "w+") as fout:
+    fout.write(main_module)
+
+try:
+    if "refs/tags/v" in sys.argv[1]:
+        versionName = sys.argv[1].replace("refs/tags/v", "")
+        del sys.argv[1]
+    else:
+        raise Exception
+except:
+    versionName = "0.1.0"
+
+setuptools.setup(
+    name="pypthon",
+    version=versionName,
+    author="Ronak Badhe",
+    author_email="ronak.badhe@gmail.com",
+    description=long_description.split("\n")[1],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/r2dev2bb8/Pypthon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"console_scripts": ["pyp=pypthon:main"]},
+    python_requires=">=3.5",
+    install_requires=requirements,
+)
+
+with open("pypthon/__init__.py", "w+") as fout:
+    fout.write(previous_init)

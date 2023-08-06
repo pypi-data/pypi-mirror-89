6d2f36c52e32883a092c370a99d8b891c9b069f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:23 +0000

    Add package pystonkslib/0.0.1a0/pystonkslib-0.0.1a0.tar.gz

---
+++ packages/pystonkslib/pystonkslib-0.0.1a0.tar.gz/pystonkslib-0.0.1a0/setup.py
@@ -0,0 +1,26 @@
+"""Setup configuration and dependencies for pystonkslib."""
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as req_file:
+    REQUIREMENTS = req_file.readlines()
+
+COMMANDS = [
+    "example_command=pystonkslib.example:main",
+    "update_tickers=pystonkslib.tools.update_tickers:main",
+]
+
+setup(
+    name="pystonkslib",
+    version="0.0.1.alpha0",
+    author="Micheal Taylor",
+    author_email="bubthegreat@gmail.com",
+    url="https://gitlab.com/bubthegreat/pystonkslib",
+    include_package_data=True,
+    description="This is a stonks data pulling library",
+    packages=find_packages("src"),
+    package_dir={"": "src",},
+    python_requires=">=3.6.6",
+    entry_points={"console_scripts": COMMANDS},
+    install_requires=REQUIREMENTS,
+)

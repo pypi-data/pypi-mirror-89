ddb7964904ada2b0347ba0c6ba5c8ec146327836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:39 +0000

    Add package Ege2021naizi/0.1.1/Ege2021naizi-0.1.1.tar.gz

---
+++ packages/Ege2021naizi/Ege2021naizi-0.1.1.tar.gz/Ege2021naizi-0.1.1/Ege2021naizi/__init__.py
@@ -0,0 +1,8 @@
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
+class ege():
+    def __init__(self):
+        self.k = 123
+
+    def vivod(self):
+        print(self.k)
---
+++ packages/Ege2021naizi/Ege2021naizi-0.1.1.tar.gz/Ege2021naizi-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Ege2021naizi',
+      version='0.1.1',
+      description='Ege po informatike na izi',
+      packages=['Ege2021naizi'],
+      author_email='gta5kostya@mail.ru',
+      zip_safe=False)

1a02487574937b3bbc4810198bfe1192a2213c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:46 +0000

    Add package imphook_py/0.0/imphook_py-0.0.tar.gz

---
+++ packages/imphook_py/imphook_py-0.0.tar.gz/imphook_py-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(name="imphook_py",
+      version="0.0",
+      description="""imphook extensions for Python source transformations""",
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT')

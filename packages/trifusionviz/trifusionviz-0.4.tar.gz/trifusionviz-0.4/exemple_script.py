e75eaa7abdb70863d48462b7bd179c1b91d76920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package trifusionviz/0.4/trifusionviz-0.4.tar.gz

---
+++ packages/trifusionviz/trifusionviz-0.4.tar.gz/trifusionviz-0.4/exemple_script.py
@@ -0,0 +1,23 @@
+import trifusionviz as tfv
+import random
+
+liste = list(range(13))
+random.shuffle(liste)
+
+t = tfv.trifusionviz(liste)
+# sortie pdf
+t.sortie("exemple_sortie")
+
+u = tfv.trifusionviz(liste)
+u.fonction_ordre = lambda x, y: str(x) < str(y)
+u.noirblanc = True
+u.profondeurs_cachees = range(2, 2 + 7)
+u.forme_diviser, u.forme_arreter, u.forme_combiner = \
+    "invhouse", "rectangle", "house"
+# sortie png
+u.sortie("exemple_sortie_lexico", "png")
+
+v = tfv.trifusionviz(range(10))
+v.style = "invis"
+# sortie png
+v.sortie("exemple_formes", "png")

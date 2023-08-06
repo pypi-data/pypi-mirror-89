7b7600a1cb1408ba4a316130230a7ec2bde1ea20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:09 +0000

    Add package pytnm/1.0.1/pytnm-1.0.1.tar.gz

---
+++ packages/pytnm/pytnm-1.0.1.tar.gz/pytnm-1.0.1/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="pytnm",
+    version="1.0.1",
+    description="FHWA TNM 2.5 Geospatial Toolkit",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fstraw/pytnm",
+    author="Brandon Batt",
+    author_email="brbatt@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["openpyxl", "pyshp"],
+)

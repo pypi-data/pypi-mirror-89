e01312f3435497a1908ecdc650660d5b41f190da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:47 +0000

    Add package flightradar24/0.3/flightradar24-0.3.tar.gz

---
+++ packages/flightradar24/flightradar24-0.3.tar.gz/flightradar24-0.3/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('long_description.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    package_license = f.read()
+
+setup(
+    name='flightradar24',
+    version='0.3',
+    description='Data library for Flight Radar 24',
+    long_description_content_type='text/x-rst',
+    long_description=readme,
+    author='Mehmet Korkmaz',
+    author_email='mehmet@mkorkmaz.com',
+    url='https://github.com/mkorkmaz/flightradar24',
+    license="MIT",
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=["requests"]
+)
+

904d389d590bdf81121604055f39a3312b6fd481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:55 +0000

    Add package s3-bucket/1.0.0/s3-bucket-1.0.0.tar.gz

---
+++ packages/s3-bucket/s3-bucket-1.0.0.tar.gz/s3-bucket-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="s3-bucket",
+    version="1.0.0",
+    author="CodeLighthouse",
+    author_email="hello@codelighthouse.io",
+    description="An easy to use client for S3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CodeLighthouse/s3-bucket",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=[
+        'boto3'
+    ]
+)

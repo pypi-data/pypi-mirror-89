72c291036f36938c5b5a51ca8baa112a3c223963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:02 +0000

    Add package splitres/1.3/splitres-1.3.tar.gz

---
+++ packages/splitres/splitres-1.3.tar.gz/splitres-1.3/setup.py
@@ -0,0 +1,13 @@
+"""File for build project in python-packet"""
+
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+setup(
+    name='splitres',
+    version='1.3',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    url='https://gitwork.ru/barabass/splitres.git',
+    author='German Borovkov'
+)

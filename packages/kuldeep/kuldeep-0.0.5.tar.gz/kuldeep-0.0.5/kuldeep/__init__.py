87a05c81cd2a790a8aa0d7ce879cd887aa4f0062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:22 +0000

    Add package kuldeep/0.0.5/kuldeep-0.0.5.tar.gz

---
+++ packages/kuldeep/kuldeep-0.0.5.tar.gz/kuldeep-0.0.5/kuldeep/__init__.py
@@ -0,0 +1,20 @@
+r"""
+
+"""
+
+import django
+
+__title__ = 'Django kuldeep'
+__version__ = '0.0.5'
+__author__ = 'kuldeep khatana'
+__license__ = 'BSD 3-Clause'
+__copyright__ = 'Copyright 2011-2019 Encode OSS Ltd'
+
+# Version synonym
+VERSION = __version__
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = 'iso-8859-1'
+
+# Default datetime input and output formats
+ISO_8601 = 'iso-8601'

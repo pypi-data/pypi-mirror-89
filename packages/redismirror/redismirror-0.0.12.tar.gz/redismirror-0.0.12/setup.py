11efd4627d219065892f848d80de46b7e785d68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:19 +0000

    Add package redismirror/0.0.12/redismirror-0.0.12.tar.gz

---
+++ packages/redismirror/redismirror-0.0.12.tar.gz/redismirror-0.0.12/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from redismirror.core.version import get_version
+
+VERSION = get_version()
+
+f = open("README.md", "r")
+LONG_DESCRIPTION = f.read()
+f.close()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="redismirror",
+    version=VERSION,
+    description="Mirror Redis Traffic to another redis node",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    setup_requires=requirements,
+    author="Ali Saleh Baker",
+    author_email="alivxlive@gmail.com",
+    url="https://github.com/alivx/redis-mirror",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup", "tests*"]),
+    package_data={"redismirror": ["templates/*"]},
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        redismirror = redismirror.main:main
+    """,
+)

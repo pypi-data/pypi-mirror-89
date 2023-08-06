5d1bfc8bf249f236a31f77c2d772eba7d3ab48bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:03 +0000

    Add package learns2/0.0.18/learns2-0.0.18.tar.gz

---
+++ packages/learns2/learns2-0.0.18.tar.gz/learns2-0.0.18/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="learns2", # Replace with your own username
+    version="0.0.18",
+    author="Joe Pringle",
+    author_email="joe@joepringle.dev",
+    description="For use with https://learns2.joepringle.dev",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joechip504/learns2/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'mpyq', 's2protocol', 'numpy'
+    ]
+)

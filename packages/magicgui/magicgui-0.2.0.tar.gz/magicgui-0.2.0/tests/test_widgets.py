e17b03dffc54aec426ce152260642ab8c40434b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:52 +0000

    Add package magicgui/0.2.0/magicgui-0.2.0.tar.gz

---
+++ packages/magicgui/magicgui-0.2.0.tar.gz/magicgui-0.2.0/tests/test_widgets.py
@@ -0,0 +1,35 @@
+import pytest
+
+from magicgui import magicgui, widgets
+
+
+@pytest.mark.parametrize(
+    "WidgetClass", [getattr(widgets, n) for n in widgets.__all__ if n != "Widget"]
+)
+def test_widgets(WidgetClass):
+    """Test that we can retrieve getters, setters, and signals for most Widgets."""
+    _ = WidgetClass()
+
+
+def test_autocall_no_runtime_error():
+    """Make sure changing a value doesn't cause an autocall infinite loop."""
+
+    @magicgui(auto_call=True, result_widget=True)
+    def func(input=1):
+        return round(input, 4)
+
+    func.input.value = 2
+
+
+def test_delete_widget():
+    """We can delete widgets from containers."""
+    a = widgets.Label(name="a")
+    container = widgets.Container(widgets=[a])
+    # we can delete widgets
+    del container.a
+    with pytest.raises(AttributeError):
+        getattr(container, "a")
+
+    # they disappear from the layout
+    with pytest.raises(ValueError):
+        container.index(a)

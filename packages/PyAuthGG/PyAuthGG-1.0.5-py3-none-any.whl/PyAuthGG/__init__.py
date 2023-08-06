7c8be57e49cced78e19feb4a62a9ecb650091797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:39 +0000

    Add package PyAuthGG/1.0.5/PyAuthGG-1.0.5-py3-none-any.whl

---
+++ packages/PyAuthGG/PyAuthGG-1.0.5-py3-none-any.whl/PyAuthGG/__init__.py
@@ -0,0 +1,13 @@
+from .Administration import *
+from .Application import *
+from .Exceptions import *
+
+Version = "1.0.5"
+Author = "xFueY"
+Source = "https://github.com/xFueY/PyAuthGG"
+Changelog = """Version 1.0.5 - 28th December 2020
+- Added ReadMe Content
+- Fixed ResetPassword
+- Fixed ChangePassword"""
+
+HEADERS = {"Content-Type" : "application/x-www-form-urlencoded", "User-Agent" : f"PyAuthGG {Version}"}

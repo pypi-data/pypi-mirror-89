9ee1b2395ae968f358e87928ee1077ff3b617bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:01 +0000

    Add package SaRaiPrank/0.2/SaRaiPrank-0.2.tar.gz

---
+++ packages/SaRaiPrank/SaRaiPrank-0.2.tar.gz/SaRaiPrank-0.2/SaRaiPrank/Shake.py
@@ -0,0 +1,36 @@
+
+
+import pyautogui as pg
+import time
+import webbrowser as web
+
+markx = 132
+marky = 698
+class Prank:
+
+    def __init__(self,name):
+        self.name = name #ใส่ไว้กันมันrunเองไม่ได้ใช้
+
+    def Earthquake(markx,marky):
+        
+        for load in range(10):
+            time.sleep(0.02)
+            print("*"*(load+1),10 * (load+1),"%")
+
+        print('Let The Fun Begin!')
+
+        while True:
+            for i in range(50):
+                pg.moveTo(markx + i,marky - i)
+    
+    def Open(url):
+        for i in range(400):
+            link = url
+            web.open(link)
+            print(i+1)
+
+Prank.Earthquake(markx,marky)
+Prank.Open('www.google.com')
+
+
+

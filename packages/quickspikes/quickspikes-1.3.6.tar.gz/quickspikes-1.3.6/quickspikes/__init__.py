edc0c2686452d52f614986519d13741467dc1922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:13 +0000

    Add package quickspikes/1.3.6/quickspikes-1.3.6.tar.gz

---
+++ packages/quickspikes/quickspikes-1.3.6.tar.gz/quickspikes-1.3.6/quickspikes/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+"""spike detection routines
+
+Copyright (C) 2013 Dan Meliza <dmeliza@gmail.com>
+Created Wed Jul 24 09:26:36 2013
+"""
+
+__version__ = "1.2.0"
+from .spikes import detector, peaks, subthreshold
+from .tools import realign_spikes, filter_times

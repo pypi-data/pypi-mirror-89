ff919fb4f2d94d9b966268956695c71a251e8975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:08 +0000

    Add package spaceone-secret/1.5.3rc1/spaceone_secret-1.5.3rc1-py3-none-any.whl

---
+++ packages/spaceone-secret/spaceone_secret-1.5.3rc1-py3-none-any.whl/spaceone/secret/conf/global_conf.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+DATABASES = {
+    'default': {
+        'db': 'secret',
+        'host': 'localhost',
+        'port': 27017,
+        'username': '',
+        'password': ''
+    }
+}
+
+CACHES = {
+    'default': {},
+    'local': {
+        'backend': 'spaceone.core.cache.local_cache.LocalCache',
+        'max_size': 128,
+        'ttl': 86400
+    }
+}
+
+HANDLERS = {}
+
+CONNECTORS = {
+    'IdentityConnector': {},
+    'AWSSecretManagerConnector': {},
+    'VaultConnector': {
+#        url = 'http://vault:8200',
+#        token = 'myroot'
+    }
+}
+
+ENDPOINTS = {}
+
+LOG = {}
+

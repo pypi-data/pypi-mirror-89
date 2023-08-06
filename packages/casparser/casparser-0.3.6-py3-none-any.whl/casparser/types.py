d459bd9909eef53e49f4255c0cdd13c411cf357c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:45 +0000

    Add package casparser/0.3.6/casparser-0.3.6-py3-none-any.whl

---
+++ packages/casparser/casparser-0.3.6-py3-none-any.whl/casparser/types.py
@@ -0,0 +1,71 @@
+from datetime import date
+from decimal import Decimal
+from typing import List, TypedDict, Union
+
+from .enums import FileType
+
+StatementPeriod = TypedDict("StatementPeriod", {"from": str, "to": str})
+
+
+class InvestorInfoType(TypedDict):
+    """Investor Info data structure."""
+
+    name: str
+    email: str
+    address: str
+    mobile: str
+
+
+class TransactionType(TypedDict):
+    """Mutual fund scheme transaction."""
+
+    date: Union[date, str]
+    description: str
+    amount: Union[Decimal, float]
+    units: Union[Decimal, float]
+    nav: Union[Decimal, float]
+    balance: Union[Decimal, float]
+
+
+class SchemeValuationType(TypedDict):
+    """Scheme valuation as of a given date."""
+
+    date: Union[date, str]
+    nav: Union[Decimal, float]
+    value: Union[Decimal, float]
+
+
+class SchemeType(TypedDict, total=False):
+    """Mutual Fund Scheme data structure."""
+
+    scheme_id: int
+    scheme: str
+    advisor: str
+    rta_code: str
+    rta: str
+    open: Union[Decimal, float]
+    close: Union[Decimal, float]
+    valuation: SchemeValuationType
+    transactions: List[TransactionType]
+
+
+class FolioType(TypedDict, total=False):
+    """Mutual Fund Folio data structure."""
+
+    folio_id: int
+    folio: str
+    amc: str
+    amc_id: int
+    PAN: str
+    KYC: str
+    PANKYC: str
+    schemes: List[SchemeType]
+
+
+class CASParserDataType(TypedDict):
+    """CAS Parser return data type."""
+
+    statement_period: StatementPeriod
+    folios: List[FolioType]
+    investor_info: InvestorInfoType
+    file_type: FileType

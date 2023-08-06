6e19d4a935eddf45a983d4ab0c73a0767455db53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:34 +0000

    Add package wagtail-personalisation-molo/3.0.3/wagtail-personalisation-molo-3.0.3.tar.gz

---
+++ packages/wagtail-personalisation-molo/wagtail-personalisation-molo-3.0.3.tar.gz/wagtail-personalisation-molo-3.0.3/src/wagtail_personalisation/utils.py
@@ -0,0 +1,114 @@
+import time
+
+from django.template.base import FilterExpression, kwarg_re
+from django.utils import timezone
+
+
+def impersonate_other_page(page, other_page):
+    """Function to change the page metadata so the user gets to see the
+    non-personalized path and page.
+
+    :param page: The page to be impersonated
+    :type page: wagtail_personalisation.models.PersonalisablePage
+    :param other_page: The page it should impersonate
+    :type other_page: wagtail_personalisation.models.PersonalisablePage
+
+    """
+    page.path = other_page.path
+    page.depth = other_page.depth
+    page.url_path = other_page.url_path
+    page.title = other_page.title
+
+
+def create_segment_dictionary(segment):
+    """Creates a dictionary with all the required segment information.
+
+    :param segment: Segment object
+    :type segment: wagtail_personalisation.models.Segment
+    :return: Dictionary with name, id, timestamp and persistent state.
+    :rtype: dict
+
+    """
+    return {
+        "encoded_name": segment.encoded_name(),
+        "id": segment.pk,
+        "timestamp": int(time.time()),
+        "persistent": segment.persistent
+    }
+
+
+def count_active_days(enable_date, disable_date):
+    """Return the number of days the segment has been active.
+
+    :param enable_date: The date the segment was enabled
+    :type enable_date: timezone.datetime
+    :param disable_date: The date the segment was disabled
+    :type disable_date: timezone.datetime
+    :returns: The amount of days a segment is/has been active
+    :rtype: int
+
+    """
+    if enable_date is not None:
+        if disable_date is None or disable_date <= enable_date:
+            # There is no disable date, or it is not relevant.
+            delta = timezone.now() - enable_date
+            return delta.days
+        if disable_date > enable_date:
+            # There is a disable date and it is relevant.
+            delta = disable_date - enable_date
+            return delta.days
+
+    return 0
+
+
+def parse_tag(token, parser):
+    """Parses template tag for name, arguments and keyword arguments.
+
+    :param token: Template token containing all the tag contents
+    :type token: django.template.base.Token
+    :param parser: Template parser
+    :type parser: django.template.base.Parser
+    :return: Tuple with tag name, arguments and keyword arguments
+    :rtype: tuple
+
+    """
+    # Split the tag content into words, respecting quoted strings.
+    bits = token.split_contents()
+
+    # Pull out the tag name.
+    tag_name = bits.pop(0)
+
+    # Parse the rest of the args, and build FilterExpressions from them so that
+    # we can evaluate them later.
+    args = []
+    kwargs = {}
+    for bit in bits:
+        # Is this a kwarg or an arg?
+        match = kwarg_re.match(bit)
+        kwarg_format = match and match.group(1)
+        if kwarg_format:
+            key, value = match.groups()
+            kwargs[key] = FilterExpression(value, parser)
+        else:
+            args.append(FilterExpression(bit, parser))
+
+    return (tag_name, args, kwargs)
+
+
+def exclude_variants(pages):
+    """Checks if page is not a variant
+
+    :param pages: List | Queryset of pages to check
+    :type pages: list or querset
+    :return: List|Queryset of pages that aren't variants
+    :rtype: list or queryset (depending on the param type)
+    """
+    for page in pages:
+        if hasattr(page, 'personalisation_metadata') is not False and \
+                page.personalisation_metadata is not None and \
+                page.personalisation_metadata.is_canonical is not True:
+            if (type(pages) == list):
+                pages.remove(page)
+            else:
+                pages = pages.exclude(pk=page.pk)
+    return pages
---
+++ packages/wagtail-personalisation-molo/wagtail-personalisation-molo-3.0.3.tar.gz/wagtail-personalisation-molo-3.0.3/src/wagtail_personalisation/wagtail_hooks.py
@@ -0,0 +1,217 @@
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+from django.urls import reverse
+from django.conf.urls import include, re_path
+from django.utils.safestring import mark_safe
+from django.template.defaultfilters import pluralize
+from django.utils.translation import ugettext_lazy as _
+from wagtail.admin.site_summary import PagesSummaryItem, SummaryItem
+from wagtail.admin.widgets import Button, ButtonWithDropdownFromHook
+from wagtail.core import hooks
+from wagtail.core.models import Page
+
+from wagtail_personalisation import admin_urls, models, utils
+from wagtail_personalisation.adapters import get_segment_adapter
+
+logger = logging.getLogger(__name__)
+
+
+@hooks.register('register_admin_urls')
+def register_admin_urls():
+    """Adds the administration urls for the personalisation apps."""
+    return [
+        re_path(r'^personalisation/', include(
+            (admin_urls, 'wagtail_personalisation'),
+            namespace='wagtail_personalisation')),
+    ]
+
+
+@hooks.register('before_serve_page')
+def set_visit_count(page, request, serve_args, serve_kwargs):
+    """Tests the provided rules to see if the request still belongs
+    to a segment.
+
+    :param page: The page being served
+    :type page: wagtail.core.models.Page
+    :param request: The http request
+    :type request: django.http.HttpRequest
+
+    """
+    adapter = get_segment_adapter(request)
+    adapter.add_page_visit(page)
+
+
+@hooks.register('before_serve_page')
+def segment_user(page, request, serve_args, serve_kwargs):
+    """Apply a segment to a visitor before serving the page.
+
+    :param page: The page being served
+    :type page: wagtail.core.models.Page
+    :param request: The http request
+    :type request: django.http.HttpRequest
+
+    """
+    adapter = get_segment_adapter(request)
+    adapter.refresh()
+
+
+@hooks.register('before_serve_page')
+def serve_variant(page, request, serve_args, serve_kwargs):
+    """Apply a segment to a visitor before serving the page.
+
+    :param page: The page being served
+    :type page: wagtail.core.models.Page
+    :param request: The http request
+    :type request: django.http.HttpRequest
+    :returns: A variant if one is available for the visitor's segment,
+              otherwise the original page
+    :rtype: wagtail.core.models.Page
+
+    """
+    user_segments = []
+    if not isinstance(page, models.PersonalisablePageMixin):
+        return
+
+    adapter = get_segment_adapter(request)
+    user_segments = adapter.get_segments()
+
+    if user_segments:
+        metadata = page.personalisation_metadata
+
+        # TODO: This is never more then one page? (fix query count)
+        metadata = metadata.metadata_for_segments(user_segments)
+        if metadata:
+            variant = metadata.first().variant.specific
+            return variant.serve(request, *serve_args, **serve_kwargs)
+
+
+@hooks.register('construct_explorer_page_queryset')
+def dont_show_variant(parent_page, pages, request):
+    return utils.exclude_variants(pages)
+
+
+@hooks.register('register_page_listing_buttons')
+def page_listing_variant_buttons(page, page_perms, is_parent=False):
+    """Adds page listing buttons to personalisable pages. Shows variants for
+    the page (if any) and a 'Create a new variant' button.
+
+    """
+    if not isinstance(page, models.PersonalisablePageMixin):
+        return
+
+    metadata = page.personalisation_metadata
+    if metadata.is_canonical:
+        yield ButtonWithDropdownFromHook(
+            _('Variants'),
+            hook_name='register_page_listing_variant_buttons',
+            page=page,
+            page_perms=page_perms,
+            is_parent=is_parent,
+            attrs={'target': '_blank', 'title': _('Create or edit a variant')},
+            priority=100)
+
+
+@hooks.register('register_page_listing_variant_buttons')
+def page_listing_more_buttons(page, page_perms, is_parent=False):
+    """Adds a 'more' button to personalisable pages allowing users to quickly
+    create a new variant for the selected segment.
+
+    """
+    if not isinstance(page, models.PersonalisablePageMixin):
+        return
+
+    metadata = page.personalisation_metadata
+
+    for vm in metadata.variants_metadata:
+        yield Button('%s variant' % (vm.segment.name),
+                     reverse('wagtailadmin_pages:edit', args=[vm.variant_id]),
+                     attrs={"title": _('Edit this variant')},
+                     classes=("icon", "icon-fa-pencil"),
+                     priority=0)
+
+    for segment in metadata.get_unused_segments():
+        yield Button('%s variant' % (segment.name),
+                     reverse('segment:copy_page', args=[page.pk, segment.pk]),
+                     attrs={"title": _('Create this variant')},
+                     classes=("icon", "icon-fa-plus"),
+                     priority=100)
+
+    yield Button(_('Create a new segment'),
+                 reverse('wagtail_personalisation_segment_modeladmin_create'),
+                 attrs={"title": _('Create a new segment')},
+                 classes=("icon", "icon-fa-snowflake-o"),
+                 priority=200)
+
+
+class CorrectedPagesSummaryPanel(PagesSummaryItem):
+    def get_context(self):
+        context = super(CorrectedPagesSummaryPanel, self).get_context()
+
+        pages = utils.exclude_variants(Page.objects.all().specific())
+
+        context['total_pages'] = len(pages) - 1
+        return context
+
+
+@hooks.register('construct_homepage_summary_items')
+def add_corrected_pages_summary_panel(request, items):
+    """Replaces the Pages summary panel to hide variants."""
+    for index, item in enumerate(items):
+        if item.__class__ is PagesSummaryItem:
+            items[index] = CorrectedPagesSummaryPanel(request)
+
+
+class SegmentSummaryPanel(SummaryItem):
+    """The segment summary panel showing the total amount of segments on the
+    site and allowing quick access to the Segment dashboard.
+
+    """
+    order = 2000
+
+    def render(self):
+        segment_count = models.Segment.objects.count()
+        target_url = reverse('wagtail_personalisation_segment_modeladmin_index')
+        title = _("Segments")
+        return mark_safe("""
+            <li class="icon icon-fa-snowflake-o">
+                <a href="{}"><span>{}</span>{}</a>
+            </li>""".format(target_url, segment_count, title))
+
+
+class PersonalisedPagesSummaryPanel(PagesSummaryItem):
+    order = 2100
+
+    def render(self):
+        page_count = models.PersonalisablePageMetadata.objects.filter(segment__isnull=True).count()
+        title = _("Personalised Page")
+        return mark_safe("""
+            <li class="icon icon-fa-file-o">
+                <span>{}</span>{}{}
+            </li>""".format(page_count, title, pluralize(page_count)))
+
+
+class VariantPagesSummaryPanel(PagesSummaryItem):
+    order = 2200
+
+    def render(self):
+        page_count = models.PersonalisablePageMetadata.objects.filter(
+            segment__isnull=False).count()
+        title = _("Variant")
+        return mark_safe("""
+                <li class="icon icon-fa-files-o">
+                    <span>{}</span>{}{}
+                </li>""".format(page_count, title, pluralize(page_count)))
+
+
+@hooks.register('construct_homepage_summary_items')
+def add_personalisation_summary_panels(request, items):
+    """Adds a summary panel to the Wagtail dashboard showing the total amount
+    of segments on the site and allowing quick access to the Segment
+    dashboard.
+
+    """
+    items.append(SegmentSummaryPanel(request))
+    items.append(PersonalisedPagesSummaryPanel(request))
+    items.append(VariantPagesSummaryPanel(request))

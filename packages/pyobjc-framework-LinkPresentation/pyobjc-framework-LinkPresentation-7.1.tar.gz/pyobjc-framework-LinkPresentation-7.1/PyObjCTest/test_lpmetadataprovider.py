688b8be4ceae44cd1f6555508d9fbdfea519006f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:38 +0000

    Add package pyobjc-framework-LinkPresentation/7.1/pyobjc-framework-LinkPresentation-7.1.tar.gz

---
+++ packages/pyobjc-framework-LinkPresentation/pyobjc-framework-LinkPresentation-7.1.tar.gz/pyobjc-framework-LinkPresentation-7.1/PyObjCTest/test_lpmetadataprovider.py
@@ -0,0 +1,8 @@
+from PyObjCTools.TestSupport import TestCase
+
+import LinkPresentation
+
+
+class TestLPMetadataProvider(TestCase):
+    def test_methods(self):
+        LinkPresentation.LPMetadataProvider

4689f10dbeee304972dcf84ec3bcca57536a0684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:53 +0000

    Add package md2zhihu/0.1.10/md2zhihu-0.1.10-py3-none-any.whl

---
+++ packages/md2zhihu/md2zhihu-0.1.10-py3-none-any.whl/md2zhihu/__init__.py
@@ -0,0 +1,13 @@
+"""
+package-name is utility to create sub process.
+
+"""
+
+__version__ = "0.1.10"
+__name__ = "md2zhihu"
+
+from . import md2zhihu
+from . import mistune
+from .md2zhihu import main
+from .md2zhihu import AssetRepo
+from .md2zhihu import Config

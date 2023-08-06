dc19dce1466ecd92564e604f0b506349a441c643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:44 +0000

    Add package imbDRL/2020.12.29.1/imbDRL-2020.12.29.1-py3-none-any.whl

---
+++ packages/imbDRL/imbDRL-2020.12.29.1-py3-none-any.whl/imbDRL/examples/ddqn/train_aki.py
@@ -0,0 +1,45 @@
+import os
+
+from imbDRL.agents.ddqn import TrainDDQN
+from imbDRL.data import get_train_test_val, load_aki
+from imbDRL.utils import rounded_dict
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # CPU is faster than GPU on structured data
+
+episodes = 25_000  # Total number of episodes
+warmup_episodes = 32_000  # Amount of warmup steps to collect data with random policy
+memory_length = warmup_episodes  # Max length of the Replay Memory
+batch_size = 32
+collect_steps_per_episode = 2000
+collect_every = episodes // 100
+
+target_model_update = episodes // 30  # Period to overwrite the target Q-network with the default Q-network
+target_update_tau = 1  # Soften the target model update
+n_step_update = 4
+
+conv_layers = None  # Convolutional layers
+dense_layers = (256, 256, )  # Dense layers
+dropout_layers = (0.2, 0.2, )  # Dropout layers
+
+lr = 0.001  # Learning rate
+gamma = 0.0  # Discount factor
+min_epsilon = 0.5  # Minimal and final chance of choosing random action
+decay_episodes = episodes // 10  # Number of episodes to decay from 1.0 to `min_epsilon`
+
+imb_rate = 0.2318  # Imbalance rate
+min_class = [1]  # Minority classes
+maj_class = [0]  # Majority classes
+X_train, y_train, X_test, y_test, = load_aki(normalization=True)
+X_train, y_train, X_test, y_test, X_val, y_val = get_train_test_val(X_train, y_train, X_test, y_test,
+                                                                    min_class, maj_class, val_frac=0.2)
+
+model = TrainDDQN(episodes, warmup_episodes, lr, gamma, min_epsilon, decay_episodes, target_model_update=target_model_update,
+                  target_update_tau=target_update_tau, batch_size=batch_size, collect_steps_per_episode=collect_steps_per_episode,
+                  memory_length=memory_length, collect_every=collect_every, n_step_update=n_step_update)
+
+model.compile_model(X_train, y_train, imb_rate, conv_layers, dense_layers, dropout_layers)
+model.train(X_val, y_val, "F1")
+
+stats = model.evaluate(X_test, y_test, X_train, y_train)
+print(rounded_dict(stats))
+# {'Gmean': 0.736675, 'F1': 0.514218, 'Precision': 0.396468, 'Recall': 0.731461, 'TP': 651, 'TN': 2849, 'FP': 991, 'FN': 239}
---
+++ packages/imbDRL/imbDRL-2020.12.29.1-py3-none-any.whl/imbDRL/utils.py
@@ -0,0 +1,90 @@
+import os
+
+import numpy as np
+import pandas as pd
+import tensorflow_probability as tfp
+from sklearn.model_selection import train_test_split
+
+
+def split_csv(fp: str = "./data/creditcard.csv", fp_dest: str = "./data",
+              name: str = "credit", test_size: int = 0.5, strat_col: str = "Class") -> None:
+    """Splits a csv file in two, in a stratified fashion.
+    Format for filenames will be `{name}0.csv and `{name}1.csv`.
+
+    :param fp: The path at which the csv file is located.
+    :type  fp: str
+    :param fp_dest: The path to save the train and test files.
+    :type  fp_dest: str
+    :param name: The prefix for the files.
+    :type  name: str
+    :param test_size: The fraction of total size for the test file.
+    :type  test_size: float
+    :param strat_col: The column in the original csv file to stratify.
+
+    :return: None, two files located at `fp_dest`.
+    :rtype: NoneType
+    """
+    if not os.path.isfile(fp):
+        raise FileNotFoundError(f"File at {fp} does not exist.")
+    if not os.path.isdir(fp_dest):
+        raise ValueError(f"Directory at {fp_dest} does not exist.")
+    if not 0 < test_size < 1:
+        raise ValueError(f"{test_size} is not in interval 0 < x < 1.")
+
+    df = pd.read_csv(fp)
+
+    if not (strat_col in df.columns):
+        raise ValueError(f"Stratify column {strat_col} not found in DataFrame.")
+
+    train, test = train_test_split(df, test_size=test_size, stratify=df[strat_col])
+
+    train.to_csv(f"{fp_dest}/{name}0.csv", index=False)
+    test.to_csv(f"{fp_dest}/{name}1.csv", index=False)
+
+
+def get_reward_distribution(imb_rate: float):
+    """Generates reward distribution based on given imbalance rate.
+
+    From the documentation of tf_agents.bandits.environments.ClassificationBanditEnvironment:
+
+    reward_distribution: a `tfd.Distribution` with event_shape
+        `[num_classes, num_actions]`. Entry `[i, j]` is the reward for taking
+        action `j` for an instance of class `i`.
+
+    The environment will always have 2 classes with each 2 actions.
+    Array[0][0] corresponds to action 0 on class 0, thus the majority class, thus True Negative
+    Array[0][1]: False Positive reward
+    Array[1][0]: False Negative reward
+    Array[1][1]: True Positive reward
+
+    :param imb_rate: The imbalance rate of the data: len(min_class) / len(maj_class)
+    :type  imb_rate: float
+
+    :return: Combined Independent distribution
+    :rtype: tensorflow_probability.Distribution
+    """
+    if not 0 < imb_rate < 1:
+        raise ValueError(f"{imb_rate} is not in interval 0 < x < 1.")
+
+    bernoulli = tfp.distributions.Bernoulli(probs=[[0, 0], [0, 0]], dtype=np.float32)
+    combined = (tfp.bijectors.Shift([[imb_rate, -imb_rate], [-1, 1]])
+                (tfp.bijectors.Scale([[1, 1], [1, 1]])
+                 (bernoulli)))
+    return tfp.distributions.Independent(combined, reinterpreted_batch_ndims=2)
+
+
+def rounded_dict(d: dict, precision: int = 6) -> dict:
+    """Rounds all values in a dictionairy to `precision` digits after the decimal point.
+
+    :param d: Dictionairy containing only floats or ints as values
+    :type  d: dict
+
+    :return: Rounded dictionairy
+    :rtype: dict
+    """
+    return {k: round(v, precision) for k, v in d.items()}
+
+
+def imbalance_ratio(y: np.array) -> float:
+    """Calculates imbalance ratio of positive class [1] and negative class [0]."""
+    return (y.sum() / (y.shape - y.sum()))[0]

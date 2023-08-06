c3017a5b7cac51e5b1da9e58b412567d2c18f81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:56 +0000

    Add package python-telnet-vlc/2.0.1/python-telnet-vlc-2.0.1.tar.gz

---
+++ packages/python-telnet-vlc/python-telnet-vlc-2.0.1.tar.gz/python-telnet-vlc-2.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-telnet-vlc",
+    version="2.0.1",
+    author="stephenmac7, rodripf, BilliAlpha, David McClosky",
+    author_email="rodripf@gmail.com",
+    description="A library to control or get information about VLC using Telnet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rodripf/python-telnet-vlc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Topic :: Multimedia"
+    ],
+)

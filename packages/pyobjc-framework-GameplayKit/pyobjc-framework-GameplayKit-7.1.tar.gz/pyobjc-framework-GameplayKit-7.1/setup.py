b051d641f3f89b40a5eb7f2264ff476fe7c098fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:37 +0000

    Add package pyobjc-framework-GameplayKit/7.1/pyobjc-framework-GameplayKit-7.1.tar.gz

---
+++ packages/pyobjc-framework-GameplayKit/pyobjc-framework-GameplayKit-7.1.tar.gz/pyobjc-framework-GameplayKit-7.1/setup.py
@@ -0,0 +1,42 @@
+"""
+Wrappers for the "GameplayKit" framework on macOS.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+
+import os
+
+from pyobjc_setup import Extension, setup
+
+VERSION = '7.1'
+
+setup(
+    name="pyobjc-framework-GameplayKit",
+    description="Wrappers for the framework GameplayKit on macOS",
+    min_os_level="10.11",
+    packages=["GameplayKit"],
+    ext_modules=[
+        Extension(
+            "GameplayKit._GameplayKit",
+            ["Modules/_GameplayKit.m"],
+            extra_link_args=["-framework", "GameplayKit"],
+            py_limited_api=True,
+            depends=[
+                os.path.join("Modules", fn)
+                for fn in os.listdir("Modules")
+                if fn.startswith("_GameplayKit")
+            ],
+        )
+    ],
+    version=VERSION,
+    install_requires=[
+        "pyobjc-core>=" + VERSION,
+        "pyobjc-framework-Cocoa>=" + VERSION,
+        "pyobjc-framework-SpriteKit>=" + VERSION,
+    ],
+    long_description=__doc__,
+    options={"bdist_wheel": {"py_limited_api": "cp36"}},
+)

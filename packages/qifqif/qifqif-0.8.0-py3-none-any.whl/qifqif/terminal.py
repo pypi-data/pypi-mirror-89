291a519c57799e55db6f7b1ebfb77192b58ca30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:31 +0000

    Add package qifqif/0.8.0/qifqif-0.8.0-py3-none-any.whl

---
+++ packages/qifqif/qifqif-0.8.0-py3-none-any.whl/qifqif/terminal.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017-2020 Fabrice Laporte - kray.me
+# The MIT License http://www.opensource.org/licenses/mit-license.php
+
+"""Wrapper around terminal capabilities"""
+
+import sys
+from contextlib import contextmanager
+
+
+class Terminus(object):
+    """blessed Terminal ersatz for Windows exhibiting minimum features set"""
+
+    OK = u"+"
+    KO = u"x"
+
+    @property
+    def clear(self):
+        return "\r"
+
+    def __getattr__(self, name):
+        def handler(*args, **kwargs):
+            return "%s" % (args or "")
+
+        if "clear" in name:
+            return self.clear
+        if "move" in name:
+            return lambda x: ""
+        return handler
+
+    def ljust(self, field, pad_width, fillchar):
+        return field.ljust(pad_width - len(field), fillchar)
+
+    @contextmanager
+    def fullscreen(self):
+        yield None
+
+    @contextmanager
+    def location(self):
+        yield None
+
+
+try:
+    from blessed import Terminal as BlessedTerminal
+
+    class Terminal(BlessedTerminal):
+        OK = u"✔"
+        KO = u"✖"
+
+        @property
+        def clear_last(self):
+            return self.move_up + self.move_x(0) + self.clear_eol
+
+        @property
+        def undo(self):
+            return 2 * self.clear
+
+    TERM = Terminal()
+
+except ImportError:
+    TERM = Terminus()
+
+    if sys.platform == "win32":
+        import codecs
+
+        codecs.register(
+            lambda name: codecs.lookup("utf-8") if name == "cp65001" else None
+        )
+
+        # use colorama to support "ANSI" terminal colors.
+        try:
+            import colorama
+        except ImportError:
+            pass
+        else:
+            colorama.init()
---
+++ packages/qifqif/qifqif-0.8.0-py3-none-any.whl/qifqif/ui.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017-2020 Fabrice Laporte - kray.me
+# The MIT License http://www.opensource.org/licenses/mit-license.php
+
+"""Utilities functions related to terminal display."""
+
+from difflib import SequenceMatcher
+from itertools import chain, combinations
+
+from qifqif.terminal import TERM
+
+
+def colorize_match(t, field, matches=None):
+    field_val = t[field]
+    if not field_val:
+        return None
+    match = matches.get(field, "") if matches else ""
+    seqmatch = SequenceMatcher(None, field_val, match)
+    a, b, size = seqmatch.find_longest_match(0, len(field_val), 0, len(match))
+    return field_val[:a] + TERM.green(field_val[a : a + size]) + field_val[a + size :]
+
+
+def complete_matches(payee):
+    """Generate a limited set of matches for payee line.
+
+    >>> complete_matches("foo bar spam")
+    ['foo', 'bar', 'spam', 'foo bar spam', 'bar spam', 'spam']
+    """
+    matches = []
+    tokens = payee.split()
+    sublists = chain(*(combinations(tokens, i) for i in range(len(tokens) + 1)))
+    for x in sublists:
+        match = " ".join(x)
+        if match and (match in payee):
+            matches.append(match)
+    return matches

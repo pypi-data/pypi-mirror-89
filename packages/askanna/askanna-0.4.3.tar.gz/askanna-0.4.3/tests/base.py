9fa020b78917bfd6f72c7b74484e21e6982aba9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:17 +0000

    Add package askanna/0.4.3/askanna-0.4.3.tar.gz

---
+++ packages/askanna/askanna-0.4.3.tar.gz/askanna-0.4.3/tests/base.py
@@ -0,0 +1,16 @@
+import unittest
+
+from click.testing import CliRunner
+
+from askanna.cli.tool import cli
+
+
+class BaseCLItest(unittest.TestCase):
+    verb = ""
+
+    def setUp(self):
+        self.result = CliRunner().invoke(cli, ['--help', self.verb, ])
+
+    def testCommandLineAccess(self):
+        assert self.result.exit_code == 0
+        assert len(self.result.output) > 0

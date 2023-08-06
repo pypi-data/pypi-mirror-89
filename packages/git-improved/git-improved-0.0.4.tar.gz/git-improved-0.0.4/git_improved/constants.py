d61367de913b2a3a5c07caffdeb7a37f994ac290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:38 +0000

    Add package git-improved/0.0.4/git-improved-0.0.4.tar.gz

---
+++ packages/git-improved/git-improved-0.0.4.tar.gz/git-improved-0.0.4/git_improved/constants.py
@@ -0,0 +1,15 @@
+
+BRANCHES_PREFIXES = {
+    'bugfix': 'Bugfix',
+    'fix': 'Bugfix', 
+    'documentation': 'Documentation', 
+    'doc': 'Documentation',
+    'feature': 'Feature', 
+    'structure': 'Structure',
+    'devops': 'CI/CD',
+    'ci': 'CI/CD',
+    'cd': 'CI/CD',
+    'enhancement': 'Enhancement',
+    'improve': 'Enhancement',
+    'improvement': 'Enhancement'
+}

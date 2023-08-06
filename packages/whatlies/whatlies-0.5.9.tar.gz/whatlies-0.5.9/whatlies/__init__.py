ddcc791666422b7f30abfdb06ed2a768ab8025e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:17 +0000

    Add package whatlies/0.5.9/whatlies-0.5.9.tar.gz

---
+++ packages/whatlies/whatlies-0.5.9.tar.gz/whatlies-0.5.9/whatlies/__init__.py
@@ -0,0 +1,10 @@
+"""
+Whatlies library
+"""
+
+__version__ = "0.5.9"
+
+from whatlies.embeddingset import EmbeddingSet
+from whatlies.embedding import Embedding
+
+__all__ = ["Embedding", "EmbeddingSet"]

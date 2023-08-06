92f17f8373e0032ef27a0c5bed425462bed2ed98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:43 +0000

    Add package prospr/0.1a4/prospr-0.1a4.tar.gz

---
+++ packages/prospr/prospr-0.1a4.tar.gz/prospr-0.1a4/prospr/core/Protein/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+File:           build.py
+Description:    This file compiles the Protein C++ files into a Python modules.
+"""
+
+from setuptools import setup, Extension
+import os
+
+
+path = os.path.dirname(__file__)
+
+Protein_module = Extension("_Protein",
+                           sources=[f"{path}/Protein_wrap.cxx",
+                                    f"{path}/Protein.cpp"],
+                           )
+
+setup(name="Protein",
+      version="0.1",
+      author="okkevaneck",
+      description="C++ core compiled into a Python module.",
+      ext_modules=[Protein_module],
+      py_modules=["Protein"],
+      license="LICENSE",
+      )

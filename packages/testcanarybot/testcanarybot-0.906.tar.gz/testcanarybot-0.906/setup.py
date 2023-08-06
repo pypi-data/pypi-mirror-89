a6afb1b08c01819a2ea2dd0f2b249f3286e5aef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:02 +0000

    Add package testcanarybot/0.906/testcanarybot-0.906.tar.gz

---
+++ packages/testcanarybot/testcanarybot-0.906.tar.gz/testcanarybot-0.906/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testcanarybot",
+    version="0.906",
+    author="andprokofieff",
+    author_email="prokofieff.help@gmail.com",
+    description="VKontakte asynchronous bot framework like Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kensoi/testcanarybot",
+    packages=["testcanarybot",
+              "testcanarybot.source"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ["aiohttp"],
+    python_requires='>=3.7',
+)
---
+++ packages/testcanarybot/testcanarybot-0.906.tar.gz/testcanarybot-0.906/testcanarybot/source/versions_list.py
@@ -0,0 +1,19 @@
+class static:
+    """
+    multisupport all versions of testcanarybot that have "static" version object
+    """
+
+main = [0.801, 0.802, 0.9, 0.906]
+beta_versions = {
+    0.9: [0.85 + 0.001 * i for i in range(1, 9)]
+}
+
+beta = []
+
+for i in beta_versions.keys(): beta.extend(beta_versions[i])
+
+supporting = [*main, *beta]
+supporting.sort()
+supporting = [static, *supporting]
+
+current = supporting[-1]

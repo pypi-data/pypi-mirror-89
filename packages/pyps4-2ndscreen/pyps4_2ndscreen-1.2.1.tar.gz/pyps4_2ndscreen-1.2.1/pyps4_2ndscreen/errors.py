d909d9107a0abfdb1850010e4fc415bbaa3a9414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:56 +0000

    Add package pyps4-2ndscreen/1.2.1/pyps4_2ndscreen-1.2.1.tar.gz

---
+++ packages/pyps4-2ndscreen/pyps4_2ndscreen-1.2.1.tar.gz/pyps4_2ndscreen-1.2.1/pyps4_2ndscreen/errors.py
@@ -0,0 +1,25 @@
+"""Exceptions Definitions."""
+
+
+class CredentialTimeout(Exception):
+    """Recieved no credentials or timed out."""
+
+
+class LoginFailed(Exception):
+    """PS4 Failed Login."""
+
+
+class NotReady(Exception):
+    """PS4 no connection."""
+
+
+class PSDataIncomplete(Exception):
+    """PS Store data missing attributes."""
+
+
+class UnknownButton(Exception):
+    """Button not valid."""
+
+
+class UnknownDDPResponse(Exception):
+    """DDP Response is Unknown."""

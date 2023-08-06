e524f2a1bd65faa993a11668f3d560a54b77fbe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:21 +0000

    Add package datacatalog-custom-entries-manager/0.1.2/datacatalog-custom-entries-manager-0.1.2.tar.gz

---
+++ packages/datacatalog-custom-entries-manager/datacatalog-custom-entries-manager-0.1.2.tar.gz/datacatalog-custom-entries-manager-0.1.2/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+setuptools.setup(
+    name='datacatalog-custom-entries-manager',
+    version='0.1.2',
+    url='https://github.com/ricardolsmendes/datacatalog-custom-entries-manager',
+    author='Ricardo Mendes',
+    author_email='ricardolsmendes@gmail.com',
+    license='MIT',
+    description='A package to manage Google Cloud Data Catalog custom entries',
+    platforms='Posix; MacOS X; Windows',
+    packages=setuptools.find_packages(where='./src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'datacatalog-custom-entries = datacatalog_custom_entries_manager:main',
+        ],
+    },
+    include_package_data=True,
+    install_requires=(
+        'google-datacatalog-connectors-commons ~= 0.5.1',
+        'numpy >= 1.19.0, <= 1.19.3',
+        'pandas ~= 1.1.4',
+    ),
+    setup_requires=('pytest-runner', ),
+    tests_require=('pytest-cov', ),
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)

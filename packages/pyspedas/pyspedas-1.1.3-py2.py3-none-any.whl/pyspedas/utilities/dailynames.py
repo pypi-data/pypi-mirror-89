c622772dbce72276c4beac77251b4b3182d5b786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:19 +0000

    Add package pyspedas/1.1.3/pyspedas-1.1.3-py2.py3-none-any.whl

---
+++ packages/pyspedas/pyspedas-1.1.3-py2.py3-none-any.whl/pyspedas/utilities/dailynames.py
@@ -0,0 +1,48 @@
+
+
+import numpy as np
+from pyspedas import time_string, time_double
+import os
+from datetime import datetime
+
+def dailynames(directory='', trange=None, res=24*3600., hour_res=False, file_format='%Y%m%d', prefix='', suffix=''):
+    if trange == None:
+        print('No trange specified')
+        return
+
+    if hour_res == True:
+        res = 3600.
+        file_format = '%Y%m%d%H'
+
+    # allows the user to pass in trange as list of datetime objects
+    if type(trange[0]) == datetime and type(trange[1]) == datetime:
+        trange = [time_string(trange[0].timestamp()), time_string(trange[1].timestamp())]
+
+    tr = [trange[0], trange[1]]
+    
+    if isinstance(trange[0], str):
+        tr[0] = time_double(trange[0])
+    if isinstance(trange[1], str):
+        tr[1] = time_double(trange[1])
+
+    # Davin's magic heisted from file_dailynames in IDL
+    mmtr = [np.floor(tr[0]/res), np.ceil(tr[1]/res)]
+
+    if mmtr[1]-mmtr[0] < 1:
+        n = 1
+    else:
+        n = int(mmtr[1]-mmtr[0])
+
+    times = [(float(num)+mmtr[0])*res for num in range(n)]
+
+    dates = []
+    files = []
+        
+    for time in times:
+        if time_string(time, fmt=file_format) not in dates:
+            dates.append(time_string(time, fmt=file_format))
+            
+    for date in dates:
+        files.append(directory + prefix + date + suffix)
+
+    return files

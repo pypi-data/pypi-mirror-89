5d97e4bd7e1c5fa7be0a53de57e4340b9ee07778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:27 +0000

    Add package symcollab-algebra/0.1.1a0/symcollab-algebra-0.1.1a0.tar.gz

---
+++ packages/symcollab-algebra/symcollab-algebra-0.1.1a0.tar.gz/symcollab-algebra-0.1.1a0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name="symcollab-algebra",
+    version="0.1.1a",
+    packages=find_namespace_packages(include=["symcollab.*"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/symcollab/cryptosolve",
+    install_requires = [
+        "matplotlib~=3.1.1",
+        "mypy~=0.740",
+        "mypy-extensions~=0.4.0",
+        "networkx~=2.3",
+        "numpy~=1.17.0",
+        "sympy==1.4",
+    ]
+)

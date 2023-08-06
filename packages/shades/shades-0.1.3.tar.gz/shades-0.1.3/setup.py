e612addcc0a0215766f814093d12893fce99a255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:16 +0000

    Add package shades/0.1.3/shades-0.1.3.tar.gz

---
+++ packages/shades/shades-0.1.3.tar.gz/shades-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name='shades',
+    version='0.1.3',
+    author='Ben Rutter',
+    description='A Python module for generative 2d image creation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/benrrutter/shades',
+    packages=find_packages(),
+    python_requires='>=3.6',
+) 

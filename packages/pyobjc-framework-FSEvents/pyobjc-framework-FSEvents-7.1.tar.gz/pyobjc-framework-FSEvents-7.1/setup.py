e434a063f5f1ded5c3186f3655e685e5e3182d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:39 +0000

    Add package pyobjc-framework-FSEvents/7.1/pyobjc-framework-FSEvents-7.1.tar.gz

---
+++ packages/pyobjc-framework-FSEvents/pyobjc-framework-FSEvents-7.1.tar.gz/pyobjc-framework-FSEvents-7.1/setup.py
@@ -0,0 +1,27 @@
+"""
+Wrappers for the "FSEvents" API in macOS. The functions in this framework
+allow you to reliably observe changes to the filesystem, even when your
+program is not running al the time.
+
+These wrappers don't include documentation, please check Apple's documention
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+from pyobjc_setup import Extension, setup
+
+VERSION = '7.1'
+
+setup(
+    name="pyobjc-framework-FSEvents",
+    description="Wrappers for the framework FSEvents on macOS",
+    min_os_level="10.5",
+    packages=["FSEvents"],
+    ext_modules=[
+        Extension("FSEvents._callbacks", ["Modules/_callbacks.m"], py_limited_api=True)
+    ],
+    version=VERSION,
+    install_requires=["pyobjc-core>=" + VERSION, "pyobjc-framework-Cocoa>=" + VERSION],
+    long_description=__doc__,
+    options={"bdist_wheel": {"py_limited_api": "cp36"}},
+)

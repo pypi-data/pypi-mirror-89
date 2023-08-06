d2eb7e9cbd0f5cb6087d4a5d8bb7859d4d423e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:16 +0000

    Add package gaus-simdist/0.1/gaus_simdist-0.1.tar.gz

---
+++ packages/gaus-simdist/gaus_simdist-0.1.tar.gz/gaus_simdist-0.1/README.md
@@ -0,0 +1,11 @@
+# Gaus_simdist
+
+Gaussian simulate distribution is a package used to simulate the gaussian distribution.
+
+## Installation
+
+Prerequisites: Python 3.5 or later 
+
+pip install gaus_simdist 
+
+
---
+++ packages/gaus-simdist/gaus_simdist-0.1.tar.gz/gaus_simdist-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaus_simdist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gaus_simdist'],
+      author = ["udemy course"],
+      zip_safe=False)

c961ba7c76270f30ec17f4df3826f19d7a949149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:31 +0000

    Add package get-cover-art/1.3.8/get_cover_art-1.3.8-py3-none-any.whl

---
+++ packages/get-cover-art/get_cover_art-1.3.8-py3-none-any.whl/get_cover_art/meta_mp4.py
@@ -0,0 +1,24 @@
+from .meta_audio import MetaAudio
+from mutagen.mp4 import MP4
+from mutagen.m4a import M4ACover
+
+class MetaMP4(MetaAudio):
+    def __init__(self, path):
+        self.audio_path = path
+        self.audio = MP4(path)
+        try:
+            self.artist = self.audio.tags['©ART'][0]
+            self.album = self.audio.tags['©alb'][0]
+            self.title = self.audio.tags['©nam'][0]
+        except:
+            raise Exception("missing XMP tags")
+    
+    def has_embedded_art(self):
+        return 'covr' in self.audio.tags
+
+    def embed_art(self, art_path):
+        covr = []
+        artworkfile = open(art_path, 'rb').read()
+        covr.append(M4ACover(artworkfile, M4ACover.FORMAT_JPEG))
+        self.audio.tags['covr'] = covr
+        self.audio.save()

d6a9194ded9c1cda2ffe4b1bf607ef3cc93d1686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:25 +0000

    Add package spell/0.41.5/spell-0.41.5-py2.py3-none-any.whl

---
+++ packages/spell/spell-0.41.5-py2.py3-none-any.whl/spell/api/projects_client.py
@@ -0,0 +1,43 @@
+from spell.api import base_client
+from spell.api.utils import url_path_join
+
+
+PROJECT_RESOURCE_URL = "projects"
+
+
+class ProjectsClient(base_client.BaseClient):
+    def create_project(self, proj_req):
+        r = self.request("post", url_path_join(PROJECT_RESOURCE_URL, self.owner), payload=proj_req)
+        self.check_and_raise(r)
+        resp = self.get_json(r)
+        return resp["project"]
+
+    def list_projects(self):
+        r = self.request("get", url_path_join(PROJECT_RESOURCE_URL, self.owner))
+        self.check_and_raise(r)
+        resp = self.get_json(r)
+        return resp["projects"]
+
+    def get_project(self, id):
+        r = self.request("get", url_path_join(PROJECT_RESOURCE_URL, self.owner, id))
+        self.check_and_raise(r)
+        resp = self.get_json(r)
+        return resp["project"]
+
+    def add_runs(self, project_id, run_ids):
+        payload = {"run_ids": run_ids}
+        r = self.request(
+            "post",
+            url_path_join(PROJECT_RESOURCE_URL, self.owner, project_id, "runs"),
+            payload=payload,
+        )
+        self.check_and_raise(r)
+
+    def remove_runs(self, project_id, run_ids):
+        payload = {"run_ids": run_ids}
+        r = self.request(
+            "delete",
+            url_path_join(PROJECT_RESOURCE_URL, self.owner, project_id, "runs"),
+            payload=payload,
+        )
+        self.check_and_raise(r)

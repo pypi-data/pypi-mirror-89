b1bc833f0117f8b0a703e7cb9ff33bd75823afb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:11 +0000

    Add package odoo10-addon-l10n-it-fatturapa-out-wt/10.0.2.0.0/odoo10_addon_l10n_it_fatturapa_out_wt-10.0.2.0.0-py2-none-any.whl

---
+++ packages/odoo10-addon-l10n-it-fatturapa-out-wt/odoo10_addon_l10n_it_fatturapa_out_wt-10.0.2.0.0-py2-none-any.whl/odoo/addons/l10n_it_fatturapa_out_wt/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Author(s): Andrea Colangelo (andreacolangelo@openforce.it)
+# Copyright 2018 Openforce Srls Unipersonale (www.openforce.it)
+# Copyright 2018 Sergio Corato (https://efatto.it)
+# Copyright 2018-2019 Lorenzo Battistini <https://github.com/eLBati>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Italian Localization - Fattura elettronica - Integrazione '
+            'ritenuta',
+    'summary': 'Modulo ponte tra emissione fatture elettroniche e ritenute.',
+    'version': '10.0.2.0.0',
+    'development_status': 'Beta',
+    'category': 'Hidden',
+    'website': 'https://github.com/OCA/l10n-italy',
+    'author': 'Sergio Corato, Odoo Community Association (OCA)',
+    'license': 'LGPL-3',
+    'depends': [
+        'l10n_it_fatturapa',
+        'l10n_it_fatturapa_out',
+        'l10n_it_withholding_tax',
+        'l10n_it_withholding_tax_causali',
+    ],
+    'data': [],
+    'installable': True,
+    'auto_install': True,
+}

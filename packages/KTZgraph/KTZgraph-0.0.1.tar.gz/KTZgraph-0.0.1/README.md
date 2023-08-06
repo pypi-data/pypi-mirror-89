a271f7226ed90d430aecae0e93f8a00ad7b9346c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:04 +0000

    Add package KTZgraph/0.0.1/KTZgraph-0.0.1.tar.gz

---
+++ packages/KTZgraph/KTZgraph-0.0.1.tar.gz/KTZgraph-0.0.1/README.md
@@ -0,0 +1,29 @@
+# KTZgraph
+
+## Installation
+
+Run the following to install:
+
+```python
+pip install KTZgraph
+```
+
+## Usage
+
+```python
+from abstract_algebra import say_hello
+
+# Generate "Hello, World!"
+say_hello()
+
+# Generate "Hello, Everybody!"
+say_hello("Everybody")
+```
+
+# Developing KTZgraph
+
+To install KTZgraph, along with the tools you need to develop and run tests, run the following in your virtualenv:
+
+```bash
+$ pip install -e .[dev]
+```
---
+++ packages/KTZgraph/KTZgraph-0.0.1.tar.gz/KTZgraph-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='KTZgraph',
+    version='0.0.1',
+    description='Say hello!',
+    py_modules=["KTZgraph"],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "blessings ~= 1.7",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=3.7",
+        ],
+    },
+    url="https://github.com/pawlaczyk/KTZgraph",
+    author="Dominika Pawlaczyk",
+    autor_email="dominika.pawlaczyk9@gmail.com"
+)

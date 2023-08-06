7627fcd5fd37cbcdc6a7fc5ca72990be0f064218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:33 +0000

    Add package pypitools/0.0.70/pypitools-0.0.70.tar.gz

---
+++ packages/pypitools/pypitools-0.0.70.tar.gz/pypitools-0.0.70/README.rst
@@ -0,0 +1,17 @@
+===========
+*pypitools*
+===========
+
+.. image:: https://img.shields.io/pypi/v/pypitools
+
+.. image:: https://img.shields.io/github/license/veltzer/pypitools
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+
+project website: https://veltzer.github.io/pypitools
+
+author: Mark Veltzer
+
+version: 0.0.70
+
+
---
+++ packages/pypitools/pypitools-0.0.70.tar.gz/pypitools-0.0.70/pypitools/process_utils.py
@@ -0,0 +1,31 @@
+import logging
+import subprocess
+import sys
+from typing import List, Tuple
+
+from pypitools import LOGGER_NAME
+
+
+def check_call_collect(args: List[str]) -> Tuple[str, str]:
+    """
+    Run a process and check that it returns an OK return code
+    Gather any output and return it to the caller.
+    If the process returns an error then print the output and
+    the errors.
+    :param args:
+    """
+    logger = get_logger()
+    logger.debug("running %s", args)
+    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE,)
+    (res_stdout, res_stderr) = process.communicate()
+    if process.returncode:
+        print(res_stdout.decode(), end="", file=sys.stdout)
+        print(res_stderr.decode(), end="", file=sys.stderr)
+        raise ValueError(
+            f"exit code from [{' '.join(args)}] was [{process.returncode}]"
+        )
+    return res_stdout.decode(), res_stderr.decode()
+
+
+def get_logger():
+    return logging.getLogger(LOGGER_NAME)

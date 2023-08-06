d6fb79f9da45b76eff0c3842fccdb0bfd9fa523e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:00 +0000

    Add package nacla/0.0.3/nacla-0.0.3.tar.gz

---
+++ packages/nacla/nacla-0.0.3.tar.gz/nacla-0.0.3/setup.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name = 'nacla',
+
+    version = '0.0.3',
+
+    keywords = ('name classify', 'nlp'),
+
+    description = '输入一个姓名，输出这个姓名输入哪个国家',
+
+    license = 'MIT',
+
+    author = 'zhaomingming',
+
+    author_email = '13271929138@163.com',
+
+    packages = find_packages(),
+
+    platforms = 'python>=3.6.7',
+
+    py_modules=['nacla.predict']
+
+)
+

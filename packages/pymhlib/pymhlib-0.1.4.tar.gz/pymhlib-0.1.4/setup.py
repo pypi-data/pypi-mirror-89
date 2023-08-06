3fd90ce3a3c3016d90431d2456c86b99b19f82f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:23 +0000

    Add package pymhlib/0.1.4/pymhlib-0.1.4.tar.gz

---
+++ packages/pymhlib/pymhlib-0.1.4.tar.gz/pymhlib-0.1.4/setup.py
@@ -0,0 +1,33 @@
+"""Setup program for installing pymhlib.
+
+To create upload a new version to PyPI:
+    - pip install --upgrade pysetup, wheel, twine
+    - Document changes in README.md
+    - Increment version also below
+    - python setup.py sdist bdist_wheel
+    - twine upload dist/*
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymhlib",
+    version="0.1.4",
+    author="Günther Raidl et al.",
+    author_email="raidl@ac.tuwien.ac.at",
+    description="pymhlib - a toolbox for metaheuristics and hybrid optimization methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ac-tuwien/pymhlib",
+    license='GPL3',
+    packages=setuptools.find_packages(),
+    package_data={"pymhlib":['demos/data/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

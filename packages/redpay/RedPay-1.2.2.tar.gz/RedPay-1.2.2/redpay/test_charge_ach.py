7a7c389b3b2398d0a6224e6b2bee19c8b9887045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:27 +0000

    Add package redpay/1.2.2/RedPay-1.2.2.tar.gz

---
+++ packages/redpay/RedPay-1.2.2.tar.gz/RedPay-1.2.2/redpay/test_charge_ach.py
@@ -0,0 +1,24 @@
+
+import json
+
+from redpay import RedPay
+
+if __name__ == "__main__":
+    redpay = RedPay("DEMO",
+                    "MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAtsQxNp3vmKVNYIxfWSi0LIRgCnPaMn0MUNouxgrs4zmg4cnvSeQ3I8YP03YbpXuWA80RvOw/nWErYAKomniJw8Y+xexMfBQ5sgJgewn3ZnRPNM9Y4Z62gwfIlsrs7Bwvpz9uUtLgeQLl1ffNaumnu1IBrqRps0EZ1QyDuu41UckTyo31C40Wez6IbeMfZeusrmPlIWqyBacdviJ5zHCA3zHNq86QMnB8HOP1U81HOSs6GTTelhD7lCoJ+fHKHxcz0MDr37fNpKpC57B0/20wBXFp9tlVtSkHcIty1lyNk2/HDH8knCdqkZk+fCvWgGwdex41x8/rM+LKC13c5J/yG6Gb2PnKhwNk4lvvnz73YAdqTUJ7qNrdtWVnOTWfbMBiNlpBCVqt8xY8UK6u83AVWrWXse0xe2Pn/kRqlXmxWT0mGEoCavjvZ9lQUL7LXAXZ1dff9r+oFUZo6xDQ3ER/OTIKa4jpvaI9S/J1drsrI1f9kkMWFwEh48dCPYplGSxzAgMBAAE=",
+                    "redpaystable.azurewebsites.net")
+
+    request = {
+        "account": "1234567890",
+        "routing": "121122676",
+        "amount": 1200,
+        "accountHolder": "Anna Conda",
+    }
+
+    print("CHARGE ACH request >>>")
+    print(json.dumps(request, indent=2))
+
+    response = redpay.ChargeACH(request)
+
+    print("CHARGE ACH response >>>")
+    print(json.dumps(response, indent=2))

de06eb433a30a13df71e73414d05b2aabf0cc278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:03 +0000

    Add package pvsfunc/3.9.2/pvsfunc-3.9.2.tar.gz

---
+++ packages/pvsfunc/pvsfunc-3.9.2.tar.gz/pvsfunc-3.9.2/pvsfunc/helpers.py
@@ -0,0 +1,182 @@
+import mimetypes
+import os
+import shutil
+import subprocess
+
+from pymediainfo import MediaInfo
+
+
+def anti_file_prefix(path: str) -> str:
+    """Remove prefixed 'file://' from path"""
+    if path.lower().startswith("file://"):
+        path = path[7:]
+        if os.name == "nt":
+            # windows sometimes adds an extra leading /
+            path = path.lstrip("/")
+    return path
+
+
+def get_mime_type(file_path: str) -> str:
+    """Get file mime-type based on file contents or extension"""
+    # initialise mime-types, let it load all mimes
+    mimetypes.init()
+    # get the file extension
+    file_ext = os.path.splitext(file_path)[-1].lower()
+    # check for special file types if theres no mime type
+    if file_ext not in mimetypes.types_map:
+        # check if the file is a D2V/DGIndexProjectFile
+        with open(file_path, mode="rb") as f:
+            if f.read(18) == "DGIndexProjectFile".encode("utf-8"):
+                if f.read(2) != bytes([0x31, 0x36]):
+                    raise ValueError(
+                        "pvsfunc.get_file_type: D2V was created with an unsupported indexer, please use DGIndex v1.5.8." +
+                        (" It works perfectly fine under Wine." if os.name != "nt" else "")
+                    )
+                return "video/d2v"
+        # DVD-Video Object File
+        if file_ext == ".vob":
+            return "video/vob"
+        # PSP UMD-VIDEO file
+        if file_ext == ".mps":
+            return "video/umd"
+        raise ValueError(f"pvsfunc.get_file_type: Unrecognised file extension ({file_ext})")
+    mime_type = mimetypes.types_map[file_ext]
+    # ensure that the mime is a video or image file
+    if not mime_type.startswith("video/") and not mime_type.startswith("image/"):
+        raise ValueError(f"pvsfunc.get_file_type: Only Video or Image files are supported. ({mime_type})")
+    # return the mime
+    return mime_type
+
+
+def get_video_codec(file_path: str) -> str:
+    """Get video codec using MediaInfo"""
+    if not os.path.exists(file_path) or not os.path.isfile(file_path):
+        return "?"
+    track = [t for t in MediaInfo.parse(
+        filename=file_path
+    ).tracks if t.track_type in ["Video", "Image"]]
+    if not track:
+        raise ValueError("No video/image track in file...")
+    track = track[0]
+    # we try both as in some cases codec_id isn't set
+    codec = track.codec_id or track.commercial_name
+    # do some squashing to reduce amount of code repetition
+    return {
+        "MPEG-1 Video": "V_MPEG1",
+        "MPEG-2 Video": "V_MPEG2"
+    }.get(codec, codec)
+
+
+def get_d2v(file_path: str) -> str:
+    """Demux video track and generate a D2V file for it if needed"""
+    IS_VOB = os.path.splitext(file_path)[-1].lower() == ".vob"
+    # create file_path location of the d2v path
+    d2v_path = f"{os.path.splitext(file_path)[0]}.d2v"
+    if os.path.exists(d2v_path):
+        print("Skipping generation as a D2V file already exists")
+        return d2v_path
+    # demux the mpeg stream if needed
+    vid_path = file_path
+    if not IS_VOB:
+        if os.path.splitext(file_path)[-1].lower() != ".mpeg":
+            vid_path = f"{os.path.splitext(file_path)[0]}.mpg"
+        if os.path.exists(vid_path):
+            print("Skipping demuxing of raw mpeg stream as it already exists")
+        else:
+            mkvextract_path = shutil.which("mkvextract")
+            if not mkvextract_path:
+                raise RuntimeError(
+                    "pvsfunc.PSourcer: Required binary 'mkvextract' not found. "
+                    "Install MKVToolNix and make sure it's binaries are in the environment path."
+                )
+            subprocess.run([
+                mkvextract_path, os.path.basename(file_path),
+                # todo ; this assumes the track with track-id of 0 is the video, not ideal
+                "tracks", f"0:{os.path.basename(vid_path)}"
+            ], cwd=os.path.dirname(file_path))
+    # use dgindex to create a d2v file for the demuxed track
+    dgindex_path = shutil.which("DGIndex.exe") or shutil.which("dgindex.exe")
+    if not dgindex_path:
+        raise RuntimeError(
+            "pvsfunc.PSourcer: This video file will need a required binary 'DGIndex.exe' which isn't found.\n"
+            "tl-dr; add DGIndex.exe to your system path. Ensure the executable is named exactly `DGIndex.exe`.\n"
+            "Windows: Start Menu -> Environment Variables, Add DGIndex's folder to `PATH` variable.\n"
+            "Linux: append to $PATH in /etc/profile, I recommend using `nano /etc/profile.d/env.sh`. Must reboot."
+        )
+    args = []
+    if dgindex_path.startswith("/"):
+        args.extend(["wine", "start", "/wait", "Z:" + dgindex_path])
+    else:
+        args.extend([dgindex_path])
+    args.extend([
+        "-ai" if IS_VOB else "-i", os.path.basename(vid_path),
+        "-ia", "5",  # iDCT Algorithm, 5=IEEE-1180 Reference
+        "-fo", "2",  # Field Operation, 2=Ignore Pulldown Flags
+        "-yr", "1",  # YUV->RGB, 1=PC Scale
+        "-om", "0",  # Output Method, 0=None (just d2v)
+        "-hide", "-exit",  # start hidden and exit when saved
+        "-o", os.path.splitext(os.path.basename(file_path))[0]
+    ])
+    subprocess.run(args, cwd=os.path.dirname(file_path))
+    # edit the video path of the d2v file if on linux
+    if dgindex_path.startswith("/"):
+        d2v_content = []
+        with open(d2v_path, "rt", encoding="utf-8") as f:
+            d2v_content = f.read().splitlines()
+        d2v_content = [(x[2:].replace("\\", "/") if x.startswith("Z:\\") else x) for x in d2v_content]
+        with open(d2v_path, "wt", encoding="utf-8") as f:
+            f.write("\n".join(d2v_content))
+    # return file path of the new d2v file
+    return d2v_path
+
+
+def remove_container_fps(file_path: str) -> str:
+    """Remove container-set FPS to only have the encoded FPS"""
+    mi = [x for x in MediaInfo.parse(file_path).tracks if x.track_type == "Video"]
+    if len(mi) == 0:
+        # does not have a video track?
+        return file_path
+    mi = mi[0]
+
+    if mi.original_frame_rate is None:
+        # does not have a container fps
+        return file_path
+
+    fps_fix_path = f"{file_path}.fpsfix.mkv"
+    if os.path.exists(fps_fix_path):
+        # an fps fix was already run on this file, re-use
+        return fps_fix_path
+
+    if mi.framerate_original_num and mi.framerate_original_den:
+        original_fps = f"{mi.framerate_original_num}/{mi.framerate_original_den}"
+    else:
+        original_fps = mi.original_frame_rate
+
+    subprocess.check_output([
+        "mkvmerge", "--output", fps_fix_path,
+        "--default-duration", f"{mi.track_id - 1}:{original_fps}fps",
+        file_path
+    ], cwd=os.path.dirname(file_path))
+
+    return fps_fix_path
+
+
+def gcd(a, b):
+    """The GCD (greatest common divisor) is the highest number that evenly divides both width and height."""
+    return a if b == 0 else gcd(b, a % b)
+
+
+def calculate_aspect_ratio(width: int, height: int) -> str:
+    """Calculate the aspect-ratio gcd string from resolution"""
+    r = gcd(width, height)
+    return f"{int(width / r)}:{int(height / r)}"
+
+
+def calculate_par(width: int, height: int, aspect_ratio_w: int, aspect_ratio_h: int) -> str:
+    """Calculate the pixel-aspect-ratio string from resolution"""
+    par_w = height * aspect_ratio_w
+    par_h = width * aspect_ratio_h
+    par_gcd = gcd(par_w, par_h)
+    par_w = int(par_w / par_gcd)
+    par_h = int(par_h / par_gcd)
+    return f"{par_w}:{par_h}"
---
+++ packages/pvsfunc/pvsfunc-3.9.2.tar.gz/pvsfunc-3.9.2/pvsfunc/pdeinterlacer.py
@@ -0,0 +1,187 @@
+import functools
+
+import mvsfunc
+from vapoursynth import core, VideoNode, ColorFamily
+
+try:
+    import havsfunc
+except ImportError:
+    raise RuntimeError(
+        "pvsfunc.PDeinterlacer: Required script havsfunc not found. "
+        "https://github.com/HomeOfVapourSynthEvolution/havsfunc"
+    )
+
+
+class PDeinterlacer:
+    """
+    PDeinterlacer (PHOENiX Deinterlacer)
+    Deinterlaces a clip with the most optimal wrapping based on the sourcer.
+    The clip will need to be loaded from PSourcer to work as it needs it's Props.
+    """
+
+    def __init__(self, clip, kernel=None, kernel_args=None, debug=False):
+        self.clip = clip
+        self.kernel = kernel
+        self.kernel_args = kernel_args or {}
+        self.debug = debug
+        # validate arguments
+        if not isinstance(self.clip, VideoNode):
+            raise TypeError("pvsfunc.PDeinterlacer: This is not a clip")
+        # set default kernel to QTGMC
+        if not self.kernel:
+            self.kernel = havsfunc.QTGMC
+        # set handler func based on Sourcer
+        self.handler = None
+        sourcer = self.clip.get_frame(0).props["PVSSourcer"].decode("utf-8")
+        if sourcer == "core.d2v.Source":
+            self.handler = self._d2v
+        elif sourcer == "core.ffms2.Source":
+            if self.kernel == havsfunc.QTGMC:
+                kernel_args["FPSDivisor"] = 2  # only supporting same-rate fps atm
+            self.handler = self._ffms2
+        elif sourcer == "core.lsmas.LWLibavSource":
+            if self.kernel == havsfunc.QTGMC:
+                kernel_args["FPSDivisor"] = 2  # only supporting same-rate fps atm
+            self.handler = self._lsmash
+        elif sourcer == "core.imwri.Read":
+            print("pvsfunc.PDeinterlacer: Warning: This source is a clip of images and cannot be deinterlaced.")
+            self.handler = lambda c: c  # do nothing
+        else:
+            raise ValueError(f"Unimplemented deinterlacer for Sourcer {sourcer}")
+        self.clip = self.handler(self.clip)
+
+    def _get_kernel(self, clip) -> tuple:
+        """
+        Apply the deinterlacing kernel to the provided clip for both
+        TFF and BFF output. The Kernel function will be provided a 
+        True/False value to the "TFF" argument if any.
+
+        Expecting a TFF argument (case-insensitive), it must be present.
+        If the function truly doesn't need it (what??) then wrap it in a lambda, e.g.
+        lambda clip, TFF: yourWeirdFunc(clip)
+
+        Returns 2 clips, one for TFF operations, and one for BFF operations
+        """
+        field_order_arg = [x for x in self.kernel.__code__.co_varnames if x.lower() == "tff"]
+        if field_order_arg:
+            return (
+                self.kernel(clip, **{**self.kernel_args, field_order_arg[0]: True}),
+                self.kernel(clip, **{**self.kernel_args, field_order_arg[0]: False})
+            )
+        clip = self.kernel(clip, **self.kernel_args)
+        return clip, clip
+
+    def _d2v(self, clip):
+        """
+        Very accurate deinterlacing using raw frame metadata to know what to
+        deinterlace when necessary. It even fixes the frame rates of progressive
+        streams and converts VFR to CFR when necessary.
+
+        For MPEG2, this is as good as it gets in terms of using a deinterlacer.
+        """
+        # 1. create a clip from the output of the kernel deinterlacer
+        deinterlaced_tff, deinterlaced_bff = self._get_kernel(clip)
+        fps_factor = deinterlaced_tff.fps.numerator / deinterlaced_tff.fps.denominator
+        fps_factor = fps_factor / (clip.fps.numerator / clip.fps.denominator)
+        if fps_factor != 1.0 and fps_factor != 2.0:
+            raise ValueError(
+                f"pvsfunc.PDeinterlacer: The deinterlacer kernel returned an unsupported frame-rate ({deinterlaced_tff.fps}). "
+                "Only single-rate and double-rate is supported with PDeinterlacer at the moment."
+            )
+
+        # 2. deinterlace whats interlaced
+        def _d(n, f, c, d_tff, d_bff, ff):
+            # compile debug information to print if requested
+            debug_info = None
+            if self.debug:
+                debug_info = f"VOB: {f.props['PVSFlagVob']}:{f.props['PVSFlagCell']} - Frame #{n:,}"
+            # progressive frame, simply skip deinterlacing
+            if f.props["PVSFlagProgressiveFrame"]:
+                rc = core.std.Interleave([c] * ff) if ff > 1 else c  # duplicate if not a single-rate fps output
+                return core.text.Text(rc, f" {debug_info} - Progressive ", alignment=1) if self.debug else rc
+            # interlaced frame, use deinterlaced clip, d_tff if TFF (2) or d_bff if BFF (1)
+            rc = {0: c, 1: d_bff, 2: d_tff}[f.props["_FieldBased"]]
+            if self.debug:
+                field_order = {0: "Progressive", 1: "BFF", 2: "TFF"}[f.props["_FieldBased"]]
+                return core.text.Text(rc, f" {debug_info} - Deinterlaced ({field_order}) ", alignment=1)
+            return rc
+
+        return core.std.FrameEval(
+            deinterlaced_tff,
+            functools.partial(
+                _d,
+                c=clip,
+                d_tff=deinterlaced_tff,
+                d_bff=deinterlaced_bff,
+                ff=fps_factor
+            ),
+            prop_src=clip
+        )
+
+    def _ffms2(self, clip):
+        """
+        Deinterlace using ffms2 (ffmpeg) using a basic FieldBased!=0 => QTGMC method
+        """
+        deinterlaced_tff, deinterlaced_bff = self._get_kernel(clip)
+        return core.std.FrameEval(
+            clip,
+            functools.partial(
+                lambda n, f, c, d_tff, d_bff: (
+                    core.text.Text(c, "Untouched Frame (_FieldBased=0)", alignment=1) if self.debug else c
+                ) if f.props["_FieldBased"] == 0 else (
+                    core.text.Text(
+                        d_tff if f.props["_FieldBased"] == 2 else d_bff,
+                        "Deinterlaced Frame (via QTGMC)",
+                        alignment=1
+                    ) if self.debug else (d_tff if f.props["_FieldBased"] == 2 else d_bff)
+                ),
+                c=clip,
+                d_tff=deinterlaced_tff,
+                d_bff=deinterlaced_bff
+            ),
+            prop_src=clip
+        )
+
+    def _lsmash(self, clip):
+        """
+        Deinterlace using lsmas (lsmash) using a basic FieldBased!=0 => QTGMC method
+        """
+        return self._ffms2(clip)  # same method as ffms2
+
+    @classmethod
+    def VoidWeave(cls, clip, tff, bob=False) -> VideoNode:
+        """
+        Weaves a 255(rgb) #00ff00(hex) green as the 2nd field of every field.
+        The purpose of this would be for machine learning in-painting over
+        the green rows with interpreted pixel data for machine learning
+        deinterlacing.
+
+        This function will convert clip's color-space to RGB24. It needs to
+        for creating a clip with linear 255 RGB green. Otherwise the machine
+        learning code may not pick up the rows green as the correct green
+        and fail. The color space has to match the void green clip and the
+        actual field clip to be able to weave them together.
+        """
+        green = [0, 255, 0]
+        # fill in the needed rows with void (255 green) for in-painting
+        if clip.format.color_family != ColorFamily.RGB:
+            # need to use RGB for the BlankClip for the correct green to be used
+            # if in-painting, RGB color-space will most likely be needed anyway
+            clip = mvsfunc.ToRGB(clip, depth=clip.format.bits_per_sample)
+        clip = core.std.SeparateFields(clip, tff=tff)
+        clip = core.std.Interleave([
+            clip,
+            core.std.BlankClip(clip, format=clip.format.id, color=green, keep=0)
+        ])
+        clip = core.std.DoubleWeave(clip, tff=tff)
+        clip = core.std.SelectEvery(clip, cycle=2, offsets=0)
+        # handle bobbing, keep every deinterlaced "field" if true
+        if bob:
+            # vertically align every even (2nd) field with every odd (1st) field
+            # by adding a 1px row of black pixels on the top, and removing from the bottom
+            odd = core.std.SelectEvery(clip, cycle=2, offsets=0)
+            even = core.std.SelectEvery(clip, cycle=2, offsets=1)
+            even = core.std.AddBorders(even, top=1, color=green)
+            even = core.std.Crop(even, bottom=1)
+            return core.std.Interleave([odd, even])
+        return core.std.SelectEvery(clip, cycle=2, offsets=0)

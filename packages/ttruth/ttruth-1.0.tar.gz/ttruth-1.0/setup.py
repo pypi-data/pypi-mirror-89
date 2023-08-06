615bfa3405a6cd8df8ea35ffe407b942fe3175d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:52 +0000

    Add package ttruth/1.0/ttruth-1.0.tar.gz

---
+++ packages/ttruth/ttruth-1.0.tar.gz/ttruth-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='ttruth',
+   version='1.0',
+   description='Module for handling labelled XML files from T-Truth',
+   author='Sohaib Zahid',
+   author_email='sohaib023@gmail.com',
+   packages=['ttruth'],  #same as name
+)

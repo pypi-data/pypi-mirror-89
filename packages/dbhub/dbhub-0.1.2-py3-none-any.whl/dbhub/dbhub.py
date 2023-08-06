3cf8642adfb5ab236914233b2b91bee8a1bd901c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:18 +0000

    Add package dbhub/0.1.2/dbhub-0.1.2-py3-none-any.whl

---
+++ packages/dbhub/dbhub-0.1.2-py3-none-any.whl/dbhub/dbhub.py
@@ -0,0 +1,68 @@
+import requests
+import json
+
+
+url = 'https://dbhub.herokuapp.com/'
+
+
+def get_database(api_key):
+    return DbHub(api_key)
+
+
+class DbHub:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_collection(self, collection_name):
+        return Collection(self.api_key, collection_name)
+
+
+class Collection:
+    def __init__(self, api_key, collection_name):
+        self.api_key = api_key
+        self.collection_name = collection_name
+
+    def create(self, doc):
+        data = {
+            'secret': self.api_key,
+            'collectionName': self.collection_name,
+            'doc': doc
+        }
+        response = requests.post(url, json=data)
+        return json.loads(response.text)
+
+    def read(self, id):
+        params = {
+            'secret': self.api_key,
+            'collectionName': self.collection_name,
+            'id': id
+        }
+        response = requests.get(url, params=params)
+        return json.loads(response.text)
+
+    def list(self):
+        params = {
+            'secret': self.api_key,
+            'collectionName': self.collection_name
+        }
+        response = requests.get(url + 'list', params=params)
+        return json.loads(response.text)
+
+    def update(self, id, doc):
+        data = {
+            'secret': self.api_key,
+            'collectionName': self.collection_name,
+            'id': id,
+            'doc': doc
+        }
+        response = requests.patch(url, json= data)
+        return json.loads(response.text)
+
+    def delete(self, id):
+        data = {
+            'secret': self.api_key,
+            'collectionName': self.collection_name,
+            'id': id
+        }
+        response = requests.delete(url, params=data)
+        return json.loads(response.text)

2ed66a200197f98bc6a13817beda44f92a619671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:56 +0000

    Add package kubewatcher/1.4.0/kubewatcher-1.4.0.tar.gz

---
+++ packages/kubewatcher/kubewatcher-1.4.0.tar.gz/kubewatcher-1.4.0/setup.py
@@ -0,0 +1,34 @@
+import inspect
+import os
+
+from setuptools import setup, find_packages
+
+from kubewatcher.kubewatcher import cli
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+def entry_point(name, func):
+    module = inspect.getmodule(func)
+    return f"{name}={module.__name__}:{func.__name__}"
+
+
+setup(
+    name='kubewatcher',
+    version='1.4.0',
+    description='See https://github.com/tonsV2/kubewatcher',
+    python_requires='>=3.8',
+    packages=find_packages(),
+    install_requires=[
+        'kubernetes~=12.0.1',
+        'ruamel.yaml~=0.16.12',
+        'envyaml~=1.1.201202',
+        'twine~=3.3.0',
+    ],
+    entry_points={
+        'console_scripts': [entry_point("kubewatcher", cli)]
+    },
+    long_description=read('README.md'),
+)

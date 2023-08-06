b984086b274be8ec54004c1953806f793cccbb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package pyequilib/0.2.0/pyequilib-0.2.0-py3-none-any.whl

---
+++ packages/pyequilib/pyequilib-0.2.0-py3-none-any.whl/equilib/equi2equi/equi2equi_torch.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+import math
+from typing import Dict, List, Tuple, Union
+
+import numpy as np
+
+import torch
+
+from equilib.grid_sample import torch_func
+from equilib.common.torch_utils import (
+    create_rotation_matrix,
+    get_device,
+    sizeof,
+)
+
+
+def pixel_wise_rot(M: torch.Tensor) -> Tuple[torch.Tensor]:
+    r"""Rotation coordinates to phi/theta of the equirectangular image
+
+    params:
+        M: torch.Tensor
+
+    return:
+        phis: torch.Tensor
+        thetas: torch.Tensor
+    """
+    if len(M.shape) == 3:
+        M = M.unsqueeze(0)
+
+    norms = torch.norm(M, dim=-1)
+    thetas = torch.atan2(M[:, :, :, 0], M[:, :, :, 2])
+    phis = torch.asin(M[:, :, :, 1] / norms)
+
+    if thetas.shape[0] == phis.shape[0] == 1:
+        thetas = thetas.squeeze(0)
+        phis = phis.squeeze(0)
+    return phis, thetas
+
+
+def create_coordinate(h_out: int, w_out: int) -> np.ndarray:
+    r"""Create mesh coordinate grid with height and width
+
+    return:
+        coordinate: numpy.ndarray
+    """
+    xs = torch.linspace(0, w_out - 1, w_out)
+    theta = xs * 2 * math.pi / w_out - math.pi
+    ys = torch.linspace(0, h_out - 1, h_out)
+    phi = ys * math.pi / h_out - math.pi / 2
+    # NOTE: https://github.com/pytorch/pytorch/issues/15301
+    # Torch meshgrid behaves differently than numpy
+    phi, theta = torch.meshgrid([phi, theta])
+    coord = torch.stack((theta, phi), axis=-1)
+    return coord
+
+
+def rotation_matrix(
+    roll: float,
+    pitch: float,
+    yaw: float,
+) -> np.ndarray:
+    r"""Create Rotation Matrix
+
+    params:
+        roll: x-axis rotation float
+        pitch: y-axis rotation float
+        yaw: z-axis rotation float
+
+    return:
+        rotation matrix: numpy.ndarray
+
+    Global coordinates -> x-axis points forward, z-axis points upward
+    """
+    R = create_rotation_matrix(x=roll, y=pitch, z=yaw)
+    return R
+
+
+def _get_img_size(img: torch.Tensor) -> Tuple[int]:
+    r"""Return height and width"""
+    # batch, channel, height, width
+    return img.shape[-2:]
+
+
+def run(
+    src: torch.Tensor,
+    rot: Union[Dict[str, float], List[Dict[str, float]]],
+    w_out: int,
+    h_out: int,
+    sampling_method: str = "torch",
+    mode: str = "bilinear",
+    debug: bool = False,
+) -> torch.Tensor:
+    r"""Run Equi2Pers
+
+    params:
+        src: equirectangular image torch.Tensor[(B), C, H, W]
+        rot: Dict[str, float] or List[Dict[str, float]]
+        sampling_method: str (default="torch")
+        mode: str (default="bilinear")
+
+    returns:
+        pers: perspective image torch.Tensor[C, H, W]
+
+    NOTE: input can be batched [B, C, H, W] or single [C, H, W]
+    NOTE: when using batches, the output types match
+    """
+    assert (
+        type(src) == torch.Tensor
+    ), "ERR: input equi expected to be `torch.Tensor` " "but got {}".format(
+        type(src)
+    )
+    _original_shape_len = len(src.shape)
+    assert _original_shape_len >= 3, "ERR: got {} for input equi".format(
+        _original_shape_len
+    )
+    if _original_shape_len == 3:
+        src = src.unsqueeze(dim=0)
+        rot = [rot]
+
+    h_equi, w_equi = _get_img_size(src)
+    if h_out is None and w_out is None:
+        h_out = h_equi
+        w_out = w_equi
+
+    if debug:
+        print("size of src: ", sizeof(src) / 10e6, "mb")
+
+    # get device
+    device = get_device(src)
+
+    # define variables
+    B = []
+    for r in rot:
+        a = create_coordinate(h_out, w_out)
+        norm_A = 1
+        x = norm_A * torch.cos(a[:, :, 1]) * torch.cos(a[:, :, 0])
+        y = norm_A * torch.cos(a[:, :, 1]) * torch.sin(a[:, :, 0])
+        z = norm_A * torch.sin(a[:, :, 1])
+        A = torch.stack((x, y, z), dim=-1)
+        R = rotation_matrix(**r)
+        _B = R @ A.unsqueeze(3)
+        _B = _B.squeeze(3)
+        B.append(_B)
+    B = torch.stack(B, dim=0).to(device)
+
+    # calculate rotations per perspective coordinates
+    norms = torch.norm(B, dim=-1)
+    theta = torch.atan2(B[:, :, :, 1], B[:, :, :, 0])
+    phi = torch.asin(B[:, :, :, 2] / norms)
+
+    # center the image and convert to pixel locatio
+    ui = (theta - math.pi) * w_equi / (2 * math.pi)
+    uj = (phi - math.pi / 2) * h_equi / math.pi
+    # out-of-bounds calculations
+    ui = torch.where(ui < 0, ui + w_equi, ui)
+    ui = torch.where(ui >= w_equi, ui - w_equi, ui)
+    uj = torch.where(uj < 0, uj + h_equi, uj)
+    uj = torch.where(uj >= h_equi, uj - h_equi, uj)
+    grid = torch.stack((uj, ui), axis=-3)  # 3rd to last
+
+    # grid sample
+    grid_sample = getattr(torch_func, sampling_method, "torch")
+    samples = grid_sample(src, grid, mode=mode)
+
+    if _original_shape_len == 3:
+        samples = samples.squeeze(axis=0)
+
+    return samples
---
+++ packages/pyequilib/pyequilib-0.2.0-py3-none-any.whl/equilib/equi2pers/base.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+from typing import Dict, List, Union
+
+import numpy as np
+
+import torch
+
+from .equi2pers_numpy import run as run_numpy
+from .equi2pers_torch import run as run_torch
+
+__all__ = ["Equi2Pers", "equi2pers"]
+
+
+class Equi2Pers(object):
+    r"""Equi2Pers"""
+
+    def __init__(
+        self,
+        w_pers: int,
+        h_pers: int,
+        fov_x: float,
+        skew: float = 0.0,
+        sampling_method: str = "default",
+        mode: str = "bilinear",
+    ) -> None:
+        r"""
+        params:
+            w_pers, h_pers (int): perspective size
+            fov_x (float): perspective image fov of x-axis
+            skew (float): skew intrinsic parameter
+        """
+        self.w_pers = w_pers
+        self.h_pers = h_pers
+        self.fov_x = fov_x
+        self.skew = skew
+        self.sampling_method = sampling_method
+        self.mode = mode
+
+    def __call__(
+        self,
+        equi: Union[
+            np.ndarray,
+            torch.Tensor,
+            List[Union[np.ndarray, torch.Tensor]],
+        ],
+        rot: Union[
+            Dict[str, float],
+            List[Dict[str, float]],
+        ],
+    ) -> Union[np.ndarray, torch.Tensor]:
+        return equi2pers(
+            equi=equi,
+            rot=rot,
+            w_pers=self.w_pers,
+            h_pers=self.h_pers,
+            fov_x=self.fov_x,
+            skew=self.skew,
+            sampling_method=self.sampling_method,
+            mode=self.mode,
+        )
+
+
+def equi2pers(
+    equi: Union[
+        np.ndarray,
+        torch.Tensor,
+        List[Union[np.ndarray]],
+    ],
+    rot: Union[
+        Dict[str, float],
+        List[Dict[str, float]],
+    ],
+    w_pers: int,
+    h_pers: int,
+    fov_x: float,
+    skew: float = 0.0,
+    sampling_method: str = "default",
+    mode: str = "bilinear",
+) -> Union[np.ndarray, torch.Tensor]:
+
+    # Try and detect which type it is ("numpy" or "torch")
+    # FIXME: any cleaner way of detecting?
+    _type = None
+    if isinstance(equi, list):
+        if isinstance(equi[0], np.ndarray):
+            _type = "numpy"
+        elif isinstance(equi[0], torch.Tensor):
+            _type = "torch"
+        else:
+            raise ValueError
+    elif isinstance(equi, np.ndarray):
+        _type = "numpy"
+    elif isinstance(equi, torch.Tensor):
+        _type = "torch"
+    else:
+        raise ValueError
+
+    if _type == "numpy":
+        return run_numpy(
+            equi=equi,
+            rot=rot,
+            w_pers=w_pers,
+            h_pers=h_pers,
+            fov_x=fov_x,
+            skew=skew,
+            sampling_method=sampling_method,
+            mode=mode,
+        )
+    elif _type == "torch":
+        return run_torch(
+            equi=equi,
+            rot=rot,
+            w_pers=w_pers,
+            h_pers=h_pers,
+            fov_x=fov_x,
+            skew=skew,
+            sampling_method=sampling_method,
+            mode=mode,
+        )
+    else:
+        raise ValueError

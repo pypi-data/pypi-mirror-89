25f3e424f556c2b510c4a56e777609a50623327e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:32 +0000

    Add package feudalAdapter/0.2.2/feudalAdapter-0.2.2.tar.gz

---
+++ packages/feudalAdapter/feudalAdapter-0.2.2.tar.gz/feudalAdapter-0.2.2/ldf_adapter/backend/__init__.py
@@ -0,0 +1,30 @@
+import sys
+import pkgutil
+import logging
+
+from ..config import CONFIG
+
+logger = logging.getLogger(__name__)
+
+__backend__ = f"{__name__}.{CONFIG['ldf_adapter']['backend']}"
+__import__(__backend__)
+
+class Backend:
+    """Need to put __getattr__ in a class for Python < 3.7 compatibility.
+
+    This class acts as a module.
+
+    See https://stackoverflow.com/questions/2447353/getattr-on-a-module#7668273
+    """
+    __backend__ = CONFIG['ldf_adapter']['backend']
+
+    def __getattr__(self, name):
+        """Return the `User` and `Group` from the configured backend."""
+        if name in ['User', 'Group']:
+            return getattr(sys.modules[f"{__name__}.{self.__backend__}"], name)
+        else:
+            raise AttributeError(f"backend module '{__name__}' has no attribute '{name}'")
+
+# Replace this very module (`ldf_adapter.backend`) with the pseudo-module above, which has the
+# effect of this ldf_adapter.backend acting as the actually configured backend module.
+sys.modules[__name__] = Backend()
---
+++ packages/feudalAdapter/feudalAdapter-0.2.2.tar.gz/feudalAdapter-0.2.2/ldf_adapter/interface.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#
+# Author: Joshua Bachmeier <joshua.bachmeier@student.kit.edu>
+#
+
+import os
+import sys
+import json
+import logging
+
+from ldf_adapter import User
+from ldf_adapter.results import ExceptionalResult
+
+def main():
+    logging.basicConfig(
+        level=os.environ.get("LOG", "INFO")
+        #format='%(asctime)s [%(levelname)s] [%(filename)s:%(funcName)s:%(lineno)d] %(message)s'
+    )
+
+    logger = logging.getLogger(__name__)
+
+    data = json.load(sys.stdin)
+
+    logger.debug(f"Attempting to reach state '{data['state_target']}'")
+
+    if data['user']['userinfo'] is None:
+        logger.error(f"Cannot process null input")
+        sys.exit(2)
+
+    try:
+        result = User(data).reach_state(data['state_target'])
+    except ExceptionalResult as result:
+        result = result.attributes
+        logger.debug("Reached state '{state}': {message}".format(**result))
+        json.dump(result, sys.stdout)
+    else:
+        result = result.attributes
+        logger.debug("Reached state '{state}': {message}".format(**result))
+        json.dump(result, sys.stdout)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())

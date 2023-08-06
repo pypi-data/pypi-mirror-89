c4537b38d8da6746edd2f3032208d7ecc3b4fd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:02 +0000

    Add package yk-face-api-model/1.0.4/yk_face_api_model-1.0.4-py3-none-any.whl

---
+++ packages/yk-face-api-model/yk_face_api_model-1.0.4-py3-none-any.whl/yk_face_api/models/face_api/__init__.py
@@ -0,0 +1,7 @@
+
+from .process_request_config import ProcessRequestConfig
+from .identify_request import IdentifyRequest, IdentifyRequestSchema
+from .template_request import TemplateRequest, TemplateRequestSchema
+from .verify_id_request import VerifyIdRequest, VerifyIdRequestSchema
+from .verify_request import VerifyRequest, VerifyRequestSchema
+from .process_request import ProcessRequest, ProcessRequestSchema

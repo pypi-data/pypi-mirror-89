a82d6b1bef02df343bd6c2940ae3c785ca286ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:06 +0000

    Add package nacla/0.0.8/nacla-0.0.8.tar.gz

---
+++ packages/nacla/nacla-0.0.8.tar.gz/nacla-0.0.8/setup.py
@@ -0,0 +1,38 @@
+#coding:utf-8
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name = 'nacla',
+
+    version = '0.0.8',
+
+    keywords = ('name classify', 'nlp'),
+
+    description = '输入一个姓名，输出这个姓名输入哪个国家',
+
+    license = 'MIT',
+
+    author = 'zhaomingming',
+
+    author_email = '13271929138@163.com',
+
+    #packages = find_packages(),
+    #py_modules=['nacla.nacla']
+    packages=find_packages(where='nacla'),  # Required
+
+    platforms = 'python>=3.6.7',
+     # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    package_data={  # Optional
+        'model': ['rnn.pth'],
+    },
+    #data_files=[('model', ['nacla/model/rnn.pth'])],
+
+    #py_modules=['nacla.nacla']
+    #packages=find_packages(where='nacla'),  # Required
+
+)
+

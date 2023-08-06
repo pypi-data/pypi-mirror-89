b9d28489e69ef2efbff3dcefc57b71b140a12f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:44 +0000

    Add package ethango-probability/0.1/ethango_probability-0.1.tar.gz

---
+++ packages/ethango-probability/ethango_probability-0.1.tar.gz/ethango_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ethango_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ethango_probability'],
+      zip_safe=False)

23db3b4650cdea3cd485de09bd60a74debf3b1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:10 +0000

    Add package gauss-binom-dist-test-by-steve/0.1/gauss_binom_dist_test_by_steve-0.1.tar.gz

---
+++ packages/gauss-binom-dist-test-by-steve/gauss_binom_dist_test_by_steve-0.1.tar.gz/gauss_binom_dist_test_by_steve-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gauss_binom_dist_test_by_steve',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gauss_binom_dist_test_by_steve'],
+      zip_safe=False)

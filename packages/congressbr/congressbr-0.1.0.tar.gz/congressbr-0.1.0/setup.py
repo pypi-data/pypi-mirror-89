d4dbafd94f26c3e58b52c31fda28410d5fc39b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:02 +0000

    Add package congressbr/0.1.0/congressbr-0.1.0.tar.gz

---
+++ packages/congressbr/congressbr-0.1.0.tar.gz/congressbr-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'congressbr',
+  packages = ['congressbr'],
+  package_dir={'congressbr': 'congressbr'},
+  package_data={'congressbr': ['data/*.pkl']},
+  version = '0.1.0',
+  license='MIT',
+  description = 'Easily download data from Brazilian Congress votations',
+  author = 'Lucas Cavalcanti Rodrigues',
+  author_email = 'lucas.ecomg@gmail.com', 
+  url = 'https://github.com/lucascr91/congressbr.git', 
+  download_url = 'https://github.com/lucascr91/congressbr/archive/v_0.1.0.tar.gz', 
+  keywords = ['CONGRESS', 'DEMOCRACY', 'BRAZIL'], 
+  install_requires=[
+          'pandas',
+          'pandas_read_xml',
+          'requests',
+          'tika'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

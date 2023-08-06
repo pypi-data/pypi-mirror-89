9b432b2088830b15907d8d9a5fbd3dd9e4817f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:10 +0000

    Add package vinorm/2.0.2/vinorm-2.0.2.tar.gz

---
+++ packages/vinorm/vinorm-2.0.2.tar.gz/vinorm-2.0.2/vinorm/__init__.py
@@ -0,0 +1,38 @@
+
+import subprocess,os
+import imp
+def TTSnorm(text, punc = False, unknown = True, lower = True, rule = False ):
+    A=imp.find_module('vinorm')[1]
+
+    print(A)
+    I=A+"/input.txt"
+    with open(I,"w+") as fw:
+        fw.write(text)
+
+    myenv = os.environ.copy()
+    myenv['LD_LIBRARY_PATH'] = A+'/lib'
+
+    E=A+"/main"
+    print(E)
+    # if punc:
+    #     E=E+" -punc"
+    # if unknown:
+    #     E=E+" -unknown"
+    # if lower:
+    #     E=E+" -lower"
+    # if rule:
+    #     E=E+" -rule"
+    subprocess.check_call([E], env=myenv, cwd=A)
+    
+    O=A+"/output.txt"
+    with open(O,"r") as fr:
+        text=fr.read()
+    TEXT=""
+    S=text.split("#line#")
+    for s in S:
+        if s=="":
+            continue
+        TEXT+=B+". "
+
+
+    return TEXT

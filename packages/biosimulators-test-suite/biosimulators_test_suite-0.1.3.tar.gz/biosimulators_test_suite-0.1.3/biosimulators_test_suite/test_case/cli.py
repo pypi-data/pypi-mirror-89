0c0816450f7a960f53d2a3466508145ad27417ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:26 +0000

    Add package biosimulators-test-suite/0.1.3/biosimulators_test_suite-0.1.3.tar.gz

---
+++ packages/biosimulators-test-suite/biosimulators_test_suite-0.1.3.tar.gz/biosimulators_test_suite-0.1.3/biosimulators_test_suite/test_case/cli.py
@@ -0,0 +1,9 @@
+""" Methods for test cases involving checking command-line interfaces
+
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2020-12-21
+:Copyright: 2020, Center for Reproducible Biomedical Modeling
+:License: MIT
+"""
+
+# from ..data_model import TestCase
---
+++ packages/biosimulators-test-suite/biosimulators_test_suite-0.1.3.tar.gz/biosimulators_test_suite-0.1.3/biosimulators_test_suite/test_case/combine_archive.py
@@ -0,0 +1,9 @@
+""" Methods for test cases involving checking support for the COMBINE/OMEX standards
+
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2020-12-21
+:Copyright: 2020, Center for Reproducible Biomedical Modeling
+:License: MIT
+"""
+
+# from ..data_model import TestCase

d46b01669192284516a75cf0434701885c4beca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:47 +0000

    Add package novelsave/0.6.0/novelsave-0.6.0.tar.gz

---
+++ packages/novelsave/novelsave-0.6.0.tar.gz/novelsave-0.6.0/novelsave/__main__.py
@@ -0,0 +1,110 @@
+import argparse
+from getpass import getpass
+from pathlib import Path
+
+from webnovel.tools import UrlTools
+
+from novelsave import NovelSave
+from novelsave.database import UserConfig
+from novelsave.tools import UiTools
+
+
+def setup_config(args):
+    user = UserConfig()
+
+    # updating storage directory
+    if args.dir:
+
+        # could throw an OSError: illegal directory names
+        args.dir = Path(args.dir).resolve().absolute()
+
+        try:
+            user.directory.put(str(args.dir))
+            UiTools.print_success(f'Updated {user.directory.name}')
+        except ValueError as e:  # check for validation failures
+            UiTools.print_error(e)
+
+        # breathe,
+        print()
+
+    # show the final results
+    print('-' * 15)
+    user.print_configs()
+    print('-' * 15)
+
+
+def process_task(args):
+    # soup novel url
+    if 'https://' not in args.action:
+        args.action = UrlTools.to_novel_url(args.action)
+
+    if args.dir:
+        args.dir = Path(args.dir).resolve()
+
+    novelsave = NovelSave(args.action, directory=args.dir)
+    novelsave.timeout = args.timeout
+    novelsave.verbose = args.verbose
+
+    # get credentials
+    if args.email is not None:
+        novelsave.email = args.email
+
+        print()  # some breathing room
+        novelsave.password = getpass('[-] password: ')
+
+    if not any([args.update, args.remove_meta, args.meta, args.pending, args.create, args.force_create]):
+        UiTools.print_error('No actions selected')
+
+    if args.update:
+        novelsave.update(force_cover=args.force_cover)
+
+    if args.remove_meta:
+        novelsave.remove_metadata(with_source=True)
+        UiTools.print_success('Removed metadata')
+
+    if args.meta:
+        novelsave.metadata(url=args.meta, force=args.force_meta)
+
+    if args.pending:
+        novelsave.download(thread_count=args.threads, limit=args.limit)
+
+    if args.create or args.force_create:
+        novelsave.create_epub(force=args.force_create)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='tool to convert novels to epub')
+    parser.add_argument('action', type=str, help="novel url for downloading novels; 'config' to change configurations")
+
+    actions = parser.add_argument_group(title='actions')
+    actions.add_argument('-u', '--update', action='store_true', help='update novel details')
+    actions.add_argument('-p', '--pending', action='store_true', help='download pending chapters')
+    actions.add_argument('-c', '--create', action='store_true', help='create epub from downloaded chapters')
+    actions.add_argument('--meta', type=str, help='metadata source url', default=None)
+    actions.add_argument('--remove-meta', action='store_true', help='remove current metadata')
+    actions.add_argument('--force-cover', action='store_true', help='download and overwrite the existing cover')
+    actions.add_argument('--force-create', action='store_true', help='force create epub')
+    actions.add_argument('--force-meta', action='store_true', help='force update metadata')
+
+    credentials = parser.add_argument_group(title='credentials')
+    credentials.add_argument('--email', type=str, help='webnovel email')
+
+    parser.add_argument('-v', '--verbose', help='enable animations; only in pending', action='store_true')
+    parser.add_argument('--threads', type=int, help='number of download threads', default=4)
+    parser.add_argument('--timeout', type=int, help='webdriver timeout', default=60)
+    parser.add_argument('--limit', type=int, help='amount of chapters to download')
+
+    config = parser.add_argument_group(title='config')
+    config.add_argument('-d', '--dir', help='directory for saving novels')
+
+    args = parser.parse_args()
+
+    # Configurations
+    if args.action == 'config':
+        setup_config(args)
+    else:
+        process_task(args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/novelsave/novelsave-0.6.0.tar.gz/novelsave-0.6.0/novelsave/sources/source.py
@@ -0,0 +1,162 @@
+import re
+from typing import Tuple, List
+from urllib.parse import urlparse
+
+import requests
+from bs4 import BeautifulSoup, Comment
+
+from ..models import Novel, Chapter
+from ..tools import StringTools
+
+header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/39.0.2171.95 Safari/537.36'}
+
+
+class Source:
+    base: str
+
+    @staticmethod
+    def of(url: str) -> bool:
+        """
+        :param url: url to test
+        :return: whether the url is from this source
+        """
+        raise NotImplementedError
+
+    def __init__(self):
+        # public
+        self.session = requests.Session()
+        self.headers = header
+
+        # private
+        self._soup_cache = {}
+
+    def login(self, email: str, password: str):
+        """
+        Login to the source and assign the required cookies
+
+        :param email: email or username
+        :param password: credential key
+        :return: None
+        """
+        raise NotImplementedError
+
+    def novel(self, url: str) -> Tuple[Novel, List[Chapter]]:
+        """
+        soup novel information from url
+
+        :param url: link pointing to novel
+        :return: novel, table of content (chapters with only field no, title and url), and volumes
+        """
+        raise NotImplementedError
+
+    def chapter(self, url: str) -> Chapter:
+        """
+        soup chapter information from url
+
+        :param url: link pointing to chapter
+        :return: chapter object
+        """
+        raise NotImplementedError
+
+    def soup(self, url: str) -> BeautifulSoup:
+        """
+        Download website html and create a bs4 object
+
+        :param url: website to be downloaded
+        :return: created bs4 object
+        """
+        response = self.session.get(url)
+        return BeautifulSoup(self._verify_response(response).content, 'lxml')
+
+    def cached_soup(self, url):
+        """
+        either return the cache response or
+        if there is no cache, download anew
+
+        :param url: website to be downloaded
+        :return: created bs4 object
+        """
+        try:
+            # get response from cache
+            response = self._soup_cache[url]
+        except KeyError:
+            # make a new request and cache the result
+            response = self.session.get(url)
+            self._soup_cache[url] = response
+
+        return BeautifulSoup(self._verify_response(response).content, 'lxml')
+
+    def _verify_response(self, response):
+        if response.status_code == 200:
+            return response
+        else:
+            raise Exception(f'{response.status_code}: {response.url}')
+
+    def source_folder_name(self):
+        """
+        :return: suitable folder name from netloc
+        """
+        return StringTools.slugify(urlparse(self.base).netloc, replace='_')
+
+    def novel_folder_name(self, url):
+        """
+        :param url: novel url
+        :return: suitable novel folder name
+        """
+        return StringTools.slugify(url.strip('/ ').split('/')[-1])
+
+    # ---- Inspired from https://github.com/dipu-bd/lightnovel-crawler ----
+    # ----      And almost a perfect copy of the functions below       ----
+
+    bad_tags = [
+        'noscript', 'script', 'iframe', 'form', 'hr', 'img', 'ins',
+        'button', 'input', 'amp-auto-ads', 'pirate'
+    ]
+
+    blacklist_patterns = [
+        r'^[\W\D]*(volume|chapter)[\W\D]+\d+[\W\D]*$',
+    ]
+
+    def is_blacklisted(self, text):
+        """
+        :return: whether the text is black listed
+        """
+        for pattern in self.blacklist_patterns:
+            if re.search(pattern, text, re.IGNORECASE):
+                return True
+
+        return False
+
+    def clean_contents(self, contents):
+        if not contents:
+            return contents
+
+        contents.attrs = {}
+        for element in contents.find_all(True):
+            # remove comments
+            if isinstance(element, Comment):
+                element.extract()
+
+            elif element.name == 'br':
+                next_element = getattr(element, 'next_sibling')
+                if next_element and next_element.name == 'br':
+                    element.extract()
+
+            # Remove bad tags
+            elif element.name in self.bad_tags:
+                element.extract()
+
+            # Remove empty elements
+            elif not element.text.strip():
+                element.extract()
+
+            # Remove blacklisted elements
+            elif self.is_blacklisted(element.text):
+                element.extract()
+
+            # Remove attributes
+            elif hasattr(element, 'attrs'):
+                element.attrs = {}
+
+        return contents

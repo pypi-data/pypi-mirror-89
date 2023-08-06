f8158b756b83327d0f37b926cb686bbb62d2e8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:22 +0000

    Add package colorsimple/0.1.3/colorsimple-0.1.3.tar.gz

---
+++ packages/colorsimple/colorsimple-0.1.3.tar.gz/colorsimple-0.1.3/README.md
@@ -0,0 +1,7 @@
+A simple package to facilitate manipulation of colors.  
+
+## Install the latest version
+
+```
+pip install git+https://github.com/carbonscott/colorsimple.git --upgrade --user
+```
---
+++ packages/colorsimple/colorsimple-0.1.3.tar.gz/colorsimple-0.1.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="colorsimple",
+    version="0.1.3",
+    author="Cong Wang",
+    author_email="wangimagine@gmail.com",
+    description="A simple package to facilitate manipulation of colors.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/carbonscott/colorsimple",
+    keywords = ['colors'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

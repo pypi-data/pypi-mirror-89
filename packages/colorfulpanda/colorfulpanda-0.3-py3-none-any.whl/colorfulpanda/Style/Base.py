b36f21c61014eedf422ebd9ad0c2ae400ce2d930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:57 +0000

    Add package colorfulpanda/0.3/colorfulpanda-0.3-py3-none-any.whl

---
+++ packages/colorfulpanda/colorfulpanda-0.3-py3-none-any.whl/colorfulpanda/Style/Base.py
@@ -0,0 +1,6 @@
+
+class BaseStyle:
+	def __init__(self, _is_center_of_col, _is_center_of_row, _colorlist):
+		self.center = _is_center_of_col
+		self.cenrow = _is_center_of_row
+		self.colorlist = _colorlist

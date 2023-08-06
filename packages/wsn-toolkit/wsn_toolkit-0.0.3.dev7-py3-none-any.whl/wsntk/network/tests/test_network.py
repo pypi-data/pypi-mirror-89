cbe617fa2bc8553346ec8930ee32583c7e8683f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:23 +0000

    Add package wsn-toolkit/0.0.3.dev7/wsn_toolkit-0.0.3.dev7-py3-none-any.whl

---
+++ packages/wsn-toolkit/wsn_toolkit-0.0.3.dev7-py3-none-any.whl/wsntk/network/tests/test_network.py
@@ -0,0 +1,12 @@
+# Author: Edielson P. Frigieri <edielsonpf@gmail.com>
+#
+# License: MIT
+
+import pytest
+
+from wsntk import network
+from wsntk.network import SensorNetwork
+
+def test_network():
+    pass
+

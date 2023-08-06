7cebf6820f41796ec770731ef3f8f1d68ccbe825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:59 +0000

    Add package colorfulpanda/0.4/colorfulpanda-0.4-py3-none-any.whl

---
+++ packages/colorfulpanda/colorfulpanda-0.4-py3-none-any.whl/colorfulpanda/utils/color.py
@@ -0,0 +1,20 @@
+red1 = 196  # light red
+red2 = 160
+red3 = 124
+red4 = 88
+red5 = 52  # dark red
+
+green1 = 46
+green2 = 40
+green3 = 34
+green4 = 28
+green5 = 22
+
+blue1 = 51
+blue2 = 45
+blue3 = 39
+blue4 = 33
+blue5 = 27
+
+white1 = 15
+white2 = 7

0da60eef75055173554f38de4cc6331e0a485ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:26 +0000

    Add package pyxll-jupyter/0.1.12/pyxll_jupyter-0.1.12-py3-none-any.whl

---
+++ packages/pyxll-jupyter/pyxll_jupyter-0.1.12-py3-none-any.whl/pyxll_jupyter/qtimports.py
@@ -0,0 +1,16 @@
+"""Common imports used by other modules in this package.
+
+They are collected here so we can switch between PySide2 and PyQt5
+depending on what's installed.
+"""
+try:
+    from PySide2.QtWidgets import QApplication, QWidget, QVBoxLayout, QTabWidget, QTabBar, QShortcut, QMessageBox
+    from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile, QWebEnginePage
+    from PySide2.QtGui import QKeySequence
+    from PySide2.QtCore import QUrl, Qt, Signal
+except ImportError:
+    from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTabWidget, QTabBar, QShortcut, QMessageBox
+    from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile, QWebEnginePage
+    from PyQt5.QtGui import QKeySequence
+    from PyQt5.QtCore import QUrl, Qt
+    from PyQt5.QtCore import pyqtSignal as Signal

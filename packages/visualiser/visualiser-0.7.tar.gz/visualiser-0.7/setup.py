b96340e71c1915eb0e8f35dae23c5c212393a8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:35 +0000

    Add package visualiser/0.7/visualiser-0.7.tar.gz

---
+++ packages/visualiser/visualiser-0.7.tar.gz/visualiser-0.7/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+    name = 'visualiser',   
+    packages = ['visualiser'],
+    version = '0.7',
+    license='MIT',
+    description = 'A terminal visualiser.',
+    author = 'possiblyhamzah',
+    author_email = 'possiblyhamzah@gmail.com',
+    url = 'https://github.com/probablyhamzah/visualiser',
+    download_url = 'https://github.com/probablyhamzah/visualiser/archive/v_07.tar.gz',
+    keywords = ['visualiser', 'terminal'],
+    install_requires=[
+            'librosa',
+            'pydub',
+            'numpy',
+            'pygame',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
